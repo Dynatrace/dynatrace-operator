811c795af2a02318a42e743a1a4befaaa2173566
Marco Mader <marco.mader@dynatrace.com>
2021-01-18 15:00:41 +0100

    Fix travis.yml

--- build/deploy.sh
+++ build/deploy.sh
@@ -31,9 +31,9 @@ failed=false
 read -ra images <<<"$IMAGES"
 for image in ${images[@]}; do
   out_image="$image:$TAG"
-  #  if [[ "$image" != "$OAO_IMAGE_RHCC_SCAN" ]]; then
-  out_image="$out_image-$TRAVIS_CPU_ARCH"
-  #  fi
+  if [[ "$image" != "$OAO_IMAGE_RHCC_SCAN" ]]; then
+    out_image="$out_image-$TRAVIS_CPU_ARCH"
+  fi
 
   echo "Building docker image: $out_image"
   docker tag "$base_image" "$out_image"
