f169deda68193eab9fcc90358340e111319b947b
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-02-15 06:35:37 +0100

    readded immutable image env to webhook

--- webhook/server/server.go
+++ webhook/server/server.go
@@ -210,6 +210,7 @@ func (m *podInjector) Handle(ctx context.Context, req admission.Request) admissi
 			{Name: "K8S_BASEPODNAME", Value: basePodName},
 			{Name: "K8S_NAMESPACE", ValueFrom: fieldEnvVar("metadata.namespace")},
 			{Name: "K8S_NODE_NAME", ValueFrom: fieldEnvVar("spec.nodeName")},
+			{Name: "USE_IMMUTABLE_IMAGE", Value: "true"},
 		},
 		SecurityContext: sc,
 		VolumeMounts: []corev1.VolumeMount{
