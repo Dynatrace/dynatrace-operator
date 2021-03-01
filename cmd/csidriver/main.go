045de2d513422a94dd03a7424f25f5ec42ef48fc
Luis Garcia <luis.garcia@dynatrace.com>
2021-03-01 10:10:08 +0100

    Update year in header

--- cmd/csidriver/main.go
+++ cmd/csidriver/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Dynatrace LLC.
+Copyright 2021 Dynatrace LLC.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
