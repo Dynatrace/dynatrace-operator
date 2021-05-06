a79db039b6b0dfa7e6cd88d99f427dfb6e66d002
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-05-06 17:47:50 +0200

    added more tests

--- controllers/csi/provisioner/reconciler_test.go
+++ controllers/csi/provisioner/reconciler_test.go
@@ -100,4 +100,68 @@ func TestOneAgentProvisioner_Reconcile(t *testing.T) {
 		assert.NotNil(t, result)
 		assert.Equal(t, reconcile.Result{}, result)
 	})
+	t.Run(`error when querying dynatrace client for connection info`, func(t *testing.T) {
+		mockClient := &dtclient.MockDynatraceClient{}
+		mockClient.On("GetConnectionInfo").Return(dtclient.ConnectionInfo{}, fmt.Errorf(testError))
+
+		r := &OneAgentProvisioner{
+			client: fake.NewClient(
+				&v1alpha1.DynaKube{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testName,
+					},
+					Spec: v1alpha1.DynaKubeSpec{
+						CodeModules: v1alpha1.CodeModulesSpec{
+							Enabled: true,
+						},
+					},
+				},
+				&v1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testName,
+					},
+				},
+			),
+			dtcBuildFunc: func(rtc client.Client, instance *v1alpha1.DynaKube, secret *v1.Secret) (dtclient.Client, error) {
+				return mockClient, nil
+			},
+		}
+		result, err := r.Reconcile(context.TODO(), reconcile.Request{NamespacedName: types.NamespacedName{Name: testName}})
+
+		assert.EqualError(t, err, "failed to fetch connection info: "+testError)
+		assert.NotNil(t, result)
+		assert.Equal(t, reconcile.Result{}, result)
+	})
+	//t.Run(`error when querying dynatrace client for connection info`, func(t *testing.T) {
+	//	mockClient := &dtclient.MockDynatraceClient{}
+	//	mockClient.On("GetConnectionInfo").Return(dtclient.ConnectionInfo{}, nil)
+	//
+	//	r := &OneAgentProvisioner{
+	//		client: fake.NewClient(
+	//			&v1alpha1.DynaKube{
+	//				ObjectMeta: metav1.ObjectMeta{
+	//					Name: testName,
+	//				},
+	//				Spec: v1alpha1.DynaKubeSpec{
+	//					CodeModules: v1alpha1.CodeModulesSpec{
+	//						Enabled: true,
+	//					},
+	//				},
+	//			},
+	//			&v1.Secret{
+	//				ObjectMeta: metav1.ObjectMeta{
+	//					Name: testName,
+	//				},
+	//			},
+	//		),
+	//		dtcBuildFunc: func(rtc client.Client, instance *v1alpha1.DynaKube, secret *v1.Secret) (dtclient.Client, error) {
+	//			return mockClient, nil
+	//		},
+	//	}
+	//	result, err := r.Reconcile(context.TODO(), reconcile.Request{NamespacedName: types.NamespacedName{Name: testName}})
+	//
+	//	assert.EqualError(t, err, "failed to fetch connection info: "+testError)
+	//	assert.NotNil(t, result)
+	//	assert.Equal(t, reconcile.Result{}, result)
+	//})
 }
