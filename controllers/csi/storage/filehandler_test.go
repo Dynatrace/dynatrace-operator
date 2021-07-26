c1661e28b782783ef0e123f3795f6b7549784a72
Gabriel Krenn <gabriel.krenn@gmail.com>
2021-07-26 17:00:30 +0200

    Add test for file path handler

---
+++ controllers/csi/storage/filehandler_test.go
@@ -0,0 +1,27 @@
+package storage
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilePathHandler(t *testing.T) {
+	rootDir := "/testroot/tmp"
+	tenantUUID := "asj23443"
+
+	fph := FilePathHandler{RootDir: rootDir}
+	fakeEnv := filepath.Join(rootDir, tenantUUID)
+	fakeVolume := "csi-sdf3ijiji3jldisomeid"
+	agentRunDirForVolume := filepath.Join(fakeEnv, "run", fakeVolume)
+
+	assert.Equal(t, fakeEnv, fph.EnvDir(tenantUUID))
+	assert.Equal(t, filepath.Join(fakeEnv, "bin"), fph.AgentBinaryDir(tenantUUID))
+	assert.Equal(t, filepath.Join(fakeEnv, "bin", "v1"), fph.AgentBinaryDirForVersion(tenantUUID, "v1"))
+	assert.Equal(t, filepath.Join(fakeEnv, "run"), fph.AgentRunDir(tenantUUID))
+	assert.Equal(t, agentRunDirForVolume, fph.AgentRunDirForVolume(tenantUUID, fakeVolume))
+	assert.Equal(t, filepath.Join(agentRunDirForVolume, "mapped"), fph.OverlayMappedDir(tenantUUID, fakeVolume))
+	assert.Equal(t, filepath.Join(agentRunDirForVolume, "var"), fph.OverlayVarDir(tenantUUID, fakeVolume))
+	assert.Equal(t, filepath.Join(agentRunDirForVolume, "work"), fph.OverlayWorkDir(tenantUUID, fakeVolume))
+}
