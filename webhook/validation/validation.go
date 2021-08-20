c6181791a6f1122fa50b2d8f7617d564e4bcf77c
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2021-08-20 08:07:05 +0200

    removed "startBootstrapperManager" as a debug subcommand fixed wrong image tag in operator deployment moved api url validation form oneagent_utils to validation webhook

--- webhook/validation/validation.go
+++ webhook/validation/validation.go
@@ -2,13 +2,14 @@ package validation
 
 import (
 	"context"
+	"net/http"
+
 	"github.com/Dynatrace/dynatrace-operator/api/v1alpha1"
 	"github.com/Dynatrace/dynatrace-operator/logger"
 	"github.com/Dynatrace/dynatrace-operator/scheme"
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/labels"
-	"net/http"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
