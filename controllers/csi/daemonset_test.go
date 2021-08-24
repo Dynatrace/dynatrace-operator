67e7cbea8b0e78973f94e668003a7f00252a0f39
Christoph Muellner <33160506+chrismuellner@users.noreply.github.com>
2021-08-24 13:38:34 +0200

    Add resource requests and limits to CSI DaemonSet (#224)

--- controllers/csi/daemonset_test.go
+++ controllers/csi/daemonset_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -68,10 +69,75 @@ func TestReconcile_CreateDaemonSet(t *testing.T) {
 	}, createdDaemonSet)
 	require.NoError(t, err)
 
-	assert.NotNil(t, createdDaemonSet.Annotations)
-	assert.Contains(t, createdDaemonSet.Annotations, kubeobjects.AnnotationHash)
-	assert.Len(t, createdDaemonSet.Spec.Template.Spec.Containers, 3)
-	assert.Len(t, createdDaemonSet.Spec.Template.Spec.Volumes, 5)
+	t.Run("metadata valid", func(t *testing.T) {
+		assert.Len(t, createdDaemonSet.Labels, 1)
+
+		assert.NotNil(t, createdDaemonSet.Annotations)
+		assert.Contains(t, createdDaemonSet.Annotations, kubeobjects.AnnotationHash)
+
+		assert.NotNil(t, createdDaemonSet.OwnerReferences)
+		assert.Len(t, createdDaemonSet.OwnerReferences, 1)
+	})
+	t.Run("containers valid", func(t *testing.T) {
+		assert.Len(t, createdDaemonSet.Spec.Template.Spec.Containers, 3)
+	})
+	t.Run("driver container valid", func(t *testing.T) {
+		driver := createdDaemonSet.Spec.Template.Spec.Containers[0]
+		assert.Equal(t, driver.Name, "driver")
+
+		assert.Len(t, driver.Args, 4)
+
+		assert.Len(t, driver.Ports, 1)
+
+		assert.Len(t, driver.Env, 2)
+
+		assert.NotNil(t, driver.Resources)
+		assert.NotNil(t, driver.Resources.Requests)
+		assert.Len(t, driver.Resources.Requests, 2)
+		testQuantity(t, driver.Resources.Requests, corev1.ResourceCPU, "200m")
+		testQuantity(t, driver.Resources.Requests, corev1.ResourceMemory, "100M")
+
+		assert.NotNil(t, driver.Resources.Limits)
+		assert.Len(t, driver.Resources.Limits, 2)
+		testQuantity(t, driver.Resources.Limits, corev1.ResourceCPU, "200m")
+		testQuantity(t, driver.Resources.Limits, corev1.ResourceMemory, "100M")
+
+		assert.NotNil(t, driver.LivenessProbe)
+
+		assert.NotNil(t, driver.SecurityContext)
+
+		assert.Len(t, driver.VolumeMounts, 4)
+	})
+	t.Run("registrar container valid", func(t *testing.T) {
+		registrar := createdDaemonSet.Spec.Template.Spec.Containers[1]
+		assert.Equal(t, registrar.Name, "registrar")
+
+		assert.Len(t, registrar.Args, 3)
+
+		assert.Len(t, registrar.Ports, 1)
+
+		assert.NotNil(t, registrar.LivenessProbe)
+
+		assert.Len(t, registrar.VolumeMounts, 2)
+	})
+	t.Run("liveness probe container valid", func(t *testing.T) {
+		livenessProbe := createdDaemonSet.Spec.Template.Spec.Containers[2]
+		assert.Equal(t, livenessProbe.Name, "liveness-probe")
+
+		assert.Len(t, livenessProbe.Args, 2)
+
+		assert.Len(t, livenessProbe.VolumeMounts, 1)
+	})
+	t.Run("volumes valid", func(t *testing.T) {
+		assert.Len(t, createdDaemonSet.Spec.Template.Spec.Volumes, 5)
+	})
+}
+
+func testQuantity(t *testing.T, resourceList corev1.ResourceList, key corev1.ResourceName, quantity string) {
+	assert.Contains(t, resourceList, key)
+	expected, err := resource.ParseQuantity(quantity)
+	require.NoError(t, err)
+	assert.Equal(t, expected, resourceList[key])
 }
 
 func TestReconcile_UpdateDaemonSet(t *testing.T) {
