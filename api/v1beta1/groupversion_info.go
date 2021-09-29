0860881c0f9c20fdb661aea572ee6cc1cb76c5a2
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2021-09-29 14:54:47 +0200

    Version fixes

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -24,7 +24,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "dynatrace.com", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "dynatrace.com", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
