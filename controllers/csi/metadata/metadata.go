e2fbc994793f5d849ead97d70da54d4c0e224cfb
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2021-11-11 09:45:50 +0100

    Extra docs

--- controllers/csi/metadata/metadata.go
+++ controllers/csi/metadata/metadata.go
@@ -1,12 +1,13 @@
 package metadata
 
+// Stores the necessary info from the Dynakube that is needed to be used during volume mount/unmount.
 type Dynakube struct {
 	Name          string
 	TenantUUID    string
 	LatestVersion string
 }
 
-// NewDynakube returns a new Tenant if all fields are set.
+// NewDynakube returns a new metadata.Dynakube if all fields are set.
 func NewDynakube(dynakubeName, tenantUUID, latestVersion string) *Dynakube {
 	if tenantUUID == "" || latestVersion == "" || dynakubeName == "" {
 		return nil
