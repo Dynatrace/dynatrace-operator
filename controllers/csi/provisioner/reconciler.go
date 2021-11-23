761f412caeba1dd02bd7547d3e839872c1d1a182
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2021-11-23 16:39:13 +0100

    Moves hostGroup injection to csidriver (#375)

--- controllers/csi/provisioner/reconciler.go
+++ controllers/csi/provisioner/reconciler.go
@@ -136,14 +136,15 @@ func (r *OneAgentProvisioner) Reconcile(ctx context.Context, request reconcile.R
 	}
 	rlog.Info("csi directories exist", "path", r.path.EnvDir(dynakube.TenantUUID))
 
-	latestProcessModuleConfig, storedRevision, err := r.getProcessModuleConfig(dtc, dynakube.TenantUUID)
+	latestProcessModuleConfig, storedHash, err := r.getProcessModuleConfig(dtc, dynakube.TenantUUID)
 	if err != nil {
 		rlog.Error(err, "error when getting the latest ruxitagentproc.conf")
 		return reconcile.Result{}, err
 	}
+	latestProcessModuleConfigCache := newProcessModuleConfigCache(addHostGroup(dk, latestProcessModuleConfig))
 
 	installAgentCfg := newInstallAgentConfig(rlog, dtc, r.path, r.fs, r.recorder, dk)
-	if updatedVersion, err := installAgentCfg.updateAgent(dynakube.LatestVersion, dynakube.TenantUUID, storedRevision, latestProcessModuleConfig); err != nil {
+	if updatedVersion, err := installAgentCfg.updateAgent(dynakube.LatestVersion, dynakube.TenantUUID, storedHash, latestProcessModuleConfigCache); err != nil {
 		rlog.Info("error when updating agent", "error", err.Error())
 		// reporting error but not returning it to avoid immediate requeue and subsequently calling the API every few seconds
 		return reconcile.Result{RequeueAfter: defaultRequeueDuration}, nil
@@ -156,7 +157,7 @@ func (r *OneAgentProvisioner) Reconcile(ctx context.Context, request reconcile.R
 		return reconcile.Result{}, err
 	}
 
-	err = r.writeProcessModuleConfigCache(dynakube.TenantUUID, latestProcessModuleConfig)
+	err = r.writeProcessModuleConfigCache(dynakube.TenantUUID, latestProcessModuleConfigCache)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
