ad3dccf611dbf7a4719c1e68c1b853055aa165de
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-01-17 11:20:32 +0100

    Adds github actions to test/lint helm chart

---
+++ config/helm/testing/lint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+for directory in "$(pwd)"; do
+
+  if [[ -d "$directory/chart/default" ]]; then
+    cd "$directory/chart/default" || exit 5
+    if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
+      echo "Linting $directory"
+
+    if ! helm template --debug --set apiUrl="test-url",apiToken="test-token",paasToken="test-token" .; then
+      echo "could not parse template. something is wrong with template files of directory '$directory'"
+      exit 10
+    fi
+
+    if ! helm lint --debug --set oapiToken="test-token",paasToken="test-token" . ; then
+        echo "linter returned with error. check yaml formatting in files of directory '$directory'." && exit 15
+    fi
+    else
+      echo "$directory does not contain Chart file. skipping..."
+    fi
+  fi
+done
---
+++ config/helm/testing/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+for directory in "$(pwd)"; do
+  echo $directory
+  if [[ -d "$directory/chart/default" ]]; then
+    cd "$directory/chart/default" || exit 5
+    if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
+      echo "Unit-testing $directory"
+
+      if ! helm unittest --helm3 -f './tests/*/*/*.yaml' -f './tests/*/*.yaml' -f './tests/*.yaml' . ; then
+        echo "some tests failed in directory '$directory'"
+        exit 10
+      fi
+    else
+      echo "$directory does not contain Chart file. skipping..."
+    fi
+  fi
+done
