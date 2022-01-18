4bcd0d610a5d5c58dfca7fdc4620fa2f8e07db23
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-01-18 10:50:33 +0100

    Simplify the testing scripts

--- config/helm/testing/lint.sh
+++ config/helm/testing/lint.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
-
-for directory in "$(pwd)"; do
-
-  if [[ -d "$directory/chart/default" ]]; then
-    cd "$directory/chart/default" || exit 5
-    if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
-      echo "Linting $directory"
-      if ! helm template --debug --set apiUrl="test-url",apiToken="test-token",paasToken="test-token" .; then
-        echo "could not parse template. something is wrong with template files of directory '$directory'"
-        exit 10
-      fi
-      if ! helm lint --debug --set apiUrl="test-url",apiToken="test-token",paasToken="test-token" .; then
-        echo "linter returned with error. check yaml formatting in files of directory '$directory'."
-        exit 15
-      fi
-    else
-      echo "$directory does not contain Chart file. skipping..."
-    fi
-  fi
-done
+cd chart/default || exit 5
+echo "Linting helm chart"
+if ! helm template --debug --set apiUrl="test-url",apiToken="test-token",paasToken="test-token" .; then
+  echo "could not parse template. something is wrong with template files"
+  exit 10
+fi
+if ! helm lint --debug --set apiUrl="test-url",apiToken="test-token",paasToken="test-token" .; then
+  echo "linter returned with error. check yaml formatting in files"
+  exit 15
+fi
--- config/helm/testing/test.sh
+++ config/helm/testing/test.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
-
-for directory in "$(pwd)"; do
-  echo $directory
-  if [[ -d "$directory/chart/default" ]]; then
-    cd "$directory/chart/default" || exit 5
-    if [ -f "Chart.yml" ] || [ -f "Chart.yaml" ]; then
-      echo "Unit-testing $directory"
-      if ! helm unittest --helm3 -f './tests/*/*/*.yaml' -f './tests/*/*.yaml' -f './tests/*.yaml' .; then
-        echo "some tests failed in directory '$directory'"
-        exit 10
-      fi
-    else
-      echo "$directory does not contain Chart file. skipping..."
-    fi
-  fi
-done
+cd chart/default || exit 5
+echo "Unit-testing helm chart"
+if ! helm unittest --helm3 -f './tests/*/*/*.yaml' -f './tests/*/*.yaml' -f './tests/*.yaml' .; then
+  echo "some tests failed"
+  exit 10
+fi
