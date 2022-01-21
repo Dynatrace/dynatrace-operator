85c83d4392efb618ca6bd0f6ead77ab98923f4f0
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-01-21 14:29:41 +0100

    Patches the logger

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -9,26 +9,6 @@ import (
 	ctrlzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
-func NewDTLogger() logr.Logger {
-	// todo exchange error logger with something that uses errorPrettify
-
-	cf := zap.NewProductionEncoderConfig()
-	cf.EncodeTime = zapcore.ISO8601TimeEncoder
-	logger := ctrlzap.New(ctrlzap.WriteTo(os.Stdout), ctrlzap.Encoder(zapcore.NewJSONEncoder(cf)))
-
-	return logger
-}
-
-/*
-import (
-	"os"
-
-	"github.com/go-logr/logr"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-	ctrlzap "sigs.k8s.io/controller-runtime/pkg/log/zap"
-)
-
 type DTLogger struct {
 	infoLogger  logr.Logger
 	errorLogger logr.Logger
@@ -38,17 +18,21 @@ func NewDTLogger() logr.Logger {
 	config := zap.NewProductionEncoderConfig()
 	config.EncodeTime = zapcore.ISO8601TimeEncoder
 
-	return DTLogger{
-		infoLogger:  ctrlzap.New(ctrlzap.WriteTo(os.Stdout), ctrlzap.Encoder(zapcore.NewJSONEncoder(config))),
-		errorLogger: ctrlzap.New(ctrlzap.WriteTo(&errorPrettify{}), ctrlzap.Encoder(zapcore.NewJSONEncoder(config))),
-	}
+	return logr.New(
+		DTLogger{
+			infoLogger:  ctrlzap.New(ctrlzap.WriteTo(os.Stdout), ctrlzap.Encoder(zapcore.NewJSONEncoder(config))),
+			errorLogger: ctrlzap.New(ctrlzap.WriteTo(&errorPrettify{}), ctrlzap.Encoder(zapcore.NewJSONEncoder(config))),
+		},
+	)
 }
 
-func (dtl DTLogger) Info(msg string, keysAndValues ...interface{}) {
+func (dtl DTLogger) Init(info logr.RuntimeInfo) {}
+
+func (dtl DTLogger) Info(level int, msg string, keysAndValues ...interface{}) {
 	dtl.infoLogger.Info(msg, keysAndValues...)
 }
 
-func (dtl DTLogger) Enabled() bool {
+func (dtl DTLogger) Enabled(level int) bool {
 	return dtl.infoLogger.Enabled()
 }
 
@@ -56,21 +40,16 @@ func (dtl DTLogger) Error(err error, msg string, keysAndValues ...interface{}) {
 	dtl.errorLogger.Error(err, msg, keysAndValues...)
 }
 
-func (dtl DTLogger) V(level int) logr.Logger {
-	return dtl.errorLogger.V(level)
-}
-
-func (dtl DTLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
+func (dtl DTLogger) WithValues(keysAndValues ...interface{}) logr.LogSink {
 	return DTLogger{
 		infoLogger:  dtl.infoLogger.WithValues(keysAndValues...),
 		errorLogger: dtl.errorLogger.WithValues(keysAndValues...),
 	}
 }
 
-func (dtl DTLogger) WithName(name string) logr.Logger {
+func (dtl DTLogger) WithName(name string) logr.LogSink {
 	return DTLogger{
 		infoLogger:  dtl.infoLogger.WithName(name),
 		errorLogger: dtl.errorLogger.WithName(name),
 	}
 }
-*/
