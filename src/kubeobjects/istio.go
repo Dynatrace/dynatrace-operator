5deb70f799bdedd67cc6063cb9a698e16a6fcd4f
albian.krasniqi <albian.krasniqi@dynatrace.com>
2022-01-24 12:08:01 +0100

    fix linting

--- src/kubeobjects/istio.go
+++ src/kubeobjects/istio.go
@@ -7,10 +7,9 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/go-logr/logr"
-
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
 	"github.com/Dynatrace/dynatrace-operator/src/dtclient"
+	"github.com/go-logr/logr"
 	istiov1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	istioclientset "istio.io/client-go/pkg/clientset/versioned"
 	"k8s.io/apimachinery/pkg/api/errors"
