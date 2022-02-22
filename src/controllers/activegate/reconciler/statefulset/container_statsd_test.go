ad1fdfb340dde98631b4fa38b54ca7b54a9c9095
Tomasz Szreder <Tomasz.Szreder@dynatrace.com>
2022-02-22 14:52:06 +0100

    Maps certain StatsD-related annotations to EEC/StatsD container resource requests or limits (#588)
    * Map annotations to EEC/StatsD resource limits/requests

--- src/controllers/activegate/reconciler/statefulset/container_statsd_test.go
+++ src/controllers/activegate/reconciler/statefulset/container_statsd_test.go
@@ -7,20 +7,15 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/src/kubeobjects"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 func TestStatsd_BuildContainerAndVolumes(t *testing.T) {
 	assertion := assert.New(t)
 	requirement := require.New(t)
 
-	instance := buildTestInstance()
-	capabilityProperties := &instance.Spec.ActiveGate.CapabilityProperties
-	stsProperties := NewStatefulSetProperties(instance, capabilityProperties,
-		"", "", "test-feature", "", "",
-		nil, nil, nil,
-	)
-
 	t.Run("happy path", func(t *testing.T) {
+		stsProperties := testBuildStsProperties()
 		statsd := NewStatsd(stsProperties)
 		container := statsd.BuildContainer()
 
@@ -53,6 +48,7 @@ func TestStatsd_BuildContainerAndVolumes(t *testing.T) {
 	})
 
 	t.Run("hardened container security context", func(t *testing.T) {
+		stsProperties := testBuildStsProperties()
 		container := NewStatsd(stsProperties).BuildContainer()
 
 		requirement.NotNil(container.SecurityContext)
@@ -68,6 +64,7 @@ func TestStatsd_BuildContainerAndVolumes(t *testing.T) {
 	})
 
 	t.Run("volumes vs volume mounts", func(t *testing.T) {
+		stsProperties := testBuildStsProperties()
 		eec := NewExtensionController(stsProperties)
 		statsd := NewStatsd(stsProperties)
 		volumes := buildVolumes(stsProperties, []kubeobjects.ContainerBuilder{eec, statsd})
@@ -76,5 +73,19 @@ func TestStatsd_BuildContainerAndVolumes(t *testing.T) {
 		for _, volumeMount := range container.VolumeMounts {
 			assertion.Truef(kubeobjects.VolumeIsDefined(volumes, volumeMount.Name), "Expected that volume mount %s has a predefined pod volume", volumeMount.Name)
 		}
+	})
+
+	t.Run("resource requirements from feature flags", func(t *testing.T) {
+		stsProperties := testBuildStsProperties()
+		stsProperties.ObjectMeta.Annotations["alpha.operator.dynatrace.com/feature-activegate-statsd-resources-requests-memory"] = "500M"
+		statsd := NewStatsd(stsProperties)
+
+		container := statsd.BuildContainer()
+
+		require.NotEmpty(t, container.Resources.Requests)
+		require.Empty(t, container.Resources.Limits)
+
+		assert.True(t, container.Resources.Requests.Cpu().IsZero())
+		assert.Equal(t, resource.NewScaledQuantity(500, resource.Mega).String(), container.Resources.Requests.Memory().String())
 	})
 }
