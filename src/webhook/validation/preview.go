a9e1f02f499c2428827a853130fe8a1350548501
Albian Krasniqi <62391739+waodim@users.noreply.github.com>
2022-03-01 13:10:08 +0100

    Remove preview warning from CSI driver (#603)
    * Removed warning messages for deployment modes including csi driver
    
    * Removed oneAgentModePreviewWarning + Adapted unit tests accordingly
    
    (cherry picked from commit 520cae581c9411ae3af57615170b5e6432765914)

--- src/webhook/validation/preview.go
+++ src/webhook/validation/preview.go
@@ -11,17 +11,6 @@ const (
 	basePreviewWarning           = "PREVIEW features are NOT production ready and you may run into bugs."
 )
 
-func oneAgentModePreviewWarning(dv *dynakubeValidator, dynakube *dynatracev1beta1.DynaKube) string {
-	if dynakube.CloudNativeFullstackMode() {
-		log.Info("DynaKube with cloudNativeFullStack was applied, warning was provided.")
-		return fmt.Sprintf(featurePreviewWarningMessage, "cloudNativeFullStack")
-	} else if dynakube.ApplicationMonitoringMode() && dynakube.NeedsCSIDriver() {
-		log.Info("DynaKube with applicationMonitoring was applied, warning was provided.")
-		return fmt.Sprintf(featurePreviewWarningMessage, "applicationMonitoring")
-	}
-	return ""
-}
-
 func metricIngestPreviewWarning(dv *dynakubeValidator, dynakube *dynatracev1beta1.DynaKube) string {
 	if dynakube.IsActiveGateMode(dynatracev1beta1.MetricsIngestCapability.DisplayName) {
 		log.Info("DynaKube with metrics-ingest was applied, warning was provided.")
