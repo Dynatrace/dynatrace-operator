b39c0053d96e16972eb918f090ca26ae8a08f0de
albian.krasniqi <albian.krasniqi@dynatrace.com>
2022-03-25 08:56:02 +0100

    Changed imagefield name to CodeModuleImage

--- src/api/v1beta1/oneagent_types.go
+++ src/api/v1beta1/oneagent_types.go
@@ -32,7 +32,7 @@ type CloudNativeFullStackSpec struct {
 	// Optional: the Dynatrace installer container image
 	// Defaults to the registry on the tenant for both Kubernetes and for OpenShift
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Image",order=12,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced","urn:alm:descriptor:com.tectonic.ui:text"}
-	Image string `json:"image,omitempty"`
+	CodueModuleImage string `json:"image,omitempty"`
 
 	// Optional: If specified, indicates the OneAgent version to use
 	// Defaults to latest
@@ -122,7 +122,7 @@ type ApplicationMonitoringSpec struct {
 	// Optional: the Dynatrace installer container image
 	// Defaults to the registry on the tenant for both Kubernetes and for OpenShift
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Image",order=12,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced","urn:alm:descriptor:com.tectonic.ui:text"}
-	Image string `json:"image,omitempty"`
+	CodeModuleImage string `json:"image,omitempty"`
 
 	// Optional: If specified, indicates the OneAgent version to use
 	// Defaults to latest
