62d672a6acdebd89139a4f1e0c985b3b48cfdf4c
Albian Krasniqi <62391739+waodim@users.noreply.github.com>
2022-04-01 14:15:55 +0200

    Update src/api/v1beta1/oneagent_types.go
    Co-authored-by: Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>

--- src/api/v1beta1/oneagent_types.go
+++ src/api/v1beta1/oneagent_types.go
@@ -121,7 +121,7 @@ type ApplicationMonitoringSpec struct {
 
 	// Optional: the Dynatrace installer container image
 	// Defaults to the registry on the tenant for both Kubernetes and for OpenShift
-	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Image",order=12,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced","urn:alm:descriptor:com.tectonic.ui:text"}
+	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="CodeModulesImage",order=12,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced","urn:alm:descriptor:com.tectonic.ui:text"}
 	CodeModulesImage string `json:"codeModulesImage,omitempty"`
 
 	// Optional: If specified, indicates the OneAgent version to use
