f934adbcd4d87e9a16fca5476610f22790435b14
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-04-01 15:38:18 +0200

    Update install script

--- hack/e2e/install-kuttl.sh
+++ hack/e2e/install-kuttl.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  arch=`uname -m`
+  ostype=darwin
+else
+  arch=x86_64
+  ostype=linux
+fi
+
+version=0.11.1
+installurl=https://github.com/kudobuilder/kuttl/releases/download/v${version}/kubectl-kuttl_${version}_${ostype}_${arch}
+
 if ! kubectl kuttl; then
-  sudo curl -Lo /usr/local/bin/kubectl-kuttl https://github.com/kudobuilder/kuttl/releases/download/v0.11.1/kubectl-kuttl_0.11.1_linux_x86_64
+  echo Download from: $installurl
+  sudo curl -Lo /usr/local/bin/kubectl-kuttl $installurl
   sudo chmod +x /usr/local/bin/kubectl-kuttl
 fi
