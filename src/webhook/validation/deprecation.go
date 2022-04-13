0cf533976dcf11e9fd93b7a650cf7ca51b04f4b2
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-04-13 10:45:07 +0200

    Makes the deprecated feature-flag warning more precise

--- src/webhook/validation/deprecation.go
+++ src/webhook/validation/deprecation.go
@@ -17,7 +17,7 @@ func deprecatedFeatureFlagFormat(dv *dynakubeValidator, dynakube *dynatracev1bet
 
 	deprecatedPrefix := dynatracev1beta1.DeprecatedFeatureFlagPrefix + dynatracev1beta1.FeatureFlagAnnotationBase
 	if len(dynatracev1beta1.FlagsWithPrefix(dynakube, deprecatedPrefix)) > 0 {
-		return fmt.Sprintf(featureDeprecatedWarningMessage, "'alpha.' prefix not necessary")
+		return fmt.Sprintf(featureDeprecatedWarningMessage, "'alpha.' prefix not necessary for dynakube feature-flags")
 	}
 
 	return ""
