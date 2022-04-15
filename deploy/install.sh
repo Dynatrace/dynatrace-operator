b02234c8c730dafb9bf9151642cd7b8bf54e83df
Gabriel Krenn <gabriel.krenn@gmail.com>
2022-04-15 14:06:53 +0200

    Replace formatting with exit code 1

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-
-echo ${bold}This script is deprecated - installation aborted.${normal}
+echo This script is deprecated - installation aborted.
 echo
 echo If your tenant references this script, we recommend that you update your tenant to the latest version.
 echo This will provide you with updated deployment instructions on the \"Monitor Kubernetes/OpenShift\" page.
@@ -13,3 +10,4 @@ echo
 echo An alternative approach is to manually set up Kubernetes monitoring.
 echo For more information, please refer to the official documentation:
 echo https://dt-url.net/deprecated-installation
+exit 1
