6b7c421b401dd66b1eb72879c966b3822616ae1e
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-04-26 14:53:23 +0200

    Simplified wait-for.sh

--- src/testing/kuttl/activegate/tests/wait-for.sh
+++ src/testing/kuttl/activegate/tests/wait-for.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 
-kind=${1}
-label=${2}
-condition=${3}
+kind="${1}"
+label="${2}"
+condition="${3}"
 
-counter=0
-
-while true; do
-  lines=$(kubectl -n dynatrace get "${kind}" --ignore-not-found -l "${label}" | wc -l)
-
-  if [ "$lines" -gt 0 ]; then
-    kubectl -n dynatrace wait "${kind}" --timeout=60s --for="${condition}" -l "${label}" 2> /dev/null
-    ret=$?
-
-    if [ $ret -eq 0 ];
-    then
-      echo "[duration: ${counter}s]"
-      exit 0
-    else
-      ((counter=counter+1))
-    fi
-  fi
-
+while ! kubectl -n dynatrace wait "${kind}" --timeout=60s --for="${condition}" -l "${label}" 2> /dev/null ; do
   sleep 1
 done
+
