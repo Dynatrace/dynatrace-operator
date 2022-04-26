87d83974ed5e33e4c180ce7e8e09d05c3102cb81
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-04-26 14:55:44 +0200

    Moved tests to expected location

--- src/testing/kuttl/activegate/tests/wait-for.sh
+++ src/testing/kuttl/activegate/tests/wait-for.sh
@@ -4,7 +4,7 @@ kind="${1}"
 label="${2}"
 condition="${3}"
 
-while ! kubectl -n dynatrace wait "${kind}" --timeout=60s --for="${condition}" -l "${label}" 2> /dev/null ; do
+while ! kubectl -n dynatrace wait "${kind}" --for="${condition}" -l "${label}" 2> /dev/null ; do
   sleep 1
 done
 
