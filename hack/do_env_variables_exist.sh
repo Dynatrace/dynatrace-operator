ff7fafb142d6b721c10d821486f839d1e82bc157
Gabriel Krenn <gabriel.krenn@gmail.com>
2022-05-05 13:07:31 +0200

    Improve kuttl test suit (#744)
    Simple improvements for our kuttl tests:
    
    Add makefile commands for kuttl
    Add check for required env variables in makefile
    Correctly set timeout in our teststeps
    Updated kind node images

---
+++ hack/do_env_variables_exist.sh
@@ -0,0 +1,14 @@
+# check if parameters are set
+if [ -z "$1" ]
+then
+  echo "Usage: $0 <env variables to check>"
+  exit 1
+fi
+
+# make sure all fields are set
+for field in $1; do
+  if [ -z "${!field}" ]; then
+    echo "Error: $field is not set"
+    exit 1
+  fi
+done
