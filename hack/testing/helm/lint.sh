5d4d07061ba2ba4bd820a88106658b5ea4ef2337
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2022-05-05 13:03:37 +0200

    merged master

--- hack/testing/helm/lint.sh
+++ hack/testing/helm/lint.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 echo "Linting helm chart"
-if ! helm template --debug ./config/helm/chart/default/ \
-  --set apiUrl="test-url" \
-  --set apiToken="test-token" \
-  --set paasToken="test-token"; then
+if ! helm template --debug ./config/helm/chart/default/; then
   echo "could not parse template. something is wrong with template files"
   exit 10
 fi
-if ! helm lint --debug ./config/helm/chart/default/ \
-  --set apiUrl="test-url" \
-  --set apiToken="test-token" \
-  --set paasToken="test-token"; then
+if ! helm lint --debug ./config/helm/chart/default/; then
   echo "linter returned with error. check yaml formatting in files"
   exit 15
 fi
