43702e0958ac262087a77569bfb5a336111d42ec
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-05-25 16:41:59 +0200

    Adds more tests for the oneagent mutator

--- src/webhook/mutation/pod/oneagent/containers_test.go
+++ src/webhook/mutation/pod/oneagent/containers_test.go
@@ -1,1 +1,35 @@
 package oneagent_mutation
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestMutateConfigureInitContainer(t *testing.T) {
+	t.Run("add envs and volume mounts (no-csi)", func(t *testing.T) {
+		mutator := createTestPodMutator([]client.Object{getTestInitSecret()})
+		request := createTestMutationRequest(getTestDynakube(), nil)
+		installerInfo := getTestInstallerInfo()
+
+		mutator.configureInitContainer(request, installerInfo)
+
+		require.Equal(t, 6, len(request.InstallContainer.Env))
+		assert.Equal(t, installerVolumeMode, request.InstallContainer.Env[4].Value)
+		assert.Equal(t, 2, len(request.InstallContainer.VolumeMounts))
+	})
+
+	t.Run("add envs and volume mounts (csi)", func(t *testing.T) {
+		mutator := createTestPodMutator([]client.Object{getTestInitSecret()})
+		request := createTestMutationRequest(getTestCSIDynakube(), nil)
+		installerInfo := getTestInstallerInfo()
+
+		mutator.configureInitContainer(request, installerInfo)
+
+		require.Equal(t, 6, len(request.InstallContainer.Env))
+		assert.Equal(t, provisionedVolumeMode, request.InstallContainer.Env[4].Value)
+		assert.Equal(t, 2, len(request.InstallContainer.VolumeMounts))
+	})
+}
--- src/webhook/mutation/pod/oneagent/env_test.go
+++ src/webhook/mutation/pod/oneagent/env_test.go
@@ -52,8 +52,15 @@ func TestAddInstallerInitEnvs(t *testing.T) {
 	t.Run("Add installer init env", func(t *testing.T) {
 		container := &corev1.Container{}
 		testVolumeMode := "testMode"
-		addInstallerInitEnvs(container, getTestInstallerInfo(), testVolumeMode)
+		installerInfo := getTestInstallerInfo()
+		addInstallerInitEnvs(container, installerInfo, testVolumeMode)
 		require.Len(t, container.Env, 6)
+		assert.Equal(t, installerInfo.flavor, container.Env[0].Value)
+		assert.Equal(t, installerInfo.technologies, container.Env[1].Value)
+		assert.Equal(t, installerInfo.installPath, container.Env[2].Value)
+		assert.Equal(t, installerInfo.installerURL, container.Env[3].Value)
+		assert.Equal(t, testVolumeMode, container.Env[4].Value)
+		assert.Equal(t, "true", container.Env[5].Value)
 	})
 }
 
