a838cda18706339576b6c6c9cca60aa02d57c5b9
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2022-06-22 16:47:06 +0200

    Adapt makefile

--- hack/make/manifests/config.mk
+++ hack/make/manifests/config.mk
@@ -11,11 +11,11 @@ HELM_CRD_DIR=$(HELM_TEMPLATES_DIR)/Common/crd/
 MANIFESTS_DIR=config/deploy/
 
 KUBERNETES_CORE_YAML=$(MANIFESTS_DIR)kubernetes/kubernetes.yaml
-KUBERNETES_CSIDRIVER_YAML=$(MANIFESTS_DIR)kubernetes/kubernetes-csidriver.yaml
+KUBERNETES_CSIDRIVER_YAML=$(MANIFESTS_DIR)kubernetes/kubernetes-csi.yaml
 KUBERNETES_OLM_YAML=$(MANIFESTS_DIR)kubernetes/kubernetes-olm.yaml
 KUBERNETES_ALL_YAML=$(MANIFESTS_DIR)kubernetes/kubernetes-all.yaml
 
 OPENSHIFT_CORE_YAML=$(MANIFESTS_DIR)openshift/openshift.yaml
-OPENSHIFT_CSIDRIVER_YAML=$(MANIFESTS_DIR)openshift/openshift-csidriver.yaml
+OPENSHIFT_CSIDRIVER_YAML=$(MANIFESTS_DIR)openshift/openshift-csi.yaml
 OPENSHIFT_OLM_YAML=$(MANIFESTS_DIR)openshift/openshift-olm.yaml
 OPENSHIFT_ALL_YAML=$(MANIFESTS_DIR)openshift/openshift-all.yaml
