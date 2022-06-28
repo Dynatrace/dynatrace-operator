505d64c7f06073dd4cdcfaa3f34b201b9edc8f82
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-06-28 06:42:56 +0200

    Map endpoints to OpenAPI schemas

--- src/dtclient/endpoints.go
+++ src/dtclient/endpoints.go
@@ -2,6 +2,7 @@ package dtclient
 
 import "fmt"
 
+// begin of `documents/tenantApi/spec3.json` defined endpoints
 func (dtc *dynatraceClient) getAgentUrl(os, installerType, flavor, arch, version string, technologies []string) string {
 	url := fmt.Sprintf("%s/v1/deployment/installer/agent/%s/%s/version/%s?flavor=%s&arch=%s&bitness=64",
 		dtc.url, os, installerType, version, flavor, arch)
@@ -31,10 +32,23 @@ func (dtc *dynatraceClient) getHostsUrl() string {
 	return fmt.Sprintf("%s/v1/entity/infrastructure/hosts?includeDetails=false", dtc.url)
 }
 
+func (dtc *dynatraceClient) getProcessModuleConfigUrl() string {
+	return fmt.Sprintf("%s/v1/deployment/installer/agent/processmoduleconfig", dtc.url)
+}
+
+// end of `documents/tenantApi/spec3.json` defined endpoints
+
+// begin of `documents/tenantApiV2/spec3.json` defined endpoints
 func (dtc *dynatraceClient) getEntitiesUrl() string {
 	return fmt.Sprintf("%s/v2/entities", dtc.url)
 }
 
+func (dtc *dynatraceClient) getEventsUrl() string {
+	return fmt.Sprintf("%s/v1/events", dtc.url)
+}
+
+// also `documents/onpremClusterApi/spec3.json`
+// also `documents/clusterApi/spec3.json`
 func (dtc *dynatraceClient) getSettingsUrl(validate bool) string {
 	validationQuery := ""
 	if !validate {
@@ -43,21 +57,19 @@ func (dtc *dynatraceClient) getSettingsUrl(validate bool) string {
 	return fmt.Sprintf("%s/v2/settings/objects%s", dtc.url, validationQuery)
 }
 
-func (dtc *dynatraceClient) getProcessModuleConfigUrl() string {
-	return fmt.Sprintf("%s/v1/deployment/installer/agent/processmoduleconfig", dtc.url)
-}
-
-func (dtc *dynatraceClient) getEventsUrl() string {
-	return fmt.Sprintf("%s/v1/events", dtc.url)
-}
-
+// also `documents/onpremClusterApi/spec3.json`
+// also `documents/clusterApi/spec3.json`
 func (dtc *dynatraceClient) getTokensLookupUrl() string {
 	return fmt.Sprintf("%s/v1/tokens/lookup", dtc.url)
 }
 
+// also `documents/onpremClusterApi/spec3.json`
+// also `documents/clusterApi/spec3.json`
 func (dtc *dynatraceClient) getActiveGateAuthTokenUrl() string {
 	return fmt.Sprintf("%s/v2/activeGateTokens", dtc.url)
 }
+
+// end of `documents/tenantApiV2/spec3.json` defined endpoints
 
 func appendTechnologies(url string, technologies []string) string {
 	for _, tech := range technologies {
