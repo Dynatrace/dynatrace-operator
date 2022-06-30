bff5ffdbe531304628abf6a654852216e3a34468
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2022-06-30 14:24:42 +0200

    Makes ruxitagentproc.conf read/writeable by all users (#905)

--- src/installer/common/consts.go
+++ src/installer/common/consts.go
@@ -1,5 +1,8 @@
 package common
 
 const (
-	AgentConfDirPath = "agent/conf"
+	AgentConfDirPath     = "agent/conf"
+	RuxitConfFileName    = "ruxitagentproc.conf"
+	MkDirFileMode        = 0755
+	ReadWriteAllFileMode = 0666
 )
--- src/installer/zip/zip.go
+++ src/installer/zip/zip.go
@@ -47,10 +47,8 @@ func extractFileFromZip(fs afero.Fs, targetDir string, file *zip.File) error {
 	}
 
 	mode := file.Mode()
-
-	// Mark all files inside ./agent/conf as group-writable
-	if file.Name != common.AgentConfDirPath && isAgentConfFile(file.Name) {
-		mode |= 020
+	if isAgentConfFile(file.Name) {
+		mode = common.ReadWriteAllFileMode
 	}
 
 	if file.FileInfo().IsDir() {
