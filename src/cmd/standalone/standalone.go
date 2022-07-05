d4646b4b7fabb1dfc19e887fb4798dcf601171dc
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-07-05 10:24:00 +0200

    Fixes codemodules install permissions

--- src/cmd/standalone/standalone.go
+++ src/cmd/standalone/standalone.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/src/standalone"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -18,7 +19,7 @@ func NewStandaloneCommand() *cobra.Command {
 }
 
 func startStandAloneInit(_ *cobra.Command, _ []string) error {
-	// TODO: make the code below testable and test it, but in another ticket because otherwise adding the other commands will take a week
+	unix.Umask(0000)
 	standaloneRunner, err := standalone.NewRunner(afero.NewOsFs())
 	if err != nil {
 		return err
