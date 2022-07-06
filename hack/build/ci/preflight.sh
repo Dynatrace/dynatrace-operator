3719ff384b54d9d3441d79fc283b23a299fa49cc
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-07-06 12:26:05 +0200

    Sync with `ci/rh-preflight`

---
+++ hack/build/ci/preflight.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -xe
+
+readonly PREFLIGHT_VERSION="${1}"
+readonly IMAGE_TAG="${2}"
+readonly PREFLIGHT_REPORT_NAME="${3}"
+
+readonly PREFLIGHT_EXECUTABLE="preflight-linux-amd64"
+readonly PREFLIGHT_LOG="preflight.log"
+
+download_preflight() {
+  curl -LO "https://github.com/redhat-openshift-ecosystem/openshift-preflight/releases/download/${PREFLIGHT_VERSION}/${PREFLIGHT_EXECUTABLE}"
+  sudo chmod +x "${PREFLIGHT_EXECUTABLE}"
+}
+
+check_image() {
+  ./"${PREFLIGHT_EXECUTABLE}" check container "${IMAGE_TAG}" 1> "${PREFLIGHT_REPORT_NAME}" 2> "${PREFLIGHT_LOG}"
+  echo "${PREFLIGHT_EXECUTABLE} returned ${?}"
+  cat "${PREFLIGHT_LOG}"
+  grep "Preflight result: PASSED" "${PREFLIGHT_LOG}" || exit 1
+}
+
+
+download_preflight
+check_image
