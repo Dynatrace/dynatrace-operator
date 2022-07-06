590f58dad099b386a8ac7485a29644705f85aa74
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-07-06 12:07:26 +0200

    Remove PROJECT_ID; upload artifact with preflight report

--- hack/build/ci/preflight.sh
+++ hack/build/ci/preflight.sh
@@ -4,9 +4,9 @@ set -xe
 
 readonly PREFLIGHT_VERSION="${1}"
 readonly IMAGE_TAG="${2}"
+readonly PREFLIGHT_REPORT_NAME="${3}"
 
 readonly PREFLIGHT_EXECUTABLE="preflight-linux-amd64"
-readonly PREFLIGHT_REPORT_JSON="report.json"
 readonly PREFLIGHT_LOG="preflight.log"
 
 download_preflight() {
@@ -15,9 +15,8 @@ download_preflight() {
 }
 
 check_image() {
-  ./"${PREFLIGHT_EXECUTABLE}" check container "${IMAGE_TAG}" 1> "${PREFLIGHT_REPORT_JSON}" 2> "${PREFLIGHT_LOG}"
+  ./"${PREFLIGHT_EXECUTABLE}" check container "${IMAGE_TAG}" 1> "${PREFLIGHT_REPORT_NAME}" 2> "${PREFLIGHT_LOG}"
   echo "${PREFLIGHT_EXECUTABLE} returned ${?}"
-  cat "${PREFLIGHT_REPORT_JSON}"
   cat "${PREFLIGHT_LOG}"
   grep "Preflight result: PASSED" "${PREFLIGHT_LOG}" || exit 1
 }
