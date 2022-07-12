404c11e9c90f6c99fd68bca8c7d6a8c0015f8afa
Gabriel Krenn <gabriel.krenn@gmail.com>
2022-07-12 15:43:30 +0200

    Remove unused key

--- src/kubesystem/kubesystem.go
+++ src/kubesystem/kubesystem.go
@@ -7,7 +7,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 const (
@@ -32,13 +31,4 @@ func IsDeployedViaOlm(clt client.Reader, podName string, podNamespace string) (b
 	}
 	_, isDeployedViaOlm := pod.Annotations[olmSpecificAnnotation]
 	return isDeployedViaOlm, nil
-}
-
-func CreateDefaultClient() (client.Client, error) {
-	kubeCfg, err := config.GetConfig()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	kubeClient, err := client.New(kubeCfg, client.Options{})
-	return kubeClient, errors.WithStack(err)
 }
