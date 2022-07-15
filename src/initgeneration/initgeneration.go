325b8fceabdb087417fffb0c80e7d74999b57cb4
albian.krasniqi <albian.krasniqi@dynatrace.com>
2022-07-15 15:24:30 +0200

    Testing logs without config

--- src/initgeneration/initgeneration.go
+++ src/initgeneration/initgeneration.go
@@ -2,7 +2,6 @@ package initgeneration
 
 import (
 	"context"
-	"encoding/json"
 	"strconv"
 
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
@@ -243,10 +242,10 @@ func (g *InitGenerator) initIMNodes() (nodeInfo, error) {
 }
 
 func (g *InitGenerator) createSecretData(config *standalone.SecretConfig) (map[string][]byte, error) {
-	jsonConfig, err := json.Marshal(*config)
+	/*jsonConfig, err := json.Marshal(*config)
 	if err != nil {
 		return nil, err
-	}
+	}*/
 	hasHost := "false"
 	if config.HasHost {
 		hasHost = "true"
@@ -258,20 +257,20 @@ func (g *InitGenerator) createSecretData(config *standalone.SecretConfig) (map[s
 	}
 
 	secretData := map[string][]byte{
-		standalone.SecretConfigFieldName: jsonConfig,
-		"apiUrl":                         []byte(config.ApiUrl),
-		"apiToken":                       []byte(config.ApiToken),
-		"paasToken":                      []byte(config.PaasToken),
-		dynatracev1beta1.ProxyKey:        []byte(config.Proxy), // needed so that it can be mounted to the user's pod without directly reading the secret
-		"networkZone":                    []byte(config.NetworkZone),
-		"trustedCAs":                     []byte(config.TrustedCAs),
-		"skipCertCheck":                  []byte(skipCertCheck),
-		"tenantUUID":                     []byte(config.TenantUUID),
-		"hasHost":                        []byte(hasHost),
-		"tlsCert":                        []byte(config.TlsCert),
-		"hostGroup":                      []byte(config.HostGroup),
-		"initialConnectRetry":            []byte(strconv.Itoa(config.InitialConnectRetry)),
-		"clusterID":                      []byte(config.ClusterID),
+		//standalone.SecretConfigFieldName: jsonConfig,
+		"apiUrl":                  []byte(config.ApiUrl),
+		"apiToken":                []byte(config.ApiToken),
+		"paasToken":               []byte(config.PaasToken),
+		dynatracev1beta1.ProxyKey: []byte(config.Proxy), // needed so that it can be mounted to the user's pod without directly reading the secret
+		"networkZone":             []byte(config.NetworkZone),
+		"trustedCAs":              []byte(config.TrustedCAs),
+		"skipCertCheck":           []byte(skipCertCheck),
+		"tenantUUID":              []byte(config.TenantUUID),
+		"hasHost":                 []byte(hasHost),
+		"tlsCert":                 []byte(config.TlsCert),
+		"hostGroup":               []byte(config.HostGroup),
+		"initialConnectRetry":     []byte(strconv.Itoa(config.InitialConnectRetry)),
+		"clusterID":               []byte(config.ClusterID),
 	}
 
 	monitoringNodesPrefix := "monitoringNodes."
