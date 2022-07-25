09bc2b8cd810aeca3f7fb8c319e91ec6ac9c2af8
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2022-07-25 08:23:25 +0200

    Revert "renames google-marketplace to google-standard"
    This reverts commit 9a8f1f9e0cdd4ed2a0637758aa097077e4e57d77.

--- hack/helm/lint.sh
+++ hack/helm/lint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Linting helm chart"
-if ! helm template --debug ./config/helm/chart/default/ --set platform=google-standard --set installCRD=true --set csidriver.enabled=true; then
+if ! helm template --debug ./config/helm/chart/default/ --set platform=google-marketplace --set installCRD=true --set csidriver.enabled=true; then
   echo "could not parse template. something is wrong with template files"
   exit 10
 fi
-if ! helm lint --debug ./config/helm/chart/default/ --set platform=google-standard --set installCRD=true --set csidriver.enabled=true; then
+if ! helm lint --debug ./config/helm/chart/default/ --set platform=google-marketplace --set installCRD=true --set csidriver.enabled=true; then
   echo "linter returned with error. check yaml formatting in files"
   exit 15
 fi
