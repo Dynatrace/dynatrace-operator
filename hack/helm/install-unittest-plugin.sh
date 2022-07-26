8c18069337c57c2237a099ab8c33012349943466
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2022-07-26 10:53:26 +0200

    adds check for operation system

--- hack/helm/install-unittest-plugin.sh
+++ hack/helm/install-unittest-plugin.sh
@@ -2,19 +2,32 @@
 
 HELM_PLUGINS="$(helm env HELM_PLUGINS)"
 
-osArch="$(uname -m)"
+architecture="$(uname -m)"
+os="$(uname -s)"
 
-if [[ "${osArch}" == "x86_64" ]]; then
-  ARCH=amd64
-elif [[ "${osArch}" == "aarch64" ]]; then
-  ARCH=arm64
+if [[ "${architecture}" == "x86_64" ]]; then
+  ARCH="amd64"
+elif [[ "${architecture}" == "aarch64" ]]; then
+  ARCH="arm64"
 else
-  echo "Unsupported architecture '${osArch}'"
+  echo "Unsupported architecture '${architecture}'"
   exit 1
 fi
 
+if [[ "${os}" == "Linux" ]]; then
+  PLATFORM="linux"
+elif [[ "${os}" == "Darwin" ]]; then
+  PLATFORM="macos"
+else
+  echo "Unsupported operating system '${os}'"
+  exit 1
+fi
+
+
 helm plugin uninstall unittest || true
-wget "https://github.com/quintush/helm-unittest/releases/download/v0.2.8/helm-unittest-linux-${ARCH}-0.2.8.tgz" -O helm-unittest.tgz
+curl \
+  -L "https://github.com/quintush/helm-unittest/releases/download/v0.2.8/helm-unittest-${PLATFORM}-${ARCH}-0.2.8.tgz" \
+  -o helm-unittest.tgz
 mkdir -p "${HELM_PLUGINS}/unittest"
 tar xzvf helm-unittest.tgz -C "${HELM_PLUGINS}/unittest"
 rm helm-unittest.tgz
