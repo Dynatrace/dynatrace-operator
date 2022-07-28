9b3ebe2420400abf771d48823f1cacdbdf04a58d
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2022-07-28 13:02:40 +0200

    Renames platform value "google" to "google-marketplace" (#997)
    (cherry picked from commit 1aa149f5f11ba32967dadfeccf514101227fb1ba)

--- hack/gcr/deployer-image.sh
+++ hack/gcr/deployer-image.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -eu
+
 export REGISTRY=gcr.io/dynatrace-marketplace-dev
 export APP_NAME=dynatrace-operator
+TAG="${1:-""}"
 
-if docker build --tag "$REGISTRY/$APP_NAME/deployer" -f config/helm/Dockerfile config/helm; then
-  docker push "$REGISTRY/$APP_NAME/deployer"
+if docker build --tag "$REGISTRY/$APP_NAME/deployer${TAG}" -f config/helm/Dockerfile config/helm; then
+  docker push "$REGISTRY/$APP_NAME/deployer${TAG}"
 fi
---
+++ hack/make/deploy/gcr.mk
@@ -0,0 +1,9 @@
+## Deploys the operator using a snapshot deployer image for a standard GKE cluster
+deploy/gke/deployer:
+	kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/application/master/deploy/kube-app-manager-aio.yaml
+	./hack/gcr/deploy.sh ":snapshot-${SNAPSHOT_SUFFIX}"
+
+## Deploys the operator using a snapshot deployer image for an autopilot GKE cluster
+deploy/autopilot/deployer:
+	kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/application/master/deploy/kube-app-manager-aio.yaml
+	./hack/gcr/deploy.sh ":snapshot-${SNAPSHOT_SUFFIX}" "google-autopilot"
