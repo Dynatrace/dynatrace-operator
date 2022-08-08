1b072629da06677c4fef55c6ff5633237fd63885
Michal Grzybek <michal.grzybek@dynatrace.com>
2022-08-08 10:19:13 +0200

    Remove TODOs

--- src/cmd/csi/server/builder.go
+++ src/cmd/csi/server/builder.go
@@ -124,7 +124,6 @@ func (builder CommandBuilder) buildRun() func(*cobra.Command, []string) error {
 			return err
 		}
 
-		// TODO: make the code below testable and test it, but in another ticket because otherwise adding the other commands will take a week
 		access, err := metadata.NewAccess(dtcsi.MetadataAccessPath)
 		if err != nil {
 			return err
