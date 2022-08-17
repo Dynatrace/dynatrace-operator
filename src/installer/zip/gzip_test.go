6b7fa1ddffb01da31cfd3e64382cc33701e3c944
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2022-08-17 13:34:30 +0200

    Makes the csi extract archives into a tmp folder (#1031)
    * Makes the csi extract archives into a tmp folder
    
    * Create `/data/<tenant>/bin` subdirectory during CSI setup
    
    * Linting
    
    * Some fixes according to reviews
    
    * Lint fix (not related to changes)
    
    Co-authored-by: Michał Grzybek <35979436+mjgrzybek@users.noreply.github.com>
    Co-authored-by: Michal Grzybek <michal.grzybek@dynatrace.com>

--- src/installer/zip/gzip_test.go
+++ src/installer/zip/gzip_test.go
@@ -1,8 +1,10 @@
 package zip
 
 import (
+	"archive/tar"
 	"testing"
 
+	"github.com/klauspost/compress/gzip"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 )
@@ -10,17 +12,25 @@ import (
 func TestExtractGzip(t *testing.T) {
 	t.Run(`path empty`, func(t *testing.T) {
 		fs := afero.NewMemMapFs()
-		err := ExtractGzip(fs, "", "")
+		extractor := createTestExtractor(fs)
+		err := extractor.ExtractGzip("", "")
 		require.Error(t, err)
 	})
+
 	t.Run(`unzip test gzip file`, func(t *testing.T) {
 		fs := afero.NewMemMapFs()
 		gzipFile := SetupTestArchive(t, fs, TestRawGzip)
+
 		defer func() { _ = gzipFile.Close() }()
 
-		err := ExtractGzip(fs, gzipFile.Name(), TestZipDirName)
-		require.NoError(t, err)
+		// afero can't rename directories properly: https://github.com/spf13/afero/issues/141
 
+		reader, err := gzip.NewReader(gzipFile)
+		require.NoError(t, err)
+		tarReader := tar.NewReader(reader)
+
+		err = extractFilesFromGzip(fs, TestZipDirName, tarReader)
+		require.NoError(t, err)
 		testUnpackedArchive(t, fs)
 	})
 }
--- src/webhook/mutation/namespace_mutator/mutator.go
+++ src/webhook/mutation/namespace_mutator/mutator.go
@@ -38,10 +38,10 @@ func (nm *namespaceMutator) InjectClient(clt client.Client) error {
 
 // Handle does the mapping between the namespace and dynakube from the namespace's side.
 // There are 2 special cases:
-// 1. ignore the webhook's namespace: this is necessary because if we want to monitor the whole cluster
-//    we would tag our own namespace which would cause the podInjector webhook to inject into our pods which can cause issues. (infra-monitoring pod injected into == bad)
-// 2. if the namespace was updated by the operator => don't do the mapping: we detect this using an annotation, we do this because the operator also does the mapping
-//    but from the dynakube's side (during dynakube reconcile) and we don't want to repeat ourselves. So we just remove the annotation.
+//  1. ignore the webhook's namespace: this is necessary because if we want to monitor the whole cluster
+//     we would tag our own namespace which would cause the podInjector webhook to inject into our pods which can cause issues. (infra-monitoring pod injected into == bad)
+//  2. if the namespace was updated by the operator => don't do the mapping: we detect this using an annotation, we do this because the operator also does the mapping
+//     but from the dynakube's side (during dynakube reconcile) and we don't want to repeat ourselves. So we just remove the annotation.
 func (nm *namespaceMutator) Handle(ctx context.Context, request admission.Request) admission.Response {
 	if nm.namespace == request.Namespace {
 		return admission.Patched("")
