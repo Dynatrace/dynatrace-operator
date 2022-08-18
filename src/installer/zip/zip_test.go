fb894a6a5ba165fb4c1a59a315f54827ec5a02a5
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2022-08-18 15:16:57 +0200

    Makes the csi extract archives into a tmp folder (#1031) (#1059)
    Co-authored-by: Michał Grzybek <35979436+mjgrzybek@users.noreply.github.com>
    Co-authored-by: Michal Grzybek <michal.grzybek@dynatrace.com>

--- src/installer/zip/zip_test.go
+++ src/installer/zip/zip_test.go
@@ -3,6 +3,7 @@ package zip
 import (
 	"testing"
 
+	"github.com/klauspost/compress/zip"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 )
@@ -10,17 +11,26 @@ import (
 func TestExtractZip(t *testing.T) {
 	t.Run(`file nil`, func(t *testing.T) {
 		fs := afero.NewMemMapFs()
-		err := ExtractZip(fs, nil, "")
+		extractor := createTestExtractor(fs)
+		err := extractor.ExtractZip(nil, "")
 		require.EqualError(t, err, "file is nil")
 	})
 	t.Run(`unzip test zip file`, func(t *testing.T) {
 		fs := afero.NewMemMapFs()
 		zipFile := SetupTestArchive(t, fs, TestRawZip)
+
 		defer func() { _ = zipFile.Close() }()
 
-		err := ExtractZip(fs, zipFile, TestZipDirName)
+		// afero can't rename directories properly: https://github.com/spf13/afero/issues/141
+
+		fileInfo, err := zipFile.Stat()
 		require.NoError(t, err)
 
+		reader, err := zip.NewReader(zipFile, fileInfo.Size())
+		require.NoError(t, err)
+
+		err = extractFilesFromZip(fs, TestZipDirName, reader)
+		require.NoError(t, err)
 		testUnpackedArchive(t, fs)
 	})
 }
