48d2d841d389a048e22969b51e2ac1df7fad4c38
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2022-09-05 13:54:40 +0200

    adds a test to check if oneagents can connect correctly and startup correctly

--- test/cloudnative_test.go
+++ test/cloudnative_test.go
@@ -3,7 +3,10 @@
 package test
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"k8s.io/client-go/kubernetes"
 	"os"
 	"path"
 	"testing"
@@ -26,7 +29,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/e2e-framework/klient/k8s"
 	"sigs.k8s.io/e2e-framework/klient/wait"
 	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
@@ -86,21 +89,44 @@ func install(t *testing.T) features.Feature {
 	defaultInstallation.Assess("operator started", operator.WaitForDeployment())
 	defaultInstallation.Assess("webhook started", webhook.WaitForDeployment())
 	defaultInstallation.Assess("csi driver started", csi.WaitForDaemonset())
-	defaultInstallation.Assess("dynakube applied", applyDynakube(secretConfig))
+	defaultInstallation.Assess("dynakube applied", applyDynakube(secretConfig.ApiUrl))
 	defaultInstallation.Assess("activegate started", activegate.WaitForStatefulSet())
 	defaultInstallation.Assess("oneagent started", oneagent.WaitForDaemonset())
 	defaultInstallation.Assess("dynakube phase changes to 'Running'", waitForDynakubePhase())
 	defaultInstallation.Assess("restart sample apps", restartSampleApps)
-	defaultInstallation.Assess("sample apps have init containers", checkForInitContainers)
+	defaultInstallation.Assess("sample apps have working init containers", checkInitContainers)
+	defaultInstallation.Assess("oneagents can connect", oneAgentsCanConnect())
 
 	return defaultInstallation.Feature()
 }
 
-func checkForInitContainers(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+func oneAgentsCanConnect() features.Func {
+	return func(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
+		resource := environmentConfig.Client().Resources()
+		clientset, err := kubernetes.NewForConfig(resource.GetConfig())
+
+		require.NoError(t, err)
+		require.NoError(t, oneagent.ForEachPod(ctx, resource, func(pod corev1.Pod) {
+			logStream, err := clientset.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{}).Stream(ctx)
+
+			require.NoError(t, err)
+
+			assertLogContains(t, logStream, "[oneagentos] [PingReceiver] Ping received: Healthy(0)")
+		}))
+
+		return ctx
+	}
+}
+
+func checkInitContainers(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
 	var pods corev1.PodList
-	resources := config.Client().Resources()
+	resources := environmentConfig.Client().Resources()
 
 	require.NoError(t, resources.WithNamespace(sampleAppsNamespace).List(ctx, &pods))
+
+	clientset, err := kubernetes.NewForConfig(resources.GetConfig())
+
+	require.NoError(t, err)
 
 	for _, pod := range pods.Items {
 		require.NotNil(t, pod)
@@ -108,13 +134,31 @@ func checkForInitContainers(ctx context.Context, t *testing.T, config *envconf.C
 		require.NotEmpty(t, pod.Spec.InitContainers)
 
 		oneAgentInstallContainer := pod.Spec.InitContainers[0]
+
 		assert.Equal(t, oneAgentInstallContainerName, oneAgentInstallContainer.Name)
+
+		logStream, err := clientset.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{
+			Container: oneAgentInstallContainerName,
+		}).Stream(ctx)
+
+		require.NoError(t, err)
+		assertLogContains(t, logStream, "standalone agent init completed")
 	}
 
 	return ctx
 }
 
+func assertLogContains(t *testing.T, logStream io.ReadCloser, contains string) {
+	buffer := new(bytes.Buffer)
+	copied, err := io.Copy(buffer, logStream)
+
+	require.NoError(t, err)
+	require.Equal(t, int64(buffer.Len()), copied)
+	assert.Contains(t, buffer.String(), contains)
+}
+
 func restartSampleApps(ctx context.Context, t *testing.T, config *envconf.Config) context.Context {
+	var sampleDeployment appsv1.Deployment
 	var pods corev1.PodList
 	resources := config.Client().Resources()
 
@@ -124,9 +168,7 @@ func restartSampleApps(ctx context.Context, t *testing.T, config *envconf.Config
 		require.NoError(t, resources.Delete(ctx, &pod))
 	}
 
-	var sampleDeployment appsv1.Deployment
 	require.NoError(t, resources.Get(ctx, sampleAppsName, sampleAppsNamespace, &sampleDeployment))
-
 	require.NoError(t, wait.For(
 		conditions.New(resources).DeploymentConditionMatch(
 			&sampleDeployment, appsv1.DeploymentAvailable, corev1.ConditionTrue)))
@@ -136,20 +178,20 @@ func restartSampleApps(ctx context.Context, t *testing.T, config *envconf.Config
 
 func dynakube() dynatracev1beta1.DynaKube {
 	return dynatracev1beta1.DynaKube{
-		ObjectMeta: v1.ObjectMeta{
+		ObjectMeta: metav1.ObjectMeta{
 			Name:      dynakubeName,
 			Namespace: dynatraceNamespace,
 		},
 	}
 }
 
-func applyDynakube(secretConfig secrets.Secrets) features.Func {
+func applyDynakube(apiUrl string) features.Func {
 	return func(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
 		require.NoError(t, dynatracev1beta1.AddToScheme(environmentConfig.Client().Resources().GetScheme()))
 
 		instance := dynakube()
 		instance.Spec = dynatracev1beta1.DynaKubeSpec{
-			APIURL: secretConfig.ApiUrl,
+			APIURL: apiUrl,
 			OneAgent: dynatracev1beta1.OneAgentSpec{
 				CloudNativeFullStack: &dynatracev1beta1.CloudNativeFullStackSpec{},
 			},
--- test/oneagent/daemonset.go
+++ test/oneagent/daemonset.go
@@ -1,10 +1,17 @@
 package oneagent
 
 import (
+	"context"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/daemonset"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
+
+type PodConsumer func(pod corev1.Pod)
 
 func WaitForDaemonset() features.Func {
 	return daemonset.WaitFor("dynakube-oneagent", "dynatrace")
@@ -12,4 +19,31 @@ func WaitForDaemonset() features.Func {
 
 func WaitForDaemonSetPodsDeletion() env.Func {
 	return daemonset.WaitForPodsDeletion("dynakube-oneagent", "dynatrace")
+}
+
+func Get(ctx context.Context, resource *resources.Resources) (appsv1.DaemonSet, error) {
+	var daemonSet appsv1.DaemonSet
+	err := resource.Get(ctx, "dynakube-oneagent", "dynatrace", &daemonSet)
+	return daemonSet, err
+}
+
+func ForEachPod(ctx context.Context, resource *resources.Resources, consumer PodConsumer) error {
+	var pods corev1.PodList
+	daemonSet, err := Get(ctx, resource)
+
+	if err != nil {
+		return err
+	}
+
+	err = resource.List(ctx, &pods, resources.WithLabelSelector(labels.FormatLabels(daemonSet.Labels)))
+
+	if err != nil {
+		return err
+	}
+
+	for _, pod := range pods.Items {
+		consumer(pod)
+	}
+
+	return nil
 }
