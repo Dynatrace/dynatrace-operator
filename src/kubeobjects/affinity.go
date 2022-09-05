9612396638f973a44ae704ab3624e24e55aa294a
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-09-05 09:50:42 +0200

    Adds toleration matching node affinity to activegate

--- src/kubeobjects/affinity.go
+++ src/kubeobjects/affinity.go
@@ -1,6 +1,11 @@
 package kubeobjects
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	// "os"
+
+	// "github.com/Dynatrace/dynatrace-operator/src/kubesystem"
+	corev1 "k8s.io/api/core/v1"
+)
 
 const (
 	kubernetesArch = "kubernetes.io/arch"
@@ -15,8 +20,30 @@ func AffinityNodeRequirement() []corev1.NodeSelectorRequirement {
 	return affinityNodeRequirementsForArches(amd64)
 }
 
+func TolerationMatchingNodeAffinity() []corev1.Toleration {
+	return []corev1.Toleration{
+		{
+			Key:      kubernetesArch,
+			Operator: corev1.TolerationOpEqual,
+			Value:    amd64,
+			Effect:   corev1.TaintEffectNoSchedule,
+		},
+	}
+}
+
 func AffinityNodeRequirementWithARM64() []corev1.NodeSelectorRequirement {
 	return affinityNodeRequirementsForArches(amd64, arm64)
+}
+
+func TolerationMatchingNodeAffinityWithARM64() []corev1.Toleration {
+	return append(TolerationMatchingNodeAffinity(),
+		corev1.Toleration{
+			Key:      kubernetesArch,
+			Operator: corev1.TolerationOpEqual,
+			Value:    arm64,
+			Effect:   corev1.TaintEffectNoSchedule,
+		},
+	)
 }
 
 func affinityNodeRequirementsForArches(arches ...string) []corev1.NodeSelectorRequirement {
