9594f3ad75eb36680f1e20012356c30f71eb16f7
adam.orcholski <adam.orcholski@dynatrace.com>
2022-09-06 15:46:04 +0200

    Dockerfile-localbuild has been removed to simplify maintenance

--- hack/build/push_image.sh
+++ hack/build/push_image.sh
@@ -12,27 +12,17 @@ base_image="dynatrace-operator"
 out_image="${IMG:-quay.io/dynatrace/dynatrace-operator}:${TAG}"
 
 if [[ "${LOCALBUILD}" ]]; then
-  export CGO_ENABLED=1
   export GOOS=linux
-  export GOARCH=amd64
-
-  go build -ldflags="${go_linker_args}" -o ./build/_output/bin/dynatrace-operator ./src/cmd/operator/
-
-  go get github.com/google/go-licenses
-  go-licenses save ./... --save_path third_party_licenses --force
-
-  docker build . -f ./Dockerfile-localbuild -t "${base_image}" --label "quay.expires-after=14d" --no-cache
-
-  rm -rf ./third_party_licenses
-else
-  # directory required by docker copy command
-  mkdir -p third_party_licenses
-  docker build . -f ./Dockerfile -t "${base_image}" \
-    --build-arg "GO_LINKER_ARGS=${go_linker_args}" \
-    --label "quay.expires-after=14d" \
-    --no-cache
-  rm -rf third_party_licenses
+  export GOARCH=${GOARCH:-amd64}
 fi
+
+# directory required by docker copy command
+mkdir -p third_party_licenses
+docker build . -f ./Dockerfile -t "${base_image}" \
+  --build-arg "GO_LINKER_ARGS=${go_linker_args}" \
+  --label "quay.expires-after=14d" \
+  --no-cache
+rm -rf third_party_licenses
 
 docker tag "${base_image}" "${out_image}"
 docker push "${out_image}"
