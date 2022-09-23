835b83582b86e111a3a53bd4b013e45a9bdf029d
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-09-23 14:13:48 +0200

    Fixes tests

--- src/webhook/mutation/pod_mutator/pod_mutator_test.go
+++ src/webhook/mutation/pod_mutator/pod_mutator_test.go
@@ -90,11 +90,18 @@ func TestHandlePodMutation(t *testing.T) {
 		require.NoError(t, err)
 		assert.NotNil(t, mutationRequest.InstallContainer)
 		assert.Len(t, mutationRequest.Pod.Spec.InitContainers, 2)
+
+		require.NotNil(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.Privileged)
 		assert.False(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.Privileged)
+
+		require.NotNil(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.AllowPrivilegeEscalation)
 		assert.False(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.AllowPrivilegeEscalation)
+
+		require.NotNil(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.ReadOnlyRootFilesystem)
 		assert.True(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.ReadOnlyRootFilesystem)
-		assert.True(t, *mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.RunAsNonRoot)
-		assert.Equal(t, mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.SeccompProfile.Type, corev1.SeccompProfileTypeRuntimeDefault)
+
+		assert.Nil(t, mutationRequest.Pod.Spec.InitContainers[1].SecurityContext.RunAsNonRoot)
+
 		assert.Equal(t, mutationRequest.Pod.Spec.InitContainers[1].Resources, testResourceRequirements)
 		assert.Equal(t, "true", mutationRequest.Pod.Annotations[dtwebhook.AnnotationDynatraceInjected])
 		mutator1.(*dtwebhook.PodMutatorMock).AssertCalled(t, "Enabled", mutationRequest.BaseRequest)
