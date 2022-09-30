16d1efc0cdcdaabb59275ff814603f3132b58188
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2022-09-30 13:46:25 +0200

    Adds label-version-detection feature flag

--- src/api/v1beta1/feature_flags.go
+++ src/api/v1beta1/feature_flags.go
@@ -82,10 +82,12 @@ const (
 	AnnotationFeatureWebhookReinvocationPolicy = AnnotationFeaturePrefix + "webhook-reinvocation-policy"
 	AnnotationFeatureMetadataEnrichment        = AnnotationFeaturePrefix + "metadata-enrichment"
 
-	AnnotationFeatureIgnoreUnknownState = AnnotationFeaturePrefix + "ignore-unknown-state"
-	AnnotationFeatureIgnoredNamespaces  = AnnotationFeaturePrefix + "ignored-namespaces"
-	AnnotationFeatureAutomaticInjection = AnnotationFeaturePrefix + "automatic-injection"
+	AnnotationFeatureIgnoreUnknownState    = AnnotationFeaturePrefix + "ignore-unknown-state"
+	AnnotationFeatureIgnoredNamespaces     = AnnotationFeaturePrefix + "ignored-namespaces"
+	AnnotationFeatureAutomaticInjection    = AnnotationFeaturePrefix + "automatic-injection"
+	AnnotationFeatureLabelVersionDetection = AnnotationFeaturePrefix + "label-version-detection"
 
+	// CSI
 	AnnotationFeatureMaxFailedCsiMountAttempts = AnnotationFeaturePrefix + "max-csi-mount-attempts"
 )
 
@@ -246,6 +248,11 @@ func (dk *DynaKube) FeatureActiveGateIgnoreProxy() bool {
 // FeatureActiveGateAuthToken is a feature flag to enable authToken usage in the activeGate
 func (dk *DynaKube) FeatureActiveGateAuthToken() bool {
 	return dk.getFeatureFlagRaw(AnnotationFeatureActiveGateAuthToken) != "false"
+}
+
+// FeatureLabelVersionDetection is a feature flag to enable injecting additional environment variables based on user labels
+func (dk *DynaKube) FeatureLabelVersionDetection() bool {
+	return dk.getFeatureFlagRaw(AnnotationFeatureLabelVersionDetection) == "true"
 }
 
 // FeatureAgentInitialConnectRetry is a feature flag to configure startup delay of standalone agents
