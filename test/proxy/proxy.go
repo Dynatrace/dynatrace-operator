7867eb9b5ca917bbe94c53b9b9e905a22b049a9b
adam.orcholski <adam.orcholski@dynatrace.com>
2022-10-03 18:37:30 +0200

    wait until proxy service is working

--- test/proxy/proxy.go
+++ test/proxy/proxy.go
@@ -5,9 +5,15 @@ import (
 	"testing"
 
 	"github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
+	"github.com/Dynatrace/dynatrace-operator/test/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/deployment"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/manifests"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/namespace"
+	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/pod"
+	"github.com/Dynatrace/dynatrace-operator/test/logs"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -15,6 +21,7 @@ import (
 const (
 	proxyNamespace  = "proxy"
 	proxyDeployment = "squid"
+	curlPodProxy    = "curl-proxy"
 )
 
 var ProxySpec = &v1beta1.DynaKubeProxy{
@@ -25,6 +32,10 @@ func InstallProxy(builder *features.FeatureBuilder, proxySpec *v1beta1.DynaKubeP
 	if proxySpec != nil {
 		builder.Assess("install proxy", manifests.InstallFromFile("../testdata/proxy/proxy.yaml"))
 		builder.Assess("proxy started", deployment.WaitFor(proxyDeployment, proxyNamespace))
+
+		builder.Assess("query webhook via proxy", manifests.InstallFromFile("../testdata/activegate/curl-pod-webhook-via-proxy.yaml"))
+		builder.Assess("query is completed", pod.WaitFor(curlPodProxy, dynakube.DynatraceNamespace))
+		builder.Assess("proxy is running", checkProxyService)
 	}
 }
 
@@ -32,4 +43,20 @@ func DeleteProxyIfExists() func(ctx context.Context, environmentConfig *envconf.
 	return func(ctx context.Context, environmentConfig *envconf.Config, t *testing.T) (context.Context, error) {
 		return namespace.DeleteIfExists(proxyNamespace)(ctx, environmentConfig, t)
 	}
+}
+
+func checkProxyService(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
+	resources := environmentConfig.Client().Resources()
+
+	clientset, err := kubernetes.NewForConfig(resources.GetConfig())
+	require.NoError(t, err)
+
+	logStream, err := clientset.CoreV1().Pods(dynakube.DynatraceNamespace).GetLogs(curlPodProxy, &corev1.PodLogOptions{
+		Container: curlPodProxy,
+	}).Stream(ctx)
+	require.NoError(t, err)
+
+	logs.AssertLogContains(t, logStream, "CONNECT dynatrace-webhook.dynatrace.svc.cluster.local")
+
+	return ctx
 }
