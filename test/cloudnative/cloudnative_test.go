1969e063077b7cd0d6236ec0928033c39277c431
waodim <krasniqi_albian@hotmail.com>
2022-11-11 09:43:37 +0100

    Remove unused command

--- test/cloudnative/cloudnative_test.go
+++ test/cloudnative/cloudnative_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/environment"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/namespace"
 	"github.com/Dynatrace/dynatrace-operator/test/oneagent"
-	"github.com/Dynatrace/dynatrace-operator/test/proxy"
 	"github.com/Dynatrace/dynatrace-operator/test/sampleapps"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 )
@@ -22,13 +21,11 @@ func TestMain(m *testing.M) {
 	testEnvironment.BeforeEachTest(dynakube.DeleteIfExists(dynakube.NewBuilder().WithDefaultObjectMeta().Build()))
 	testEnvironment.BeforeEachTest(oneagent.WaitForDaemonSetPodsDeletion())
 	testEnvironment.BeforeEachTest(namespace.Recreate(namespace.NewBuilder(dynakube.Namespace).Build()))
-	testEnvironment.BeforeEachTest(proxy.DeleteProxyIfExists())
 
 	testEnvironment.AfterEachTest(namespace.Delete(sampleapps.Namespace))
 	testEnvironment.AfterEachTest(dynakube.DeleteIfExists(dynakube.NewBuilder().WithDefaultObjectMeta().Build()))
 	testEnvironment.AfterEachTest(oneagent.WaitForDaemonSetPodsDeletion())
 	testEnvironment.AfterEachTest(namespace.Delete(dynakube.Namespace))
-	testEnvironment.AfterEachTest(proxy.DeleteProxyIfExists())
 
 	testEnvironment.Run(m)
 }
