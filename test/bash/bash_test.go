d3d8c15a8091f8807ddfbc46df6db0c785bc3ad3
Stefan Hauth <stefan.hauth@dynatrace.com>
2022-12-02 09:07:03 +0100

    fixup! Add support-archive E2E test.

--- test/bash/bash_test.go
+++ test/bash/bash_test.go
@@ -6,9 +6,7 @@ import (
 )
 
 func TestStringifier(t *testing.T) {
-	cmd := Pipe(DiskUsageWithTotal("/foobar"), FilterLastLineOnly())
-
 	assert.Equal(t, "du -c /foobar", DiskUsageWithTotal("/foobar").String())
 	assert.Equal(t, "tail -n 1", FilterLastLineOnly().String())
-	assert.Equal(t, "du -c /foobar | tail -n 1", cmd.String())
+	assert.Equal(t, "du -c /foobar | tail -n 1", Pipe(DiskUsageWithTotal("/foobar"), FilterLastLineOnly()).String())
 }
