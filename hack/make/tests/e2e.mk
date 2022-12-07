0034fe3026227f0a0fe946a5e6ad7f4a92c6d572
adam.orcholski <adam.orcholski@dynatrace.com>
2022-12-07 14:13:42 +0100

    test/scenarios folder contains test definitions

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -3,32 +3,32 @@ test/e2e: manifests/branch test/e2e/cloudnative test/e2e/applicationmonitoring t
 
 ## Runs ActiveGate e2e test only
 test/e2e/activegate: manifests
-	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/activegate
+	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/scenarios/activegate
 
 ## Runs CloudNative e2e test only
 test/e2e/cloudnative: manifests
-	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/cloudnative
+	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/scenarios/cloudnative/basic
 
 ## Runs CloudNative e2e test only
 test/e2e/classic: manifests
-	go test -v -tags e2e -timeout 10m -count=1 -failfast ./test/classic
+	go test -v -tags e2e -timeout 10m -count=1 -failfast ./test/scenarios/classic
 
 ## Runs CloudNative istio e2e test only
 test/e2e/cloudnative/istio: manifests
-	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/cloudnative/istio
+	go test -v -tags e2e -timeout 20m -count=1 -failfast ./test/scenarios/cloudnative/istio
 
 ## Runs CloudNative proxy e2e test only
 test/e2e/cloudnative/proxy: manifests
-	go test -v -tags e2e -count=1 -failfast ./test/cloudnative/proxy
+	go test -v -tags e2e -count=1 -failfast ./test/scenarios/cloudnative/proxy
 
 ## Runs CloudNative network problem e2e test only
 test/e2e/cloudnative/network: manifests
-	go test -v -tags e2e -timeout 10m -count=1 -failfast ./test/cloudnative/network
+	go test -v -tags e2e -timeout 10m -count=1 -failfast ./test/scenarios/cloudnative/network
 
 ## Runs Application Monitoring e2e test only
 test/e2e/applicationmonitoring: manifests
-	go test -v -tags e2e -count=1 -failfast ./test/appmon
+	go test -v -tags e2e -count=1 -failfast ./test/scenarios/applicationmonitoring
 
 ## Runs Application Monitoring e2e test only
 test/e2e/supportarchive:
-	go test -v -tags e2e -count=1 -failfast ./test/support_archive
+	go test -v -tags e2e -count=1 -failfast ./test/scenarios/support_archive
