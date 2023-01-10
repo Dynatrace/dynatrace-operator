5fd2ba601b80872d753ca6bf654a0c34c428c484
StefanHauth <63204425+StefanHauth@users.noreply.github.com>
2023-01-10 12:34:48 +0100

    Add collection of selected K8s object so support-archive. (#1425)

--- src/cmd/support_archive/config.go
+++ src/cmd/support_archive/config.go
@@ -2,6 +2,9 @@ package support_archive
 
 const OperatorVersionFileName = "operator-version.txt"
 const LogsDirectoryName = "logs"
+const ManifestsDirectoryName = "manifests"
+const InjectedNamespacesManifestsDirectoryName = "injected_namespaces"
+const ManifestsFileExtension = ".json"
 
 const operatorVersionCollectorName = "operatorVersionCollector"
 const logCollectorName = "logCollector"
