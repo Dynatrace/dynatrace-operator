140a07d40f2d49bf103656f510e71c1976212498
StefanHauth <63204425+StefanHauth@users.noreply.github.com>
2023-01-11 15:57:26 +0100

    E2E test for support archive K8s object collection. (#1461)

--- test/csi/daemonset.go
+++ test/csi/daemonset.go
@@ -4,26 +4,27 @@ import (
 	"context"
 
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/daemonset"
+	"github.com/Dynatrace/dynatrace-operator/test/operator"
 	appsv1 "k8s.io/api/apps/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
 )
 
 const (
-	name      = "dynatrace-oneagent-csi-driver"
-	namespace = "dynatrace"
+	Name      = "dynatrace-oneagent-csi-driver"
+	Namespace = operator.Namespace
 )
 
 func Get(ctx context.Context, resource *resources.Resources) (appsv1.DaemonSet, error) {
 	return daemonset.NewQuery(ctx, resource, client.ObjectKey{
-		Name:      name,
-		Namespace: namespace,
+		Name:      Name,
+		Namespace: Namespace,
 	}).Get()
 }
 
 func ForEachPod(ctx context.Context, resource *resources.Resources, consumer daemonset.PodConsumer) error {
 	return daemonset.NewQuery(ctx, resource, client.ObjectKey{
-		Name:      name,
-		Namespace: namespace,
+		Name:      Name,
+		Namespace: Namespace,
 	}).ForEachPod(consumer)
 }
--- test/scenarios/support_archive/support_archive_test.go
+++ test/scenarios/support_archive/support_archive_test.go
@@ -13,17 +13,22 @@ import (
 
 	"github.com/Dynatrace/dynatrace-operator/src/cmd/support_archive"
 	"github.com/Dynatrace/dynatrace-operator/src/functional"
+	"github.com/Dynatrace/dynatrace-operator/src/webhook"
+	"github.com/Dynatrace/dynatrace-operator/test/csi"
 	"github.com/Dynatrace/dynatrace-operator/test/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/environment"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/namespace"
 	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/pod"
+	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/replicaset"
+	"github.com/Dynatrace/dynatrace-operator/test/kubeobjects/service"
 	"github.com/Dynatrace/dynatrace-operator/test/oneagent"
 	"github.com/Dynatrace/dynatrace-operator/test/operator"
 	"github.com/Dynatrace/dynatrace-operator/test/setup"
+	e2ewebhook "github.com/Dynatrace/dynatrace-operator/test/webhook"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/slices"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/e2e-framework/klient/conf"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
@@ -32,12 +37,21 @@ import (
 
 var testEnvironment env.Environment
 
+const testAppNameNotInjected = "application1"
+const testAppNameInjected = "application2"
+
 func TestMain(m *testing.M) {
 	testEnvironment = environment.Get()
 	testEnvironment.BeforeEachTest(dynakube.DeleteIfExists(dynakube.NewBuilder().WithDefaultObjectMeta().Build()))
 	testEnvironment.BeforeEachTest(oneagent.WaitForDaemonSetPodsDeletion())
 	testEnvironment.BeforeEachTest(namespace.Recreate(namespace.NewBuilder(dynakube.Namespace).Build()))
+	testEnvironment.BeforeEachTest(namespace.Recreate(namespace.NewBuilder(testAppNameNotInjected).Build()))
+	testEnvironment.BeforeEachTest(namespace.Recreate(namespace.NewBuilder(testAppNameInjected).WithLabels(map[string]string{
+		webhook.InjectionInstanceLabel: "abc12345",
+	}).Build()))
 
+	testEnvironment.AfterEachTest(namespace.Delete(testAppNameInjected))
+	testEnvironment.AfterEachTest(namespace.Delete(testAppNameNotInjected))
 	testEnvironment.AfterEachTest(dynakube.DeleteIfExists(dynakube.NewBuilder().WithDefaultObjectMeta().Build()))
 	testEnvironment.AfterEachTest(oneagent.WaitForDaemonSetPodsDeletion())
 	testEnvironment.AfterEachTest(namespace.Delete(dynakube.Namespace))
@@ -55,49 +69,43 @@ func SupportArchiveExecution(t *testing.T) features.Feature {
 	supportArchiveExecution := features.New("support archive execution")
 	setup.InstallDynatraceFromSource(supportArchiveExecution, &secretConfig)
 	setup.AssessOperatorDeployment(supportArchiveExecution)
-	supportArchiveExecution.Assess("support archive subcommand can be executed correctly", checkSupportArchiveExecution())
+
+	supportArchiveExecution.Assess("support archive subcommand can be executed correctly", testSupportArchiveCommand())
 	return supportArchiveExecution.Feature()
 }
 
-func checkSupportArchiveExecution() features.Func {
+func testSupportArchiveCommand() features.Func {
 	return func(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
-		result := executeSupportArchive(ctx, t, environmentConfig, "--stdout")
+		result := executeSupportArchiveCommand(ctx, t, environmentConfig, "--stdout")
 		require.NotNil(t, result)
 
 		zipReader, err := gzip.NewReader(result.StdOut)
 		require.NoError(t, err)
-
-		requiredFiles := make([]string, 0)
-		requiredFiles = append(requiredFiles, support_archive.OperatorVersionFileName)
-		requiredFiles = append(requiredFiles, getRequiredLogFiles(t, ctx, environmentConfig)...)
-
 		tarReader := tar.NewReader(zipReader)
+
+		requiredFiles := collectRequiredFiles(t, ctx, environmentConfig)
 
 		hdr, err := tarReader.Next()
 		for err == nil {
-			index := slices.IndexFunc(requiredFiles, func(file string) bool { return file == hdr.Name })
-			assert.NotEqualf(t, -1, index, "Found unexpected file %s.", hdr.Name)
-
-			if index != -1 {
-				requiredFiles = slices.Delete(requiredFiles, index, index+1)
-			}
-
-			assert.NotZerof(t, hdr.Size, "File %s is empty.", hdr.Name)
+			requiredFiles = assertFile(t, requiredFiles, *hdr)
 			hdr, err = tarReader.Next()
 		}
+
 		require.Equal(t, io.EOF, err)
+
 		assert.Emptyf(t, requiredFiles, "Support archive does not contain all expected files.")
+		logMissingFiles(t, requiredFiles)
 		return ctx
 	}
 }
 
-func executeSupportArchive(ctx context.Context, t *testing.T, environmentConfig *envconf.Config, cmdLineArguments string) *pod.ExecutionResult {
+func executeSupportArchiveCommand(ctx context.Context, t *testing.T, environmentConfig *envconf.Config, cmdLineArguments string) *pod.ExecutionResult {
 	resources := environmentConfig.Client().Resources()
 
-	pods := operator.Get(t, ctx, resources)
+	pods := pod.List(t, ctx, resources, operator.Namespace)
 	require.NotNil(t, pods.Items)
 
-	operatorPods := functional.Filter(pods.Items, func(podItem v1.Pod) bool {
+	operatorPods := functional.Filter(pods.Items, func(podItem corev1.Pod) bool {
 		return strings.Contains(podItem.Name, "dynatrace-operator")
 	})
 
@@ -114,26 +122,122 @@ func executeSupportArchive(ctx context.Context, t *testing.T, environmentConfig 
 	return executionResult
 }
 
-func getRequiredLogFiles(t *testing.T, ctx context.Context, envConfig *envconf.Config) []string {
-	resources := envConfig.Client().Resources()
-	opPods := operator.Get(t, ctx, resources)
+func collectRequiredFiles(t *testing.T, ctx context.Context, environmentConfig *envconf.Config) []string {
+	requiredFiles := make([]string, 0)
+	requiredFiles = append(requiredFiles, support_archive.OperatorVersionFileName)
+	requiredFiles = append(requiredFiles, getRequiredPodFiles(t, ctx, environmentConfig)...)
+	requiredFiles = append(requiredFiles, getRequiredReplicasetFiles(t, ctx, environmentConfig)...)
+	requiredFiles = append(requiredFiles, getRequiredServiceFiles(t, ctx, environmentConfig)...)
+	requiredFiles = append(requiredFiles, getRequiredWorkloadFiles()...)
+	requiredFiles = append(requiredFiles, getRequiredNamespaceFiles()...)
+	return requiredFiles
+}
 
-	requiredLogs := make([]string, 0)
-
+func getRequiredPodFiles(t *testing.T, ctx context.Context, environmentConfig *envconf.Config) []string {
+	opPods := pod.List(t, ctx, environmentConfig.Client().Resources(), operator.Namespace)
+	requiredFiles := make([]string, 0)
 	for _, pod := range opPods.Items {
+		requiredFiles = append(requiredFiles,
+			fmt.Sprintf("%s/%s/Pod/%s%s", support_archive.ManifestsDirectoryName, pod.Namespace, pod.Name, support_archive.ManifestsFileExtension))
 		for _, container := range pod.Spec.Containers {
-			requiredLogs = append(requiredLogs, fmt.Sprintf("%s/%s/%s.log",
-				support_archive.LogsDirectoryName, pod.Name, container.Name))
+			requiredFiles = append(requiredFiles,
+				fmt.Sprintf("%s/%s/%s.log", support_archive.LogsDirectoryName, pod.Name, container.Name))
 		}
 	}
-	return requiredLogs
+	return requiredFiles
+}
+
+func getRequiredReplicasetFiles(t *testing.T, ctx context.Context, environmentConfig *envconf.Config) []string {
+	replicaSets := replicaset.List(t, ctx, environmentConfig.Client().Resources(), operator.Namespace)
+	requiredFiles := make([]string, 0)
+	for _, replicaSet := range replicaSets.Items {
+		requiredFiles = append(requiredFiles,
+			fmt.Sprintf("%s/%s/ReplicaSet/%s%s", support_archive.ManifestsDirectoryName, replicaSet.Namespace, replicaSet.Name, support_archive.ManifestsFileExtension))
+	}
+	return requiredFiles
+}
+
+func getRequiredServiceFiles(t *testing.T, ctx context.Context, environmentConfig *envconf.Config) []string {
+	services := service.List(t, ctx, environmentConfig.Client().Resources(), operator.Namespace)
+	requiredFiles := make([]string, 0)
+	for _, service := range services.Items {
+		requiredFiles = append(requiredFiles,
+			fmt.Sprintf("%s/%s/Service/%s%s", support_archive.ManifestsDirectoryName, service.Namespace, service.Name, support_archive.ManifestsFileExtension))
+	}
+	return requiredFiles
+}
+
+func getRequiredWorkloadFiles() []string {
+	requiredFiles := make([]string, 0)
+	requiredFiles = append(requiredFiles,
+		fmt.Sprintf("%s/%s/%s/%s%s",
+			support_archive.ManifestsDirectoryName,
+			operator.Namespace,
+			"Deployment",
+			operator.Name,
+			support_archive.ManifestsFileExtension))
+	requiredFiles = append(requiredFiles,
+		fmt.Sprintf("%s/%s/%s/%s%s",
+			support_archive.ManifestsDirectoryName,
+			operator.Namespace,
+			"Deployment",
+			e2ewebhook.Name,
+			support_archive.ManifestsFileExtension))
+	requiredFiles = append(requiredFiles,
+		fmt.Sprintf("%s/%s/%s/%s%s",
+			support_archive.ManifestsDirectoryName,
+			operator.Namespace,
+			"DaemonSet",
+			csi.Name,
+			support_archive.ManifestsFileExtension))
+	return requiredFiles
+}
+
+func getRequiredNamespaceFiles() []string {
+	requiredFiles := make([]string, 0)
+	requiredFiles = append(requiredFiles,
+		fmt.Sprintf("%s/%s/Namespace-%s%s",
+			support_archive.ManifestsDirectoryName,
+			operator.Namespace,
+			operator.Namespace,
+			support_archive.ManifestsFileExtension))
+	requiredFiles = append(requiredFiles,
+		fmt.Sprintf("%s/%s/Namespace-%s%s",
+			support_archive.ManifestsDirectoryName,
+			support_archive.InjectedNamespacesManifestsDirectoryName,
+			testAppNameInjected,
+			support_archive.ManifestsFileExtension))
+	return requiredFiles
+}
+
+func assertFile(t *testing.T, requiredFiles []string, hdr tar.Header) []string {
+	index := slices.IndexFunc(requiredFiles, func(file string) bool { return file == hdr.Name })
+	assert.NotEqualf(t, -1, index, "Found unexpected file %s.", hdr.Name)
+
+	if index != -1 {
+		requiredFiles = slices.Delete(requiredFiles, index, index+1)
+	}
+
+	assert.NotZerof(t, hdr.Size, "File %s is empty.", hdr.Name)
+
+	return requiredFiles
+}
+
+func logMissingFiles(t *testing.T, requiredFiles []string) {
+	if len(requiredFiles) > 0 {
+		missingFilesLog := "Missing files:"
+		for _, file := range requiredFiles {
+			missingFilesLog = fmt.Sprintf("%s\n%s", missingFilesLog, file)
+		}
+		t.Log(missingFilesLog)
+	}
 }
 
 // Note: mainly for dev purposes, test requires a running cluster with deployed operator to be successful
 func TestExecSupportArchive(t *testing.T) {
 	t.Skip("dev helper test")
 	kubeConfigPath := conf.ResolveKubeConfigFile()
-	envConfig := envconf.NewWithKubeConfig(kubeConfigPath)
+	environmentConfig := envconf.NewWithKubeConfig(kubeConfigPath)
 
-	checkSupportArchiveExecution()(context.TODO(), t, envConfig)
+	testSupportArchiveCommand()(context.TODO(), t, environmentConfig)
 }
