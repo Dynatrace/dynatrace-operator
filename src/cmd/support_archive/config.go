cdf9c84cafd99c6fd7cdd33d559dbc68e5023ff5
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2023-03-02 16:25:05 +0100

    Collect `.yaml` instead of `.json` manifests in support archive (#1585)
    * Collect yaml instead of json manifests
    
    * fix linting

--- src/cmd/support_archive/config.go
+++ src/cmd/support_archive/config.go
@@ -4,7 +4,7 @@ const OperatorVersionFileName = "operator-version.txt"
 const LogsDirectoryName = "logs"
 const ManifestsDirectoryName = "manifests"
 const InjectedNamespacesManifestsDirectoryName = "injected_namespaces"
-const ManifestsFileExtension = ".json"
+const ManifestsFileExtension = ".yaml"
 
 const operatorVersionCollectorName = "operatorVersionCollector"
 const logCollectorName = "logCollector"
