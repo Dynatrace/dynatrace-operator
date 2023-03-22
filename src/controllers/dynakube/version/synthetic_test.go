4e5ad9f7a268d702d41fa8f64aaf05450ae7003d
romr-of-dt <roman.rycaj@dynatrace.com>
2023-03-22 09:18:10 +0100

    semiscaled synthetic capability (3272)     A logic which reflects the properties of the synthetic image     into the DynaKube status has been animated.

---
+++ src/controllers/dynakube/version/synthetic_test.go
@@ -0,0 +1,43 @@
+package version
+
+import (
+	"context"
+	"testing"
+
+	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
+	"github.com/Dynatrace/dynatrace-operator/src/dockerconfig"
+	"github.com/Dynatrace/dynatrace-operator/src/dtclient"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestSyntheticUseDefaults(t *testing.T) {
+	t.Run("default image specified", func(t *testing.T) {
+		dynakube := &dynatracev1beta1.DynaKube{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					dynatracev1beta1.AnnotationFeatureSyntheticLocationEntityId: "non-existent",
+				},
+			},
+			Spec: dynatracev1beta1.DynaKubeSpec{
+				APIURL: testApiUrl,
+			},
+			Status: dynatracev1beta1.DynaKubeStatus{
+				Synthetic: dynatracev1beta1.SyntheticStatus{
+					VersionStatus: dynatracev1beta1.VersionStatus{
+						Version: "non-empty",
+					},
+				},
+			},
+		}
+		expectedImage := dynakube.DefaultSyntheticImage()
+		mockClient := &dtclient.MockDynatraceClient{}
+		registry := newFakeRegistryForImages(expectedImage)
+		updater := newSyntheticUpdater(dynakube, mockClient, registry.ImageVersionExt)
+
+		err := updater.UseDefaults(context.TODO(), &dockerconfig.DockerConfig{})
+		require.NoError(t, err, "default image set")
+		assertVersionStatusEquals(t, registry, getTaggedReference(t, expectedImage), dynakube.Status.Synthetic.VersionStatus)
+		require.Empty(t, dynakube.Status.Synthetic.Version, "zero version reported")
+	})
+}
