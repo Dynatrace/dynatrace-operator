03e5c36e3e31c905fd032bbff8f2a5f06a2572d1
waodim <krasniqi_albian@hotmail.com>
2023-04-07 08:35:08 +0200

    Rework OneAgentCanConnect check

--- test/helpers/components/oneagent/daemonset.go
+++ test/helpers/components/oneagent/daemonset.go
@@ -4,13 +4,20 @@ package oneagent
 
 import (
 	"context"
+	"testing"
+	"time"
 
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/kubeobjects/daemonset"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/kubeobjects/pod"
+	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
 
@@ -34,4 +41,17 @@ func ForEachPod(ctx context.Context, resource *resources.Resources, dynakube dyn
 		Name:      dynakube.OneAgentDaemonsetName(),
 		Namespace: dynakube.Namespace,
 	}).ForEachPod(actionFunc)
+}
+
+func OneAgentPodsAreReady(dynakube dynatracev1beta1.DynaKube) features.Func {
+	return func(ctx context.Context, t *testing.T, environmentConfig *envconf.Config) context.Context {
+		resource := environmentConfig.Client().Resources()
+
+		require.NoError(t, ForEachPod(ctx, resource, dynakube, func(pod corev1.Pod) {
+			err := wait.For(conditions.New(resource).PodReady(&pod), wait.WithTimeout(time.Minute*10))
+			require.NoError(t, err)
+		}))
+
+		return ctx
+	}
 }
