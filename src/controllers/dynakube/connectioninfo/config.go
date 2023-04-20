76fd77099580d730090967f446b48e671e4cd2d9
Stefan Hauth <stefan.hauth@dynatrace.com>
2023-04-20 14:15:10 +0200

    fixup! Cache connection info in Dynakube status. Remove communication hosts from connection info config map.

--- src/controllers/dynakube/connectioninfo/config.go
+++ src/controllers/dynakube/connectioninfo/config.go
@@ -7,8 +7,7 @@ import (
 const (
 	TenantTokenName            = "tenant-token"
 	CommunicationEndpointsName = "communication-endpoints"
-	//	CommunicationHosts         = "communication-hosts"
-	TenantUUIDName = "tenant-uuid"
+	TenantUUIDName             = "tenant-uuid"
 
 	TokenBasePath         = "/var/lib/dynatrace/secrets/tokens"
 	TenantTokenMountPoint = TokenBasePath + "/tenant-token"
