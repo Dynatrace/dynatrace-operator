fb57974c408f12a1ef1552553aa58eef0462d722
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-04-21 12:51:21 +0200

    Add general deploy undeploy commands

--- hack/make/deploy/kubernetes.mk
+++ hack/make/deploy/kubernetes.mk
@@ -1,3 +1,17 @@
+ENABLE_CSI ?= true
+
+## Deploy the operator in a cluster configured in ~/.kube/config where platform and version are autodetected
+deploy: manifests/crd/helm
+	helm upgrade dynatrace-operator config/helm/chart/default \
+			--namespace dynatrace \
+			--create-namespace \
+			--install \
+			--atomic \
+			--set installCRD=true \
+			--set csidriver.enabled=$(ENABLE_CSI) \
+			--set manifests=true \
+			--set image="$(IMAGE_URI)"
+
 ## Deploy the operator in the Kubernetes cluster configured in ~/.kube/config
 deploy/kubernetes: manifests/crd/helm
 	kubectl get namespace dynatrace || kubectl create namespace dynatrace
@@ -5,19 +19,13 @@ deploy/kubernetes: manifests/crd/helm
 			--namespace dynatrace \
 			--set installCRD=true \
 			--set platform="kubernetes" \
-			--set csidriver.enabled=true \
+			--set csidriver.enabled=$(ENABLE_CSI) \
 			--set manifests=true \
 			--set image="$(IMAGE_URI)" | kubectl apply -f -
 
 ## Deploy the operator in the Kubernetes cluster configured in ~/.kube/config
-deploy/kubernetes-no-csi: manifests/crd/helm
-	kubectl get namespace dynatrace || kubectl create namespace dynatrace
-	helm template dynatrace-operator config/helm/chart/default \
-			--namespace dynatrace \
-			--set installCRD=true \
-			--set platform="kubernetes" \
-			--set manifests=true \
-			--set image="$(IMAGE_URI)" | kubectl apply -f -
+deploy/kubernetes-no-csi:
+	ENABLE_CSI=false $(MAKE) deploy/kubernetes
 
 ## Deploy the operator in the Google Autopilot cluster configured in ~/.kube/config
 deploy/gke-autopilot: manifests/crd/helm
@@ -29,7 +37,11 @@ deploy/gke-autopilot: manifests/crd/helm
 			--set manifests=true \
 			--set image="$(IMAGE_URI)" | kubectl apply -f -
 
-
+## Undeploy the operator in a cluster configured in ~/.kube/config where platform and k8s version are autodetected
+undeploy: manifests/crd/helm
+	helm uninstall dynatrace-operator \
+			--namespace dynatrace
+	kubectl delete namespace dynatrace
 
 ## Undeploy the operator in the Kubernetes cluster configured in ~/.kube/config
 undeploy/kubernetes: manifests/crd/helm
@@ -37,18 +49,13 @@ undeploy/kubernetes: manifests/crd/helm
 			--namespace dynatrace \
 			--set installCRD=true \
 			--set platform="kubernetes" \
-			--set csidriver.enabled=true \
+			--set csidriver.enabled=$(ENABLE_CSI) \
 			--set manifests=true \
 			--set image="$(IMAGE_URI)" | kubectl delete -f -
 
 ## Undeploy the operator in the Kubernetes cluster configured in ~/.kube/config
-undeploy/kubernetes-no-csi: manifests/crd/helm
-	helm template dynatrace-operator config/helm/chart/default \
-			--namespace dynatrace \
-			--set installCRD=true \
-			--set platform="kubernetes" \
-			--set manifests=true \
-			--set image="$(IMAGE_URI)" | kubectl delete -f -
+undeploy/kubernetes-no-csi:
+	ENABLE_CSI=false $(MAKE) undeploy/kubernetes
 
 ## Undeploy the operator in the Google Autopilot cluster configured in ~/.kube/config
 undeploy/gke-autopilot: manifests/crd/helm
