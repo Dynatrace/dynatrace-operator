9f5c8b1aa0b2c89c5096b20b1dac92f8ca837eb2
wepudt <werner.putschoegl@dynatrace.com>
2023-05-16 13:03:17 +0200

    reintroduced curl due to github ratelimiting our downloads via api calls

--- hack/helm/install-unittest-plugin.sh
+++ hack/helm/install-unittest-plugin.sh
@@ -1,7 +1,37 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 desired_version="0.3.2"
 
-if ! helm plugin list | grep -q "^unittest.*$desired_version"; then
-  helm plugin install https://github.com/helm-unittest/helm-unittest.git --version v$desired_version
+HELM_PLUGINS="$(helm env HELM_PLUGINS)"
+
+architecture="$(uname -m)"
+os="$(uname -s)"
+
+if [[ "${architecture}" == "x86_64" ]]; then
+  ARCH="amd64"
+elif [[ "${architecture}" == "aarch64" ]]; then
+  ARCH="arm64"
+elif [[ "${architecture}" == "arm64" ]]; then
+  ARCH="arm64"
+else
+  echo "Unsupported architecture '${architecture}'"
+  exit 1
 fi
+
+if [[ "${os}" == "Linux" ]]; then
+  PLATFORM="linux"
+elif [[ "${os}" == "Darwin" ]]; then
+  PLATFORM="macos"
+else
+  echo "Unsupported operating system '${os}'"
+  exit 1
+fi
+
+
+helm plugin uninstall unittest || true
+curl \
+  -L "https://github.com/helm-unittest/helm-unittest/releases/download/v${desired_version}/helm-unittest-${PLATFORM}-${ARCH}-${desired_version}.tgz" \
+  -o helm-unittest.tgz
+mkdir -p "${HELM_PLUGINS}/unittest"
+tar xzvf helm-unittest.tgz -C "${HELM_PLUGINS}/unittest"
+rm helm-unittest.tgz
