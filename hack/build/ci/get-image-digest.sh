5d081b5582205958e03c21381366f36ceffb0c4c
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2023-06-20 10:58:48 +0200

    Add sbom to image during publish image step

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 digest=$(skopeo inspect docker://"${IMAGE}" --format "{{.Digest}}")
+digest_formatted=$(echo ${digest} | tr : -)
 echo "digest=${digest}">> "$GITHUB_OUTPUT"
+echo "digest_formatted=${digest_formatted}">> "$GITHUB_OUTPUT"
 
