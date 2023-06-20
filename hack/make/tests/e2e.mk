6367c7a5168d6a5593927a5c882806433f0690bb
waodim <albian.krasniqi@dynatrace.com>
2023-06-20 10:19:17 +0200

    Remove namespace deletion and comment in make target

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -42,7 +42,6 @@ test/e2e/cloudnative/network: manifests/crd/helm
 test/e2e/cloudnative/proxy: manifests/crd/helm
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 20m -count=1 ./test/scenarios/cloudnative/proxy $(SKIPCLEANUP)
 
-## Runs CloudNative public registry e2e test only
 test/e2e/cloudnative/publicregistry: manifests/crd/helm
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 20m -count=1 ./test/scenarios/cloudnative/public_registry $(SKIPCLEANUP)
 
