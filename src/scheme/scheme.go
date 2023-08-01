cb98d9921c3bb2802cf1aad8aadb17e6b47bccfe
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-08-01 14:38:09 +0200

    Fix GroupVersion and Scheme weirdness

--- src/scheme/scheme.go
+++ src/scheme/scheme.go
@@ -18,10 +18,10 @@ package scheme
 
 import (
 	dynatracev1alpha1 "github.com/Dynatrace/dynatrace-operator/src/api/v1alpha1"
-	// needed to ensure proper package initialization order
 	_ "github.com/Dynatrace/dynatrace-operator/src/api/v1alpha1/dynakube"
 	_ "github.com/Dynatrace/dynatrace-operator/src/api/v1alpha1/edgeconnect"
-	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1/dynakube"
+	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
+	_ "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1/dynakube"
 	istiov1alpha3 "istio.io/client-go/pkg/apis/networking/v1alpha3"
 	corev1 "k8s.io/api/core/v1"
 	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
