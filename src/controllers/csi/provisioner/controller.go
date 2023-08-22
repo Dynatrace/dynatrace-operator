0a68ad4e43c0d98f0cf2751729cccbffce0229a5
Stefan Hauth <stefan.hauth@dynatrace.com>
2023-08-22 13:16:29 +0200

    fixup! Set empty proxy instead of none if no proxy is configured in DynaKube.

--- src/controllers/csi/provisioner/controller.go
+++ src/controllers/csi/provisioner/controller.go
@@ -215,10 +215,9 @@ func (provisioner *OneAgentProvisioner) updateAgentInstallation(ctx context.Cont
 	latestProcessModuleConfig = latestProcessModuleConfig.
 		AddHostGroup(dk.HostGroup()).
 		AddConnectionInfo(dk.Status.OneAgent.ConnectionInfoStatus, tenantToken).
+		// set proxy explicitly empty, so old proxy settings get deleted where necessary
 		AddProxy("")
 	latestProcessModuleConfigCache = newProcessModuleConfigCache(latestProcessModuleConfig)
-
-	// Idea: do not write added settings to cache, just add everytime and write to final location, but keep cache in sync with tenant
 
 	if dk.NeedsOneAgentProxy() {
 		proxy, err := dk.Proxy(ctx, provisioner.apiReader)
