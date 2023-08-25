87c543ef03530747e2bd3c2deea057d2749d5bca
Stefan Hauth <stefan.hauth@dynatrace.com>
2023-08-25 10:31:31 +0200

    fixup! Fix unittests after controller-runtime and K8s api upgrade.

--- src/cmd/csi/provisioner/manager_test.go
+++ src/cmd/csi/provisioner/manager_test.go
@@ -24,6 +24,7 @@ func TestCsiDriverManagerProvider(t *testing.T) {
 		options := csiManagerProvider.createOptions("namespace")
 
 		assert.NotNil(t, options)
+		assert.Contains(t, options.Cache.DefaultNamespaces, "namespace")
 		assert.Equal(t, scheme.Scheme, options.Scheme)
 		assert.Equal(t, metricsBindAddress, options.Metrics.BindAddress)
 		assert.Equal(t, "", options.HealthProbeBindAddress)
