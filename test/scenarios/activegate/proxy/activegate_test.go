07effe5401c59f531a60079bd9ae608eb8bfb8c1
StefanHauth <63204425+StefanHauth@users.noreply.github.com>
2023-09-01 22:05:07 +0200

    Set logger for controller-runtime. (#2081)

--- test/scenarios/activegate/proxy/activegate_test.go
+++ test/scenarios/activegate/proxy/activegate_test.go
@@ -5,16 +5,20 @@ package activegateproxy
 import (
 	"testing"
 
+	"github.com/Dynatrace/dynatrace-operator/src/logger"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/istio"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/kubeobjects/environment"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/proxy"
 	"github.com/Dynatrace/dynatrace-operator/test/scenarios/activegate"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 )
 
 var testEnvironment env.Environment
 
 func TestMain(m *testing.M) {
+	log.SetLogger(logger.Factory.GetLogger("e2e-activegate-proxy"))
+
 	testEnvironment = environment.Get()
 	testEnvironment.BeforeEachTest(istio.AssertIstioNamespace())
 	testEnvironment.BeforeEachTest(istio.AssertIstiodDeployment())
--- test/scenarios/cloudnative/codemodules/cloudnative_test.go
+++ test/scenarios/cloudnative/codemodules/cloudnative_test.go
@@ -5,15 +5,18 @@ package codemodules
 import (
 	"testing"
 
+	"github.com/Dynatrace/dynatrace-operator/src/logger"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/istio"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/kubeobjects/environment"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/proxy"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 )
 
 var testEnvironment env.Environment
 
 func TestMain(m *testing.M) {
+	log.SetLogger(logger.Factory.GetLogger("e2e-cloudnative-codemodules"))
 	testEnvironment = environment.Get()
 	testEnvironment.Run(m)
 }
