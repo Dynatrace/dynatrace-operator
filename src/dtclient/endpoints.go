d489faa61d41533e6e3148300ea2fe051daffccf
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2023-09-01 17:50:03 +0200

    Refactor

--- src/dtclient/endpoints.go
+++ src/dtclient/endpoints.go
@@ -27,17 +27,15 @@ func (dtc *dynatraceClient) getAgentVersionsUrl(os, installerType, flavor, arch 
 func (dtc *dynatraceClient) getOneAgentConnectionInfoUrl() string {
 	if dtc.networkZone != "" {
 		return fmt.Sprintf("%s/v1/deployment/installer/agent/connectioninfo?networkZone=%s&defaultZoneFallback=true", dtc.url, dtc.networkZone)
-	} else {
-		return fmt.Sprintf("%s/v1/deployment/installer/agent/connectioninfo", dtc.url)
 	}
+	return fmt.Sprintf("%s/v1/deployment/installer/agent/connectioninfo", dtc.url)
 }
 
 func (dtc *dynatraceClient) getActiveGateConnectionInfoUrl() string {
 	if dtc.networkZone != "" {
 		return fmt.Sprintf("%s/v1/deployment/installer/gateway/connectioninfo?networkZone=%s", dtc.url, dtc.networkZone)
-	} else {
-		return fmt.Sprintf("%s/v1/deployment/installer/gateway/connectioninfo", dtc.url)
 	}
+	return fmt.Sprintf("%s/v1/deployment/installer/gateway/connectioninfo", dtc.url)
 }
 
 func (dtc *dynatraceClient) getHostsUrl() string {
