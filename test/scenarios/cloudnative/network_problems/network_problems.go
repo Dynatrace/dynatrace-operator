2de2e84a8ca4121ace7e5114d034f1a6fbfd4809
wepudt <werner.putschoegl@dynatrace.com>
2023-09-08 15:56:24 +0200

    uses new helpers

--- test/scenarios/cloudnative/network_problems/network_problems.go
+++ test/scenarios/cloudnative/network_problems/network_problems.go
@@ -15,8 +15,7 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/sampleapps"
 	sample "github.com/Dynatrace/dynatrace-operator/test/helpers/sampleapps/base"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/shell"
-	"github.com/Dynatrace/dynatrace-operator/test/helpers/steps/assess"
-	"github.com/Dynatrace/dynatrace-operator/test/helpers/steps/teardown"
+	"github.com/Dynatrace/dynatrace-operator/test/helpers/steps"
 	"github.com/Dynatrace/dynatrace-operator/test/helpers/tenant"
 	"github.com/Dynatrace/dynatrace-operator/test/project"
 	"github.com/Dynatrace/dynatrace-operator/test/scenarios/cloudnative"
@@ -53,27 +52,20 @@ func networkProblems(t *testing.T) features.Feature {
 	sampleApp.WithNamespace(sampleNamespace)
 	builder.Assess("create sample namespace", sampleApp.InstallNamespace())
 
-	// Register operator install
-	operatorNamespaceBuilder := namespace.NewBuilder(testDynakube.Namespace)
-	operatorNamespaceBuilder = operatorNamespaceBuilder.WithLabels(istio.InjectionLabel)
-	assess.InstallOperatorFromSourceWithCustomNamespace(builder, operatorNamespaceBuilder.Build(), testDynakube)
-
-	// Register network policy to block csi driver traffic
-	assess.InstallManifest(builder, csiNetworkPolicy)
-
-	// Register dynakube install
-	assess.InstallDynakube(builder, &secretConfig, testDynakube)
-
+	operatorNamespace := namespace.NewBuilder(testDynakube.Namespace).WithLabels(istio.InjectionLabel).Build()
+	steps.CreateFeatureEnvironment(builder,
+		steps.CreateNamespaceWithoutTeardown(operatorNamespace),
+		steps.InstallManifestFromFile(csiNetworkPolicy),
+		steps.DeployOperatorViaMake(operatorNamespace.Name, testDynakube.NeedsCSIDriver()),
+		steps.CreateDynakube(secretConfig, testDynakube),
+	)
 	// Register sample app install
 	builder.Assess("install sample-apps", sampleApp.Install())
 
 	// Register actual test
 	builder.Assess("check for dummy volume", checkForDummyVolume(sampleApp))
 
-	// Register network-policy, sample, dynakube and operator uninstall
-	teardown.UninstallManifest(builder, csiNetworkPolicy)
 	builder.Teardown(sampleApp.UninstallNamespace())
-	teardown.UninstallDynatrace(builder, testDynakube)
 
 	return builder.Feature()
 }
