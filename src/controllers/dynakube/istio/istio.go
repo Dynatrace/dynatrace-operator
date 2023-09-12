5448230ec0dacc7c49ebe11a8b8c093a73f3c481
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-09-12 15:44:21 +0200

    Update e2e test

--- src/controllers/dynakube/istio/istio.go
+++ src/controllers/dynakube/istio/istio.go
@@ -1,12 +1,8 @@
 package istio
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"strings"
 
-	"github.com/Dynatrace/dynatrace-operator/src/dtclient"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
@@ -17,17 +13,6 @@ var (
 	IstioGVRVersion = "v1alpha3"
 	IstioGVR        = fmt.Sprintf("%s/%s", IstioGVRName, IstioGVRVersion)
 )
-
-// TODO: Remove (e2e)
-// BuildNameForEndpoint returns a name to be used as a base to identify Istio objects.
-func BuildNameForEndpoint(commHosts []dtclient.CommunicationHost, name string) string {
-	result := make([]string, len(commHosts))
-	for index, commHost := range commHosts {
-		result[index] = fmt.Sprintf("%s-%s-%s-%d", name, commHost.Protocol, commHost.Host, commHost.Port)
-	}
-	sum := sha256.Sum256([]byte(strings.Join(result, "\n")))
-	return hex.EncodeToString(sum[:])
-}
 
 // TODO: Maybe move to Client
 // CheckIstioInstalled run discovery query for server resource for group version
--- src/controllers/dynakube/istio/istio_test.go
+++ src/controllers/dynakube/istio/istio_test.go
@@ -3,7 +3,6 @@ package istio
 import (
 	"testing"
 
-	"github.com/Dynatrace/dynatrace-operator/src/dtclient"
 	"github.com/stretchr/testify/assert"
 	fakeistio "istio.io/client-go/pkg/clientset/versioned/fake"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,38 +35,6 @@ func TestIstio(t *testing.T) {
 			isInstalled, err := CheckIstioInstalled(fakeDiscovery)
 			assert.Equal(t, tc.want, isInstalled)
 			assert.ErrorIs(t, tc.wantErr, err)
-		})
-	}
-}
-
-func TestBuildNameForEndpoint(t *testing.T) {
-	type args struct {
-		name      string
-		commHosts []dtclient.CommunicationHost
-	}
-
-	type test struct {
-		name string
-		args args
-		want string
-	}
-
-	tests := []test{
-		{
-			name: "empty host list",
-			args: args{name: "test", commHosts: []dtclient.CommunicationHost{}},
-			want: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-		},
-		{
-			name: "single host in list",
-			args: args{name: "test", commHosts: []dtclient.CommunicationHost{{Protocol: "http", Host: "mydynatrace.somedomain", Port: 27018}}},
-			want: "f694c984dc5db78631ca837b529d044e46a8594607156d4a2a8b93e7d488e47c",
-		},
-	}
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			result := BuildNameForEndpoint(tc.args.commHosts, tc.args.name)
-			assert.Equal(t, tc.want, result)
 		})
 	}
 }
