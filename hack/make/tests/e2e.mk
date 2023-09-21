33a4b459093bca2e10b55cbde80222a562f6925d
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-09-21 09:15:05 +0200

    Small rename for consistency

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -57,8 +57,8 @@ test/e2e/cloudnative/switchmodes: manifests/crd/helm
 test/e2e/cloudnative/upgrade: manifests/crd/helm
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 20m -count=1 ./test/scenarios/cloudnative/upgrade $(SKIPCLEANUP)
 
-## Runs Application Monitoring default e2e test only
-test/e2e/applicationmonitoring/default: manifests/crd/helm
+## Runs Application Monitoring dataingest e2e test only
+test/e2e/applicationmonitoring/dataingest: manifests/crd/helm
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)"  -count=1 ./test/scenarios/applicationmonitoring  -run ^TestDataIngest$  $(SKIPCLEANUP)
 
 ## Runs Application Monitoring label versio detection e2e test only
@@ -81,8 +81,8 @@ test/e2e/supportarchive: manifests/crd/helm
 test/e2e/gke-autopilot: manifests/kubernetes/gke-autopilot
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 30m -count=1 ./test/scenarios/applicationmonitoring $(SKIPCLEANUP)
 
-## Runs Application Monitoring default e2e test only on gke-autopilot
-test/e2e/gke-autopilot/applicationmonitoring/default: manifests/kubernetes/gke-autopilot
+## Runs Application Monitoring dataingest e2e test only on gke-autopilot
+test/e2e/gke-autopilot/applicationmonitoring/dataingest: manifests/kubernetes/gke-autopilot
 	go test -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -count=1 ./test/scenarios/applicationmonitoring  -run ^TestDataIngest$  $(SKIPCLEANUP)
 
 ## Runs Application Monitoring label versio detection e2e test only on gke-autopilot
