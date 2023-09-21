422dcdf789dcc22647fcd89e6268f6e79ecaf257
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-09-21 09:18:44 +0200

    Fix CodeModulesImage e2e test The 1.  dynakube (cloudNativeDynakube) was not created

--- test/scenarios/cloudnative/codemodules/codemodules.go
+++ test/scenarios/cloudnative/codemodules/codemodules.go
@@ -107,7 +107,9 @@ func InstallFromImage(t *testing.T, istioEnabled bool) features.Feature {
 	// Register dynakube install
 	steps := setup.NewEnvironmentSetup(
 		setup.CreateNamespaceWithoutTeardown(operatorNamespaceBuilder.Build()),
-		setup.DeployOperatorViaMake(cloudNativeDynakube.NeedsCSIDriver()))
+		setup.DeployOperatorViaMake(cloudNativeDynakube.NeedsCSIDriver()),
+		setup.CreateDynakube(secretConfigs[0], cloudNativeDynakube),
+	)
 	steps.CreateSetupSteps(builder)
 
 	// Register sample app install
@@ -127,7 +129,6 @@ func InstallFromImage(t *testing.T, istioEnabled bool) features.Feature {
 
 	// Register sample, dynakube and operator uninstall
 	builder.Teardown(sampleApp.UninstallNamespace())
-	teardown.DeleteDynakube(builder, cloudNativeDynakube)
 	steps.CreateTeardownSteps(builder)
 
 	return builder.Feature()
