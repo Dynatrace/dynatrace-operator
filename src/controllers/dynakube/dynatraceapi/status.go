ce05c20174572758c38f7a86a41c2dc42c14467b
aorcholski <84514340+aorcholski@users.noreply.github.com>
2023-10-02 12:15:39 +0200

    Fix operator behavior related to `503 ServiceUnavailable` DynatraceApi error (#2158)
    * 503-ServiceUnavailable error retriggers Dynakube reconciler after a
    minute
    
    * concatErrors test implemented as table driven unit test
    
    * isDynatraceAPIUnreachable wrapper removed
    
    * dynakube reconciler returns either non-empty result or error

---
+++ src/controllers/dynakube/dynatraceapi/status.go
@@ -0,0 +1,36 @@
+package dynatraceapi
+
+import (
+	"net/http"
+
+	"github.com/Dynatrace/dynatrace-operator/src/dtclient"
+	"github.com/pkg/errors"
+)
+
+const (
+	NoError = 0
+)
+
+func IsUnreachable(err error) bool {
+	var serverErr dtclient.ServerError
+	if errors.As(err, &serverErr) && (serverErr.Code == http.StatusTooManyRequests || serverErr.Code == http.StatusServiceUnavailable) {
+		return true
+	}
+	return false
+}
+
+func StatusCode(err error) int {
+	var serverErr dtclient.ServerError
+	if errors.As(err, &serverErr) {
+		return serverErr.Code
+	}
+	return 0
+}
+
+func Message(err error) string {
+	var serverErr dtclient.ServerError
+	if errors.As(err, &serverErr) {
+		return serverErr.Message
+	}
+	return ""
+}
