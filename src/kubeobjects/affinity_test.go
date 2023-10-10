9e6ebd5218bad6c34dd1f712e8b82d15df63d140
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2023-10-10 17:49:54 +0200

    Remove tolerations

--- src/kubeobjects/affinity_test.go
+++ src/kubeobjects/affinity_test.go
@@ -12,30 +12,6 @@ func TestAffinityNodeRequirement(t *testing.T) {
 	assert.Contains(t, AffinityNodeRequirementForSupportedArches(), linuxRequirement())
 }
 
-func TestTolerationsForAllArches(t *testing.T) {
-	assert.Equal(t, TolerationForSupportedArches(), tolerationsForArches(amd64, arm64, ppc64le))
-	assert.Contains(t, TolerationForSupportedArches(), armToleration())
-	assert.Contains(t, TolerationForSupportedArches(), amdToleration())
-}
-
-func armToleration() corev1.Toleration {
-	return corev1.Toleration{
-		Key:      kubernetesArch,
-		Operator: corev1.TolerationOpEqual,
-		Value:    arm64,
-		Effect:   corev1.TaintEffectNoSchedule,
-	}
-}
-
-func amdToleration() corev1.Toleration {
-	return corev1.Toleration{
-		Key:      kubernetesArch,
-		Operator: corev1.TolerationOpEqual,
-		Value:    amd64,
-		Effect:   corev1.TaintEffectNoSchedule,
-	}
-}
-
 func linuxRequirement() corev1.NodeSelectorRequirement {
 	return corev1.NodeSelectorRequirement{
 		Key:      kubernetesOS,
