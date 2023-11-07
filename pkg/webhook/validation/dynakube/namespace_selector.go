e882aeaaf490d813352e636f0cd9f7b03a66ed27
wepudt <werner.putschoegl@dynatrace.com>
2023-11-07 10:40:38 +0100

    using validation.ValidateLabels function and add more information to error message.

--- pkg/webhook/validation/dynakube/namespace_selector.go
+++ pkg/webhook/validation/dynakube/namespace_selector.go
@@ -2,10 +2,12 @@ package dynakube
 
 import (
 	"context"
+	"fmt"
 
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/injection/namespace/mapper"
-	"k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
 const (
@@ -15,7 +17,7 @@ Make sure the namespaceSelector doesn't conflict with other Dynakubes namespaceS
 	errorConflictingNamespaceSelectorNoSelector = `The DynaKube does not specificy namespaces where it should inject into while another Dynakube already injects into namespaces, which is not supported.
 Make sure you have a namespaceSelector doesn't conflict with other Dynakubes namespaceSelector
 `
-	errorNamespaceSelectorMatchLabelsViolateLabelSpec = "The DynaKube's namespaceSelector contains matchLabels that are not conform to spec. A valid label must be an empty string or consist of alphanumeric characters, '-', '_' or '.', and must start and end with an alphanumeric character."
+	errorNamespaceSelectorMatchLabelsViolateLabelSpec = "The DynaKube's namespaceSelector contains matchLabels that are not conform to spec."
 )
 
 func conflictingNamespaceSelector(ctx context.Context, dv *dynakubeValidator, dynakube *dynatracev1beta1.DynaKube) string {
@@ -37,15 +39,12 @@ func conflictingNamespaceSelector(ctx context.Context, dv *dynakubeValidator, dy
 }
 
 func namespaceSelectorMatchLabelsViolateLabelSpec(ctx context.Context, _ *dynakubeValidator, dynakube *dynatracev1beta1.DynaKube) string {
-	var errs []string
 	matchLabels := dynakube.NamespaceSelector().MatchLabels
-	for _, v := range matchLabels {
-		if v != "" {
-			errs = validation.IsValidLabelValue(v)
-		}
-	}
+
+	errs := validation.ValidateLabels(matchLabels, field.NewPath("spec", "namespaceSelector", "matchLabels"))
 	if len(errs) == 0 {
 		return ""
 	}
-	return errorNamespaceSelectorMatchLabelsViolateLabelSpec
+
+	return fmt.Sprintf("%s (%s)", errorNamespaceSelectorMatchLabelsViolateLabelSpec, errs)
 }
