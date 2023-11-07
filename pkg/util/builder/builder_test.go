18d59d603aff860653175afd477df0aa833be1ed
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-11-07 13:19:31 +0100

    Replace custom modifier test code with mockery generated one (#2321)
    This PR replaces the mock structs we used with mockery generated ones.

--- pkg/util/builder/builder_test.go
+++ pkg/util/builder/builder_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/builder/mocks"
+	modifiermock "github.com/Dynatrace/dynatrace-operator/test/mocks/pkg/util/builder"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 )
 
-func TestStatefulsetBuilder(t *testing.T) {
+func TestBuilder(t *testing.T) {
 	t.Run("Simple, no modifiers", func(t *testing.T) {
 		b := GenericBuilder[mocks.DataMock]{}
 		actual, err := b.Build()
@@ -19,11 +21,12 @@ func TestStatefulsetBuilder(t *testing.T) {
 	t.Run("One modifier", func(t *testing.T) {
 		b := GenericBuilder[mocks.DataMock]{}
 
-		modifierMock := mocks.NewModifierMock[mocks.DataMock]()
+		modifierMock := modifiermock.NewModifier[mocks.DataMock](t)
 		modifierMock.On("Modify", mock.Anything).Return(nil)
 		modifierMock.On("Enabled").Return(true)
 
-		actual, _ := b.AddModifier(modifierMock).Build()
+		actual, err := b.AddModifier(modifierMock).Build()
+		require.NoError(t, err)
 
 		modifierMock.AssertNumberOfCalls(t, "Modify", 1)
 
@@ -33,11 +36,12 @@ func TestStatefulsetBuilder(t *testing.T) {
 	t.Run("One modifier, not enabled", func(t *testing.T) {
 		b := GenericBuilder[mocks.DataMock]{}
 
-		modifierMock := mocks.NewModifierMock[mocks.DataMock]()
-		modifierMock.On("Modify", mock.Anything).Return(nil)
+		modifierMock := modifiermock.NewModifier[mocks.DataMock](t)
+		modifierMock.On("Modify", mock.Anything).Return(nil).Maybe()
 		modifierMock.On("Enabled").Return(false)
 
-		actual, _ := b.AddModifier(modifierMock).Build()
+		actual, err := b.AddModifier(modifierMock).Build()
+		require.NoError(t, err)
 
 		modifierMock.AssertNumberOfCalls(t, "Modify", 0)
 
@@ -47,14 +51,15 @@ func TestStatefulsetBuilder(t *testing.T) {
 	t.Run("Two modifiers, one used twice", func(t *testing.T) {
 		b := GenericBuilder[mocks.DataMock]{}
 
-		modifierMock0 := mocks.NewModifierMock[mocks.DataMock]()
+		modifierMock0 := modifiermock.NewModifier[mocks.DataMock](t)
 		modifierMock0.On("Modify", mock.Anything).Return(nil)
 		modifierMock0.On("Enabled").Return(true)
-		modifierMock1 := mocks.NewModifierMock[mocks.DataMock]()
+		modifierMock1 := modifiermock.NewModifier[mocks.DataMock](t)
 		modifierMock1.On("Modify", mock.Anything).Return(nil)
 		modifierMock1.On("Enabled").Return(true)
 
-		actual, _ := b.AddModifier(modifierMock0, modifierMock0, modifierMock1).Build()
+		actual, err := b.AddModifier(modifierMock0, modifierMock0, modifierMock1).Build()
+		require.NoError(t, err)
 
 		modifierMock0.AssertNumberOfCalls(t, "Modify", 2)
 		modifierMock1.AssertNumberOfCalls(t, "Modify", 1)
@@ -65,14 +70,15 @@ func TestStatefulsetBuilder(t *testing.T) {
 	t.Run("Chain of modifiers", func(t *testing.T) {
 		b := GenericBuilder[mocks.DataMock]{}
 
-		modifierMock0 := mocks.NewModifierMock[mocks.DataMock]()
+		modifierMock0 := modifiermock.NewModifier[mocks.DataMock](t)
 		modifierMock0.On("Modify", mock.Anything).Return(nil)
 		modifierMock0.On("Enabled").Return(true)
-		modifierMock1 := mocks.NewModifierMock[mocks.DataMock]()
+		modifierMock1 := modifiermock.NewModifier[mocks.DataMock](t)
 		modifierMock1.On("Modify", mock.Anything).Return(nil)
 		modifierMock1.On("Enabled").Return(true)
 
-		actual, _ := b.AddModifier(modifierMock0, modifierMock0).AddModifier(modifierMock1).Build()
+		actual, err := b.AddModifier(modifierMock0, modifierMock0).AddModifier(modifierMock1).Build()
+		require.NoError(t, err)
 
 		modifierMock0.AssertNumberOfCalls(t, "Modify", 2)
 		modifierMock1.AssertNumberOfCalls(t, "Modify", 1)
