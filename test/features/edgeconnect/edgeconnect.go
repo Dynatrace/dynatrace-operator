edd9763186c2fab011497cd239f9aaefca5c618c
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-11-08 10:36:46 +0100

    Add target for edgeconnect e2e test

--- test/features/edgeconnect/edgeconnect.go
+++ test/features/edgeconnect/edgeconnect.go
@@ -14,6 +14,7 @@ import (
 
 func Feature(t *testing.T) features.Feature {
 	builder := features.New("install edgeconnect")
+	builder.WithLabel("name", "edgeconnect-install")
 
 	secretConfig := tenant.GetEdgeConnectTenantSecret(t)
 
