b48f6f38e2c8053967e532a9fb17d50b0aa078a4
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-11-10 14:39:58 +0100

    Create optional otel secret for e2e tests

--- test/helpers/tenant/secrets.go
+++ test/helpers/tenant/secrets.go
@@ -15,14 +15,20 @@ import (
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+const (
+	otelSecretName = "dynatrace-operator-otel-config"
 )
 
 var (
 	defaultSingleTenant      = path.Join(project.TestDataDir(), "secrets/single-tenant.yaml")
 	defaultMultiTenant       = path.Join(project.TestDataDir(), "secrets/multi-tenant.yaml")
 	defaultEdgeConnectTenant = path.Join(project.TestDataDir(), "secrets/edgeconnect-tenant.yaml")
+	defaultOtelTenant        = path.Join(project.TestDataDir(), "secrets/otel-tenant.yaml")
 )
 
 type Secrets struct {
@@ -43,6 +49,11 @@ type EdgeConnectSecret struct {
 	ApiServer         string `yaml:"apiServer"`
 	OauthClientId     string `yaml:"oAuthClientId"`
 	OauthClientSecret string `yaml:"oAuthClientSecret"`
+}
+
+type OtelSecret struct {
+	Endpoint string `yaml:"endpoint"`
+	ApiToken string `yaml:"apiToken"`
 }
 
 func manyFromConfig(fs afero.Fs, path string) ([]Secret, error) {
@@ -151,5 +162,37 @@ func CreateClientSecret(secretConfig EdgeConnectSecret, name, namespace string) 
 		require.NoError(t, err)
 
 		return ctx
+	}
+}
+
+func CreateOtelSecret(namespace string) env.Func {
+	return func(ctx context.Context, envConfig *envconf.Config) (context.Context, error) {
+		secretConfigFile, err := afero.ReadFile(afero.NewOsFs(), defaultOtelTenant)
+		if err != nil {
+			// swallow error, as it's not vital for e2e test itself
+			return ctx, nil //nolint:nilerr
+		}
+		var secret OtelSecret
+		err = yaml.Unmarshal(secretConfigFile, &secret)
+		if err != nil {
+			return ctx, errors.WithStack(err)
+		}
+
+		defaultSecret := corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      otelSecretName,
+				Namespace: namespace,
+			},
+			Data: map[string][]byte{
+				"apiToken": []byte(secret.ApiToken),
+				"endpoint": []byte(secret.Endpoint),
+			},
+		}
+
+		err = envConfig.Client().Resources().Create(ctx, &defaultSecret)
+		if k8serrors.IsAlreadyExists(err) {
+			return ctx, envConfig.Client().Resources().Update(ctx, &defaultSecret)
+		}
+		return ctx, err
 	}
 }
