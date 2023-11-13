59c1fc369462164c9a422a4a6b8c35f65acabd72
waodim <albian.krasniqi@dynatrace.com>
2023-11-13 14:26:09 +0100

    Fix linting

--- pkg/controllers/dynakube/activegate/internal/statefulset/statefulset_test.go
+++ pkg/controllers/dynakube/activegate/internal/statefulset/statefulset_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubeobjects/resources"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	slices "golang.org/x/exp/slices"
+	"golang.org/x/exp/slices"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
