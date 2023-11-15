64e76babe9d379a2109a172a970345b9542753c2
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2023-11-15 11:05:16 +0100

    new PR

---
+++ pkg/util/kubeobjects/object_test.go
@@ -0,0 +1,25 @@
+package kubeobjects
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
+)
+
+func TestKey(t *testing.T) {
+	type args struct {
+		object client.Object
+	}
+	tests := []struct {
+		name string
+		args args
+		want client.ObjectKey
+	}{
+		{name: "tttttt", args: args{object: nil}, want: client.ObjectKey{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, Key(tt.args.object), "Key(%v)", tt.args.object)
+		})
+	}
+}
