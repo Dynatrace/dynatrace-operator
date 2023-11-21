46441c52de4a2fa900feef26f5ef9e0fc8174b5f
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-11-21 10:22:11 +0100

    Split prerequisites make commands + some general cleanup around them

--- hack/build/command.sh
+++ hack/build/command.sh
@@ -2,7 +2,6 @@
 set -e
 
 cmd="${1}"
-package="${2}"
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 if [ "$(go env GOBIN)" = "" ]; then
@@ -18,20 +17,6 @@ fi
 
 if which "${cmd}" &>/dev/null ; then
   which "${cmd}" 2>/dev/null
-  exit 0
-fi
-
-# Return command path only unless a package to install is provided
-if [ "${package}" != "" ]; then
-  GEN_TMP_DIR="$(mktemp -d)"
-  cd "${GEN_TMP_DIR}"
-  go mod init tmp
-  go install "${package}"
-  rm -rf "${GEN_TMP_DIR}"
-fi
-
-if [ -x "${GOBIN}/${cmd}" ]; then
-  echo "${GOBIN}/${cmd}"
   exit 0
 fi
 
