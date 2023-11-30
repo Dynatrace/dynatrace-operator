1f8d49b98422e4b3ea764bbbdd49394274a018d9
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2023-11-30 14:40:39 +0100

    Add markdownlint as prerequistie for doc/gen-gomarkdoc

--- hack/make/doc.mk
+++ hack/make/doc.mk
@@ -12,6 +12,6 @@ doc/permissions: manifests
 	python3 ./hack/doc/role-permissions2md.py ./config/deploy/openshift/openshift-all.yaml > permissions.md
 
 ## Run scripts that generate markdown documentation using gomarkdoc (./hack/doc)
-doc/gen-gomarkdoc: prerequisites/gomarkdoc
+doc/gen-gomarkdoc: prerequisites/gomarkdoc prerequisites/markdownlint
 	./hack/doc/gen_e2e_features.sh
 	./hack/doc/gen_feature_flags.sh
