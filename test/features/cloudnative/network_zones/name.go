2068e4c9a40409fd73ef8959c8bb059bd47e77d8
Stefan Hauth <stefan.hauth@dynatrace.com>
2023-11-30 10:23:56 +0100

    fixup! Check and wait that network zone has been created correctly before continuing test.

--- test/features/cloudnative/network_zones/name.go
+++ test/features/cloudnative/network_zones/name.go
@@ -6,24 +6,23 @@ import (
 
 const prefix = "op-e2e-"
 const defaultName = "test-network-zone"
+const nameLength = 8
 
 // Use a randomized network zone names, to avoid problems with improper or slow cleanup of network zones on DT cluster side.
 // With randomized names a fresh start is guaranteed for each test run.
 func getNetworkZoneName() string {
-	length := 8
-
-	b := make([]byte, length)
+	b := make([]byte, nameLength)
 	_, err := rand.Read(b)
 	if err != nil {
 		return prefix + defaultName
 	}
-	return prefix + encode(b)
+	return prefix + encode(b)[:nameLength]
 }
 
 func encode(randBytes []byte) string {
 	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 
-	result := make([]rune, len(randBytes))
+	result := make([]rune, 0, len(randBytes))
 	for _, b := range randBytes {
 		result = append(result, letterRunes[b%byte(len(letterRunes))])
 	}
