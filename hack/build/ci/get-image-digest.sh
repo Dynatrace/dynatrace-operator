57f9ca7ad7d334529889588042d2e2a4dfe3cb6c
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-11-30 16:31:58 +0100

    Change back to online version for manifests

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-digest=$(skopeo inspect docker-daemon:"${IMAGE}" --format "{{.Digest}}")
-echo "${DIGEST_KEY}=${digest}">> "$GITHUB_OUTPUT"
+readonly online="${1}"
 
+if [ "$#" -ne 1 ];
+then
+  echo "Usage: $0 <fetch_from_registry>"
+  exit 1
+fi
+
+if [ "$online" == "true" ];
+then
+  readonly PROVIDER="docker://"
+else
+  readonly PROVIDER="docker-daemon:"
+fi
+
+digest=$(skopeo inspect ${PROVIDER}"${IMAGE}" --format "{{.Digest}}")
+echo "digest=${digest}">> "$GITHUB_OUTPUT"
