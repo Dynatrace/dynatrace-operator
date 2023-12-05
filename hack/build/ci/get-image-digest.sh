8bab1fe864453f6b34c053934e977cffeea69c96
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-12-05 18:19:51 +0100

    Remove online parameter

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-readonly online="${1}"
-
-if [ "$#" -ne 1 ];
-then
-  echo "Usage: $0 <fetch_from_registry>"
-  exit 1
-fi
-
-if [ "$online" == "true" ];
-then
-  readonly PROVIDER="docker://"
-else
-  readonly PROVIDER="docker-daemon:"
-fi
-
-digest=$(skopeo inspect ${PROVIDER}"${IMAGE}" --format "{{.Digest}}")
+digest=$(skopeo inspect docker-daemon:"${IMAGE}" --format "{{.Digest}}")
 echo "digest=${digest}">> "$GITHUB_OUTPUT"
