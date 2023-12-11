3def5bb6dcd3ba7632d00b7d4e3a10cfc5c26e97
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-12-11 15:22:30 +0100

    Use local digest for signing the docker images (#2426)
    * Make digest handling local
    
    * Change back to online version for manifests
    
    * Use manifest digest returned from registry
    
    * Remove online parameter
    
    * Remove quote
    
    * Add whitespace
    
    ---------
    
    Co-authored-by: Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-digest=$(skopeo inspect docker://"${IMAGE}" --format "{{.Digest}}")
+digest=$(skopeo inspect docker-daemon:"${IMAGE}" --format "{{.Digest}}")
 echo "digest=${digest}">> "$GITHUB_OUTPUT"
-
