c7df72b0cc628612c6bfd289fc090ba422a2294b
Stefan Hauth <stefan.hauth@dynatrace.com>
2024-01-08 10:11:02 +0100

    fixup! Display warning if ONEAGENT_INSTALLER_SCRIPT_URL or ONEAGENT_INSTALLER_TOKEN environemnt variables are used for OneAgent.

--- pkg/webhook/validation/dynakube/oneagent_test.go
+++ pkg/webhook/validation/dynakube/oneagent_test.go
@@ -2,10 +2,10 @@ package dynakube
 
 import (
 	"fmt"
-	corev1 "k8s.io/api/core/v1"
 	"testing"
 
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
