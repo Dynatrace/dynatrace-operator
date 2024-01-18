4bbde6aa828f9639cf4227b8d54818c3bc2553c9
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2024-01-18 16:26:39 +0100

    Increase util/kubeobjects/pod test covarage

--- pkg/util/kubeobjects/pod/pod_test.go
+++ pkg/util/kubeobjects/pod/pod_test.go
@@ -33,3 +33,29 @@ func TestGetPod(t *testing.T) {
 		assert.Nil(t, pod)
 	})
 }
+
+func TestGetName(t *testing.T) {
+	t.Run("get pod name", func(t *testing.T) {
+		podName := "superpod"
+		testPod := corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: podName,
+			},
+		}
+		got := GetName(testPod)
+		assert.Equal(t, podName, got)
+	})
+	t.Run("get pod generateName", func(t *testing.T) {
+		podName := ""
+		podGenerateName := "gen-name-"
+		testPod := corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:         podName,
+				GenerateName: podGenerateName,
+			},
+		}
+		// testPod.Gene
+		got := GetName(testPod)
+		assert.Equal(t, podGenerateName, got)
+	})
+}
