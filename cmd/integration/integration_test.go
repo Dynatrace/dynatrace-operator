31933399c9685dc614d4606f88cd9386c3573d88
waodim <albian.krasniqi@dynatrace.com>
2024-01-25 12:09:32 +0100

    Simplify test and exclude integration test from unit tests

--- cmd/integration/integration_test.go
+++ cmd/integration/integration_test.go
@@ -1,67 +1,35 @@
+//go:build integration
+
 package integration
 
 import (
-	"fmt"
-	"github.com/Dynatrace/dynatrace-operator/pkg/version"
+	"github.com/stretchr/testify/assert"
 	"os/exec"
 	"strings"
 	"testing"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/version"
 )
 
 func TestBuildVariables(t *testing.T) {
-	cmd := exec.Command("git", "branch")
-	output, err := cmd.CombinedOutput()
+	cmd := exec.Command("git", "branch", "--show-current")
+	currentBranch, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Failed to run 'git branch': %v", err)
 	}
 
-	currentBranch, err := getCurrentBranch(string(output))
-	if err != nil {
-		t.Fatalf("Failed to get current branch: %v", err)
-	}
-
 	// get the current git commit
-	currentGitCommit := exec.Command("git", "log", "-n", "1", currentBranch)
+	currentGitCommit := exec.Command("git", "rev-parse", "HEAD")
 	gitCommitOutput, err := currentGitCommit.CombinedOutput()
 	if err != nil {
 		t.Fatalf("Failed to run 'git log': %v", err)
 	}
 
-	commitHash, err := extractCommitHash(string(gitCommitOutput))
-	if err != nil {
-		t.Fatalf("Failed to run 'git branch': %v", err)
-	}
-
+	commitHash := strings.TrimSpace(string(gitCommitOutput))
 	expectedGitCommitHash := version.Commit
-	if commitHash != expectedGitCommitHash {
-		t.Errorf("Git commits not equal. Expected: %s, Actual: %s", expectedGitCommitHash, commitHash)
-	}
+	assert.Equal(t, expectedGitCommitHash, commitHash)
 
 	expectedVersion := version.Version
-	if !strings.Contains(expectedVersion, currentBranch) {
-		t.Errorf("Versions not equal. Expected: %s, Actual: %s", expectedVersion, currentBranch)
-	}
-}
-
-func getCurrentBranch(output string) (string, error) {
-	lines := strings.Split(output, "\n")
-	for _, line := range lines {
-		if strings.HasPrefix(line, "*") {
-			return strings.TrimSpace(strings.TrimPrefix(line, "*")), nil
-		}
-	}
-	return "", fmt.Errorf("could not find current branch")
-}
-
-func extractCommitHash(input string) (string, error) {
-	lines := strings.Split(input, "\n")
-
-	for _, line := range lines {
-		if strings.Contains(line, "commit") {
-			commitHash := strings.Split(line, " ")
-			return strings.TrimSpace(commitHash[1]), nil
-		}
-	}
-
-	return "", fmt.Errorf("commit hash not found in input")
+	curBranch := strings.TrimSpace(string(currentBranch))
+	assert.Equal(t, expectedVersion, curBranch)
 }
