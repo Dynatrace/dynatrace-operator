39ea6bfcf9d8166ce8f47722ca96e3c496763749
Alberto Gil de Dios <61692514+albertogdd@users.noreply.github.com>
2024-03-04 10:09:08 +0100

    Set E2E execution at 0AM and tweak dispatchable workfow (#2826)

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -6,12 +6,12 @@ DEFAULT_TIMEOUT="60m" # K8s takes <10min, Openshift >40min
 
 kubectl version
 
-echo "Creating environment '$ENVIRONMENT' in namespace '$NAMESPACE'"
+echo "Creating environment '$FLC_ENVIRONMENT' in namespace '$FLC_NAMESPACE'"
 
-kubectl get flcenvironments --namespace $NAMESPACE
+kubectl get flcenvironments --namespace $FLC_NAMESPACE
 
-echo "Patching environment '$ENVIRONMENT' to 'deployed'"
-kubectl patch --namespace $NAMESPACE --type merge --patch '{"spec": {"desiredState": "environment-deployed"}}' flcenvironment $ENVIRONMENT
+echo "Patching environment '$FLC_ENVIRONMENT' to 'deployed'"
+kubectl patch --namespace $FLC_NAMESPACE --type merge --patch '{"spec": {"desiredState": "environment-deployed"}}' flcenvironment $FLC_ENVIRONMENT
 
-echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$ENVIRONMENT'"
-kubectl wait --namespace $NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $ENVIRONMENT
+echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
+kubectl wait --namespace $FLC_NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $FLC_ENVIRONMENT
