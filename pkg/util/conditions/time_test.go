631cd0768771826f835d682cc409fbebc361c1c9
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-03-20 16:01:24 +0100

    Add simple test

---
+++ pkg/util/conditions/time_test.go
@@ -0,0 +1,50 @@
+package conditions
+
+import (
+	"testing"
+	"time"
+
+	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/timeprovider"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsOutdated(t *testing.T) {
+	testingConditionType := "testing "
+
+	t.Run("empty condition => outdated", func(t *testing.T) {
+		tp := timeprovider.New()
+		dk := &dynatracev1beta1.DynaKube{}
+
+		assert.True(t, IsOutdated(tp, dk, testingConditionType))
+	})
+
+	t.Run("False condition => outdated", func(t *testing.T) {
+		tp := timeprovider.New()
+		dk := &dynatracev1beta1.DynaKube{}
+
+		SetDynatraceApiErrorCondition(dk.Conditions(), testingConditionType, errors.New("boom"))
+
+		assert.True(t, IsOutdated(tp, dk, testingConditionType))
+	})
+
+	t.Run("True condition + current timestamp => NOT outdated", func(t *testing.T) {
+		tp := timeprovider.New()
+		dk := &dynatracev1beta1.DynaKube{}
+
+		SetSecretCreatedCondition(dk.Conditions(), testingConditionType, "")
+
+		assert.False(t, IsOutdated(tp, dk, testingConditionType))
+	})
+
+	t.Run("old timestamp => outdated", func(t *testing.T) {
+		tp := timeprovider.New()
+		dk := &dynatracev1beta1.DynaKube{}
+
+		SetSecretCreatedCondition(dk.Conditions(), testingConditionType, "")
+		tp.Set(tp.Now().Add(time.Minute * 60))
+
+		assert.True(t, IsOutdated(tp, dk, testingConditionType))
+	})
+}
