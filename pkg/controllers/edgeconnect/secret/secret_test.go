fe498fa6628fea367138e3c2ef2613d0e5aa7673
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-03-27 14:44:29 +0100

    init configure edgeconnect.yaml

--- pkg/controllers/edgeconnect/secret/secret_test.go
+++ pkg/controllers/edgeconnect/secret/secret_test.go
@@ -25,22 +25,22 @@ const (
 )
 
 func Test_prepareEdgeConnectConfigFile(t *testing.T) {
-	testEdgeConnect := &edgeconnectv1alpha1.EdgeConnect{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      testName,
-			Namespace: testNamespace,
-		},
-		Spec: edgeconnectv1alpha1.EdgeConnectSpec{
-			ApiServer: "abc12345.dynatrace.com",
-			OAuth: edgeconnectv1alpha1.OAuthSpec{
-				Endpoint:     "https://sso-dev.dynatracelabs.com/sso/oauth2/token",
-				Resource:     "urn:dtenvironment:test12345",
-				ClientSecret: "test-secret",
-			},
-		},
-	}
-
 	t.Run("Create basic config", func(t *testing.T) {
+		testEdgeConnect := &edgeconnectv1alpha1.EdgeConnect{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      testName,
+				Namespace: testNamespace,
+			},
+			Spec: edgeconnectv1alpha1.EdgeConnectSpec{
+				ApiServer: "abc12345.dynatrace.com",
+				OAuth: edgeconnectv1alpha1.OAuthSpec{
+					Endpoint:     "https://sso-dev.dynatracelabs.com/sso/oauth2/token",
+					Resource:     "urn:dtenvironment:test12345",
+					ClientSecret: "test-secret",
+				},
+			},
+		}
+
 		testSecretName := "test-secret"
 		kubeReader := fake.NewClient(createClientSecret(testSecretName, testNamespace))
 		cfg, err := PrepareConfigFile(testEdgeConnect, kubeReader)
@@ -54,6 +54,50 @@ oauth:
     client_id: created-client-id
     client_secret: created-client-secret
     resource: urn:dtenvironment:test12345
+`
+		assert.Equal(t, expected, string(cfg))
+	})
+
+	t.Run("Create full config", func(t *testing.T) {
+		testEdgeConnect := &edgeconnectv1alpha1.EdgeConnect{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      testName,
+				Namespace: testNamespace,
+			},
+			Spec: edgeconnectv1alpha1.EdgeConnectSpec{
+				ApiServer: "abc12345.dynatrace.com",
+				OAuth: edgeconnectv1alpha1.OAuthSpec{
+					Endpoint:     "https://sso-dev.dynatracelabs.com/sso/oauth2/token",
+					Resource:     "urn:dtenvironment:test12345",
+					ClientSecret: "test-secret",
+				},
+				CaCertsRef: "certs",
+				Proxy: &edgeconnectv1alpha1.ProxySpec{
+					Host:    "proxy.com",
+					NoProxy: "*.internal.com",
+					Port:    443,
+				},
+			},
+		}
+		testSecretName := "test-secret"
+		kubeReader := fake.NewClient(createClientSecret(testSecretName, testNamespace))
+		cfg, err := PrepareConfigFile(testEdgeConnect, kubeReader)
+
+		require.NoError(t, err)
+
+		expected := `name: test-name-edgeconnectv1alpha1
+api_endpoint_host: abc12345.dynatrace.com
+oauth:
+    endpoint: https://sso-dev.dynatracelabs.com/sso/oauth2/token
+    client_id: created-client-id
+    client_secret: created-client-secret
+    resource: urn:dtenvironment:test12345
+root_certificate_paths:
+    - /etc/ssl/certificate.cer
+proxy:
+    server: proxy.com
+    exceptions: '*.internal.com'
+    port: 443
 `
 		assert.Equal(t, expected, string(cfg))
 	})
