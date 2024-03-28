fc7b8664bfe94d5d9c8243b03a0ef231a4ab7ed1
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-03-28 15:15:39 +0100

    Fix linting

--- pkg/controllers/dynakube/connectioninfo/activegate/reconciler.go
+++ pkg/controllers/dynakube/connectioninfo/activegate/reconciler.go
@@ -2,6 +2,7 @@ package activegate
 
 import (
 	"context"
+
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube"
 	dtclient "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
 	"github.com/Dynatrace/dynatrace-operator/pkg/controllers"
@@ -44,7 +45,6 @@ func NewReconciler(clt client.Client, apiReader client.Reader, scheme *runtime.S
 func (r *reconciler) Reconcile(ctx context.Context) error {
 	if !r.dynakube.NeedsActiveGate() {
 		// TODO: Add check to only clean up if condition is not nil (condition is added in another change)
-
 		r.dynakube.Status.ActiveGate.ConnectionInfoStatus = dynatracev1beta1.ActiveGateConnectionInfoStatus{}
 		query := k8ssecret.NewQuery(ctx, r.client, r.apiReader, log)
 
