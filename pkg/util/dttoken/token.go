e10e44d6acd1443911d598c0cf01d546b462c90b
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-04-23 15:40:49 +0200

    EdgeConnect connection token generator

--- pkg/util/dttoken/token.go
+++ pkg/util/dttoken/token.go
@@ -1,16 +1,39 @@
 package dttoken
 
+import (
+	"crypto/rand"
+	"encoding/base32"
+	"fmt"
+)
+
+const (
+	publicPortionSize  = 24
+	privatePortionSize = 64
+)
+
+// Token represents <prefix>.<24-character-public-portion>.<64-character-private-portion>
 type Token struct {
 	prefix  string
 	public  string
 	private string
 }
 
-func New(prefix string) *Token {
-
-	return &Token{prefix: prefix}
+func (t Token) String() string {
+	return fmt.Sprintf("%s.%s.%s", t.prefix, t.public, t.private)
 }
 
-func (t *Token) generate() string {
-	return ""
+func New(prefix string) *Token {
+	return &Token{prefix: prefix, public: generateRandom(publicPortionSize), private: generateRandom(privatePortionSize)}
+}
+
+// generate base32 encoded random string using base32.StdEncoding
+func generateRandom(size int) string {
+	b := make([]byte, size)
+	_, err := rand.Read(b)
+
+	if err != nil {
+		return ""
+	}
+
+	return base32.StdEncoding.EncodeToString(b)[:size]
 }
--- pkg/util/dttoken/token_test.go
+++ pkg/util/dttoken/token_test.go
@@ -1,8 +1,10 @@
 package dttoken
 
 import (
-	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateToken(t *testing.T) {
@@ -11,5 +13,20 @@ func TestGenerateToken(t *testing.T) {
 		tokenB := New("test")
 
 		assert.NotEqual(t, tokenA, tokenB)
+	})
+	t.Run("string representation of token", func(t *testing.T) {
+		tokenA := New("test")
+		tokenParts := strings.Split(tokenA.String(), ".")
+		assert.Len(t, tokenParts, 3)
+		assert.Equal(t, "test", tokenParts[0])
+		assert.Len(t, tokenParts[1], publicPortionSize)
+		assert.Len(t, tokenParts[2], privatePortionSize)
+	})
+}
+
+func Test_generateRandom(t *testing.T) {
+	t.Run("check random length", func(t *testing.T) {
+		assert.Len(t, generateRandom(10), 10)
+		assert.Len(t, generateRandom(32), 32)
 	})
 }
