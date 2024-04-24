0e8c09e02a0e308da88c740009582d49e44b89d6
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2024-04-24 10:15:29 +0200

    Refactor fakes interface

--- pkg/controllers/csi/metadata/fakes.go
+++ pkg/controllers/csi/metadata/fakes.go
@@ -44,44 +44,11 @@ func (f *FakeFailDB) SchemaMigration() error {
 	return sql.ErrTxDone
 }
 
-func (f *FakeFailDB) CreateTenantConfig(tenantConfig *TenantConfig) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) UpdateTenantConfig(tenantConfig *TenantConfig) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) DeleteTenantConfig(tenantConfig *TenantConfig, cascade bool) error {
-	return sql.ErrTxDone
-}
 func (f *FakeFailDB) ReadTenantConfig(tenantConfig TenantConfig) (*TenantConfig, error) {
 	return nil, sql.ErrTxDone
 }
-func (f *FakeFailDB) ReadTenantConfigs() ([]TenantConfig, error) {
-	return nil, sql.ErrTxDone
-}
-func (f *FakeFailDB) CreateCodeModule(codeModule *CodeModule) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) DeleteCodeModule(codeModule *CodeModule) error {
-	return sql.ErrTxDone
-}
 func (f *FakeFailDB) ReadCodeModule(codeModule CodeModule) (*CodeModule, error) {
 	return nil, sql.ErrTxDone
-}
-func (f *FakeFailDB) ReadCodeModules() ([]CodeModule, error) {
-	return nil, sql.ErrTxDone
-}
-func (f *FakeFailDB) IsCodeModuleOrphaned(codeModule *CodeModule) (bool, error) {
-	return false, sql.ErrTxDone
-}
-func (f *FakeFailDB) CreateOSMount(osMount *OSMount) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) UpdateOSMount(osMount *OSMount) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) DeleteOSMount(osMount *OSMount) error {
-	return sql.ErrTxDone
 }
 func (f *FakeFailDB) ReadOSMount(osMount OSMount) (*OSMount, error) {
 	return nil, sql.ErrTxDone
@@ -89,29 +56,67 @@ func (f *FakeFailDB) ReadOSMount(osMount OSMount) (*OSMount, error) {
 func (f *FakeFailDB) ReadUnscopedOSMount(osMount OSMount) (*OSMount, error) {
 	return nil, sql.ErrTxDone
 }
-func (f *FakeFailDB) ReadOSMounts() ([]OSMount, error) {
+func (f *FakeFailDB) ReadVolumeMeta(volumeMeta VolumeMeta) (*VolumeMeta, error) {
 	return nil, sql.ErrTxDone
 }
-func (f *FakeFailDB) CreateAppMount(appMount *AppMount) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) UpdateAppMount(appMount *AppMount) error {
-	return sql.ErrTxDone
-}
-func (f *FakeFailDB) DeleteAppMount(appMount *AppMount) error {
-	return sql.ErrTxDone
-}
 func (f *FakeFailDB) ReadAppMount(appMount AppMount) (*AppMount, error) {
+	return nil, sql.ErrTxDone
+}
+
+func (f *FakeFailDB) ReadTenantConfigs() ([]TenantConfig, error) {
+	return nil, sql.ErrTxDone
+}
+func (f *FakeFailDB) ReadCodeModules() ([]CodeModule, error) {
+	return nil, sql.ErrTxDone
+}
+func (f *FakeFailDB) ReadOSMounts() ([]OSMount, error) {
 	return nil, sql.ErrTxDone
 }
 func (f *FakeFailDB) ReadAppMounts() ([]AppMount, error) {
 	return nil, sql.ErrTxDone
 }
-func (f *FakeFailDB) ReadVolumeMeta(volumeMeta VolumeMeta) (*VolumeMeta, error) {
-	return nil, sql.ErrTxDone
-}
 func (f *FakeFailDB) ReadVolumeMetas() ([]VolumeMeta, error) {
 	return nil, sql.ErrTxDone
+}
+
+func (f *FakeFailDB) CreateTenantConfig(tenantConfig *TenantConfig) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) CreateCodeModule(codeModule *CodeModule) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) CreateOSMount(osMount *OSMount) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) CreateAppMount(appMount *AppMount) error {
+	return sql.ErrTxDone
+}
+
+func (f *FakeFailDB) UpdateTenantConfig(tenantConfig *TenantConfig) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) UpdateOSMount(osMount *OSMount) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) UpdateAppMount(appMount *AppMount) error {
+	return sql.ErrTxDone
+}
+
+func (f *FakeFailDB) DeleteTenantConfig(tenantConfig *TenantConfig, cascade bool) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) DeleteCodeModule(codeModule *CodeModule) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) DeleteOSMount(osMount *OSMount) error {
+	return sql.ErrTxDone
+}
+func (f *FakeFailDB) DeleteAppMount(appMount *AppMount) error {
+	return sql.ErrTxDone
+}
+
+func (f *FakeFailDB) IsCodeModuleOrphaned(codeModule *CodeModule) (bool, error) {
+	return false, sql.ErrTxDone
 }
 func (f *FakeFailDB) RestoreOSMount(osMount *OSMount) (*OSMount, error) {
 	return nil, sql.ErrTxDone
