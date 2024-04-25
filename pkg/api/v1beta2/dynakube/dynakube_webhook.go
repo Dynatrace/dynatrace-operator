b375ba896c8552756e1074a153352395da1c3048
Albian Krasniqi <62391739+waodim@users.noreply.github.com>
2024-04-25 12:05:16 +0200

    Remove builder tags from v1beta2 to make conversion webhook work (#3073)

--- pkg/api/v1beta2/dynakube/dynakube_webhook.go
+++
@@ -1,9 +0,0 @@
-package dynakube
-
-import ctrl "sigs.k8s.io/controller-runtime"
-
-func (r *DynaKube) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewWebhookManagedBy(mgr).
-		For(r).
-		Complete()
-}
