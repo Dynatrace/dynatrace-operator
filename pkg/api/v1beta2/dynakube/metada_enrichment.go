6cfeda3738a79013e7ec2f0b2bc7700851617e1f
waodim <albian.krasniqi@dynatrace.com>
2024-05-13 14:14:12 +0200

    Remove wrong noling directive

--- pkg/api/v1beta2/dynakube/metada_enrichment.go
+++ pkg/api/v1beta2/dynakube/metada_enrichment.go
@@ -2,7 +2,7 @@ package dynakube
 
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-type MetaDataEnrichment struct { //nolint:fieldalignment
+type MetaDataEnrichment struct {
 	// The namespaces where you want Dynatrace Operator to inject enrichment.
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Namespace Selector",xDescriptors="urn:alm:descriptor:com.tectonic.ui:selector:core:v1:Namespace"
 	NamespaceSelector metav1.LabelSelector `json:"namespaceSelector,omitempty"`
