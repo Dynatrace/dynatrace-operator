7554b4181246d6db1637e3598017e7536800200b
aorcholski <84514340+aorcholski@users.noreply.github.com>
2024-06-05 16:46:52 +0200

    Update pkg/webhook/validation/edgeconnect/kubernetes_automation.go
    Co-authored-by: StefanHauth <63204425+StefanHauth@users.noreply.github.com>

--- pkg/webhook/validation/edgeconnect/kubernetes_automation.go
+++ pkg/webhook/validation/edgeconnect/kubernetes_automation.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	defaultServiceAccountName               = "dynatrace-edgeconnect"
-	errorKubernetesAutomationNotImplemented = `you tried to enable the EdgeConnect Kubernetes Automation feature and/or manually set a service account. This feature is still a work in progress and is not supported yet.`
+	errorKubernetesAutomationNotImplemented = `You tried to enable the EdgeConnect Kubernetes Automation feature and/or manually set a service account. This feature is still a work in progress and is not supported yet.`
 )
 
 func isKubernetesAutomationEnabled(_ context.Context, _ *edgeconnectValidator, edgeConnect *edgeconnect.EdgeConnect) string {
