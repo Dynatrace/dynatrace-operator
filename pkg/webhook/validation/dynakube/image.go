0bd44c657ea322cc332d834f2a22c075e87163ca
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-06-10 10:31:22 +0200

    Add validation for the custom image fields in the DynaKube

---
+++ pkg/webhook/validation/dynakube/image.go
@@ -0,0 +1,62 @@
+package dynakube
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta2/dynakube"
+	"github.com/google/go-containerregistry/pkg/name"
+)
+
+const (
+	errorUsingTenantImageAsCustom = `The 'image' field in the %s section can't be referencing an image directly from the Dynatrace Environment.`
+
+	errorUnparsableImageRef = `The 'image' field in the %s section can't be parsed, make sure it's a valid image reference.`
+)
+
+func imageFieldHasTenantImage(_ context.Context, _ *dynakubeValidator, dk *dynakube.DynaKube) string {
+	tenantRepoBase := strings.Replace(dk.ApiUrl(), "/api", "", 1)
+
+	type imageField struct {
+		value   string
+		section string
+	}
+
+	imageFields := []imageField{
+		{
+			section: "ActiveGate",
+			value:   dk.CustomActiveGateImage(),
+		},
+		{
+			section: "OneAgent",
+			value:   dk.CustomOneAgentImage(),
+		},
+	}
+
+	messages := []string{}
+
+	for _, field := range imageFields {
+		message := checkImageField(field.value, field.section, tenantRepoBase)
+		if message != "" {
+			messages = append(messages, message)
+		}
+	}
+
+	return strings.Join(messages, ";")
+}
+
+func checkImageField(image, section, disallowedPrefix string) (errorMsg string) {
+	if image != "" {
+		ref, err := name.ParseReference(image)
+		if err != nil {
+			return fmt.Sprintf(errorUnparsableImageRef, section)
+		}
+
+		if strings.HasPrefix(ref.Name(), disallowedPrefix) {
+			return fmt.Sprintf(errorUsingTenantImageAsCustom, section)
+		}
+	}
+
+	return ""
+}
