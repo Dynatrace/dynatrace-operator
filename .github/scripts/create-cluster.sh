ae6b861ff7d219a5a7e9651be305606058255e58
wepudt <werner.putschoegl@dynatrace.com>
2024-06-13 14:13:03 +0200

    introduce wait for ocp to let flc create the kubectl cfg and check deployment worked.

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -3,6 +3,7 @@
 set -x
 
 DEFAULT_TIMEOUT="60m" # K8s takes <10min, Openshift >40min
+desired_state="environment-deployed"
 
 kubectl version
 
@@ -15,3 +16,17 @@ kubectl patch --namespace $FLC_NAMESPACE --type merge --patch '{"spec": {"desire
 
 echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
 kubectl wait --namespace $FLC_NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $FLC_ENVIRONMENT
+
+if [ "$PLATFORM" == "ocp" ]; then
+  echo "PLATFORM is set to 'ocp'. Waiting for 5 minutes..."
+  sleep 300  # Wait for 5 minutes (300 seconds)
+  echo "done"
+fi
+
+echo "Checking currentState='$desired_state' for '$FLCENV_NAME'..."
+flc_state=$(kubectl get flcenvironment "$FLCENV_NAME" --namespace "$NAMESPACE" -ojsonpath="{.status.currentState}")
+if [[ "$flc_state" != "$desired_state" ]]; then
+  echo "Pipeline deployment did not reach expected state '$desired_state', currentState: ${flc_state}"
+  exit 1
+fi
+echo "done"
