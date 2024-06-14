fc908e9679324a0d2b854dfb1e45435671a3f65e
wepudt <werner.putschoegl@dynatrace.com>
2024-06-14 09:36:57 +0200

    fix ocp delay

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -10,16 +10,16 @@ kubectl version
 echo "Creating environment '$FLC_ENVIRONMENT' in namespace '$FLC_NAMESPACE'"
 
 kubectl get flcenvironments --namespace $FLC_NAMESPACE
+#
+#echo "Patching environment '$FLC_ENVIRONMENT' to 'deployed'"
+#kubectl patch --namespace $FLC_NAMESPACE --type merge --patch '{"spec": {"desiredState": "environment-deployed"}}' flcenvironment $FLC_ENVIRONMENT
+#
+#echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
+#kubectl wait --namespace $FLC_NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $FLC_ENVIRONMENT
 
-echo "Patching environment '$FLC_ENVIRONMENT' to 'deployed'"
-kubectl patch --namespace $FLC_NAMESPACE --type merge --patch '{"spec": {"desiredState": "environment-deployed"}}' flcenvironment $FLC_ENVIRONMENT
-
-echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
-kubectl wait --namespace $FLC_NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $FLC_ENVIRONMENT
-
-if [ $FLC_PLATFORM == "ocp" ]; then
+if [ "$FLC_PLATFORM" == 'ocp' ]; then
   echo "PLATFORM is set to 'ocp'. Waiting for 5 minutes..."
-  sleep 300  # Wait for 5 minutes (300 seconds)
+  sleep 3 #300  # Wait for 5 minutes (300 seconds)
   echo "done"
 fi
 
