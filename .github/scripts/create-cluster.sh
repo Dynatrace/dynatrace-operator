a92132447f4baaa9f4c71be6573caba99a56c0ac
wepudt <werner.putschoegl@dynatrace.com>
2024-06-18 08:35:43 +0200

    checking deployment results and waiting before ocp teardown

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -3,6 +3,7 @@
 set -x
 
 DEFAULT_TIMEOUT="60m" # K8s takes <10min, Openshift >40min
+desired_state="environment-deployed"
 
 kubectl version
 
@@ -15,3 +16,19 @@ kubectl patch --namespace $FLC_NAMESPACE --type merge --patch '{"spec": {"desire
 
 echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
 kubectl wait --namespace $FLC_NAMESPACE --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment $FLC_ENVIRONMENT
+
+if [[ "$FLC_ENVIRONMENT" == *"ocp"* ]]; then
+  echo "PLATFORM is set to 'ocp'. Waiting for 5 minutes..."
+  sleep 300  # Wait for 5 minutes (300 seconds)
+  echo "done"
+fi
+
+echo "Checking currentState='$desired_state' for '$FLC_ENVIRONMENT'..."
+flc_state=$(kubectl get flcenvironment "$FLC_ENVIRONMENT" --namespace "$FLC_NAMESPACE" -ojsonpath="{.status.currentState}")
+if [[ "$flc_state" != "$desired_state" ]]; then
+  echo "Pipeline deployment did not reach expected state '$desired_state', currentState: ${flc_state}..."
+  exit 1
+  else
+    echo "successful..."
+fi
+echo "done"
