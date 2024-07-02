9bd1c71847b34b3b6ef10867126e17b129e906dd
adam.orcholski <adam.orcholski@dynatrace.com>
2024-07-02 16:47:37 +0200

    truncated description fields

--- hack/make/doc.mk
+++ hack/make/doc.mk
@@ -4,8 +4,8 @@ doc: doc/api-ref doc/gen-gomarkdoc
 
 ## Generate API docs for custom resources
 doc/api-ref: manifests prerequisites/python
-	source local/.venv/bin/activate && python3 ./hack/doc/custom_resource_params_to_md.py ./config/crd/bases/dynatrace.com_dynakubes.yaml > ./doc/api/dynakube-api-ref.md
-	source local/.venv/bin/activate && python3 ./hack/doc/custom_resource_params_to_md.py ./config/crd/bases/dynatrace.com_edgeconnects.yaml > ./doc/api/edgeconnect-api-ref.md
+	source local/.venv/bin/activate && python3 ./hack/doc/custom_resource_params_to_md.py ./config/crd/longdescbases/dynatrace.com_dynakubes.yaml > ./doc/api/dynakube-api-ref.md
+	source local/.venv/bin/activate && python3 ./hack/doc/custom_resource_params_to_md.py ./config/crd/longdescbases/dynatrace.com_edgeconnects.yaml > ./doc/api/edgeconnect-api-ref.md
 
 ## Create a table containing permissions needed by Operator components
 doc/permissions: manifests prerequisites/python
