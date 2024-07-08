8127bb5da76dceefdce1c4c5a0c6c9aa73051923
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2024-07-08 18:40:48 +0200

    Tweak unit tests

--- pkg/controllers/dynakube/extension/reconciler_test.go
+++ pkg/controllers/dynakube/extension/reconciler_test.go
@@ -7,7 +7,10 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/scheme/fake"
 	dynatracev1beta3 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -26,6 +29,10 @@ func TestReconciler_Reconcile(t *testing.T) {
 		r := NewReconciler(fakeClient, fakeClient, instance)
 		err := r.Reconcile(context.Background())
 		require.NoError(t, err)
+
+		var secretFound corev1.Secret
+		err = fakeClient.Get(context.Background(), client.ObjectKey{Name: testName + "-extensions-token", Namespace: testNamespace}, &secretFound)
+		require.True(t, k8serrors.IsNotFound(err))
 	})
 	t.Run(`Prometheus enabled`, func(t *testing.T) {
 		instance := &dynatracev1beta3.DynaKube{
@@ -44,6 +51,10 @@ func TestReconciler_Reconcile(t *testing.T) {
 		fakeClient := fake.NewClient()
 		r := NewReconciler(fakeClient, fakeClient, instance)
 		err := r.Reconcile(context.Background())
+		require.NoError(t, err)
+
+		var secretFound corev1.Secret
+		err = fakeClient.Get(context.Background(), client.ObjectKey{Name: testName + "-extensions-token", Namespace: testNamespace}, &secretFound)
 		require.NoError(t, err)
 	})
 }
