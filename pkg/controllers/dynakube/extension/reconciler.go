fc5f3bb7bc58d1856391ede6b1797f46851403e0
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2024-07-09 08:38:50 +0200

    Create secret.go file

--- pkg/controllers/dynakube/extension/reconciler.go
+++ pkg/controllers/dynakube/extension/reconciler.go
@@ -3,13 +3,9 @@ package extension
 import (
 	"context"
 
-	dynatracev1beta3 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/controllers"
-	"github.com/Dynatrace/dynatrace-operator/pkg/util/dttoken"
-	k8ssecret "github.com/Dynatrace/dynatrace-operator/pkg/util/kubeobjects/secret"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/timeprovider"
-	corev1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -18,20 +14,14 @@ type reconciler struct {
 	apiReader    client.Reader
 	timeProvider *timeprovider.Provider
 
-	dynakube *dynatracev1beta3.DynaKube
+	dynakube *dynakube.DynaKube
 }
 
-type ReconcilerBuilder func(clt client.Client, apiReader client.Reader, dynakube *dynatracev1beta3.DynaKube) controllers.Reconciler
+type ReconcilerBuilder func(clt client.Client, apiReader client.Reader, dynakube *dynakube.DynaKube) controllers.Reconciler
 
 var _ ReconcilerBuilder = NewReconciler
 
-const (
-	tokenKey         = "eec-token"
-	tokenValuePrefix = "EEC dt0x01"
-	secretSuffix     = "-extensions-token"
-)
-
-func NewReconciler(clt client.Client, apiReader client.Reader, dynakube *dynatracev1beta3.DynaKube) controllers.Reconciler {
+func NewReconciler(clt client.Client, apiReader client.Reader, dynakube *dynakube.DynaKube) controllers.Reconciler {
 	return &reconciler{
 		client:       clt,
 		apiReader:    apiReader,
@@ -44,7 +34,7 @@ func (r *reconciler) Reconcile(ctx context.Context) error {
 	log.Info("start reconciling extensions")
 
 	if r.dynakube.PrometheusEnabled() {
-		err := r.reconcileSecret(ctx)
+		err := reconcileSecret(ctx, r.dynakube, r.client, r.apiReader)
 		if err != nil {
 			setSecretCreatedFalse(r.dynakube.Conditions(), err)
 
@@ -55,45 +45,4 @@ func (r *reconciler) Reconcile(ctx context.Context) error {
 	}
 
 	return nil
-}
-
-func (r *reconciler) reconcileSecret(ctx context.Context) error {
-	log.Info("reconciling secret")
-
-	query := k8ssecret.NewQuery(ctx, r.client, r.apiReader, log)
-
-	_, err := query.Get(client.ObjectKey{Name: r.secretName(), Namespace: r.dynakube.Namespace})
-	if err != nil && !k8serrors.IsNotFound(err) {
-		return err
-	}
-
-	if k8serrors.IsNotFound(err) {
-		log.Info("creating secret")
-
-		newToken, err := dttoken.New(tokenValuePrefix)
-		if err != nil {
-			return err
-		}
-
-		newSecret, err := r.buildSecret(*newToken)
-		if err != nil {
-			return err
-		}
-
-		query.CreateOrUpdate(*newSecret)
-	}
-
-	return nil
-}
-
-func (r *reconciler) secretName() string {
-	return r.dynakube.Name + secretSuffix
-}
-
-func (r *reconciler) buildSecret(token dttoken.Token) (*corev1.Secret, error) {
-	secretData := map[string][]byte{
-		tokenKey: []byte(token.String()),
-	}
-
-	return k8ssecret.Create(r.dynakube, k8ssecret.NewNameModifier(r.secretName()), k8ssecret.NewNamespaceModifier(r.dynakube.GetNamespace()), k8ssecret.NewDataModifier(secretData))
 }
---
+++ pkg/controllers/dynakube/extension/secret.go
@@ -0,0 +1,68 @@
+package extension
+
+import (
+	"context"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/dttoken"
+	k8ssecret "github.com/Dynatrace/dynatrace-operator/pkg/util/kubeobjects/secret"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	eecTokenKey         = "eec-token"
+	eecTokenValuePrefix = "EEC dt0x01"
+	secretSuffix        = "-extensions-token"
+)
+
+func reconcileSecret(ctx context.Context, dk *dynakube.DynaKube, kubeClient client.Client, apiReader client.Reader) error {
+	log.Info("reconciling secret")
+
+	query := k8ssecret.NewQuery(ctx, kubeClient, apiReader, log)
+
+	_, err := query.Get(client.ObjectKey{Name: getSecretName(dk.Name), Namespace: dk.Namespace})
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	}
+
+	if errors.IsNotFound(err) {
+		log.Info("creating secret")
+
+		newEecToken, err := newEecToken()
+		if err != nil {
+			return err
+		}
+
+		newSecret, err := buildSecret(dk, *newEecToken)
+		if err != nil {
+			return err
+		}
+
+		query.CreateOrUpdate(*newSecret)
+	}
+
+	return nil
+}
+
+func getSecretName(dynakubeName string) string {
+	return dynakubeName + secretSuffix
+}
+
+func buildSecret(dk *dynakube.DynaKube, token dttoken.Token) (*corev1.Secret, error) {
+	secretData := map[string][]byte{
+		eecTokenKey: []byte(token.String()),
+	}
+
+	return k8ssecret.Create(dk, k8ssecret.NewNameModifier(getSecretName(dk.Name)), k8ssecret.NewNamespaceModifier(dk.GetNamespace()), k8ssecret.NewDataModifier(secretData))
+}
+
+func newEecToken() (*dttoken.Token, error) {
+	newToken, err := dttoken.New(eecTokenValuePrefix)
+	if err != nil {
+		return nil, err
+	}
+
+	return newToken, nil
+}
