c5632c752096b1da312e1a56e3842708180ed38d
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-07-10 10:11:47 +0200

    automate otel via renovate

--- pkg/util/dtotel/otel.go
+++ pkg/util/dtotel/otel.go
@@ -9,6 +9,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/sdk/resource"
+	// renovate depName=go.opentelemetry.io/otel/semconv
 	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/types"
