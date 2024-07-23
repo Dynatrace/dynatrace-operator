f5e265d3a93fd5bca8fb1a1c64f37fe69e29b356
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-07-23 13:54:21 +0200

    Match the env name to the actual property name

--- pkg/consts/enrichment_injection.go
+++ pkg/consts/enrichment_injection.go
@@ -3,9 +3,9 @@ package consts
 const (
 	EnrichmentEndpointSecretName = "dynatrace-metadata-enrichment-endpoint"
 	EnrichmentInjectedEnv        = "METADATA_ENRICHMENT_INJECTED"
-	EnrichmentWorkloadKindEnv    = "DT_WORKLOAD_KIND"
-	EnrichmentWorkloadNameEnv    = "DT_WORKLOAD_NAME"
-	EnrichmentClusterNameEnv     = "DT_CLUSTER_NAME"
+	EnrichmentWorkloadKindEnv    = "K8S_WORKLOAD_KIND"
+	EnrichmentWorkloadNameEnv    = "K8S_WORKLOAD_NAME"
+	EnrichmentClusterNameEnv     = "K8S_CLUSTER_NAME"
 	EnrichmentPropertiesFilename = "dt_metadata.properties"
 	EnrichmentJsonFilename       = "dt_metadata.json"
 	EnrichmentMountPath          = "/var/lib/dynatrace/enrichment"
