21e134838c8d3a7ae95e768e5d01779bb29a5247
Lukas Hinterreiter <90035514+luhi-DT@users.noreply.github.com>
2024-09-04 16:03:14 +0200

    Change default value of metadata enrichment (#3716)

--- pkg/api/v1beta2/dynakube/metada_enrichment.go
+++ pkg/api/v1beta2/dynakube/metada_enrichment.go
@@ -7,8 +7,8 @@ type MetadataEnrichment struct {
 	// The namespaces where you want Dynatrace Operator to inject enrichment.
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Namespace Selector",xDescriptors="urn:alm:descriptor:com.tectonic.ui:selector:core:v1:Namespace"
 	NamespaceSelector metav1.LabelSelector `json:"namespaceSelector,omitempty"`
-	// Enables MetadataEnrichment, `true` by default.
-	// +kubebuilder:default:=true
+	// Enables MetadataEnrichment, `false` by default.
+	// +kubebuilder:default:=false
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="MetaDataEnrichment",xDescriptors="urn:alm:descriptor:com.tectonic.ui:selector:booleanSwitch"
 	Enabled bool `json:"enabled"`
 }
