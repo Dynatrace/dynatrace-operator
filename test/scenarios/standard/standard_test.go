59be321c81e1fcdb8c1694eeba26595ae604fb6b
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-09-05 13:54:30 +0200

    Remove network-zone e2e test code
    It is flaky, hasn't been used for a while, will be redone in the future

--- test/scenarios/standard/standard_test.go
+++ test/scenarios/standard/standard_test.go
@@ -46,7 +46,6 @@ func TestMain(m *testing.M) {
 
 func TestStandard(t *testing.T) {
 	feats := []features.Feature{
-		// network_zones.Feature(t), // TODO: Fix so it can be enabled during pipeline tests, because if other tests deploy an ActiveGate then the network zone CAN still be present, which will make the test fail
 		activegate.Feature(t, nil),
 		cloudnativeDefault.Feature(t, false),
 		applicationmonitoring.MetadataEnrichment(t),
