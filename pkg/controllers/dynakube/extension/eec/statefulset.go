46647da84ba3a377b018b651b4614ede2034e34b
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2024-09-12 15:38:08 +0200

    Fix lint

--- pkg/controllers/dynakube/extension/eec/statefulset.go
+++ pkg/controllers/dynakube/extension/eec/statefulset.go
@@ -17,7 +17,7 @@ import (
 	"golang.org/x/net/context"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
