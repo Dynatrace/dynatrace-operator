4382a539ba7c819e021bcc81f832bb8e6737883d
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-09-16 11:36:28 +0200

    auto update release branch ondemand e2e (#3775)

---
+++ hack/build/ci/update-e2e-ondemand-pipeline.py
@@ -0,0 +1,26 @@
+import sys
+
+from ruamel.yaml import YAML
+
+# version file contains a list of strings
+version_file = sys.argv[1]
+ondemand_file = sys.argv[2]
+
+version = ""
+# read versions to list
+with open(version_file, "r") as f:
+    version = f.readline().strip().replace("origin/", "")
+
+yaml = YAML()
+yaml.width = 4096
+
+# read ondemand_file file to dict and update
+with open(ondemand_file, "r") as f:
+    data = yaml.load(f)
+    data["on"]["workflow_dispatch"]["inputs"]["target"]["default"] = version
+
+print(data)
+# write ondemand_file renovate file
+with open(ondemand_file, "wb") as output:
+    yaml.indent(mapping=2, sequence=4, offset=2)
+    yaml.dump(data, output)
