45ec5172853e9173b2c04288b368ba67244c290e
Gabriel Krenn <gabriel.krenn@gmail.com>
2024-09-18 14:07:20 +0200

    Update error message according to review

--- pkg/api/v1beta1/dynakube/validation/oneagent.go
+++ pkg/api/v1beta1/dynakube/validation/oneagent.go
@@ -19,7 +19,7 @@ const (
 	errorImageFieldSetWithoutCSIFlag = `The DynaKube's specification tries to enable ApplicationMonitoring mode and get the respective image, but the CSI driver is not enabled.`
 
 	errorNodeSelectorConflict = `The DynaKube specification attempts to deploy a %s, which conflicts with another DynaKube's %s. Only one Agent per node is supported.
-Conflicting DynaKube: %s`
+Use a nodeSelector to avoid this conflict. Conflicting DynaKube: %s`
 
 	errorVolumeStorageReadOnlyModeConflict = `The DynaKube's specification specifies a read-only host file system and OneAgent has volume storage enabled.`
 
