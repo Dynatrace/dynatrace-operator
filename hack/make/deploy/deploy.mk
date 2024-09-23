3e1961ca3aee402d8afe61c6688c7d598335711c
Stefan Hauth <stefan.hauth@dynatrace.com>
2024-09-23 09:55:13 +0200

    Preview helpers.

--- hack/make/deploy/deploy.mk
+++ hack/make/deploy/deploy.mk
@@ -19,3 +19,15 @@ deploy: manifests/crd/helm
 			--set csidriver.enabled=$(ENABLE_CSI) \
 			--set manifests=true \
 			--set image="$(IMAGE_URI)" | kubectl apply -f -
+
+preview/%:
+	@make PLATFORM=$(@F) $(@D)
+
+preview: manifests/crd/helm
+	helm template dynatrace-operator config/helm/chart/default \
+			--namespace dynatrace \
+			--set installCRD=true \
+			--set platform=$(PLATFORM) \
+			--set csidriver.enabled=$(ENABLE_CSI) \
+			--set manifests=true \
+			--set image="$(IMAGE_URI)" > operator-preview-$(PLATFORM).yaml
