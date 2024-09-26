7377a387aaf631e201ff2e7b2cf637eb26ecb638
Gabriel Krenn <gabriel.krenn@gmail.com>
2024-09-26 10:54:16 +0200

    Add debug command for provisioner

--- hack/make/debug/debug.mk
+++ hack/make/debug/debug.mk
@@ -2,8 +2,14 @@
 debug/prepare/csi-server:
 	git apply hack/make/debug/csi-server.patch
 
-debug/tunnel/csi-server:
+debug/prepare/csi-provisioner:
+	git apply hack/make/debug/csi-provisioner.patch
+
+debug/tunnel/csi-driver:
 	kubectl port-forward -n dynatrace $(kubectl get pod -n dynatrace -l app.kubernetes.io/component=csi-driver -o jsonpath='{.items[0].metadata.name}') 40000:40000
 
 debug/remove/csi-server:
 	git apply -R hack/make/debug/csi-server.patch
+
+debug/remove/csi-provisioner:
+	git apply -R hack/make/debug/csi-provisioner.patch
