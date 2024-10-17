5dd1e29651f678ce1808155fbe36d710ec539466
luhi-DT <lukas.hinterreiter@dynatrace.com>
2024-10-17 18:57:37 +0200

    Fix suggestions

--- pkg/controllers/dynakube/kspm/token/reconciler.go
+++ pkg/controllers/dynakube/kspm/token/reconciler.go
@@ -1,28 +1,49 @@
-package kspm
+package token
 
 import (
 	"context"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
-	"github.com/Dynatrace/dynatrace-operator/pkg/controllers/dynakube/kspm/consts"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/dttoken"
 	k8ssecret "github.com/Dynatrace/dynatrace-operator/pkg/util/kubeobjects/secret"
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func ensureKSPMSecret(ctx context.Context, client client.Client, apiReader client.Reader, dk *dynakube.DynaKube) error {
-	kspmSecretName := dk.Name + "-" + consts.KSPMSecretKey
+type Reconciler struct {
+	client    client.Client
+	apiReader client.Reader
+	dk        *dynakube.DynaKube
+}
 
+type ReconcilerBuilder func(client client.Client, apiReader client.Reader, dk *dynakube.DynaKube) *Reconciler
+
+func NewReconciler(client client.Client, apiReader client.Reader, dk *dynakube.DynaKube) *Reconciler {
+	return &Reconciler{
+		client:    client,
+		dk:        dk,
+		apiReader: apiReader,
+	}
+}
+
+func (r *Reconciler) Reconcile(ctx context.Context) error {
+	if r.dk.IsKSPMEnabled() {
+		return ensureKSPMSecret(ctx, r.client, r.apiReader, r.dk)
+	}
+
+	return removeKSPMSecret(ctx, r.client, r.apiReader, r.dk)
+}
+
+func ensureKSPMSecret(ctx context.Context, client client.Client, apiReader client.Reader, dk *dynakube.DynaKube) error {
 	query := k8ssecret.Query(client, apiReader, log)
-	_, err := query.Get(ctx, types.NamespacedName{Name: kspmSecretName, Namespace: dk.Namespace})
+	_, err := query.Get(ctx, types.NamespacedName{Name: dk.GetKSPMSecretName(), Namespace: dk.Namespace})
 
 	if err != nil && k8serrors.IsNotFound(err) {
 		log.Info("creating new token for kspm", "error", err.Error())
 
-		secretConfig, err := generateKSPMTokenSecret(kspmSecretName, dk)
+		secretConfig, err := generateKSPMTokenSecret(dk.GetKSPMSecretName(), dk)
 
 		if err != nil {
 			return err
@@ -40,7 +61,7 @@ func ensureKSPMSecret(ctx context.Context, client client.Client, apiReader clien
 }
 
 func removeKSPMSecret(ctx context.Context, client client.Client, apiReader client.Reader, dk *dynakube.DynaKube) error {
-	kspmSecretName := dk.Name + "-" + consts.KSPMSecretKey
+	kspmSecretName := dk.Name + "-" + dynakube.KSPMSecretKey
 
 	query := k8ssecret.Query(client, apiReader, log)
 	secret, err := query.Get(ctx, types.NamespacedName{Name: kspmSecretName, Namespace: dk.Namespace})
@@ -64,14 +85,14 @@ func removeKSPMSecret(ctx context.Context, client client.Client, apiReader clien
 	return nil
 }
 
-func generateKSPMTokenSecret(name string, dk *dynakube.DynaKube) (secret *v1.Secret, err error) {
+func generateKSPMTokenSecret(name string, dk *dynakube.DynaKube) (secret *corev1.Secret, err error) {
 	newToken, err := dttoken.New("dt0n01")
 	if err != nil {
 		return nil, err
 	}
 
 	secretData := make(map[string][]byte)
-	secretData[consts.KSPMSecretKey] = []byte(newToken.String())
+	secretData[dynakube.KSPMSecretKey] = []byte(newToken.String())
 	secretConfig, err := k8ssecret.Build(dk,
 		name,
 		secretData,
--- pkg/controllers/dynakube/kspm/token/reconciler_test.go
+++ pkg/controllers/dynakube/kspm/token/reconciler_test.go
@@ -1,13 +1,14 @@
-package kspm
+package token
 
 import (
 	"context"
 	"testing"
 
 	dtfake "github.com/Dynatrace/dynatrace-operator/pkg/api/scheme/fake"
-	"github.com/Dynatrace/dynatrace-operator/pkg/controllers/dynakube/kspm/consts"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube/kspm"
 	"github.com/stretchr/testify/require"
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -25,8 +26,8 @@ func TestTokenCreation(t *testing.T) {
 		err := ensureKSPMSecret(ctx, clt, clt, &dk)
 		require.NoError(t, err)
 
-		var secret v1.Secret
-		err = clt.Get(ctx, types.NamespacedName{Name: dk.Name + "-" + consts.KSPMSecretKey, Namespace: dk.Namespace}, &secret)
+		var secret corev1.Secret
+		err = clt.Get(ctx, types.NamespacedName{Name: dk.GetKSPMSecretName(), Namespace: dk.Namespace}, &secret)
 
 		require.NotEmpty(t, secret)
 		require.NoError(t, err)
@@ -35,44 +36,67 @@ func TestTokenCreation(t *testing.T) {
 	t.Run("does not create secret if exists", func(t *testing.T) {
 		dk := createDynaKube(true)
 		objs := []client.Object{
-			&v1.Secret{
+			&corev1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      dk.Name + "-" + consts.KSPMSecretKey,
+					Name:      dk.GetKSPMSecretName(),
 					Namespace: dk.Namespace,
 				},
 			},
 		}
 		clt := dtfake.NewClient(objs...)
 
-		err := ensureKSPMSecret(ctx, clt, clt, &dk)
+		reconciler := &Reconciler{
+			client:    clt,
+			apiReader: clt,
+			dk:        &dk,
+		}
+
+		err := reconciler.Reconcile(ctx)
 		require.NoError(t, err)
 
-		var secret v1.Secret
-		err = clt.Get(ctx, types.NamespacedName{Name: dk.Name + "-" + consts.KSPMSecretKey, Namespace: dk.Namespace}, &secret)
+		var secret corev1.Secret
+		err = clt.Get(ctx, types.NamespacedName{Name: dk.GetKSPMSecretName(), Namespace: dk.Namespace}, &secret)
 
 		require.NotEmpty(t, secret)
 		require.NoError(t, err)
 	})
 
 	t.Run("removes secret if exists", func(t *testing.T) {
-		dk := createDynaKube(true)
+		dk := createDynaKube(false)
 		objs := []client.Object{
-			&v1.Secret{
+			&corev1.Secret{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      dk.Name + "-" + consts.KSPMSecretKey,
+					Name:      dk.GetKSPMSecretName(),
 					Namespace: dk.Namespace,
 				},
 			},
 		}
 		clt := dtfake.NewClient(objs...)
 
-		err := removeKSPMSecret(ctx, clt, clt, &dk)
+		reconciler := &Reconciler{
+			client:    clt,
+			apiReader: clt,
+			dk:        &dk,
+		}
+
+		err := reconciler.Reconcile(ctx)
 		require.NoError(t, err)
 
-		var secret v1.Secret
-		err = clt.Get(ctx, types.NamespacedName{Name: dk.Name + "-" + consts.KSPMSecretKey, Namespace: dk.Namespace}, &secret)
+		var secret corev1.Secret
+		err = clt.Get(ctx, types.NamespacedName{Name: dk.GetKSPMSecretName(), Namespace: dk.Namespace}, &secret)
 
 		require.Empty(t, secret)
 		require.Error(t, err)
 	})
+}
+
+func createDynaKube(kspmEnabled bool) dynakube.DynaKube {
+	return dynakube.DynaKube{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-dk",
+		},
+		Spec: dynakube.DynaKubeSpec{
+			Kspm: kspm.Spec{Enabled: kspmEnabled},
+		},
+	}
 }
