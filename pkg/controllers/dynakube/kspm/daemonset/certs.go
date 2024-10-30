e6f1ddca7ee12efa1e5cb0d23fe91f7949eb7b07
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-10-30 11:01:56 +0100

    Fix cert handling
    The volume was configrued incorrectly 🙈

--- pkg/controllers/dynakube/kspm/daemonset/certs.go
+++ pkg/controllers/dynakube/kspm/daemonset/certs.go
@@ -1,16 +1,13 @@
 package daemonset
 
 import (
-	"path"
-
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 	corev1 "k8s.io/api/core/v1"
 )
 
 const (
 	certVolumeName = "certs"
-	certFileName   = "ca-chain.pem"
-	certFolderPath = "/var/lib/dynatrace/ncc/customkeys"
+	certFolderPath = "/var/lib/dynatrace/ncc/customkeys/ca-chain.pem"
 	certFileEnv    = "DT_CA_CERTIFICATE_FILE"
 )
 
@@ -20,12 +17,6 @@ func getCertVolume(dk dynakube.DynaKube) corev1.Volume {
 		VolumeSource: corev1.VolumeSource{
 			Secret: &corev1.SecretVolumeSource{
 				SecretName: dk.ActiveGate().TlsSecretName,
-				Items: []corev1.KeyToPath{
-					{
-						Key:  dynakube.TLSCertKey,
-						Path: certFileName,
-					},
-				},
 			},
 		},
 	}
@@ -35,13 +26,13 @@ func getCertMount() corev1.VolumeMount {
 	return corev1.VolumeMount{
 		Name:      certVolumeName,
 		MountPath: certFolderPath,
-		SubPath:   certFileName,
+		SubPath:   dynakube.TLSCertKey,
 	}
 }
 
 func getCertEnv() corev1.EnvVar {
 	return corev1.EnvVar{
 		Name:  certFileEnv,
-		Value: path.Join(certFolderPath, certFileName),
+		Value: certFolderPath,
 	}
 }
