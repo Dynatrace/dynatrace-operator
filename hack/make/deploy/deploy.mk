848107d6f38f8f359214512dadacec8132e9bc94
Stefan Hauth <stefan.hauth@dynatrace.com>
2024-11-08 13:36:28 +0100

    Preview helpers.

--- hack/make/deploy/deploy.mk
+++ hack/make/deploy/deploy.mk
@@ -1,6 +1,9 @@
 ENABLE_CSI ?= true
 PLATFORM ?= "kubernetes"
 
+BRANCH ?= $(shell git branch --show-current)
+SNAPSHOT_SUFFIX ?= $(shell echo "${BRANCH}" | sed "s/[^a-zA-Z0-9_-]/-/g")
+
 ## Deploy the operator without the csi-driver, with platform specified in % (kubernetes or openshift)
 deploy/%/no-csi:
 	@make ENABLE_CSI=false $(@D)
@@ -19,3 +22,16 @@ deploy: manifests/crd/helm
 			--set csidriver.enabled=$(ENABLE_CSI) \
 			--set manifests=true \
 			--set image="$(IMAGE_URI)" | kubectl apply -f -
+
+preview/%:
+	@make PLATFORM=$(@F) $(@D)
+
+preview: manifests/crd/helm
+	kubectl get namespace dynatrace || kubectl create namespace dynatrace
+	helm template dynatrace-operator config/helm/chart/default \
+			--namespace dynatrace \
+			--set installCRD=true \
+			--set platform=$(PLATFORM) \
+			--set csidriver.enabled=$(ENABLE_CSI) \
+			--set manifests=true \
+			--set image="$(IMAGE_URI)" > operator-preview-$(PLATFORM)-$(SNAPSHOT_SUFFIX).yaml
