a86f2ee6ec9b29e4d1966715dc3e9105095e8431
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-11-22 14:28:03 +0100

    test

--- test/features/consts/consts.go
+++ test/features/consts/consts.go
@@ -6,6 +6,6 @@ const (
 	AgCertificateAndPrivateKeyField = "server.p12"
 	AgSecretName                    = "ag-ca"
 	DevRegistryPullSecretName       = "devregistry"
-	EecImageRepo                    = "478983378254.dkr.ecr.us-east-1.amazonaws.com/dynatrace/dynatrace-eec"
+	EecImageRepo                    = "extk8sregistry.azurecr.io/eec/dynatrace-eec"
 	EecImageTag                     = "1.303.0.20240930-183404"
 )
