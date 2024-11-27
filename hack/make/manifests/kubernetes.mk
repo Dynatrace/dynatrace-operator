5c2d6b10f358cd2a79f7cc743d593b8eb8e7c738
adam.orcholski <adam.orcholski@dynatrace.com>
2024-11-27 13:29:22 +0100

    fixes 'make manifests' bug caused by overwritten generate_manifest definition

--- hack/make/manifests/kubernetes.mk
+++ hack/make/manifests/kubernetes.mk
@@ -1,4 +1,4 @@
-define generate_manifest
+define generate_k8s_manifest
 	helm template dynatrace-operator config/helm/chart/default \
 		--namespace dynatrace \
 		--set csidriver.enabled=$(1) \
@@ -11,15 +11,15 @@ endef
 
 ## Generates a Kubernetes manifest including CRD and CSI driver
 manifests/kubernetes/csi: manifests/crd/helm
-	$(call generate_manifest,true,$(KUBERNETES_CSIDRIVER_YAML))
+	$(call generate_k8s_manifest,true,$(KUBERNETES_CSIDRIVER_YAML))
 
 ## Generates a Kubernetes manifest including CRD without CSI driver
 manifests/kubernetes/core: manifests/crd/helm
-	$(call generate_manifest,false,$(KUBERNETES_CORE_YAML))
+	$(call generate_k8s_manifest,false,$(KUBERNETES_CORE_YAML))
 
 ## Generates a Kubernetes manifest including CRD and CSI driver with OLM set to true
 manifests/kubernetes/olm: manifests/crd/helm
-	OLM=true $(call generate_manifest,true,$(KUBERNETES_OLM_YAML))
+	OLM=true $(call generate_k8s_manifest,true,$(KUBERNETES_OLM_YAML))
 
 ## Generates a manifest for Kubernetes including a CRD, a CSI driver deployment
 manifests/kubernetes: manifests/kubernetes/core manifests/kubernetes/csi
