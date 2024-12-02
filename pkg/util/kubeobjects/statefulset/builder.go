3a1dd4f900b040e064e2a2e0562f8270905f0f49
aorcholski <84514340+aorcholski@users.noreply.github.com>
2024-12-02 14:31:39 +0100

    Recreate EEC statefulset for PVC changes only (#4131)

--- pkg/util/kubeobjects/statefulset/builder.go
+++ pkg/util/kubeobjects/statefulset/builder.go
@@ -1,12 +1,18 @@
 package statefulset
 
 import (
+	"github.com/Dynatrace/dynatrace-operator/pkg/api"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/hasher"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubeobjects/internal/builder"
 	maputils "github.com/Dynatrace/dynatrace-operator/pkg/util/map"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
+)
+
+const (
+	pvcAnnotationHash = api.InternalFlagPrefix + "pvc-hash"
 )
 
 var (
@@ -25,6 +31,8 @@ func Build(owner metav1.Object, name string, container corev1.Container, options
 		setNamespace(owner.GetNamespace()),
 	}
 	neededOpts = append(neededOpts, options...)
+
+	neededOpts = append(neededOpts, setPVCAnnotation())
 
 	return builder.Build(owner, &appsv1.StatefulSet{}, neededOpts...)
 }
@@ -109,5 +117,17 @@ func SetSecurityContext(securityContext *corev1.PodSecurityContext) builder.Opti
 func SetUpdateStrategy(updateStartegy appsv1.StatefulSetUpdateStrategy) builder.Option[*appsv1.StatefulSet] {
 	return func(s *appsv1.StatefulSet) {
 		s.Spec.UpdateStrategy = updateStartegy
+	}
+}
+
+func setPVCAnnotation() builder.Option[*appsv1.StatefulSet] {
+	return func(s *appsv1.StatefulSet) {
+		if s.Spec.VolumeClaimTemplates != nil {
+			if s.ObjectMeta.Annotations == nil {
+				s.ObjectMeta.Annotations = map[string]string{}
+			}
+
+			s.ObjectMeta.Annotations[pvcAnnotationHash], _ = hasher.GenerateHash(s.Spec.VolumeClaimTemplates)
+		}
 	}
 }
