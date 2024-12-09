a9b4ba799370c34e78b627ef2e4fd761f7164885
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2024-12-09 14:50:37 +0100

    Remove db

--- pkg/controllers/csi/provisioner/config.go
+++ pkg/controllers/csi/provisioner/config.go
@@ -4,11 +4,6 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/pkg/logd"
 )
 
-const (
-	failedInstallAgentVersionEvent = "FailedInstallAgentVersion"
-	installAgentVersionEvent       = "InstallAgentVersion"
-)
-
 var (
 	log = logd.Get().WithName("csi-provisioner")
 )
