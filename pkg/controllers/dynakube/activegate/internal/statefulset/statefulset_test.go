547da0a420aa0931fe7b90cba26dd9c83cf77ffc
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-12-19 11:09:00 +0100

    manually fixed ptr.To from old address.Of

--- pkg/controllers/dynakube/activegate/internal/statefulset/statefulset_test.go
+++ pkg/controllers/dynakube/activegate/internal/statefulset/statefulset_test.go
@@ -553,7 +553,7 @@ func TestPVC(t *testing.T) {
 		dk := getTestDynakube()
 		multiCapability := capability.NewMultiCapability(&dk)
 		myPVCspec := corev1.PersistentVolumeClaimSpec{
-			StorageClassName: address.Of("test"),
+			StorageClassName: ptr.To("test"),
 			VolumeName:       "foo-pv",
 		}
 		dk.Spec.EnableOTLPingest = true
