5146f09797ccb4bc1378ba522413d07feef1c55d
Gabriel Krenn <gabriel.krenn@gmail.com>
2024-12-19 12:50:45 +0100

    Unify make commands for deployment (#4214)
    Co-authored-by: Andrii Soldatenko <andrii.soldatenko@dynatrace.com>

--- hack/make/deploy/deploy.mk
+++ hack/make/deploy/deploy.mk
@@ -1,21 +1,28 @@
 ENABLE_CSI ?= true
 PLATFORM ?= "kubernetes"
 
-## Deploy the operator without the csi-driver, with platform specified in % (kubernetes or openshift)
-deploy/%/no-csi:
+## Deploy the operator without the csi-driver
+deploy/no-csi:
 	@make ENABLE_CSI=false $(@D)
 
-## Deploy the operator with csi-driver, with platform specified in % (kubernetes or openshift)
-deploy/%:
-	@make PLATFORM=$(@F) $(@D)
-
-## Deploy the operator with csi-driver, on kubernetes
+## Deploy the operator with csi-driver
 deploy: manifests/crd/helm
-	kubectl get namespace dynatrace || kubectl create namespace dynatrace
-	helm template dynatrace-operator config/helm/chart/default \
+	helm upgrade dynatrace-operator config/helm/chart/default \
+			--install \
 			--namespace dynatrace \
+			--create-namespace \
+			--atomic \
 			--set installCRD=true \
-			--set platform=$(PLATFORM) \
 			--set csidriver.enabled=$(ENABLE_CSI) \
 			--set manifests=true \
-			--set image="$(IMAGE_URI)" | kubectl apply -f -
+			--set image="$(IMAGE_URI)" \
+			--set debug=$(DEBUG)
+
+## Undeploy the current operator installation
+undeploy:
+	kubectl delete dynakube --all -n dynatrace
+	kubectl delete edgeconnect --all -n dynatrace
+	kubectl -n dynatrace wait pod --for=delete -l app.kubernetes.io/managed-by=dynatrace-operator --timeout=300s
+
+	helm uninstall dynatrace-operator \
+			--namespace dynatrace
