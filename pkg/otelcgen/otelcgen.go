630852a882580bca8a4be962447a8fa367c335f9
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-01-09 14:00:42 +0100

    add statsd

--- pkg/otelcgen/otelcgen.go
+++ pkg/otelcgen/otelcgen.go
@@ -2,14 +2,36 @@ package otelcgen
 
 import (
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/config/configgrpc"
-	"go.opentelemetry.io/collector/config/confignet"
-	"go.opentelemetry.io/collector/config/configtls"
 	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/otelcol"
-	"go.opentelemetry.io/collector/receiver/otlpreceiver"
-	"sigs.k8s.io/yaml"
+	"gopkg.in/yaml.v3"
 )
+
+type Histogram struct {
+	MaxSize int32 `mapstructure:"max_size"`
+}
+
+type TimerHistogramMapping struct {
+	StatsDType   string    `mapstructure:"statsd_type"`
+	ObserverType string    `mapstructure:"observer_type"`
+	Histogram    Histogram `mapstructure:"histogram"`
+}
+
+// TLSSetting is based on:
+// "go.opentelemetry.io/collector/config/configtls.TLSSetting"
+// with reduced number of attributes to reduce the number of dependencies.
+type TLSSetting struct {
+	KeyFile  string `mapstructure:"key_file"`
+	CertFile string `mapstructure:"cert_file"`
+}
+
+// ServerConfig is based on "go.opentelemetry.io/collector/config/configgrpc.ServerConfig" and
+// "go.opentelemetry.io/collector/config/confighttp.ServerConfig" with reduced number of attributes
+// to reduce the number of dependencies.
+type ServerConfig struct {
+	Endpoint   string      `mapstructure:"endpoint"`
+	TLSSetting *TLSSetting `mapstructure:"tls,omitempty"`
+}
 
 type Protocol string
 
@@ -20,6 +42,16 @@ const (
 	ZipkinProtocol Protocol = "zipkin"
 	OtlpProtocol   Protocol = "otlp"
 	StatsdProtocol Protocol = "statsd"
+
+	StatsdDefaultEndpoint = "test"
+	ZipkinDefaultEndpoint = "test"
+)
+
+var (
+	JagerID  = component.MustNewID(string(JagerProtocol))
+	OtlpID   = component.MustNewID(string(OtlpProtocol))
+	StatsdID = component.MustNewID(string(StatsdProtocol))
+	ZipkinID = component.MustNewID(string(ZipkinProtocol))
 )
 
 type Config struct {
@@ -29,11 +61,12 @@ type Config struct {
 func (c Config) Marshal() ([]byte, error) {
 	conf := confmap.New()
 	err := conf.Marshal(c.cfg)
+
 	if err != nil {
 		return nil, err
 	}
-	m := conf.ToStringMap()
-	return yaml.Marshal(m)
+
+	return yaml.Marshal(conf.ToStringMap())
 }
 
 type Option func(c *Config)
@@ -50,34 +83,45 @@ func NewConfig(options ...Option) (*Config, error) {
 	return &c, nil
 }
 
+// func
+// receivers
+func buildReceiverComponent(componentID component.ID) component.Config {
+	switch componentID {
+	case JagerID:
+		return map[string]any{"protocols": map[string]any{"grpc": &ServerConfig{
+			Endpoint: "test",
+			TLSSetting: &TLSSetting{
+				KeyFile:  "/run/opensignals/tls/tls.key",
+				CertFile: " /run/opensignals/tls/tls.crt",
+			},
+		}, "http": &ServerConfig{
+			Endpoint: "test",
+			TLSSetting: &TLSSetting{
+				KeyFile:  "/run/opensignals/tls/tls.key",
+				CertFile: " /run/opensignals/tls/tls.crt",
+			},
+		},
+		}}
+	case StatsdID:
+		return map[string]any{
+			"endpoint": "test",
+			"timer_histogram_mapping": []TimerHistogramMapping{{
+				StatsDType: "histogram", ObserverType: "histogram", Histogram: Histogram{MaxSize: 10},
+			}},
+		}
+	}
+
+	return nil
+}
+
 func WithProtocols(protocols ...string) Option {
 	if len(protocols) == 0 {
 		// means all protocols
 		protocols = []string{string(JagerProtocol)}
 	}
 
-	// TODO: dynamically and conditionally create all maps in a loop
-	//jagerID := component.MustNewID(string(JagerProtocol))
-	otlpID := component.MustNewID(string(OtlpProtocol))
-
-	//jagerExporter, _ := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint("http://localhost:14268/api/traces")))
-	//exporters := map[component.ID]component.Config{}
-
 	receivers := map[component.ID]component.Config{
-		otlpID: otlpreceiver.Protocols{
-			GRPC: &configgrpc.ServerConfig{
-				NetAddr: confignet.AddrConfig{
-					Endpoint: "test",
-				},
-				TLSSetting: &configtls.ServerConfig{
-					Config: configtls.Config{
-						CAFile:  "/run/opensignals/tls/tls.crt",
-						KeyFile: "/run/opensignals/tls/tls.key",
-					},
-				},
-			},
-			HTTP: nil,
-		},
+		StatsdID: buildReceiverComponent(StatsdID),
 	}
 
 	return func(c *Config) {
