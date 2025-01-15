2b203a55d6e515dc3ad8862883e71ffa77c55306
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-01-15 14:29:37 +0100

    add processors and tests

--- pkg/otelcgen/otelcgen_test.go
+++ pkg/otelcgen/otelcgen_test.go
@@ -3,7 +3,6 @@ package otelcgen
 import (
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,9 +19,7 @@ func TestNewConfig(t *testing.T) {
 		expectedOutput, err := os.ReadFile(filepath.Join("testdata", "receivers_statsd.yaml"))
 		require.NoError(t, err)
 
-		expected := strings.ReplaceAll(strings.ReplaceAll(string(expectedOutput), "\n", ""), "\r", "")
-		actual := strings.ReplaceAll(strings.ReplaceAll(string(c), "\n", ""), "\r", "")
-		assert.Equal(t, expected, actual)
+		assert.YAMLEq(t, string(expectedOutput), string(c))
 	})
 
 	t.Run("with zipkin protocol only with tls key and tls cert", func(t *testing.T) {
@@ -37,9 +34,46 @@ func TestNewConfig(t *testing.T) {
 
 		expectedOutput, err := os.ReadFile(filepath.Join("testdata", "receivers_zipkin_only.yaml"))
 		require.NoError(t, err)
+		assert.YAMLEq(t, string(expectedOutput), string(c))
+	})
 
-		expected := strings.ReplaceAll(strings.ReplaceAll(string(expectedOutput), "\n", ""), "\r", "")
-		actual := strings.ReplaceAll(strings.ReplaceAll(string(c), "\n", ""), "\r", "")
-		assert.Equal(t, expected, actual)
+	t.Run("with jaeger protocol only with tls key and tls cert", func(t *testing.T) {
+		cfg, err := NewConfig(
+			WithTLSCert("/run/opensignals/tls/tls.crt"),
+			WithTLSKey("/run/opensignals/tls/tls.key"),
+			WithProtocols("jaeger"),
+		)
+		require.NoError(t, err)
+		c, err := cfg.Marshal()
+		require.NoError(t, err)
+
+		expectedOutput, err := os.ReadFile(filepath.Join("testdata", "receivers_jaeger_only.yaml"))
+		require.NoError(t, err)
+
+		assert.YAMLEq(t, string(expectedOutput), string(c))
+	})
+
+	t.Run("with otlp protocol only with tls key and tls cert", func(t *testing.T) {
+		cfg, err := NewConfig(
+			WithTLSCert("/run/opensignals/tls/tls.crt"),
+			WithTLSKey("/run/opensignals/tls/tls.key"),
+			WithProtocols("otlp"),
+		)
+		require.NoError(t, err)
+		c, err := cfg.Marshal()
+		require.NoError(t, err)
+
+		expectedOutput, err := os.ReadFile(filepath.Join("testdata", "receivers_otlp_only.yaml"))
+		require.NoError(t, err)
+
+		assert.YAMLEq(t, string(expectedOutput), string(c))
+	})
+
+	t.Run("with unknown protocol", func(t *testing.T) {
+		_, err := NewConfig(
+			WithProtocols("unknown"),
+		)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "unknown protocol")
 	})
 }
