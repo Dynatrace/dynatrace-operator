479502a810d3b78462991e197fbb481cf14ea48b
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-01-16 09:55:50 +0100

    add extensions

---
+++ pkg/otelcgen/extensions_test.go
@@ -0,0 +1,24 @@
+package otelcgen
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewConfigWithExtensions(t *testing.T) {
+	cfg, err := NewConfig(
+		WithExtensions(),
+	)
+	require.NoError(t, err)
+	c, err := cfg.Marshal()
+	require.NoError(t, err)
+
+	expectedOutput, err := os.ReadFile(filepath.Join("testdata", "extensions_only.yaml"))
+	require.NoError(t, err)
+
+	assert.YAMLEq(t, string(expectedOutput), string(c))
+}
