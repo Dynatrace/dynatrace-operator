22500eae7037789a3119b7fd397043969beae53a
Stefan Hauth <stefan.hauth@dynatrace.com>
2025-01-20 17:40:50 +0100

    Valdiation webhook to make sure kubernetes_monitoring is activated along with extensions.

---
+++ pkg/api/validation/dynakube/extensions.go
@@ -0,0 +1,19 @@
+package validation
+
+import (
+	"context"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+)
+
+const (
+	errorExtensionsWithoutK8SMonitoring = "The Dynakube's specification enables extensions without an ActiveGate which has Kubernetes monitoring enabled. This is not feasible, as the cluster will not be visible in Dynatrace without the Kubernetes monitoring feature."
+)
+
+func extensionsWithoutK8SMonitoring(ctx context.Context, dv *Validator, dk *dynakube.DynaKube) string {
+	if dk.IsExtensionsEnabled() && !dk.ActiveGate().IsKubernetesMonitoringEnabled() {
+		return errorExtensionsWithoutK8SMonitoring
+	}
+
+	return ""
+}
