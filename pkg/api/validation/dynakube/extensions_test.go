37c7c5035b5969afae557fcf1c00cc68591eb22e
Stefan Hauth <stefan.hauth@dynatrace.com>
2025-01-20 17:40:50 +0100

    Valdiation webhook to make sure kubernetes_monitoring is activated along with extensions.

---
+++ pkg/api/validation/dynakube/extensions_test.go
@@ -0,0 +1,53 @@
+package validation
+
+import (
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/shared/image"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube/activegate"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const testDynakubeName = "dynakube"
+
+func TestExtensionsWithoutK8SMonitoring(t *testing.T) {
+	t.Run("no error if extensions are enabled with activegate with k8s-monitoring", func(t *testing.T) {
+		dk := createStandaloneExtensionsDynakube(testDynakubeName, testApiUrl)
+		dk.Spec.ActiveGate = activegate.Spec{
+			Capabilities: []activegate.CapabilityDisplayName{
+				activegate.KubeMonCapability.DisplayName,
+			},
+		}
+
+		assertAllowed(t, dk)
+	})
+	t.Run("error if extensions are enabled without activegate with k8s-monitoring", func(t *testing.T) {
+		assertDenied(t,
+			[]string{errorExtensionsWithoutK8SMonitoring},
+			createStandaloneExtensionsDynakube(testDynakubeName, testApiUrl))
+	})
+}
+
+func createStandaloneExtensionsDynakube(name, apiUrl string) *dynakube.DynaKube {
+	dk := &dynakube.DynaKube{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: testNamespace,
+		},
+		Spec: dynakube.DynaKubeSpec{
+			APIURL:     apiUrl,
+			Extensions: &dynakube.ExtensionsSpec{},
+			Templates: dynakube.TemplatesSpec{
+				ExtensionExecutionController: dynakube.ExtensionExecutionControllerSpec{
+					ImageRef: image.Ref{
+						Repository: "repo/image",
+						Tag:        "version",
+					},
+				},
+			},
+		},
+	}
+
+	return dk
+}
