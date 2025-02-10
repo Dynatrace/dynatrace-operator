d4ebd5b45390c35794c88b96e57378a491ccdc47
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-02-10 09:32:43 +0100

    add full test

--- pkg/otelcgen/otelcgen_test.go
+++ pkg/otelcgen/otelcgen_test.go
@@ -1,1 +1,29 @@
 package otelcgen
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewConfigFull(t *testing.T) {
+	cfg, err := NewConfig(
+		WithPodIP("test"),
+		WithProtocols("otlp", "jaeger", "zipkin", "statsd"),
+		WithExtensions(),
+		WithExporters(),
+		WithServices(),
+		WithProcessors(),
+	)
+	require.NoError(t, err)
+	c, err := cfg.Marshal()
+	require.NoError(t, err)
+
+	expectedOutput, err := os.ReadFile(filepath.Join("testdata", "full_config.yaml"))
+	require.NoError(t, err)
+
+	assert.YAMLEq(t, string(expectedOutput), string(c))
+}
