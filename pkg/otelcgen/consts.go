b4229f8fcaa9c16f55e0b6ea1218557016c58b2d
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-02-11 10:00:45 +0100

    New OpenSignal package to generate configurations for OTEL collector (#4306)

---
+++ pkg/otelcgen/consts.go
@@ -0,0 +1,17 @@
+package otelcgen
+
+const (
+	OtlpGrpcPort = 4317
+	OtlpHTTPPort = 4318
+
+	JaegerGrpcPort          = 14250
+	JaegerThriftBinaryPort  = 6832
+	JaegerThriftCompactPort = 6831
+	JaegerThriftHTTPPort    = 14268
+
+	ZipkinPort = 9411
+
+	StatsdPort = 8125
+
+	ExtensionsHealthCheckPort = 13133
+)
