48d8d32d5bd0cb511a7068a22a2430fde45dc1a7
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-02-28 14:00:03 +0100

    added split for volumes for extensions and telemetryService

--- pkg/controllers/dynakube/otelc/statefulset/volumes.go
+++ pkg/controllers/dynakube/otelc/statefulset/volumes.go
@@ -22,9 +22,12 @@ const (
 )
 
 func setVolumes(dk *dynakube.DynaKube) func(o *appsv1.StatefulSet) {
-	return func(o *appsv1.StatefulSet) {
-		o.Spec.Template.Spec.Volumes = []corev1.Volume{
-			{
+	var volumes []corev1.Volume
+
+	if dk.IsExtensionsEnabled() {
+		volumes = append(
+			volumes,
+			corev1.Volume{
 				Name: consts.ExtensionsTokensVolumeName,
 				VolumeSource: corev1.VolumeSource{
 					Secret: &corev1.SecretVolumeSource{
@@ -39,78 +42,82 @@ func setVolumes(dk *dynakube.DynaKube) func(o *appsv1.StatefulSet) {
 					},
 				},
 			},
-		}
-		if dk.Spec.TrustedCAs != "" {
-			o.Spec.Template.Spec.Volumes = append(o.Spec.Template.Spec.Volumes, corev1.Volume{
-				Name: caCertsVolumeName,
+			corev1.Volume{
+				Name: dk.ExtensionsTLSSecretName(),
 				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: dk.Spec.TrustedCAs,
-						},
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: dk.ExtensionsTLSSecretName(),
 						Items: []corev1.KeyToPath{
 							{
-								Key:  "certs",
-								Path: trustedCAsFile,
+								Key:  consts.TLSCrtDataName,
+								Path: consts.TLSCrtDataName,
 							},
 						},
 					},
 				},
-			})
-		}
+			},
+		)
+	}
 
-		o.Spec.Template.Spec.Volumes = append(o.Spec.Template.Spec.Volumes, corev1.Volume{
-			Name: dk.ExtensionsTLSSecretName(),
+	if dk.Spec.TrustedCAs != "" {
+		volumes = append(volumes, corev1.Volume{
+			Name: caCertsVolumeName,
 			VolumeSource: corev1.VolumeSource{
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: dk.ExtensionsTLSSecretName(),
+				ConfigMap: &corev1.ConfigMapVolumeSource{
+					LocalObjectReference: corev1.LocalObjectReference{
+						Name: dk.Spec.TrustedCAs,
+					},
 					Items: []corev1.KeyToPath{
 						{
-							Key:  consts.TLSCrtDataName,
-							Path: consts.TLSCrtDataName,
+							Key:  "certs",
+							Path: trustedCAsFile,
 						},
 					},
 				},
 			},
 		})
+	}
 
-		if dk.TelemetryService().IsEnabled() {
-			if dk.TelemetryService().Spec.TlsRefName != "" {
-				o.Spec.Template.Spec.Volumes = append(o.Spec.Template.Spec.Volumes, corev1.Volume{
-					Name: customTlsCertVolumeName,
-					VolumeSource: corev1.VolumeSource{
-						Secret: &corev1.SecretVolumeSource{
-							SecretName: dk.TelemetryService().Spec.TlsRefName,
-							Items: []corev1.KeyToPath{
-								{
-									Key:  consts.TLSCrtDataName,
-									Path: consts.TLSCrtDataName,
-								},
-								{
-									Key:  consts.TLSKeyDataName,
-									Path: consts.TLSKeyDataName,
-								},
-							},
-						},
-					},
-				})
-			}
-
-			o.Spec.Template.Spec.Volumes = append(o.Spec.Template.Spec.Volumes, corev1.Volume{
-				Name: dataIngestTokenVolumeName,
+	if dk.TelemetryService().IsEnabled() {
+		if dk.TelemetryService().Spec.TlsRefName != "" {
+			volumes = append(volumes, corev1.Volume{
+				Name: customTlsCertVolumeName,
 				VolumeSource: corev1.VolumeSource{
 					Secret: &corev1.SecretVolumeSource{
-						SecretName: dk.Name,
+						SecretName: dk.TelemetryService().Spec.TlsRefName,
 						Items: []corev1.KeyToPath{
 							{
-								Key:  dynatrace.DataIngestToken,
-								Path: dynatrace.DataIngestToken,
+								Key:  consts.TLSCrtDataName,
+								Path: consts.TLSCrtDataName,
+							},
+							{
+								Key:  consts.TLSKeyDataName,
+								Path: consts.TLSKeyDataName,
 							},
 						},
 					},
 				},
 			})
 		}
+
+		volumes = append(volumes, corev1.Volume{
+			Name: dataIngestTokenVolumeName,
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: dk.Name,
+					Items: []corev1.KeyToPath{
+						{
+							Key:  dynatrace.DataIngestToken,
+							Path: dynatrace.DataIngestToken,
+						},
+					},
+				},
+			},
+		})
+	}
+
+	return func(o *appsv1.StatefulSet) {
+		o.Spec.Template.Spec.Volumes = volumes
 	}
 }
 
