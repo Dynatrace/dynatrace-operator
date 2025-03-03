7166c0e1097868215cf4abc47407f228beb5181c
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-03 13:38:36 +0100

    otelc generatro: remove required telemetry service section

--- pkg/otelcgen/services.go
+++ pkg/otelcgen/services.go
@@ -3,7 +3,6 @@ package otelcgen
 import (
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/pipeline"
-	"go.opentelemetry.io/collector/service"
 	"go.opentelemetry.io/collector/service/extensions"
 	"go.opentelemetry.io/collector/service/pipelines"
 	"go.opentelemetry.io/collector/service/telemetry"
@@ -16,14 +15,21 @@ var (
 	debug   = component.MustNewID("debug")
 )
 
-func (c *Config) buildServices() service.Config {
-	return service.Config{
-		Telemetry: telemetry.Config{
-			Logs:     telemetry.LogsConfig{},
-			Metrics:  telemetry.MetricsConfig{},
-			Traces:   telemetry.TracesConfig{},
-			Resource: nil,
-		},
+// ServiceConfig defines the configurable components of the Service.
+// based on "go.opentelemetry.io/collector/service.Config
+type ServiceConfig struct {
+	// Telemetry is the configuration for collector's own telemetry.
+	Telemetry telemetry.Config `mapstructure:"telemetry,omitempty"`
+
+	// Pipelines are the set of data pipelines configured for the service.
+	Pipelines pipelines.Config `mapstructure:"pipelines"`
+
+	// Extensions are the ordered list of extensions configured for the service.
+	Extensions extensions.Config `mapstructure:"extensions"`
+}
+
+func (c *Config) buildServices() ServiceConfig {
+	return ServiceConfig{
 		Extensions: extensions.Config{healthCheck},
 		Pipelines: pipelines.Config{
 			traces: &pipelines.PipelineConfig{
