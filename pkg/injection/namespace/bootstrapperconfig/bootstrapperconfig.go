620f24f7dfff0800ac907b1ecab832f0c80b10a6
Stefan Hauth <stefan.hauth@dynatrace.com>
2025-03-05 15:30:34 +0100

    Update to latest changes from main.

--- pkg/injection/namespace/bootstrapperconfig/bootstrapperconfig.go
+++ pkg/injection/namespace/bootstrapperconfig/bootstrapperconfig.go
@@ -8,7 +8,7 @@ import (
 	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/ca"
 	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/curl"
 	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/pmc"
-	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4/dynakube"
 	dtclient "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
 	"github.com/Dynatrace/dynatrace-operator/pkg/consts"
 	"github.com/Dynatrace/dynatrace-operator/pkg/injection/namespace/mapper"
