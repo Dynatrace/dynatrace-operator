98efe531d33592edb2a991f5539e201d92ba8d20
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-03-07 13:54:49 +0100

    Make pretty

--- pkg/api/latest/dynakube/dynakube.go
+++ pkg/api/latest/dynakube/dynakube.go
@@ -4,12 +4,10 @@ import (
 	lts "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 )
 
-type DynaKube = lts.DynaKube
-
-type DynaKubeList = lts.DynaKubeList
-
-type DynaKubeSpec = lts.DynaKubeSpec
-
-type DynaKubeStatus = lts.DynaKubeStatus
-
-type TemplatesSpec = lts.TemplatesSpec
+type (
+	DynaKube       = lts.DynaKube
+	DynaKubeList   = lts.DynaKubeList
+	DynaKubeSpec   = lts.DynaKubeSpec
+	DynaKubeStatus = lts.DynaKubeStatus
+	TemplatesSpec  = lts.TemplatesSpec
+)
