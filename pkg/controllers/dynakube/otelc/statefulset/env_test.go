8ec70a56dab4f89cd913b785da040e0a6c724579
Stefan Hauth <stefan.hauth@dynatrace.com>
2025-03-12 22:23:32 +0100

    Set HTTPS_PROXY and NO_PROXY envs.

--- pkg/controllers/dynakube/otelc/statefulset/env_test.go
+++ pkg/controllers/dynakube/otelc/statefulset/env_test.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/shared/value"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4/dynakube/activegate"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4/dynakube/telemetryingest"
 	"github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
 	"github.com/Dynatrace/dynatrace-operator/pkg/consts"
@@ -105,4 +108,185 @@ func TestEnvironmentVariables(t *testing.T) {
 			},
 		}}, statefulSet.Spec.Template.Spec.Containers[0].Env[11])
 	})
+}
+
+func TestProxyEnvs(t *testing.T) {
+	const testProxySecretName = "test-proxy-secret"
+
+	const testProxyValue = "test.proxy.com:8888"
+
+	testActiveGate := activegate.Spec{
+		CapabilityProperties: activegate.CapabilityProperties{},
+		Capabilities:         []activegate.CapabilityDisplayName{"otlp-ingest"},
+		UseEphemeralVolume:   false,
+	}
+
+	tests := []struct {
+		name            string
+		extensions      *dynakube.ExtensionsSpec
+		telemetryIngest *telemetryingest.Spec
+		activeGate      *activegate.Spec
+		proxy           *value.Source
+
+		expectedNoProxy string
+	}{
+		{
+			name:            "extensions without proxy",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: nil,
+			proxy:           nil,
+			expectedNoProxy: "",
+		},
+		{
+			name:            "extensions with proxy secret",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: nil,
+			proxy: &value.Source{
+				ValueFrom: testProxySecretName,
+			},
+			expectedNoProxy: "dynakube-extensions-controller.dynatrace,dynakube-activegate.dynatrace",
+		},
+		{
+			name:            "extensions with proxy value",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: nil,
+			proxy: &value.Source{
+				Value: testProxyValue,
+			},
+			expectedNoProxy: "dynakube-extensions-controller.dynatrace,dynakube-activegate.dynatrace",
+		},
+		{
+			name:            "telemetryIngest, public AG, without proxy",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy:           nil,
+			expectedNoProxy: "",
+		},
+		{
+			name:            "telemetryIngest, public AG, with proxy secret",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy: &value.Source{
+				ValueFrom: testProxySecretName,
+			},
+			expectedNoProxy: "",
+		},
+		{
+			name:            "telemetryIngest, public AG, with proxy value",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy: &value.Source{
+				Value: testProxyValue,
+			},
+			expectedNoProxy: "",
+		},
+		{
+			name:            "telemetryIngest, local AG, without proxy",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      &testActiveGate,
+			proxy:           nil,
+			expectedNoProxy: "",
+		},
+		{
+			name:            "telemetryIngest, local AG, with proxy secret",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      &testActiveGate,
+			proxy: &value.Source{
+				ValueFrom: testProxySecretName,
+			},
+			expectedNoProxy: "dynakube-activegate.dynatrace",
+		},
+		{
+			name:            "telemetryIngest, local AG, with proxy value",
+			extensions:      nil,
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      &testActiveGate,
+			proxy: &value.Source{
+				Value: testProxyValue,
+			},
+			expectedNoProxy: "dynakube-activegate.dynatrace",
+		},
+		{
+			name:            "telemetryIngest, extensions, private AG, without proxy",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy:           nil,
+			expectedNoProxy: "",
+		},
+		{
+			name:            "telemetryIngest, extensions, private AG, with proxy secret",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy: &value.Source{
+				ValueFrom: testProxySecretName,
+			},
+			expectedNoProxy: "dynakube-extensions-controller.dynatrace,dynakube-activegate.dynatrace",
+		},
+		{
+			name:            "telemetryIngest, extensions, private AG, with proxy value",
+			extensions:      &dynakube.ExtensionsSpec{},
+			telemetryIngest: &telemetryingest.Spec{},
+			activeGate:      nil,
+			proxy: &value.Source{
+				Value: testProxyValue,
+			},
+			expectedNoProxy: "dynakube-extensions-controller.dynatrace,dynakube-activegate.dynatrace",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dk := getTestDynakube()
+			dk.Spec.Extensions = tt.extensions
+			dk.Spec.TelemetryIngest = tt.telemetryIngest
+			dk.Spec.Proxy = tt.proxy
+
+			if tt.activeGate != nil {
+				dk.Spec.ActiveGate = *tt.activeGate
+			}
+
+			dataIngestToken := getTokens(dk.Name, dk.Namespace)
+			configMap := getConfigConfigMap(dk.Name, dk.Namespace)
+			statefulSet := getStatefulset(t, dk, &dataIngestToken, &configMap)
+
+			switch {
+			case tt.proxy == nil:
+				{
+					assert.NotContains(t, statefulSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{Name: envProxy})
+				}
+			case tt.proxy.ValueFrom != "":
+				{
+					assert.Contains(t, statefulSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{
+						Name: envProxy,
+						ValueFrom: &corev1.EnvVarSource{
+							SecretKeyRef: &corev1.SecretKeySelector{
+								LocalObjectReference: corev1.LocalObjectReference{Name: tt.proxy.ValueFrom},
+								Key:                  dynakube.ProxyKey,
+							},
+						},
+					})
+				}
+			default:
+				{
+					assert.Contains(t, statefulSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{
+						Name:  envProxy,
+						Value: tt.proxy.Value,
+					})
+				}
+			}
+
+			if tt.proxy != nil {
+				assert.Contains(t, statefulSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{Name: envNoProxy, Value: tt.expectedNoProxy})
+			} else {
+				assert.NotContains(t, statefulSet.Spec.Template.Spec.Containers[0].Env, corev1.EnvVar{Name: envNoProxy})
+			}
+		})
+	}
 }
