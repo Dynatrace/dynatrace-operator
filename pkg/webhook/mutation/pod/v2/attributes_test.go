c19d1f1db7a6bcf194a5c87c8b3bda9266f6ab3b
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-03-13 13:46:51 +0100

    Tests for container-attributes

--- pkg/webhook/mutation/pod/v2/attributes_test.go
+++ pkg/webhook/mutation/pod/v2/attributes_test.go
@@ -1,10 +1,16 @@
 package v2
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
 	containerattr "github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/attributes/container"
+	dtwebhook "github.com/Dynatrace/dynatrace-operator/pkg/webhook"
+	"github.com/Dynatrace/dynatrace-operator/pkg/webhook/mutation/pod/v2/common/volumes"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func TestAddPodAttributes(t *testing.T) {
@@ -12,7 +18,137 @@ func TestAddPodAttributes(t *testing.T) {
 }
 
 func TestAddContainerAttributes(t *testing.T) {
+	validateContainerArgs := func(t *testing.T, pod corev1.Pod, args []string) {
+		t.Helper()
 
+		require.NotEmpty(t, args)
+
+		for _, arg := range args {
+			splitArg := strings.Split(arg, "=")
+			require.Len(t, splitArg, 2)
+			var attr containerattr.Attributes
+			require.NoError(t, json.Unmarshal([]byte(splitArg[1]), &attr))
+			assert.Contains(t, pod.Spec.Containers, corev1.Container{
+				Name:  attr.ContainerName,
+				Image: attr.ToURI(),
+			})
+		}
+	}
+
+	t.Run("add attributes, do not change pod or app-container", func(t *testing.T) {
+		app1Container := corev1.Container{
+			Name:  "app-1-name",
+			Image: "registry1.example.com/repository/image:tag",
+		}
+		app2Container := corev1.Container{
+			Name:  "app-2-name",
+			Image: "registry2.example.com/repository/image:tag",
+		}
+		initContainer := corev1.Container{
+			Args: []string{},
+		}
+		pod := corev1.Pod{
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					app1Container,
+					app2Container,
+				},
+			},
+		}
+
+		expectedPod := pod.DeepCopy()
+
+		request := dtwebhook.MutationRequest{
+			BaseRequest: &dtwebhook.BaseRequest{
+				Pod: &pod,
+			},
+			InstallContainer: &initContainer,
+		}
+
+		addContainerAttributes(&request)
+
+		require.Equal(t, *expectedPod, *request.BaseRequest.Pod)
+
+		validateContainerArgs(t, pod, initContainer.Args)
+	})
+
+	t.Run("no new container ==> no new arg", func(t *testing.T) {
+		app1Container := corev1.Container{
+			Name:  "app-1-name",
+			Image: "registry1.example.com/repository/image:tag",
+		}
+		volumes.AddConfigVolumeMount(&app1Container)
+		app2Container := corev1.Container{
+			Name:  "app-2-name",
+			Image: "registry2.example.com/repository/image:tag",
+		}
+		volumes.AddConfigVolumeMount(&app2Container)
+
+		initContainer := corev1.Container{
+			Args: []string{},
+		}
+		pod := corev1.Pod{
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					app1Container,
+					app2Container,
+				},
+			},
+		}
+
+		expectedPod := pod.DeepCopy()
+
+		request := dtwebhook.MutationRequest{
+			BaseRequest: &dtwebhook.BaseRequest{
+				Pod: &pod,
+			},
+			InstallContainer: &initContainer,
+		}
+
+		addContainerAttributes(&request)
+
+		require.Equal(t, *expectedPod, *request.BaseRequest.Pod)
+		require.Empty(t, initContainer.Args)
+	})
+
+	t.Run("partially new => only add new", func(t *testing.T) {
+		app1Container := corev1.Container{
+			Name:  "app-1-name",
+			Image: "registry1.example.com/repository/image:tag",
+		}
+		volumes.AddConfigVolumeMount(&app1Container)
+		app2Container := corev1.Container{
+			Name:  "app-2-name",
+			Image: "registry2.example.com/repository/image:tag",
+		}
+
+		initContainer := corev1.Container{
+			Args: []string{},
+		}
+		pod := corev1.Pod{
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					app1Container,
+					app2Container,
+				},
+			},
+		}
+
+		expectedPod := pod.DeepCopy()
+
+		request := dtwebhook.MutationRequest{
+			BaseRequest: &dtwebhook.BaseRequest{
+				Pod: &pod,
+			},
+			InstallContainer: &initContainer,
+		}
+
+		addContainerAttributes(&request)
+
+		require.Equal(t, *expectedPod, *request.BaseRequest.Pod)
+		require.Len(t, initContainer.Args, 1)
+		validateContainerArgs(t, pod, initContainer.Args)
+	})
 }
 
 func TestCreateImageInfo(t *testing.T) {
