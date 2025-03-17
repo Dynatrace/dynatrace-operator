1222e38c4bcd90a6dd6b9dfd08b5288df0c9a973
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-03-17 16:29:48 +0100

    Update doc string

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -34,7 +34,7 @@ test/e2e/standard: manifests/crd/helm
 test/e2e/istio: manifests/crd/helm
 	$(GOTESTCMD) -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 200m -count=1 ./test/scenarios/istio -args $(SKIPCLEANUP)
 
-## Run release e2e test only
+## Run no-csi e2e test only
 test/e2e/no-csi: manifests/crd/helm
 	$(GOTESTCMD) -v -tags "$(shell ./hack/build/create_go_build_tags.sh true)" -timeout 20m -count=1 ./test/scenarios/no_csi -args $(SKIPCLEANUP)
 
