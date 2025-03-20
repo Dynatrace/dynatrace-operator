5bc1b76d80d92ee398d3292a7d2f1aa3d5db5976
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-20 15:33:56 +0100

    we still need to tag it before pushing

--- hack/build/ci/upload-podman-image.sh
+++ hack/build/ci/upload-podman-image.sh
@@ -5,8 +5,9 @@ set -x
 target_image="${1}"
 readonly image_tar_path="/tmp/operator-all-platforms.tar"
 
-podman load -i "${image_tar_path}"
+src_image=$(podman load -i "${image_tar_path}" | cut -d' ' -f3)
 
+# we need retag it because we load it as localhost
 podman tag "${src_image}" "${target_image}"
 
 podman push "${target_image}"
