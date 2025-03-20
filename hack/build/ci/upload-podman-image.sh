a569cd56f78e2519289401dd432f6407074bb973
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-20 14:44:28 +0100

    refactor to push all platforms in single image and manifest

--- hack/build/ci/upload-podman-image.sh
+++ hack/build/ci/upload-podman-image.sh
@@ -2,27 +2,12 @@
 
 set -x
 
-readonly platform="${1}"
 target_image="${2}"
-readonly skip_platform_suffix="${3}"
-readonly image_tar_path="/tmp/operator-${platform}.tar"
+readonly image_tar_path="/tmp/operator-all-platforms.tar"
 
-if [ -z "${skip_platform_suffix}" ]
-then
-  target_image=${target_image}-${platform}
-fi
+podman load -i "${image_tar_path}"
 
-# $podman load -i /tmp/alpine.tar
-# Loaded image: alpine:latest
-#
-# we're interested in "alpine:latest", that's field=3
-src_image=$(podman load -i "${image_tar_path}" | cut -d' ' -f3)
+podman push "${target_image}"
 
-podman tag "${src_image}" "${target_image}"
-pushinfo=$(podman push "${target_image}")
-
-echo $pushinfo
-
-# filtering by image-tag directly does not work currently see: https://github.com/moby/moby/issues/29901
-digest=$(echo "$pushinfo" | tail -n 1 | cut -d " " -f 3)
-echo "digest=${digest}">> "$GITHUB_OUTPUT"
+# TODO: add digest later
+# echo "digest=${digest}">> "$GITHUB_OUTPUT"
