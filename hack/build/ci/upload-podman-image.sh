793ae500990fb8eb191f3a9f4cefbbe439b9c857
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-20 15:13:11 +0100

    we still need to tag it before pushing

--- hack/build/ci/upload-podman-image.sh
+++ hack/build/ci/upload-podman-image.sh
@@ -7,7 +7,11 @@ readonly image_tar_path="/tmp/operator-all-platforms.tar"
 
 podman load -i "${image_tar_path}"
 
+podman tag "${src_image}" "${target_image}"
+
 podman push "${target_image}"
+
+podman manifest inspect "${target_image}"
 
 # TODO: add digest later
 # echo "digest=${digest}">> "$GITHUB_OUTPUT"
