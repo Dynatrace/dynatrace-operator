95e1e3f9947de6213574253e01a81bb419e0d179
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-23 18:43:57 +0100

    add draft annotate-manifest

--- hack/build/ci/annotate-manifest.sh
+++ hack/build/ci/annotate-manifest.sh
@@ -1,14 +1,24 @@
+#!/bin/bash
 
+set -x # # activate debugging
 
-image=${2}
+if [ -z "$2" ]
+then
+  echo "Usage: $0 <image_name> <annotation>"
+  exit 1
+fi
+
+image=${1}
 annotation=${2}
 
 podman pull $image
 
-podman manifest inspect $image
+podman manifest inspect $image | jq
 
 
-for
-  podman manifest annotate --annotation $image $digest
-
+for digest in ${digest}
+do
+  echo digest
+  # podman manifest annotate --annotation $annotation $image $digest
+done
 
