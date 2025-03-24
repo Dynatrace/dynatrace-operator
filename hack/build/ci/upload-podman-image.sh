78d8a2f167eb1f4d481048a5f39d29b4f8b76f6d
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-24 10:17:31 +0100

    push in oci format

--- hack/build/ci/upload-podman-image.sh
+++ hack/build/ci/upload-podman-image.sh
@@ -10,7 +10,9 @@ src_image=$(podman load -i "${image_tar_path}" | cut -d' ' -f3)
 # we need retag it because we load it as localhost
 podman tag "${src_image}" "${target_image}"
 
-podman push "${target_image}"
+# --format, -f=format
+# Manifest Type (oci, v2s2, or v2s1) to use when pushing an image.
+podman push --format oci "${target_image}"
 
 # TODO: add digest later
 # echo "digest=${digest}">> "$GITHUB_OUTPUT"
