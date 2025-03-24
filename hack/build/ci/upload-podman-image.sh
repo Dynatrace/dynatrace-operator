fd3793961f1906bc3ac24bb9e60bcff33bdedebf
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-24 11:14:03 +0100

    revert back to build image one per platform

--- hack/build/ci/upload-podman-image.sh
+++ hack/build/ci/upload-podman-image.sh
@@ -2,17 +2,21 @@
 
 set -x
 
-target_image="${1}"
-readonly image_tar_path="/tmp/operator-all-platforms.tar"
+readonly platform="${1}"
+target_image="${2}"
+readonly skip_platform_suffix="${3}"
+
+readonly image_tar_path="/tmp/operator-${platform}.tar"
+
+if [ -z "${skip_platform_suffix}" ]
+then
+  target_image=${target_image}-${platform}
+fi
 
 src_image=$(podman load -i "${image_tar_path}" | cut -d' ' -f3)
 
 # we need retag it because we load it as localhost
 podman tag "${src_image}" "${target_image}"
 
-# --format, -f=format
-# Manifest Type (oci, v2s2, or v2s1) to use when pushing an image.
+# --format is manifest type (oci, v2s2, or v2s1) to use when pushing an image.
 podman push --format oci "${target_image}"
-
-# TODO: add digest later
-# echo "digest=${digest}">> "$GITHUB_OUTPUT"
