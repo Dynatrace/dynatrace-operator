2fc72c3a2f2677b169a4b3e1ab4884a512313d00
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-24 09:51:48 +0100

    add annotate-manifest script after build image

--- hack/build/ci/annotate-manifest.sh
+++ hack/build/ci/annotate-manifest.sh
@@ -2,23 +2,35 @@
 
 set -x # # activate debugging
 
-if [ -z "$2" ]
+if [ -z "$3" ]
 then
-  echo "Usage: $0 <image_name> <annotation>"
+  echo "Usage: $0 <image_name> <raw_platforms> <annotation>"
+  echo "<raw_platforms> single platform or comma separated platforms in buildah format"
+  echo "<annotation> should be key=value string"
   exit 1
 fi
 
-image=${1}
-annotation=${2}
+image=$1
+raw_platforms=$2
+annotation=$3
 
-podman pull $image
+buildah manifest inspect ${image}
 
-podman manifest inspect $image | jq
+# Used for testing
+# raw_platforms="linux/amd64"
+# raw_platforms="linux/amd64, linux/arm64"
+# raw_platforms="linux/amd64,linux/arm64"
 
+# we want same format as buildah doing internally during building multi-arch image
+# which is `platformarch` without slashes and spaces
+platforms=($(echo ${raw_platforms//\//} | tr "," "\n"))
+# debug array
+echo "${platforms[@]}"
 
-for digest in ${digest}
+for platform in "${platforms[@]}"
 do
-  echo digest
-  # podman manifest annotate --annotation $annotation $image $digest
+  echo "$platform"
+  buildah manifest annotate --annotation $annotation "${image}" "${image}-${platform}"
 done
 
+buildah manifest inspect ${image}
