c1ffb1dcb25e57a81444ce450b1177d6f8d7d251
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-25 14:52:01 +0100

    try with crane

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -13,8 +13,7 @@ annotation=$4
 
 image="${image_name}:${image_tag}"
 
-echo "This script is based on podman version 5.4.1"
-echo "current version of podman is $(/usr/local/bin/podman --version)"
+echo "current version of podman is $(podman --version)"
 
 platforms=($(echo "${raw_platforms}" | tr "," "\n"))
 
@@ -24,22 +23,17 @@ images=()
 
 for platfrom in "${platforms[@]}"
 do
-   /usr/local/bin/podman pull "${image}-${platfrom}"
+   podman pull "${image}-${platfrom}"
    images+=("${image}-${platfrom}")
 done
 
-/usr/local/bin/podman manifest create --annotation "${annotation}" "${image}" "${images[@]}"
+podman manifest create "${image}"
 
-if [ -z "${annotation}" ]
-then
- /usr/local/bin/podman manifest create "${image}" "${images[@]}"
-else
-  /usr/local/bin/podman manifest create --annotation "${annotation}" "${image}" "${images[@]}"
-fi
+podman manifest add "${image}" "${images[@]}"
 
-/usr/local/bin/podman manifest inspect "${image}"
+podman manifest inspect "${image}"
 
-/usr/local/bin/podman manifest push --format oci --digestfile=digestfile.sha256 "${image}"
+podman manifest push --format oci --digestfile=digestfile.sha256 "${image}"
 
 sha256=$(cat digestfile.sha256)
 
