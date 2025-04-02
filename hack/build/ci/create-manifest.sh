9545b1699086e294dd12c468601b6119642160d0
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-04-02 17:13:12 +0200

    Reintroduce the new manifest logic but with docker

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-if [ -z "$2" ]
+if [ -z "$3" ]
 then
-  echo "Usage: $0 <image_name> <image_tag> <enable_multiplatform>"
+  echo "Usage: $0 <image_name> <image_tag> <platforms> <annotation>"
   exit 1
 fi
 
 image_name=$1
 image_tag=$2
-multiplatform=$3
+raw_platforms=$3
+annotation=$4
+
 image="${image_name}:${image_tag}"
 
-gcp_annotation="com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
+platforms=($(echo "${raw_platforms}" | tr "," "\n"))
 
-if [ "$multiplatform" == "true" ]
+echo "Creating manifest for ${platforms[*]}"
+
+images=()
+
+for platfrom in "${platforms[@]}"
+do
+   docker pull "${image}-${platfrom}"
+   images+=("${image}-${platfrom}")
+done
+
+docker manifest create "${image}"
+
+if [ -z "${annotation}" ]
 then
-  supported_architectures=("amd64" "arm64" "ppc64le" "s390x")
-  images=()
-  echo "Creating manifest for ${supported_architectures[*]}"
-
-  for architecture in "${supported_architectures[@]}"
-  do
-    docker pull "${image}-${architecture}"
-    images+=("${image}-${architecture}")
-  done
-  docker manifest create "${image}" "${images[@]}"
-  docker manifest add --annotation "${gcp_annotation}" "${image}" "${images[@]}"
+  docker manifest add "${image}" "${images[@]}"
 else
-  echo "Creating manifest for the AMD image "
-  docker pull "${image}-amd64"
-  docker pull "${image}-arm64"
-  docker manifest create "${image}" "${image}-amd64" "${image}-arm64"
-  docker manifest add --annotation "${gcp_annotation}" "${image}" "${image}-amd64" "${image}-arm64"
+  docker manifest add --annotation "${annotation}" "${image}" "${images[@]}"
 fi
 
-sha256=$(docker manifest push "${image}")
+docker manifest inspect "${image}"
+
+docker manifest push --format oci --digestfile=digestfile.sha256 "${image}"
+
+sha256=$(cat digestfile.sha256)
+
 echo "digest=${sha256}">> $GITHUB_OUTPUT
