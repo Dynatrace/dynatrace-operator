022218f9a7a87d70b59991c8eeabed0d211a6de8
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-04-02 15:28:44 +0200

    Move annotation logic

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -11,6 +11,8 @@ image_tag=$2
 multiplatform=$3
 image="${image_name}:${image_tag}"
 
+gcp_annotation="com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
+
 if [ "$multiplatform" == "true" ]
 then
   supported_architectures=("amd64" "arm64" "ppc64le" "s390x")
@@ -23,6 +25,7 @@ then
     images+=("${image}-${architecture}")
   done
   docker manifest create "${image}" "${images[@]}"
+  docker manifest add --annotation "${gcp_annotation}" "${image}" "${images[@]}"
 else
   echo "Creating manifest for the AMD image "
   docker pull "${image}-amd64"
