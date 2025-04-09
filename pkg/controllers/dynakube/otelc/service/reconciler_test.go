a394d0421aa660c40014c83833c2fbe71484737a
StefanHauth <63204425+StefanHauth@users.noreply.github.com>
2025-04-09 14:20:09 +0200

    Switch to UPD ports for StatsD & Jaeger thrift/compact (#4727)

--- pkg/controllers/dynakube/otelc/service/reconciler_test.go
+++ pkg/controllers/dynakube/otelc/service/reconciler_test.go
@@ -52,34 +52,42 @@ func TestService(t *testing.T) {
 		assert.Equal(t, otlpGrpcPortName, service.Spec.Ports[0].Name)
 		assert.Equal(t, int32(4317), service.Spec.Ports[0].Port)
 		assert.Equal(t, int32(4317), service.Spec.Ports[0].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolTCP, service.Spec.Ports[0].Protocol)
 
 		assert.Equal(t, otlpHttpPortName, service.Spec.Ports[1].Name)
 		assert.Equal(t, int32(4318), service.Spec.Ports[1].Port)
 		assert.Equal(t, int32(4318), service.Spec.Ports[1].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolTCP, service.Spec.Ports[1].Protocol)
 
 		assert.Equal(t, jaegerGrpcPortName, service.Spec.Ports[2].Name)
 		assert.Equal(t, int32(14250), service.Spec.Ports[2].Port)
 		assert.Equal(t, int32(14250), service.Spec.Ports[2].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolTCP, service.Spec.Ports[2].Protocol)
 
 		assert.Equal(t, jaegerThriftBinaryPortName, service.Spec.Ports[3].Name)
 		assert.Equal(t, int32(6832), service.Spec.Ports[3].Port)
 		assert.Equal(t, int32(6832), service.Spec.Ports[3].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolUDP, service.Spec.Ports[3].Protocol)
 
 		assert.Equal(t, jaegerThriftCompactPortName, service.Spec.Ports[4].Name)
 		assert.Equal(t, int32(6831), service.Spec.Ports[4].Port)
 		assert.Equal(t, int32(6831), service.Spec.Ports[4].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolUDP, service.Spec.Ports[4].Protocol)
 
 		assert.Equal(t, jaegerThriftHttpPortName, service.Spec.Ports[5].Name)
 		assert.Equal(t, int32(14268), service.Spec.Ports[5].Port)
 		assert.Equal(t, int32(14268), service.Spec.Ports[5].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolTCP, service.Spec.Ports[5].Protocol)
 
 		assert.Equal(t, statsdPortName, service.Spec.Ports[6].Name)
 		assert.Equal(t, int32(8125), service.Spec.Ports[6].Port)
 		assert.Equal(t, int32(8125), service.Spec.Ports[6].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolUDP, service.Spec.Ports[6].Protocol)
 
 		assert.Equal(t, zipkinPortName, service.Spec.Ports[7].Name)
 		assert.Equal(t, int32(9411), service.Spec.Ports[7].Port)
 		assert.Equal(t, int32(9411), service.Spec.Ports[7].TargetPort.IntVal)
+		assert.Equal(t, corev1.ProtocolTCP, service.Spec.Ports[7].Protocol)
 
 		require.Len(t, dk.Status.Conditions, 1)
 		assert.Equal(t, serviceConditionType, dk.Status.Conditions[0].Type)
