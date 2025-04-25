4ca1a53505d7d7411a9d1f696601fb163cc24aec
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-04-25 08:44:05 +0200

    Add unit test

---
+++ cmd/operator/manager_test.go
@@ -0,0 +1,21 @@
+package operator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetControllerAddFuncs(t *testing.T) {
+	t.Run("without OLM", func(t *testing.T) {
+		funcs := getControllerAddFuncs(false)
+
+		assert.Len(t, funcs, 4) // dk, ec, nodes, certs
+	})
+
+	t.Run("with OLM", func(t *testing.T) {
+		funcs := getControllerAddFuncs(true)
+
+		assert.Len(t, funcs, 3) // dk, ec, nodes
+	})
+}
