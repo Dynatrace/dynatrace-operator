9bc8e4fd9add85ee590da323f61fdcd7c31ea966
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2025-04-29 10:12:03 +0200

    Add `cmd/bootstrapper/download` package (#4818)

---
+++ cmd/bootstrapper/download/client.go
@@ -0,0 +1,93 @@
+package download
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/ca"
+	dtclient "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
+	"github.com/Dynatrace/dynatrace-operator/pkg/injection/codemodule/installer/url"
+	"github.com/spf13/afero"
+)
+
+type Client struct {
+	newInstaller url.NewFunc
+	newDTClient  dtclient.NewFunc
+}
+
+type Option func(*Client)
+
+func WithInstaller(builder url.NewFunc) Option {
+	return func(cl *Client) {
+		cl.newInstaller = builder
+	}
+}
+
+func WithDTClient(builder dtclient.NewFunc) Option {
+	return func(cl *Client) {
+		cl.newDTClient = builder
+	}
+}
+
+func New(options ...Option) *Client {
+	cl := &Client{
+		newInstaller: url.NewUrlInstaller,
+		newDTClient:  dtclient.NewClient,
+	}
+
+	for _, opt := range options {
+		opt(cl)
+	}
+
+	return cl
+}
+
+func (cl *Client) Do(ctx context.Context, fs afero.Afero, inputDir string, targetDir string, props url.Properties) error {
+	client, err := cl.createDTClientFromFs(fs, inputDir)
+	if err != nil {
+		return err
+	}
+
+	oneAgentInstaller := cl.newInstaller(
+		fs,
+		client,
+		&props,
+	)
+
+	_, err = oneAgentInstaller.InstallAgent(ctx, targetDir)
+
+	return err
+}
+
+func (cl *Client) createDTClientFromFs(fs afero.Afero, inputDir string) (dtclient.Client, error) {
+	config, err := configFromFs(fs, inputDir)
+	if err != nil {
+		return nil, err
+	}
+
+	caFile := filepath.Join(inputDir, ca.TrustedCertsInputFile) // TODO: Replace with ca.GetFromFs
+
+	certs, err := fs.ReadFile(caFile)
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	options := config.toDTClientOptions()
+
+	if len(certs) > 0 {
+		options = append(options, dtclient.Certs(certs))
+	}
+
+	client, err := cl.newDTClient(
+		config.URL,
+		config.ApiToken,
+		config.ApiToken,
+		options...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
---
+++ cmd/bootstrapper/download/config.go
@@ -0,0 +1,65 @@
+package download
+
+import (
+	"encoding/json"
+	"path/filepath"
+
+	dtclient "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
+	"github.com/spf13/afero"
+)
+
+const (
+	InputFileName = "dtclient.config"
+)
+
+type Config struct {
+	URL      string `json:"url"`
+	ApiToken string `json:"apiToken"`
+
+	Proxy       string `json:"proxy"`
+	NoProxy     string `json:"noProxy"`
+	NetworkZone string `json:"networkZone"`
+	HostGroup   string `json:"hostGroup"`
+
+	SkipCertCheck bool `json:"skipCertCheck"`
+}
+
+func (c Config) toDTClientOptions() []dtclient.Option {
+	options := []dtclient.Option{}
+
+	if c.HostGroup != "" {
+		options = append(options, dtclient.HostGroup(c.HostGroup))
+	}
+
+	if c.NetworkZone != "" {
+		options = append(options, dtclient.NetworkZone(c.NetworkZone))
+	}
+
+	if c.Proxy != "" {
+		options = append(options, dtclient.Proxy(c.Proxy, c.NoProxy))
+	}
+
+	if c.SkipCertCheck {
+		options = append(options, dtclient.SkipCertificateValidation(c.SkipCertCheck))
+	}
+
+	return options
+}
+
+func configFromFs(fs afero.Afero, inputDir string) (*Config, error) {
+	inputFile := filepath.Join(inputDir, InputFileName)
+
+	content, err := fs.ReadFile(inputFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
