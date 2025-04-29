25a00bcbfac5c417f5dde634f9ff52ffa9ceeff2
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-04-29 14:36:31 +0200

    remove unnesesary change

--- hack/build/create_go_build_tags.sh
+++ hack/build/create_go_build_tags.sh
@@ -7,11 +7,7 @@ fi
 
 needs_e2e_tag=$1
 
-go_build_tags=(
-     # If CGO is enabled, certain standard libraries will also use CGO, these explicitly disallow that
-    "osusergo"
-    "netgo"
-)
+go_build_tags=()
 
 if "${needs_e2e_tag}"; then
     # Used for enabling e2e testing code
