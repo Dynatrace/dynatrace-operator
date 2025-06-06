09123e705f0c222380a34cfd7190c412a31c2fee
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-06-06 10:25:41 +0200

    Bump bootstrapper version Update cmd/bootstrapper/cmd.go accordingly

--- cmd/bootstrapper/cmd_test.go
+++ cmd/bootstrapper/cmd_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Dynatrace/dynatrace-bootstrapper/cmd/configure"
 	"github.com/Dynatrace/dynatrace-bootstrapper/cmd/configure/attributes/container"
 	"github.com/Dynatrace/dynatrace-bootstrapper/cmd/configure/attributes/pod"
+	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/pmc"
 	"github.com/Dynatrace/dynatrace-bootstrapper/pkg/configure/oneagent/preload"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
@@ -100,27 +101,15 @@ func TestBootstrapArgs(t *testing.T) {
 }
 
 func TestBootstrapConfigurationStep(t *testing.T) {
-	t.Run("check if configuration files are created", func(t *testing.T) {
-		const (
-			targetPath = "/mnt/bin"
-			configPath = "/mnt/config"
-			inputPath  = "/mnt/input"
-		)
+	const (
+		targetPath = "/mnt/bin"
+		configPath = "/mnt/config"
+		inputPath  = "/mnt/input"
+	)
 
+	setupFS := func(t *testing.T) afero.Afero {
+		t.Helper()
 		fs := afero.Afero{Fs: afero.NewMemMapFs()}
-
-		cmd := newCmd(fs)
-		cmd.SetArgs([]string{
-			"bootstrap",
-			"--config-directory=" + configPath,
-			"--input-directory=" + inputPath,
-			"--attribute-container={\"registry\":\"registry\"}",
-			"--source=/opt/dynatrace/oneagent",
-			"--target=" + targetPath,
-			"--install-path=/opt/dynatrace/oneagent-paas",
-			"--technologies=java",
-			"--attribute=k8s.cluster.name=dynakube",
-		})
 
 		createFile(t, fs, filepath.Join(inputPath, "ruxitagentproc.json"), `
 {
@@ -135,10 +124,30 @@ func TestBootstrapConfigurationStep(t *testing.T) {
 }
 `)
 
-		createFile(t, fs, filepath.Join(targetPath, "agent/conf/_ruxitagentproc.conf"), "[general]\n")
+		createFile(t, fs, filepath.Join(targetPath, "agent/conf/ruxitagentproc.conf"), "[general]\n")
 
 		createFile(t, fs, filepath.Join(inputPath, "endpoint.properties"), `DT_METRICS_INGEST_URL=http://ingest-url
 	DT_METRICS_INGEST_API_TOKEN=apitoken`)
+
+		return fs
+	}
+
+	t.Run("only OA", func(t *testing.T) {
+		fs := setupFS(t)
+
+		cmd := newCmd(fs)
+		cmd.SetArgs([]string{
+			"bootstrap",
+			"--metadata-enrichment=false",
+			"--config-directory=" + configPath,
+			"--input-directory=" + inputPath,
+			"--attribute-container={\"k8s.container.name\":\"test-container\"}",
+			"--source=/opt/dynatrace/oneagent",
+			"--target=" + targetPath,
+			"--install-path=/opt/dynatrace/oneagent-paas",
+			"--technologies=java",
+			"--attribute=k8s.cluster.name=dynakube",
+		})
 
 		err := cmd.Execute()
 		require.NoError(t, err)
@@ -147,11 +156,79 @@ func TestBootstrapConfigurationStep(t *testing.T) {
 		require.NoError(t, err)
 		assert.True(t, exists)
 
-		exists, err = fs.Exists(filepath.Join(targetPath, "agent/conf/ruxitagentproc.conf"))
+		containerConfigPath := filepath.Join(configPath, "test-container")
+
+		exists, err = fs.Exists(pmc.GetDestinationRuxitAgentProcFilePath(containerConfigPath))
 		require.NoError(t, err)
 		assert.True(t, exists)
 
-		exists, err = fs.Exists(filepath.Join(configPath, "enrichment/endpoint/endpoint.properties"))
+		exists, err = fs.Exists(filepath.Join(containerConfigPath, "enrichment/endpoint/endpoint.properties"))
+		require.NoError(t, err)
+		assert.False(t, exists)
+	})
+
+	t.Run("only metadata", func(t *testing.T) {
+		fs := setupFS(t)
+
+		cmd := newCmd(fs)
+		cmd.SetArgs([]string{
+			"bootstrap",
+			"--metadata-enrichment",
+			"--config-directory=" + configPath,
+			"--input-directory=" + inputPath,
+			"--attribute-container={\"k8s.container.name\":\"test-container\"}",
+			"--attribute=k8s.cluster.name=dynakube",
+		})
+
+		err := cmd.Execute()
+		require.NoError(t, err)
+
+		exists, err := fs.Exists(filepath.Join(configPath, preload.ConfigPath))
+		require.NoError(t, err)
+		assert.False(t, exists)
+
+		containerConfigPath := filepath.Join(configPath, "test-container")
+
+		exists, err = fs.Exists(pmc.GetDestinationRuxitAgentProcFilePath(containerConfigPath))
+		require.NoError(t, err)
+		assert.False(t, exists)
+
+		exists, err = fs.Exists(filepath.Join(containerConfigPath, "enrichment/endpoint/endpoint.properties"))
+		require.NoError(t, err)
+		assert.True(t, exists)
+	})
+
+	t.Run("OA + metadata", func(t *testing.T) {
+		fs := setupFS(t)
+
+		cmd := newCmd(fs)
+		cmd.SetArgs([]string{
+			"bootstrap",
+			"--metadata-enrichment",
+			"--config-directory=" + configPath,
+			"--input-directory=" + inputPath,
+			"--attribute-container={\"k8s.container.name\":\"test-container\"}",
+			"--source=/opt/dynatrace/oneagent",
+			"--target=" + targetPath,
+			"--install-path=/opt/dynatrace/oneagent-paas",
+			"--technologies=java",
+			"--attribute=k8s.cluster.name=dynakube",
+		})
+
+		err := cmd.Execute()
+		require.NoError(t, err)
+
+		exists, err := fs.Exists(filepath.Join(configPath, preload.ConfigPath))
+		require.NoError(t, err)
+		assert.True(t, exists)
+
+		containerConfigPath := filepath.Join(configPath, "test-container")
+
+		exists, err = fs.Exists(pmc.GetDestinationRuxitAgentProcFilePath(containerConfigPath))
+		require.NoError(t, err)
+		assert.True(t, exists)
+
+		exists, err = fs.Exists(filepath.Join(containerConfigPath, "enrichment/endpoint/endpoint.properties"))
 		require.NoError(t, err)
 		assert.True(t, exists)
 	})
