31efe62da519ffbb69daf92ceead091af4624c77
Olivier Karasangabo <olivier.karasangabo@dynatrace.com>
2025-06-18 13:21:53 +0200

    Remove printer column & corresponding TODO

--- pkg/api/latest/dynakube/dynakube_status.go
+++ pkg/api/latest/dynakube/dynakube_status.go
@@ -42,8 +42,6 @@ type DynaKubeStatus struct { //nolint:revive
 	UpdatedTimestamp metav1.Time `json:"updatedTimestamp,omitempty"`
 
 	// Observed state of Dynatrace API
-	// TODO: Rename printer column to match Go variable name (currently kept as-is to ensure backwards compatibility)
-	// +kubebuilder:printcolumn:name="dynatraceApi",type=string,JSONPath=`.spec.status.dynatraceApi`
 	DynatraceAPI DynatraceAPIStatus `json:"dynatraceApi,omitempty"`
 
 	// Defines the current state (Running, Updating, Error, ...)
