a6d010fcea9e144893bb5d1b7e69aca47fb8fcb8
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-06-23 16:58:59 +0200

    Fail fast when creating/destroying e2e clusters

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-DEFAULT_TIMEOUT="60m" # K8s takes <10min, Openshift >40min
+DEFAULT_TIMEOUT="80m" # K8s takes <10min, Openshift >40min
 DESIRED_STATE="environment-deployed"
 
 kubectl version
@@ -12,7 +12,7 @@ echo "Creating environment '$FLC_ENVIRONMENT' in namespace '$FLC_NAMESPACE'"
 kubectl get flcenvironments --namespace "$FLC_NAMESPACE"
 
 echo "Patching environment '$FLC_ENVIRONMENT' to 'deployed'"
-kubectl patch --namespace "$FLC_NAMESPACE" --type merge --patch '{"spec": {"desiredState": "environment-deployed"}}' flcenvironment "$FLC_ENVIRONMENT"
+kubectl patch --namespace "$FLC_NAMESPACE" --type merge --patch "{\"spec\": {\"desiredState\": \"$DESIRED_STATE\"}}" flcenvironment "$FLC_ENVIRONMENT"
 
 echo "Waiting up to '$DEFAULT_TIMEOUT' for successful deployment of environment '$FLC_ENVIRONMENT'"
 kubectl wait --namespace "$FLC_NAMESPACE" --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment "$FLC_ENVIRONMENT"
--- .github/scripts/destroy-cluster.sh
+++ .github/scripts/destroy-cluster.sh
@@ -3,10 +3,30 @@
 set -x
 
 DEFAULT_TIMEOUT="20m"
+DESIRED_STATE="environment-not-deployed"
 
 echo "Destroying environment '$FLC_ENVIRONMENT' in namespace '$FLC_NAMESPACE'"
 
 kubectl get flcenvironments --namespace "$FLC_NAMESPACE"
 
 echo "Patching environment '$FLC_ENVIRONMENT' to 'not-deployed'"
-kubectl patch --namespace "$FLC_NAMESPACE" --type merge --patch '{"spec": {"desiredState": "environment-not-deployed"}}' flcenvironment "$FLC_ENVIRONMENT"
+kubectl patch --namespace "$FLC_NAMESPACE" --type merge --patch "{\"spec\": {\"desiredState\": \"$DESIRED_STATE\"}}" flcenvironment "$FLC_ENVIRONMENT"
+
+echo "Waiting up to '$DEFAULT_TIMEOUT' for successful destruction of environment '$FLC_ENVIRONMENT'"
+kubectl wait --namespace "$FLC_NAMESPACE" --timeout="$DEFAULT_TIMEOUT" --for=condition=InTransition=false flcenvironment "$FLC_ENVIRONMENT"
+
+if [[ "$FLC_ENVIRONMENT" == *"ocp"* ]]; then
+  echo "PLATFORM is set to 'ocp'. Waiting for 5 minutes..."
+  sleep 300  # Wait for 5 minutes (300 seconds)
+  echo "done"
+fi
+
+echo "Checking currentState='$DESIRED_STATE' for '$FLC_ENVIRONMENT'..."
+flc_state=$(kubectl get flcenvironment "$FLC_ENVIRONMENT" --namespace "$FLC_NAMESPACE" -ojsonpath="{.status.currentState}")
+if [[ "$flc_state" != "$DESIRED_STATE" ]]; then
+  echo "Pipeline destruction did not reach expected state '$DESIRED_STATE', currentState: ${flc_state}..."
+  exit 1
+  else
+    echo "successful..."
+fi
+echo "done"
