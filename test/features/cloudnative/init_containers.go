b35485b92654167add2c44fd0ccb327bf9218938
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-07-16 16:13:52 +0200

    Fix e2e log check

--- test/features/cloudnative/init_containers.go
+++ test/features/cloudnative/init_containers.go
@@ -63,7 +63,7 @@ func checkInitContainers(sampleApp *sample.App) features.Func {
 			}).Stream(ctx)
 
 			require.NoError(t, err)
-			logs.AssertContains(t, logStream, "finished oneagent configuration")
+			logs.AssertContains(t, logStream, "init completed")
 
 			ifNotEmptyCommand := shell.Shell(shell.CheckIfNotEmpty("/opt/dynatrace/oneagent-paas/log/php/"))
 			executionResult, err := pod.Exec(ctx, resources, podItem, sampleApp.ContainerName(), ifNotEmptyCommand...)
