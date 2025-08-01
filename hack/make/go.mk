e2701e0ec3d3c24d243f9604925746715904ae45
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-08-01 15:38:04 +0200

    Cleanup

--- hack/make/go.mk
+++ hack/make/go.mk
@@ -1,5 +1,3 @@
-LINT_TARGET ?= ./...
-
 ## Runs golangci-lint
 go/golangci:
 	golangci-lint run --build-tags "$(shell ./hack/build/create_go_build_tags.sh true)" --timeout 300s
@@ -30,7 +28,7 @@ go/gen_mocks: prerequisites/mockery
 ## Runs deadcode https://go.dev/blog/deadcode
 go/deadcode: prerequisites/go-deadcode
 	# we add `tee` in the end to make it fail if it finds dead code, by default deadcode always return exit code 0
-	deadcode -test -tags="$(shell ./hack/build/create_go_build_tags.sh true)" $(LINT_TARGET) | tee deadcode.out && [ ! -s deadcode.out ]
+	deadcode -test -tags="$(shell ./hack/build/create_go_build_tags.sh true)" ./... | tee deadcode.out && [ ! -s deadcode.out ]
 
 ## Runs go-test-coverage tool
 go/check-coverage: prerequisites/go-test-coverage
