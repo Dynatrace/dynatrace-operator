b5870b5d7a00ab1e927cb7729b07e86390792750
Olivier Karasangabo <olivier.karasangabo@dynatrace.com>
2025-08-21 17:03:08 +0200

    Remove git revision from nightly tag names
    The images should already be labeled accordingly

--- hack/build/ci/push-nightly-helm-chart.sh
+++ hack/build/ci/push-nightly-helm-chart.sh
@@ -20,6 +20,7 @@ output=$(oras push "${REGISTRY_URL}/${REPOSITORY_NAME}:${MUTABLE_NIGHTLY_TAG}" \
   --annotation "org.opencontainers.image.url=${url}" \
   --annotation "org.opencontainers.image.version=${IMAGE_VERSION}" \
   --annotation "quay.expires-after=14d" \
+  --annotation "vcs-ref=${GITHUB_SHA}" \
   --config "${HELM_CONFIG_FILE}:application/vnd.cncf.helm.config.v1+json" 2>&1)
 exit_status=$?
 
