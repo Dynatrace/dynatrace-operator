cb234a6d5a653046365bcb5e56ad3367ec8e1bb2
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2025-09-02 09:36:12 +0200

    small renames

--- pkg/util/hasher/hasher.go
+++ pkg/util/hasher/hasher.go
@@ -18,8 +18,8 @@ const AnnotationHash = api.InternalFlagPrefix + "template-hash"
 // GenerateHash creates a hash from the provided input.
 // This hash is meant to be used for simplifying detecting differences between 2 objects.
 // Uses uses FNV-1 hashing, should be used hashing not sensitive data.
-func GenerateHash(ds any) (string, error) {
-	data, err := json.Marshal(ds)
+func GenerateHash(input any) (string, error) {
+	data, err := json.Marshal(input)
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
@@ -37,8 +37,8 @@ func GenerateHash(ds any) (string, error) {
 // GenerateSecureHash creates a hash from the provided input.
 // This hash is meant to be used for simplifying detecting differences between 2 values.
 // Uses uses SHA256 hashing, can be used for hashing sensitive data.
-func GenerateSecureHash(ds any) (string, error) {
-	data, err := json.Marshal(ds)
+func GenerateSecureHash(input any) (string, error) {
+	data, err := json.Marshal(input)
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
