76d0a43c91406dc1544fd517222113d87a7b7007
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-09-10 10:34:32 +0200

    fixup!!

--- pkg/api/validation/dynakube/validation.go
+++ pkg/api/validation/dynakube/validation.go
@@ -3,7 +3,6 @@ package validation
 import (
 	"context"
 	"errors"
-
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
 	v1beta1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube" //nolint:staticcheck
 	v1beta2 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta2/dynakube" //nolint:staticcheck
@@ -163,38 +162,38 @@ func (v *Validator) runUpdateValidators(ctx context.Context, updateValidators []
 	return results
 }
 
-func getDynakube(obj runtime.Object) (dk *dynakube.DynaKube, err error) {
-	dk = &dynakube.DynaKube{}
+func getDynakube(obj runtime.Object) (*dynakube.DynaKube, error) {
+	dk := &dynakube.DynaKube{}
 
 	switch v := obj.(type) {
 	case *dynakube.DynaKube:
 		dk = v
 	case *v1beta5.DynaKube:
-		err = v.ConvertTo(dk)
+		err := v.ConvertTo(dk)
 		if err != nil {
 			return dk, err
 		}
 	case *v1beta4.DynaKube:
-		err = v.ConvertTo(dk)
+		err := v.ConvertTo(dk)
 		if err != nil {
 			return dk, err
 		}
 	case *v1beta3.DynaKube:
-		err = v.ConvertTo(dk)
+		err := v.ConvertTo(dk)
 		if err != nil {
 			return dk, err
 		}
 	case *v1beta2.DynaKube:
-		err = v.ConvertTo(dk)
+		err := v.ConvertTo(dk)
 		if err != nil {
 			return dk, err
 		}
 	case *v1beta1.DynaKube:
-		err = v.ConvertTo(dk)
+		err := v.ConvertTo(dk)
 		if err != nil {
 			return dk, err
 		}
 	}
 
-	return dk, err
+	return dk, nil
 }
