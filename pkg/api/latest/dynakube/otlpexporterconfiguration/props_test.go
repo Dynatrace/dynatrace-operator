4133dba13e5c3351af484dd68dd39ae75260c4bd
Florian Bacher <florian.bacher@dynatrace.com>
2025-09-16 14:33:02 +0200

    fix linting

--- pkg/api/latest/dynakube/otlpexporterconfiguration/props_test.go
+++ pkg/api/latest/dynakube/otlpexporterconfiguration/props_test.go
@@ -17,8 +17,9 @@ limitations under the License.
 package otlpexporterconfiguration
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSpec_IsOverrideEnabled(t *testing.T) {
