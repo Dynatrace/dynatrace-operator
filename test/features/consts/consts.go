0395be62114e39f4fca75bbebf961013fc8f655e
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-09-18 08:26:59 +0200

    Migrate to the public EEC container image (#5416)

--- test/features/consts/consts.go
+++ test/features/consts/consts.go
@@ -7,9 +7,8 @@ const (
 	AgSecretName                    = "ag-ca"
 	TelemetryIngestTLSSecretName    = "telemetry-ingest-tls"
 
-	DevRegistryPullSecretName = "devregistry"
-	EecImageRepo              = "478983378254.dkr.ecr.us-east-1.amazonaws.com/dynatrace/dynatrace-eec"
-	EecImageTag               = "1.303.0.20240930-183404"
-	LogMonitoringImageRepo    = "public.ecr.aws/dynatrace/dynatrace-logmodule"
-	LogMonitoringImageTag     = "1.309.59.20250319-140247"
+	EecImageRepo           = "public.ecr.aws/dynatrace/dynatrace-eec"
+	EecImageTag            = "1.319.26.20250711-102845"
+	LogMonitoringImageRepo = "public.ecr.aws/dynatrace/dynatrace-logmodule"
+	LogMonitoringImageTag  = "1.309.59.20250319-140247"
 )
