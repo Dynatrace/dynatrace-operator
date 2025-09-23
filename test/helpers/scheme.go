e205376d3a8311335687b073d498462e0c5f9421
Mario Valderrama <mario.valderrama@dynatrace.com>
2025-09-23 10:32:38 +0200

    Remove v1beta1 and v1beta2 API versions (#5418)

--- test/helpers/scheme.go
+++ test/helpers/scheme.go
@@ -11,10 +11,6 @@ import (
 	_ "github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha1/edgeconnect"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha2"
 	_ "github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha2/edgeconnect"
-	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1"
-	_ "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta1/dynakube" //nolint:staticcheck
-	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta2"
-	_ "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta2/dynakube" //nolint:staticcheck
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3"
 	_ "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4"
@@ -38,14 +34,6 @@ func SetScheme(ctx context.Context, envConfig *envconf.Config) (context.Context,
 		return ctx, err
 	}
 	err = v1beta3.AddToScheme(envConfig.Client().Resources().GetScheme())
-	if err != nil {
-		return ctx, err
-	}
-	err = v1beta2.AddToScheme(envConfig.Client().Resources().GetScheme())
-	if err != nil {
-		return ctx, err
-	}
-	err = v1beta1.AddToScheme(envConfig.Client().Resources().GetScheme())
 	if err != nil {
 		return ctx, err
 	}
