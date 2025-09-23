8f851580a38c4ca00d8175aec6aefa01e51f965e
Olivier Karasangabo <olivier.karasangabo@dynatrace.com>
2025-09-23 10:15:21 +0200

    Inline variable

--- pkg/api/latest/dynakube/metadata_enrichment_props.go
+++ pkg/api/latest/dynakube/metadata_enrichment_props.go
@@ -3,9 +3,7 @@ package dynakube
 import "github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/metadataenrichment"
 
 func (dk *DynaKube) MetadataEnrichment() *metadataenrichment.MetadataEnrichment {
-	m := &metadataenrichment.MetadataEnrichment{
+	return &metadataenrichment.MetadataEnrichment{
 		Spec: &dk.Spec.MetadataEnrichment,
 	}
-
-	return m
 }
