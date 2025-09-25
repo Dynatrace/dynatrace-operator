c5ecb786ebc00efc487f6bc0828113d80c2457f3
Florian Bacher <florian.bacher@dynatrace.com>
2025-09-25 14:47:02 +0200

    Add OTLP webhook handler  (#5434)

---
+++ pkg/webhook/mutation/pod/mutator/otlp/resourceattributes/mutator.go
@@ -0,0 +1,40 @@
+package resourceattributes
+
+import (
+	"github.com/Dynatrace/dynatrace-operator/pkg/logd"
+	dtwebhook "github.com/Dynatrace/dynatrace-operator/pkg/webhook/mutation/pod/mutator"
+)
+
+var (
+	log = logd.Get().WithName("otlp-resource-attributes-pod-mutation")
+)
+
+type Mutator struct{}
+
+func New() dtwebhook.Mutator {
+	return &Mutator{}
+}
+
+func (Mutator) IsEnabled(_ *dtwebhook.BaseRequest) bool {
+	log.Debug("checking if OTLP resource attributes injection is enabled")
+
+	return false
+}
+
+func (Mutator) IsInjected(_ *dtwebhook.BaseRequest) bool {
+	log.Debug("checking if OTLP resource attributes have already been injected")
+
+	return false
+}
+
+func (Mutator) Mutate(_ *dtwebhook.MutationRequest) error {
+	log.Debug("injecting OTLP resource attributes")
+
+	return nil
+}
+
+func (Mutator) Reinvoke(_ *dtwebhook.ReinvocationRequest) bool {
+	log.Debug("reinvocation of OTLP resource attribute mutator")
+
+	return false
+}
