6756e4adf9611a33ea55b484ab4996d312d77025
Florian Bacher <florian.bacher@dynatrace.com>
2025-09-30 09:58:32 +0200

    additional unit test cases

--- pkg/webhook/mutation/pod/mutator/otlp/exporter/mutator_test.go
+++ pkg/webhook/mutation/pod/mutator/otlp/exporter/mutator_test.go
@@ -3,6 +3,7 @@ package exporter
 import (
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/exp"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/activegate"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/otlpexporterconfiguration"
 	"github.com/Dynatrace/dynatrace-operator/pkg/webhook/mutation/pod/mutator"
 	"github.com/stretchr/testify/assert"
@@ -98,78 +99,6 @@ func TestMutator_IsEnabled(t *testing.T) {
 	})
 }
 
-func createTestMutationRequest(t *testing.T, dk *dynakube.DynaKube) *mutator.MutationRequest {
-	return mutator.NewMutationRequest(t.Context(), *getTestNamespace(), nil, getTestPod(), *dk)
-}
-
-func getTestNamespace() *corev1.Namespace {
-	return &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: testNamespaceName,
-			Labels: map[string]string{
-				mutator.InjectionInstanceLabel: testDynakubeName,
-			},
-		},
-	}
-}
-
-func getTestDynakube() *dynakube.DynaKube {
-	return &dynakube.DynaKube{
-		ObjectMeta: getTestDynakubeMeta(),
-		Spec: dynakube.DynaKubeSpec{
-			OTLPExporterConfiguration: &otlpexporterconfiguration.Spec{
-				Signals: otlpexporterconfiguration.SignalConfiguration{
-					Metrics: &otlpexporterconfiguration.MetricsSignal{},
-					Traces:  &otlpexporterconfiguration.TracesSignal{},
-					Logs:    &otlpexporterconfiguration.LogsSignal{},
-				},
-			},
-		},
-	}
-}
-
-func getTestDynakubeMeta() metav1.ObjectMeta {
-	return metav1.ObjectMeta{
-		Name:      testDynakubeName,
-		Namespace: testNamespaceName,
-		Annotations: map[string]string{
-			exp.OTLPExporterConfigurationKey: "true",
-		},
-	}
-}
-
-func getTestPod() *corev1.Pod {
-	return &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:        testPodName,
-			Namespace:   testNamespaceName,
-			Annotations: map[string]string{},
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:  "container",
-					Image: "alpine",
-				},
-			},
-			InitContainers: []corev1.Container{
-				{
-					Name:  "init-container",
-					Image: "alpine",
-				},
-			},
-			Volumes: []corev1.Volume{
-				{
-					Name: "volume",
-					VolumeSource: corev1.VolumeSource{
-						EmptyDir: &corev1.EmptyDirVolumeSource{},
-					},
-				},
-			},
-		},
-	}
-}
-
 func TestMutator_IsInjected(t *testing.T) {
 	t.Run("env vars for OTLP exporter already injected", func(t *testing.T) {
 		m := Mutator{}
@@ -190,6 +119,44 @@ func TestMutator_IsInjected(t *testing.T) {
 }
 
 func TestMutator_Mutate(t *testing.T) {
+	t.Run("no OTLP exporter configuration present on DynaKube - do not modify anything", func(t *testing.T) {
+		m := Mutator{}
+
+		dk := getTestDynakube()
+
+		dk.Spec.OTLPExporterConfiguration = nil
+
+		request := createTestMutationRequest(t, dk)
+
+		err := m.Mutate(request)
+
+		require.NoError(t, err)
+
+		containerEnvVars := request.Pod.Spec.Containers[0].Env
+
+		assert.Empty(t, containerEnvVars)
+	})
+	t.Run("no OTLP ingest endpoint available - do not modify anything, return error", func(t *testing.T) {
+		m := Mutator{}
+
+		dk := getTestDynakube()
+
+		dk.Spec.ActiveGate = activegate.Spec{
+			Capabilities: []activegate.CapabilityDisplayName{
+				activegate.MetricsIngestCapability.DisplayName,
+			},
+		}
+
+		request := createTestMutationRequest(t, dk)
+
+		err := m.Mutate(request)
+
+		require.Error(t, err)
+
+		containerEnvVars := request.Pod.Spec.Containers[0].Env
+
+		assert.Empty(t, containerEnvVars)
+	})
 	t.Run("no user defined env vars present, add OTLP exporter env vars", func(t *testing.T) {
 		m := Mutator{}
 
@@ -570,4 +537,101 @@ func TestMutator_Mutate(t *testing.T) {
 		assert.False(t, isEnvVarSet(containerEnvVars, OTLPLogsEndpointEnv))
 		assert.False(t, isEnvVarSet(containerEnvVars, OTLPLogsProtocolEnv))
 	})
+}
+
+func TestMutator_Reinvoke(t *testing.T) {
+	t.Run("return true if pod is modified", func(t *testing.T) {
+		m := Mutator{}
+
+		request := createTestMutationRequest(t, getTestDynakube())
+
+		mutated := m.Reinvoke(request.ToReinvocationRequest())
+
+		require.True(t, mutated)
+	})
+	t.Run("return false if pod is not modified", func(t *testing.T) {
+		m := Mutator{}
+
+		dk := getTestDynakube()
+
+		dk.Spec.OTLPExporterConfiguration = nil
+
+		request := createTestMutationRequest(t, dk)
+
+		mutated := m.Reinvoke(request.ToReinvocationRequest())
+
+		require.False(t, mutated)
+	})
+}
+
+func createTestMutationRequest(t *testing.T, dk *dynakube.DynaKube) *mutator.MutationRequest {
+	return mutator.NewMutationRequest(t.Context(), *getTestNamespace(), nil, getTestPod(), *dk)
+}
+
+func getTestNamespace() *corev1.Namespace {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testNamespaceName,
+			Labels: map[string]string{
+				mutator.InjectionInstanceLabel: testDynakubeName,
+			},
+		},
+	}
+}
+
+func getTestDynakube() *dynakube.DynaKube {
+	return &dynakube.DynaKube{
+		ObjectMeta: getTestDynakubeMeta(),
+		Spec: dynakube.DynaKubeSpec{
+			OTLPExporterConfiguration: &otlpexporterconfiguration.Spec{
+				Signals: otlpexporterconfiguration.SignalConfiguration{
+					Metrics: &otlpexporterconfiguration.MetricsSignal{},
+					Traces:  &otlpexporterconfiguration.TracesSignal{},
+					Logs:    &otlpexporterconfiguration.LogsSignal{},
+				},
+			},
+		},
+	}
+}
+
+func getTestDynakubeMeta() metav1.ObjectMeta {
+	return metav1.ObjectMeta{
+		Name:      testDynakubeName,
+		Namespace: testNamespaceName,
+		Annotations: map[string]string{
+			exp.OTLPExporterConfigurationKey: "true",
+		},
+	}
+}
+
+func getTestPod() *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        testPodName,
+			Namespace:   testNamespaceName,
+			Annotations: map[string]string{},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "container",
+					Image: "alpine",
+				},
+			},
+			InitContainers: []corev1.Container{
+				{
+					Name:  "init-container",
+					Image: "alpine",
+				},
+			},
+			Volumes: []corev1.Volume{
+				{
+					Name: "volume",
+					VolumeSource: corev1.VolumeSource{
+						EmptyDir: &corev1.EmptyDirVolumeSource{},
+					},
+				},
+			},
+		},
+	}
 }
