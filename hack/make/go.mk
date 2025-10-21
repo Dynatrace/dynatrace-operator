31a034697aeada484bdf01ff191863c33c4ca1d9
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-10-21 16:11:29 +0200

    Add prerequisite to integration tests

--- hack/make/go.mk
+++ hack/make/go.mk
@@ -18,7 +18,7 @@ go/coverage: go/test
 	go tool cover -html=./coverage.txt
 
 ## Runs go integration test
-go/integration_test:
+go/integration_test: prerequisites/setup-envtest
 	go test -ldflags="-X 'github.com/Dynatrace/dynatrace-operator/pkg/version.Commit=$(shell git rev-parse HEAD)' -X 'github.com/Dynatrace/dynatrace-operator/pkg/version.Version=$(shell git branch --show-current)'" ./test/integration/*
 
 ## creates mocks from .mockery.yaml
