806304db5f8f5e17c307ee3b7e1f41ff699272f7
Gabriel Krenn <gabriel.krenn@gmail.com>
2025-10-30 14:36:40 +0100

    Add make command

--- hack/make/deploy/deploy.mk
+++ hack/make/deploy/deploy.mk
@@ -33,6 +33,13 @@ undeploy:
 	helm uninstall dynatrace-operator \
 			--namespace dynatrace
 
+## Remove all Dynatrace Operator resources from the cluster and node filesystem
+cleanup: cleanup/cluster cleanup/node-fs
+
 ## Remove all Dynatrace Operator resources from the cluster
-cleanup:
+cleanup/cluster: 
 	@./hack/cluster/cleanup-dynatrace-objects.sh
+
+## Remove node filesystem leftovers
+cleanup/node-fs:
+	@./hack/cluster/cleanup-node-fs.sh
