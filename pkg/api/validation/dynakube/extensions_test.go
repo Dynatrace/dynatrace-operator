1e6ee1a997f26d4a2c1e11cc8c0c635803f65dd1
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-11-03 15:03:27 +0100

    add test prometheus without an image for otelc

--- pkg/api/validation/dynakube/extensions_test.go
+++ pkg/api/validation/dynakube/extensions_test.go
@@ -64,6 +64,20 @@ func TestExtensionsWithoutK8SMonitoring(t *testing.T) {
 	)
 }
 
+
+func TestExtensionsWithoutOtelcImage(t *testing.T) {
+	t.Run("error when image is not specified", func(t *testing.T) {
+		assertDenied(t,  []string{errorOtelCollectorMissingImage},
+			&dynakube.DynaKube{
+				ObjectMeta: defaultDynakubeObjectMeta,
+				Spec: dynakube.DynaKubeSpec{
+					APIURL:          testAPIURL,
+					Extensions: &extensions.Spec{Prometheus: &extensions.PrometheusSpec{}},
+				},
+			})
+	})
+}
+
 func createStandaloneExtensionsDynakube(name, apiURL string) *dynakube.DynaKube {
 	dk := &dynakube.DynaKube{
 		ObjectMeta: metav1.ObjectMeta{
