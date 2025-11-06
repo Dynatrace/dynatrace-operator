12b508218f656099cd872e9b7c01de914632f1f7
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-11-06 14:12:56 +0100

    make it work for bash/make

--- hack/build/ci/third-party-licenses.sh
+++ hack/build/ci/third-party-licenses.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #renovate depName=github.com/google/go-licenses
-GOLANG_LICENSES_VERSION ?= v2.0.1
+GOLANG_LICENSES_VERSION=v2.0.1
 
 # get licenses if no cache exists
 if ! [ -d ./third_party_licenses ]; then
-  go install github.com/google/go-licenses/v2@${GOLANG_LICENSES_VERSION} && go-licenses save ./... --save_path third_party_licenses --force
+  go install github.com/google/go-licenses/v2@"$GOLANG_LICENSES_VERSION" && go-licenses save ./... --save_path third_party_licenses --force
 fi
