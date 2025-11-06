55c3658019f4a242ac4ac3d2ff8f3c58394587ad
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-11-06 11:12:29 +0100

    fixup!

--- hack/make/prerequisites.mk
+++ hack/make/prerequisites.mk
@@ -9,29 +9,29 @@ GOBIN ?= $(LOCALBIN)
 LOCALBIN_NPM ?= $(shell pwd)/node_modules/.bin
 
 #renovate depName=sigs.k8s.io/kustomize/kustomize/v5
-KUSTOMIZE_VERSION ?= v5.7.1
+kustomize_version=v5.7.1
 #renovate depName=sigs.k8s.io/controller-tools/cmd
-CONTROLLER_GEN_VERSION ?= v0.19.0
+controller_gen_version=v0.19.0
 # renovate depName=github.com/golangci/golangci-lint/v2
-GOLANGCI_LINT_VERSION ?= v2.5.0
+golangci_lint_version=v2.5.0
 # renovate depName=golang.org/x/tools
-GOLANG_TOOLS_VERSION ?= v0.38.0
+golang_tools_version=v0.38.0
 # renovate depName=github.com/vektra/mockery
-MOCKERY_VERSION ?= v3.5.5
+mockery_version=v3.5.5
 # renovate depName=github.com/igorshubovych/markdownlint-cli
-MARKDOWNLINT_CLI_VERSION ?= v0.45.0
+markdownlint_cli_version=v0.45.0
 # renovate depName=github.com/helm-unittest/helm-unittest
-HELMUNITTEST_VERSION ?= v1.0.3
+helm_unittest_version=v1.0.3
 # renovate depName=github.com/CycloneDX/cyclonedx-gomod/cmd/cyclonedx-gomod
-CYCLONEDX_GOMOD_VERSION ?= v1.9.0
+cyclonedx_gomod_version=v1.9.0
 # renovate depName=github.com/mikefarah/yq/v4
-YQ_VERSION ?= v4.48.1
+yq_version=v4.48.1
 # renovate depName=github.com/vladopajic/go-test-coverage/v2
-GO_TEST_COVERAGE_VERSION ?= v2.17.0
+Go_test_coverage_version=v2.17.0
 # renovate depName=github.com/dkorunic/betteralign/cmd/betteralign
-BETTERALIGN_VERSION ?= v0.7.3
+betteralign_version=v0.7.3
 #SETUP_ENVTEST_VERSION is the version of controller-runtime release branch to fetch the envtest setup script (i.e. release-0.20)
-SETUP_ENVTEST_VERSION ?= $(shell go list -m -f "{{ .Version }}" sigs.k8s.io/controller-runtime | awk -F'[v.]' '{printf "release-%d.%d", $$2, $$3}')
+setup_envtest_version=$(shell go list -m -f "{{ .Version }}" sigs.k8s.io/controller-runtime | awk -F'[v.]' '{printf "release-%d.%d", $$2, $$3}')
 
 ## Tool Binaries
 KUSTOMIZE ?= $(LOCALBIN)/kustomize
@@ -56,59 +56,59 @@ prerequisites/setup-go-dev-dependencies: prerequisites/kustomize prerequisites/c
 
 ## Install 'controller-gen' if it is missing
 prerequisites/controller-gen:
-	$(call go-install-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen,$(CONTROLLER_GEN_VERSION))
+	$(call go-install-tool,$(CONTROLLER_GEN),sigs.k8s.io/controller-tools/cmd/controller-gen,$(controller_gen_version))
 
 ## Install go linters
 prerequisites/go-linting: prerequisites/go-deadcode
-	$(call go-install-tool,$(GOLANGCI_LINT),github.com/golangci/golangci-lint/v2/cmd/golangci-lint,$(GOLANGCI_LINT_VERSION))
+	$(call go-install-tool,$(GOLANGCI_LINT),github.com/golangci/golangci-lint/v2/cmd/golangci-lint,$(golangci_lint_version))
 
 ## Install go deadcode
 prerequisites/go-deadcode:
-	$(call go-install-tool,$(DEADCODE),golang.org/x/tools/cmd/deadcode,$(GOLANG_TOOLS_VERSION))
+	$(call go-install-tool,$(DEADCODE),golang.org/x/tools/cmd/deadcode,$(golang_tools_version))
 
 ## Install go test coverage
 prerequisites/go-test-coverage:
-	$(call go-install-tool,$(GO_TEST_COVERAGE),github.com/vladopajic/go-test-coverage/v2,$(GO_TEST_COVERAGE_VERSION))
+	$(call go-install-tool,$(GO_TEST_COVERAGE),github.com/vladopajic/go-test-coverage/v2,$(go_test_coverage_version))
 
 ## Installs 'kustomize' if it is missing
 prerequisites/kustomize:
-	$(call go-install-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v5,$(KUSTOMIZE_VERSION))
+	$(call go-install-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v5,$(kustomize_version))
 
 ## Install verktra/mockery
 prerequisites/mockery:
-	$(call go-install-tool,$(MOCKERY),github.com/vektra/mockery/v3,$(MOCKERY_VERSION))
+	$(call go-install-tool,$(MOCKERY),github.com/vektra/mockery/v3,$(mockery_version))
 
 ## Install 'cyclonedx-gomod' if it is missing
 prerequisites/cyclonedx-gomod:
-	$(call go-install-tool,$(CYCLONEDX_GOMOD),github.com/CycloneDX/cyclonedx-gomod/cmd/cyclonedx-gomod,$(CYCLONEDX_GOMOD_VERSION))
+	$(call go-install-tool,$(CYCLONEDX_GOMOD),github.com/CycloneDX/cyclonedx-gomod/cmd/cyclonedx-gomod,$(cyclonedx_gomod_version))
 
 ## Install 'yq' if it is missing
 prerequisites/yq:
-	$(call go-install-tool,$(YQ),github.com/mikefarah/yq/v4,$(YQ_VERSION))
+	$(call go-install-tool,$(YQ),github.com/mikefarah/yq/v4,$(yq_version))
 
 ## Install setup-envtest locally
 prerequisites/envtest:
-	$(call go-install-tool,$(SETUP_ENVTEST),sigs.k8s.io/controller-runtime/tools/setup-envtest,$(SETUP_ENVTEST_VERSION))
+	$(call go-install-tool,$(SETUP_ENVTEST),sigs.k8s.io/controller-runtime/tools/setup-envtest,$(setup_envtest_version))
 
 ## Setup envtest binaries for the specified Kubernetes version
 prerequisites/setup-envtest: prerequisites/envtest
-	@echo "Setting up envtest binaries for Kubernetes version $(ENVTEST_K8S_VERSION)..."
-	@$(SETUP_ENVTEST) use $(ENVTEST_K8S_VERSION) --bin-dir $(LOCALBIN) -p path || { \
-		echo "Error: Failed to set up envtest binaries for version $(ENVTEST_K8S_VERSION)."; \
+	@echo "Setting up envtest binaries for Kubernetes version $(envtest_k8s_version)..."
+	@$(SETUP_ENVTEST) use $(envtest_k8s_version) --bin-dir $(LOCALBIN) -p path || { \
+		echo "Error: Failed to set up envtest binaries for version $(envtest_k8s_version)."; \
 		exit 1; \
 	}
 	@echo
-	@$(SETUP_ENVTEST) cleanup "<$(ENVTEST_K8S_VERSION)" --bin-dir $(LOCALBIN)
+	@$(SETUP_ENVTEST) cleanup "<$(envtest_k8s_version)" --bin-dir $(LOCALBIN)
 	@echo "Setup of envtest binaries completed."
 
 ## Install 'helm' if it is missing
 ## TODO: Have version accessible by renovate?
 prerequisites/helm-unittest:
-	hack/helm/install-unittest-plugin.sh $(HELMUNITTEST_VERSION)
+	hack/helm/install-unittest-plugin.sh $(helm_unittest_version)
 
 ## Install 'markdownlint' if it is missing
 prerequisites/markdownlint:
-	npm install --force markdownlint-cli@$(MARKDOWNLINT_CLI_VERSION)
+	npm install --force markdownlint-cli@$(markdownlint_cli_version)
 
 ## Install python dependencies
 prerequisites/python:
