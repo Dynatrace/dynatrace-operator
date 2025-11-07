a5ca6deabad5cebfa56d4fa50c3ecca1267fd86d
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-11-07 17:03:45 +0100

    fixup after upgrade golang-ci-lint to 2.6.1 (#5631)

--- pkg/webhook/validation/message.go
+++ pkg/webhook/validation/message.go
@@ -1,12 +1,17 @@
 package validation
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func SumErrors(validationErrors []string, customResourceType string) string {
-	summedErrors := fmt.Sprintf("\n%d error(s) found in the %s", len(validationErrors), customResourceType)
+	var summedErrors strings.Builder
+	summedErrors.WriteString(fmt.Sprintf("\n%d error(s) found in the %s", len(validationErrors), customResourceType))
+
 	for i, errMsg := range validationErrors {
-		summedErrors += fmt.Sprintf("\n %d. %s", i+1, errMsg)
+		summedErrors.WriteString(fmt.Sprintf("\n %d. %s", i+1, errMsg))
 	}
 
-	return summedErrors
+	return summedErrors.String()
 }
