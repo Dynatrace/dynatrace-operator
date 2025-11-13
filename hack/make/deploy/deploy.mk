550b232b6380acf178e566efaa682e376439f8fe
Gabriel Krenn <gabriel.krenn@gmail.com>
2025-11-13 16:14:20 +0100

    Move cleanup into own file

--- hack/make/deploy/deploy.mk
+++ hack/make/deploy/deploy.mk
@@ -32,14 +32,3 @@ undeploy:
 
 	helm uninstall dynatrace-operator \
 			--namespace dynatrace
-
-## Remove all Dynatrace Operator resources from the cluster and node filesystem
-cleanup: cleanup/cluster cleanup/node-fs
-
-## Remove all Dynatrace Operator resources from the cluster
-cleanup/cluster: 
-	@./hack/cluster/cleanup-dynatrace-objects.sh
-
-## Remove node filesystem leftovers
-cleanup/node-fs:
-	@SKIP_RUNNING_PODS_WARNING=true ./hack/cluster/cleanup-node-fs.sh dynatrace
