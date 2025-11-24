2d8b234237223641ef510d7952f15ffe2711ac51
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-11-24 12:55:39 +0100

    final test

---
+++ hack/slack/README.md
@@ -0,0 +1,11 @@
+## How to use
+
+
+```
+source local/.venv/bin/activate
+```
+
+
+```
+python3 hack/slack/update_e2e_ondemand.py > hack/slack/slack-e2e-ondemand-payload.json
+```
--- hack/slack/update_e2e_ondemand.py
+++ hack/slack/update_e2e_ondemand.py
@@ -1,25 +1,35 @@
 import json
+from ruamel.yaml import YAML
+
+ONDEMAND_FILE = "./.github/workflows/e2e-tests-ondemand.yaml"
 
 
-supported_k8s = [
-    "k8s-1.28",
-    "k8s-1.29",
-    "k8s-1.30",
-    "k8s-1.31",
-    "k8s-1.32",
-    "k8s-1.33",
-    "k8s-1.34",
-]
+yaml = YAML()
+yaml.width = 4096
 
-supported_ocps = [
-    "ocp-4.12",
-    "ocp-4.13",
-    "ocp-4.14",
-    "ocp-4.15",
-    "ocp-4.16",
-    "ocp-4.17",
-    "ocp-4.18",
-]
+# read ondemand_file file to dict and update
+with open(ONDEMAND_FILE, "r") as f:
+    data = yaml.load(f)
+
+# extract matrix include to dynamically generate slack message table rows
+matrix = data["jobs"]["run-matrix"]["strategy"]["matrix"]["include"]
+
+
+supported_k8s = sorted(
+    [
+        f"{item['platform']}_{item['version']}"
+        for item in matrix
+        if item["platform"] == "k8s"
+    ]
+)
+supported_ocps = sorted(
+    [
+        f"{item['platform']}_{item['version']}"
+        for item in matrix
+        if item["platform"] == "ocp"
+    ]
+)
+
 
 table_header = [
     {
@@ -64,84 +74,103 @@ table_header = [
     },
 ]
 
+title_section = {
+    "type": "section",
+    "text": {
+        "type": "mrkdwn",
+        "text": "E2E ondemand test results for `${{ github.sha }}` ",
+    },
+}
+
+footer_section = {
+    "type": "rich_text",
+    "elements": [
+        {
+            "type": "rich_text_section",
+            "elements": [
+                {
+                    "type": "link",
+                    "text": "detailed logs of run #${{ github.runNumber }}",
+                    "url": "https://github.com/${{ github.payload.repository.full_name }}/actions/runs/${{ github.runId }}",
+                }
+            ],
+        }
+    ],
+}
+
+base = {"blocks": []}
+
+table_section = {"type": "table", "rows": []}
 
 rows = [table_header]
 
-with open("./hack/slack/slack-e2e-ondemand-payload.json") as f:
-    payload = json.load(f)
 
+for k8s_env, ocp_env in zip(supported_k8s, supported_ocps):
+    k8s_env_clean = k8s_env.replace("-", "_").replace(".", "_").upper()
+    ocp_env_clean = ocp_env.replace("-", "_").replace(".", "_").upper()
+    row = [
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [{"type": "text", "text": k8s_env}],
+                }
+            ],
+        },
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [
+                        {
+                            "type": "link",
+                            "url": "${{{{ env.{}_RUN_ID_URL }}}}".format(k8s_env_clean),
+                            "text": "tests ",
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "${{{{ env.{}_EMOJI }}}}".format(k8s_env_clean),
+                        },
+                    ],
+                }
+            ],
+        },
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [{"type": "text", "text": ocp_env}],
+                }
+            ],
+        },
+        {
+            "type": "rich_text",
+            "elements": [
+                {
+                    "type": "rich_text_section",
+                    "elements": [
+                        {
+                            "type": "link",
+                            "url": "${{{{ env.{}_RUN_ID_URL }}}}".format(ocp_env_clean),
+                            "text": "tests ",
+                        },
+                        {
+                            "type": "emoji",
+                            "name": "${{{{ env.{}_EMOJI }}}}".format(ocp_env_clean),
+                        },
+                    ],
+                }
+            ],
+        },
+    ]
 
-with open("./hack/slack/slack-e2e-ondemand-payload.json", "w") as f1:
-    for k8s_env, ocp_env in zip(supported_k8s, supported_ocps):
-        k8s_env_clean = k8s_env.replace("-", "_").replace(".", "_").upper()
-        ocp_env_clean = ocp_env.replace("-", "_").replace(".", "_").upper()
-        row = [
-            {
-                "type": "rich_text",
-                "elements": [
-                    {
-                        "type": "rich_text_section",
-                        "elements": [{"type": "text", "text": k8s_env}],
-                    }
-                ],
-            },
-            {
-                "type": "rich_text",
-                "elements": [
-                    {
-                        "type": "rich_text_section",
-                        "elements": [
-                            {
-                                "type": "link",
-                                "url": "${{{{ env.{}_RUN_ID_URL }}}}".format(
-                                    k8s_env_clean
-                                ),
-                                "text": "tests ",
-                            },
-                            {
-                                "type": "emoji",
-                                "name": "${{{{ env.{}_EMOJI }}}}".format(k8s_env_clean),
-                            },
-                        ],
-                    }
-                ],
-            },
-            {
-                "type": "rich_text",
-                "elements": [
-                    {
-                        "type": "rich_text_section",
-                        "elements": [{"type": "text", "text": ocp_env}],
-                    }
-                ],
-            },
-            {
-                "type": "rich_text",
-                "elements": [
-                    {
-                        "type": "rich_text_section",
-                        "elements": [
-                            {
-                                "type": "link",
-                                "url": "${{{{ env.{}_RUN_ID_URL }}}}".format(
-                                    ocp_env_clean
-                                ),
-                                "text": "tests ",
-                            },
-                            {
-                                "type": "emoji",
-                                "name": "${{{{ env.{}_EMOJI }}}}".format(ocp_env_clean),
-                            },
-                        ],
-                    }
-                ],
-            },
-        ]
+    rows.append(row)
 
-        rows.append(row)
+table_section["rows"] = rows
 
-    payload["blocks"][1] = {
-        "type": "table",
-        "rows": rows,
-    }
-    json.dump(payload, f1, indent=2)
+base["blocks"].extend([title_section, table_section, footer_section])
+
+print(json.dumps(base, indent=2))
