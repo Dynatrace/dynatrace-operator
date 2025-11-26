4d91d2f8b89ca244380b865b066ad37041e103a1
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-11-26 10:58:06 +0100

    Add dynakube status integration test

--- pkg/api/latest/dynakube/dynakube_status_test.go
+++ pkg/api/latest/dynakube/dynakube_status_test.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/oneagent"
-	"github.com/Dynatrace/dynatrace-operator/pkg/util/conditions"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/integrationtests"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -15,6 +14,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+const testDynakubeName = "dynatrace"
 const testNamespace = "dynatrace"
 
 func TestStatus(t *testing.T) {
@@ -29,7 +29,7 @@ func TestStatus(t *testing.T) {
 	t.Run("dynakube status conditions are unique", func(t *testing.T) {
 		dk := &dynakube.DynaKube{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "dynakube",
+				Name:      testDynakubeName,
 				Namespace: testNamespace,
 				Annotations: map[string]string{},
 			},
@@ -40,9 +40,18 @@ func TestStatus(t *testing.T) {
 			},
 			Status: dynakube.DynaKubeStatus{},
 		}
+
+		testCondition := metav1.Condition{
+		Type:    "dummy-type",
+		Status:  metav1.ConditionTrue,
+		Reason:  "dummy-reason",
+		Message: "dummy-message",
+	}
+
+		*dk.Conditions() = append(*dk.Conditions(), testCondition)
+		*dk.Conditions() = append(*dk.Conditions(), testCondition)
 		
-		conditions.SetDaemonSetCreated(dk.Conditions(), "dummy-type", "dummy-daemonset-name")
-		conditions.SetDaemonSetCreated(dk.Conditions(), "dummy-type", "dummy-daemonset-name")
+		assert.Len(t, *dk.Conditions(), 2)
 
 		createDynaKube(t, clt, dk)
 
