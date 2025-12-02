3dca09026fb5b517da04c3ddeab6f1c8f0078be1
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-12-02 18:55:15 +0100

    fixup: undo refactoring

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -22,10 +22,40 @@ export HELM_CHART
 
 echo "Preparing test tenant secrets..."
 
-pwd
+mkdir -p test/testdata/secrets/
 
-# should be the same as we run inside GA
-ref/.github/scripts/prepare-e2e-secrets.sh
+pushd test/testdata/secrets/
+
+cat << EOF > single-tenant.yaml
+tenantUid: $TENANT1_NAME
+apiUrl: https://$TENANT1_NAME.dev.dynatracelabs.com/api
+apiToken: $TENANT1_APITOKEN
+apiTokenNoSettings: $TENANT1_APITOKEN_NOSETTINGS
+dataIngestToken: $TENANT1_DATAINGESTTOKEN
+EOF
+
+cat << EOF > multi-tenant.yaml
+tenants:
+  - tenantUid: $TENANT1_NAME
+    apiUrl: https://$TENANT1_NAME.dev.dynatracelabs.com/api
+    apiToken: $TENANT1_APITOKEN
+    dataIngestToken: $TENANT1_DATAINGESTTOKEN
+  - tenantUid: $TENANT2_NAME
+    apiUrl: https://$TENANT2_NAME.dev.dynatracelabs.com/api
+    apiToken: $TENANT2_APITOKEN
+    dataIngestToken: $TENANT2_DATAINGESTTOKEN
+EOF
+
+cat << EOF > edgeconnect-tenant.yaml
+name: e2e-test
+tenantUid: $TENANT1_NAME
+apiServer: $TENANT1_NAME.dev.apps.dynatracelabs.com
+oAuthClientId: $TENANT1_OAUTH_CLIENT_ID
+oAuthClientSecret: $TENANT1_OAUTH_SECRET
+resource: $TENANT1_OAUTH_URN
+EOF
+
+popd
 
 echo "Running tests for environment '$FLC_ENVIRONMENT'..."
 
