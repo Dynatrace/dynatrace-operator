7d3aa94b837e2d30fc29b9415d7a7e105e498287
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-12-03 16:01:37 +0100

    Add APP_VERSION env var to Helm chart

--- pkg/util/kubernetes/fields/k8scrd/crd.go
+++ pkg/util/kubernetes/fields/k8scrd/crd.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"os"
 
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8senv"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8slabel"
 	"github.com/pkg/errors"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -13,18 +15,15 @@ import (
 const (
 	DynaKubeName     = "dynakubes.dynatrace.com"
 	EdgeConnectName  = "edgeconnect.dynatrace.com"
-
-	appVersionEnv      = "APP_VERSION"
-	appVersionLabelKey = "app.kubernetes.io/version"
 )
 
 // CheckVersion checks if the CRD version matches the application version and logs an error if they do not match.
 func CheckVersion(ctx context.Context, apiReader client.Reader, crdName string) (error) {
 	crd := apiextensionsv1.CustomResourceDefinition{}
 
-	appVersion := os.Getenv(appVersionEnv)
+	appVersion := os.Getenv(k8senv.AppVersion)
 	if len(appVersion) == 0 {
-		return errors.Errorf("%s env missing, can't check for CRD version", appVersionEnv)
+		return errors.Errorf("%s env missing, can't check for CRD version", k8senv.AppVersion)
 	}
 	
 	err := apiReader.Get(ctx, types.NamespacedName{Name: crdName}, &crd)
@@ -36,9 +35,9 @@ func CheckVersion(ctx context.Context, apiReader client.Reader, crdName string) 
 		return errors.Errorf("no labels on %s, mismatch found", crdName)
 	}
 
-	crdVersion, ok := crd.Labels[appVersionLabelKey]
+	crdVersion, ok := crd.Labels[k8slabel.AppVersionLabel]
 	if !ok {
-		return errors.Errorf("missing version label '%s' on %s, mismatch found", appVersionLabelKey, crdName)
+		return errors.Errorf("missing version label '%s' on %s, mismatch found", k8slabel.AppVersionLabel, crdName)
 	}
 
 	if appVersion != crdVersion {
