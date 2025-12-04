651ecfbe8d48745ddefb4864db76a14620ccc9ef
Alberto Gil de Dios <alberto.gildedios@dynatrace.com>
2025-12-04 12:45:44 +0100

    Rename func

--- pkg/util/kubernetes/fields/k8scrd/crd_test.go
+++ pkg/util/kubernetes/fields/k8scrd/crd_test.go
@@ -11,7 +11,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestCheckVersion(t *testing.T) {
+func TestIsLatestVersion(t *testing.T) {
 	testDynaKubeCRD := getTestCRD(DynaKubeName)
 	testEdgeConnectCRD := getTestCRD(EdgeConnectName)
 
