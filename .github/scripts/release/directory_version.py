80835626c68670cc558effcb830347655c07de08
Albian Krasniqi <krasniqi_albian@hotmail.com>
2025-12-05 14:37:38 +0100

    Remove set_property.py as it's not needed
    Instead use pre-installed yq; also remove unneeded code

--- .github/scripts/release/directory_version.py
+++ .github/scripts/release/directory_version.py
@@ -1,7 +1,71 @@
 #!/usr/bin/env python3
 import argparse
 
-from version.collection import get_latest_version_from_collection
+
+def _compare_normalized_version_arrays(a, b):
+    a_is_parseable = True
+    b_is_parseable = True
+    for i in range(0, 3):
+        version_num_a = 0
+        version_num_b = 0
+
+        try:
+            version_num_a = int(a[i])
+        except ValueError:
+            a_is_parseable = False
+        try:
+            version_num_b = int(b[i])
+        except ValueError:
+            b_is_parseable = False
+
+        if not a_is_parseable and not b_is_parseable:
+            return 0
+        elif not a_is_parseable:
+            return -1
+        elif not b_is_parseable:
+            return 1
+
+        if version_num_a > version_num_b:
+            return 1
+        elif version_num_b > version_num_a:
+            return -1
+    return 0
+
+
+def normalize_version(version):
+    version_array = _split_semantic_version(version)
+    version_array = _normalize_version_array(version_array)
+    return ".".join(version_array)
+
+
+def _split_semantic_version(version):
+    return version.split(".")
+
+
+def _normalize_version_array(version_array):
+    while len(version_array) < 3:
+        version_array.append("0")
+    return version_array[0:3]
+
+
+def get_latest_version_from_collection(version_collection):
+    if len(version_collection) <= 0:
+        return ""
+
+    if len(version_collection) == 1:
+        return version_collection[0]
+
+    latest_version = ""
+    for version in version_collection:
+        latest = _normalize_version_array(_split_semantic_version(latest_version))
+        current = _normalize_version_array(_split_semantic_version(version))
+        comparison = _compare_normalized_version_arrays(latest, current)
+
+        if comparison < 0:
+            latest_version = version
+
+    return normalize_version(latest_version)
+
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser(
