3684bf91fd6096b6485230ccde2a798f4029528e
Alberto Gil de Dios <61692514+albertogdd@users.noreply.github.com>
2025-12-16 16:35:50 +0100

    Send event on CRD version mismatch (#5876)
    Co-authored-by: Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>

---
+++ pkg/util/kubernetes/objects/k8sevent/event.go
@@ -0,0 +1,16 @@
+package k8sevent
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/record"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	crdVersionMismatchReason  = "CRDVersionMismatch"
+	crdVersionMismatchMessage = "The CustomResourceDefinition doesn't match version with the operator. Please update the CRD to avoid potential issues."
+)
+
+func SendCRDVersionMismatch(eventRecorder record.EventRecorder, object client.Object) {
+	eventRecorder.Event(object, corev1.EventTypeWarning, crdVersionMismatchReason, crdVersionMismatchMessage)
+}
