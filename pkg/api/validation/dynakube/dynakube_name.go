b5a47659e89c2ef1e9c9ab81be027e6683cbf258
Mario Valderrama <mario.valderrama@dynatrace.com>
2025-12-19 12:25:12 +0100

    Limit DynaKube names to 32 characters (#5891)

--- pkg/api/validation/dynakube/dynakube_name.go
+++ pkg/api/validation/dynakube/dynakube_name.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
-	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/extensions"
 	"k8s.io/apimachinery/pkg/util/validation"
 )
 
@@ -16,9 +15,6 @@ const (
 
 	errorNameTooLong = `The length limit for the name of a DynaKube is %d, because it is the base for the name of resources related to the DynaKube. (example: dkName-activegate-<some-hash>).
     The limit is necessary because kubernetes uses the name of some resources (example: StatefulSet) for the label value, which has a limit of 63 characters. (see https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set)`
-
-	sqlExecutorTooLongSuffix = `.
-    When using SQL extension executors, the Deployment name format requires the DynaKube name to be shorter than usual.`
 )
 
 func nameInvalid(_ context.Context, _ *Validator, dk *dynakube.DynaKube) string {
@@ -48,35 +44,9 @@ func nameTooLong(dk *dynakube.DynaKube) string {
 	nameLen := len(dk.Name)
 	maxLength := dynakube.MaxNameLength
 
-	var suffix string
-
-	if dk.Extensions().IsDatabasesEnabled() {
-		// Extensions make use Deployments which have different name length requirements than StatefulSet and DaemonSets.
-		maxLength = maxNameLengthForSQLExecutor(dk)
-		if maxLength < dynakube.MaxNameLength {
-			suffix = sqlExecutorTooLongSuffix
-		}
-	}
-
 	if nameLen > maxLength {
-		return fmt.Sprintf(errorNameTooLong, maxLength) + suffix
+		return fmt.Sprintf(errorNameTooLong, maxLength)
 	}
 
 	return ""
-}
-
-func maxNameLengthForSQLExecutor(dk *dynakube.DynaKube) int {
-	// Pod names are limited to 63 characters and Kubernetes will cut off characters from the owner resource name to ensure that they can be deployed.
-	// Max length for a Deployment to ensure that nothing gets cut off is: 57 (+5 for random suffix +1 for separating hyphen)
-	const (
-		maxDeploymentNameLength = 57
-		infixLen                = len(extensions.SQLExecutorInfix)
-	)
-
-	var maxIDLen int
-	for _, db := range dk.Extensions().Databases {
-		maxIDLen = max(maxIDLen, len(db.ID))
-	}
-
-	return min(dynakube.MaxNameLength, maxDeploymentNameLength-infixLen-maxIDLen)
 }
