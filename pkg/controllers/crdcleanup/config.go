b5f7767080e0d5eeec39ad664ca355b9a4bad592
Gabriel Krenn <gabriel.krenn@gmail.com>
2026-01-09 13:35:41 +0100

    Add helm tests

--- pkg/controllers/crdcleanup/config.go
+++ pkg/controllers/crdcleanup/config.go
@@ -1,14 +1,9 @@
 package crdcleanup
 
 import (
-	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 var (
 	log = ctrl.Log.WithName("crdcleanup.controller")
 )
-
-func SetLogger(logger logr.Logger) {
-	log = logger
-}
