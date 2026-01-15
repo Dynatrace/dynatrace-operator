847c60f7f5bacf8b428661fb6e4f15a5f88e0667
Julia Okuniewska <julia.okuniewska@dynatrace.com>
2026-01-15 10:16:51 +0100

    Remove oneagentapm package (#6003)

--- cmd/troubleshoot/cmd.go
+++ cmd/troubleshoot/cmd.go
@@ -70,13 +70,6 @@ func run(cmd *cobra.Command, args []string) error {
 }
 
 func RunTroubleshootCmd(ctx context.Context, log logd.Logger, namespaceName string, kubeConfig *rest.Config) {
-	err := checkOneAgentAPM(log, kubeConfig)
-	if err != nil {
-		logErrorf(log, "prerequisite checks failed, aborting (%v)", err)
-
-		return
-	}
-
 	apiReader, err := GetK8SClusterAPIReader(kubeConfig)
 	if err != nil {
 		return
