563d62e180f4c761b2df4f91612bdb480ece3abe
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-01-22 15:05:29 +0100

    POC: Image unpack using upstream libraries

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/Dynatrace/dynatrace-bootstrapper v1.1.3
 	github.com/container-storage-interface/spec v1.12.0
+	github.com/containerd/containerd v1.7.30
 	github.com/docker/cli v29.1.5+incompatible
 	github.com/evanphx/json-patch v5.9.11+incompatible
 	github.com/go-logr/logr v1.4.3
@@ -46,10 +47,16 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/Microsoft/hcsshim v0.11.7 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v5 v5.0.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/cgroups v1.1.0 // indirect
+	github.com/containerd/continuity v0.4.4 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
 	github.com/containerd/stargz-snapshotter/estargz v0.18.1 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/docker/distribution v2.8.3+incompatible // indirect
@@ -66,6 +73,8 @@ require (
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/btree v1.1.3 // indirect
 	github.com/google/gnostic-models v0.7.0 // indirect
@@ -87,6 +96,8 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/moby/spdystream v0.5.0 // indirect
 	github.com/moby/sys/mountinfo v0.7.2 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
@@ -103,6 +114,7 @@ require (
 	github.com/vbatts/tar-split v0.12.2 // indirect
 	github.com/vladimirvivien/gexe v0.4.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/collector/component/componentstatus v0.144.0 // indirect
 	go.opentelemetry.io/collector/component/componenttest v0.144.0 // indirect
@@ -166,7 +178,7 @@ require (
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/term v0.39.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	gonum.org/v1/gonum v0.17.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20251222181119-0a764e51fe1b // indirect
--- pkg/injection/codemodule/installer/image/unpack.go
+++ pkg/injection/codemodule/installer/image/unpack.go
@@ -3,15 +3,21 @@ package image
 import (
 	"context"
 	"encoding/base64"
+	goerrors "errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/arch"
 	"github.com/Dynatrace/dynatrace-operator/pkg/injection/codemodule/installer/common"
+	"github.com/containerd/containerd/archive"
 	"github.com/google/go-containerregistry/pkg/crane"
 	"github.com/google/go-containerregistry/pkg/name"
 	containerv1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/partial"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/pkg/errors"
 )
@@ -22,20 +28,35 @@ type imagePullInfo struct {
 }
 
 func (installer *Installer) extractAgentBinariesFromImage(pullInfo imagePullInfo, imageName string) error {
-	img, err := installer.pullImageInfo(imageName)
-	if err != nil {
-		log.Info("pullImageInfo", "error", err)
+	ctx := context.TODO()
 
-		return err
+	ref, err := name.ParseReference(imageName)
+	if err != nil {
+		return errors.WithMessagef(err, "parse reference %s", imageName)
 	}
 
-	image := *img
-
-	err = installer.pullOCIimage(image, imageName, pullInfo.imageCacheDir, pullInfo.targetDir)
+	image, err := remote.Image(ref, remote.WithContext(ctx),
+		remote.WithAuthFromKeychain(installer.keychain),
+		remote.WithTransport(installer.transport),
+		remote.WithPlatform(arch.ImagePlatform),
+	)
 	if err != nil {
-		log.Info("pullOCIimage", "err", err)
+		return errors.WithMessagef(err, "get image %s", imageName)
+	}
 
-		return err
+	unpackDir := filepath.Join(pullInfo.imageCacheDir, base64.StdEncoding.EncodeToString([]byte(ref.String())))
+	if err := unpackImage(ctx, image, unpackDir); err != nil {
+		return errors.WithMessagef(err, "unpack image %s", imageName)
+	}
+
+	log.Info("moving unpacked archive to target", "targetDir", pullInfo.targetDir)
+
+	if err := os.Rename(filepath.Join(unpackDir, "opt", "dynatrace", "oneagent"), pullInfo.targetDir); err != nil {
+		if !os.IsNotExist(err) {
+			return errors.WithMessagef(err, "move unpacked image %s", imageName)
+		}
+
+		return errors.WithMessagef(os.Rename(unpackDir, pullInfo.targetDir), "move unpacked image %s", imageName)
 	}
 
 	return nil
@@ -122,4 +143,80 @@ func (installer *Installer) unpackOciImage(layers []containerv1.Layer, imageCach
 	log.Info("unpackOciImage", "targetDir", targetDir)
 
 	return nil
+}
+
+// Unpack remote OCI image into targetDir
+func unpackImage(ctx context.Context, remoteImage containerv1.Image, targetDir string) error {
+	if err := os.MkdirAll(targetDir, common.MkDirFileMode); err != nil {
+		return errors.WithMessage(err, "create target directory")
+	}
+
+	remoteLayers, err := remoteImage.Layers()
+	if err != nil {
+		return errors.WithMessage(err, "get image layers")
+	}
+
+	// Could append into the remote image instead, but using this empty image requires less work when merging.
+	localImage, _ := partial.UncompressedToImage(emptyImage{})
+
+	for _, layer := range remoteLayers {
+		mediaType, _ := layer.MediaType()
+		if mediaType != types.DockerLayer && mediaType != types.OCILayer {
+			return errors.Errorf("media type %s is not implemented", mediaType)
+		}
+
+		digest, _ := layer.Digest()
+		size, _ := layer.Size()
+		log.Debug("downloading layer", "size", size, "digest", digest.Hex)
+
+		downloaded, err := tarball.LayerFromOpener(layer.Compressed)
+		if err != nil {
+			return errors.WithMessage(err, "create tar reader")
+		}
+
+		// Alternatively, read the compressed data into a file and use the path instead.
+		// remote, err := layer.Compressed()
+		// local, err := os.Create("")
+		// io.Copy(local, remote)
+		// tarball.LayerFromFile(local)
+
+		localImage, err = mutate.Append(localImage, mutate.Addendum{Layer: downloaded})
+		if err != nil {
+			return err
+		}
+	}
+
+	// This reader can also be used with tar.NewReader directly (no gzip needed)
+	xr := mutate.Extract(localImage)
+	_, err = archive.Apply(ctx, targetDir, xr, archive.WithNoSameOwner())
+
+	return goerrors.Join(err, xr.Close())
+}
+
+// Copied from github.com/google/go-containerregistry@v0.20.7/pkg/v1/empty/image.go
+// Because the exported variable is mutable and should not be reused.
+type emptyImage struct{}
+
+// MediaType implements partial.UncompressedImageCore.
+func (i emptyImage) MediaType() (types.MediaType, error) {
+	return types.DockerManifestSchema2, nil
+}
+
+// RawConfigFile implements partial.UncompressedImageCore.
+func (i emptyImage) RawConfigFile() ([]byte, error) {
+	return partial.RawConfigFile(i)
+}
+
+// ConfigFile implements v1.Image.
+func (i emptyImage) ConfigFile() (*containerv1.ConfigFile, error) {
+	return &containerv1.ConfigFile{
+		RootFS: containerv1.RootFS{
+			// Some clients check this.
+			Type: "layers",
+		},
+	}, nil
+}
+
+func (i emptyImage) LayerByDiffID(h containerv1.Hash) (partial.UncompressedLayer, error) {
+	return nil, fmt.Errorf("LayerByDiffID(%s): empty image", h)
 }
