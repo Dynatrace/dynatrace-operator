e9f0f98e2e5d7d74794c50ceaffd91340be1d928
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-01-23 13:22:01 +0100

    Remove flakiness

--- hack/make/go.mk
+++ hack/make/go.mk
@@ -9,14 +9,14 @@ go/check-version:
 
 ## Runs golangci-lint
 go/golangci: go/check-version
-	$(GOLANGCI_LINT) run --fix --build-tags "$(shell ./hack/build/create_go_build_tags.sh true)" --timeout 300s
+	$(GOLANGCI_LINT) run --fix --timeout 300s
 
 ## Runs all the linting tools
 go/lint: prerequisites/go-linting go/golangci go/deadcode
 
 ## Runs all go unit and integration tests and writes the coverprofile to coverage.txt
 go/test: prerequisites/setup-envtest go/check-version
-	go test ./... -coverprofile=coverage.txt -covermode=atomic -coverpkg=./... -tags "$(shell ./hack/build/create_go_build_tags.sh false)"
+	go test ./... -coverprofile=coverage.txt -covermode=atomic -coverpkg=./...
 
 ## Runs all go unit tests and opens coverage report in a browser
 go/coverage: go/test
@@ -33,7 +33,7 @@ go/gen_mocks: prerequisites/mockery
 ## Runs deadcode https://go.dev/blog/deadcode
 go/deadcode: prerequisites/go-deadcode
 	# we add `tee` in the end to make it fail if it finds dead code, by default deadcode always return exit code 0
-	$(DEADCODE) -test -tags="$(shell ./hack/build/create_go_build_tags.sh true)" ./... | tee deadcode.out && [ ! -s deadcode.out ]
+	$(DEADCODE) -test -tags=e2e ./... | tee deadcode.out && [ ! -s deadcode.out ]
 
 ## Runs go-test-coverage tool
 go/check-coverage: prerequisites/go-test-coverage
