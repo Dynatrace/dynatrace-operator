753436a4117b1e97123f7c47f96531dee2ead2af
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2026-01-26 12:42:37 +0100

    Add KSPM settings reconciler

---
+++ pkg/controllers/dynakube/kspm/settings/conditions.go
@@ -0,0 +1,55 @@
+package settings
+
+import (
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	alreadyExistReason = "AlreadyExist"
+	skippedReason      = "Skipped"
+	errorReason        = "Error"
+	createdReason      = "Created"
+
+	ConditionType = "KSPMSettings"
+)
+
+func setCreatedCondition(conditions *[]metav1.Condition) {
+	condition := metav1.Condition{
+		Type:    ConditionType,
+		Status:  metav1.ConditionTrue,
+		Reason:  createdReason,
+		Message: "KSPM settings have been created.",
+	}
+	_ = meta.SetStatusCondition(conditions, condition)
+}
+
+func setAlreadyExistsCondition(conditions *[]metav1.Condition) {
+	condition := metav1.Condition{
+		Type:    ConditionType,
+		Status:  metav1.ConditionTrue,
+		Reason:  alreadyExistReason,
+		Message: "KSPM settings already exist, will not create new ones.",
+	}
+	_ = meta.SetStatusCondition(conditions, condition)
+}
+
+func setErrorCondition(conditions *[]metav1.Condition, message string) {
+	condition := metav1.Condition{
+		Type:    ConditionType,
+		Status:  metav1.ConditionFalse,
+		Reason:  errorReason,
+		Message: "KSPM settings creation was skipped: " + message,
+	}
+	_ = meta.SetStatusCondition(conditions, condition)
+}
+
+func setSkippedCondition(conditions *[]metav1.Condition, message string) {
+	condition := metav1.Condition{
+		Type:    ConditionType,
+		Status:  metav1.ConditionFalse,
+		Reason:  skippedReason,
+		Message: "KSPM settings creation was skipped: " + message,
+	}
+	_ = meta.SetStatusCondition(conditions, condition)
+}
---
+++ pkg/controllers/dynakube/kspm/settings/reconciler.go
@@ -0,0 +1,110 @@
+package settings
+
+import (
+	"context"
+	"strings"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
+	dtclient "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace"
+	dtsettings "github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace/settings"
+	"github.com/Dynatrace/dynatrace-operator/pkg/logd"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8sconditions"
+	"github.com/Dynatrace/dynatrace-operator/pkg/util/timeprovider"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+)
+
+var (
+	log = logd.Get().WithName("kspm-settings")
+)
+
+type Reconciler struct {
+	timeProvider *timeprovider.Provider
+}
+
+func NewReconciler() *Reconciler {
+	return &Reconciler{
+		timeProvider: timeprovider.New(),
+	}
+}
+
+func (r *Reconciler) Reconcile(ctx context.Context, dtc dtsettings.APIClient, dk *dynakube.DynaKube) error {
+	if !k8sconditions.IsOutdated(r.timeProvider, dk, ConditionType) {
+		return nil
+	}
+
+	if !dk.KSPM().IsEnabled() {
+		_ = meta.RemoveStatusCondition(dk.Conditions(), ConditionType)
+
+		return nil
+	}
+
+	hasReadScope := k8sconditions.IsOptionalScopeAvailable(dk, dtclient.ConditionTypeAPITokenSettingsRead)
+	hasWriteScope := k8sconditions.IsOptionalScopeAvailable(dk, dtclient.ConditionTypeAPITokenSettingsWrite)
+
+	var missingScopes []string
+	if !hasReadScope {
+		missingScopes = append(missingScopes, dtclient.TokenScopeSettingsRead)
+	}
+
+	if !hasWriteScope {
+		missingScopes = append(missingScopes, dtclient.TokenScopeSettingsWrite)
+	}
+
+	if len(missingScopes) > 0 {
+		message := strings.Join(missingScopes, ", ") + " scope(s) missing: cannot query existing kspm monitoring setting and/or safely create new one."
+		k8sconditions.SetOptionalScopeMissing(dk.Conditions(), ConditionType, message)
+		log.Info(message)
+
+		return nil
+	} else {
+		log.Info("necessary scopes for kspm settings creation is available, proceeding with reconciliation")
+	}
+
+	err := r.checkKSPMSettings(ctx, dtc, dk)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Reconciler) checkKSPMSettings(ctx context.Context, dtc dtsettings.APIClient, dk *dynakube.DynaKube) error {
+	log.Info("start reconciling kspm settings")
+
+	if dk.Status.KubernetesClusterMEID == "" {
+		msg := "kubernetesClusterMEID is not available, which is needed for kspm settings creation, will skip it for now"
+		log.Info(msg)
+
+		setSkippedCondition(dk.Conditions(), msg)
+
+		return nil
+	}
+
+	kspmSettings, err := dtc.GetKSPMSettings(ctx, dk.Status.KubernetesClusterMEID)
+	if err != nil {
+		setErrorCondition(dk.Conditions(), err.Error())
+
+		return errors.WithMessage(err, "error trying to check if setting exists")
+	}
+
+	if kspmSettings.TotalCount > 0 {
+		log.Info("there are already settings", "settings", kspmSettings)
+
+		setAlreadyExistsCondition(dk.Conditions())
+
+		return nil
+	}
+
+	objectID, err := dtc.CreateKSPMSetting(ctx, dk.Status.KubernetesClusterMEID, true)
+	if err != nil {
+		setErrorCondition(dk.Conditions(), err.Error())
+
+		return err
+	}
+
+	setCreatedCondition(dk.Conditions())
+	log.Info("kspm setting created", "settings", objectID)
+
+	return nil
+}
