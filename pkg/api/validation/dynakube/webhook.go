7ef380bc5aa934b9617311f30a3a7a58c2088da3
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-01-28 09:13:08 +0100

    refactor

--- pkg/api/validation/dynakube/webhook.go
+++ pkg/api/validation/dynakube/webhook.go
@@ -5,31 +5,29 @@ import (
 	v1beta3 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta3/dynakube"
 	v1beta4 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta4/dynakube"
 	v1beta5 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta5/dynakube"
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
+
+func setupWebhookWithManager(mgr ctrl.Manager, obj runtime.Object, validator admission.Validator[runtime.Object]) error {
+	return ctrl.NewWebhookManagedBy(mgr, obj).WithCustomValidator(validator).Complete() // nolint:staticcheck
+}
 
 func SetupWebhookWithManager(mgr ctrl.Manager) error {
 	validator := New(mgr.GetAPIReader(), mgr.GetConfig())
 
-	if err := ctrl.NewWebhookManagedBy(mgr, &v1beta3.DynaKube{}).
-		WithCustomValidator(validator).
-		Complete(); err != nil {
+	if err := setupWebhookWithManager(mgr, &v1beta3.DynaKube{}, validator); err != nil {
 		return err
 	}
 
-	if err := ctrl.NewWebhookManagedBy(mgr, &v1beta4.DynaKube{}).
-		WithCustomValidator(validator).
-		Complete(); err != nil {
+	if err := setupWebhookWithManager(mgr, &v1beta4.DynaKube{}, validator); err != nil {
 		return err
 	}
 
-	if err := ctrl.NewWebhookManagedBy(mgr, &v1beta5.DynaKube{}).
-		WithCustomValidator(validator).
-		Complete(); err != nil {
+	if err := setupWebhookWithManager(mgr, &v1beta5.DynaKube{}, validator); err != nil {
 		return err
 	}
 
-	return ctrl.NewWebhookManagedBy(mgr, &latest.DynaKube{}).
-		WithCustomValidator(validator).
-		Complete()
+	return setupWebhookWithManager(mgr, &latest.DynaKube{}, validator)
 }
--- pkg/api/validation/edgeconnect/webhook.go
+++ pkg/api/validation/edgeconnect/webhook.go
@@ -3,19 +3,21 @@ package validation
 import (
 	v1alpha1 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha1/edgeconnect"
 	v1alpha2 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha2/edgeconnect"
+	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
+
+func setupWebhookWithManager(mgr ctrl.Manager, obj runtime.Object, validator admission.Validator[runtime.Object]) error {
+	return ctrl.NewWebhookManagedBy(mgr, obj).WithCustomValidator(validator).Complete() // nolint:staticcheck
+}
 
 func SetupWebhookWithManager(mgr ctrl.Manager) error {
 	validator := New(mgr.GetAPIReader(), mgr.GetConfig())
 
-	if err := ctrl.NewWebhookManagedBy(mgr, &v1alpha1.EdgeConnect{}).
-		WithCustomValidator(validator). //nolint
-		Complete(); err != nil {
+	if err := setupWebhookWithManager(mgr, &v1alpha1.EdgeConnect{}, validator); err != nil {
 		return err
 	}
 
-	return ctrl.NewWebhookManagedBy(mgr, &v1alpha2.EdgeConnect{}).
-		WithCustomValidator(validator). //nolint
-		Complete()
+	return setupWebhookWithManager(mgr, &v1alpha2.EdgeConnect{}, validator)
 }
