4181556e95de708c6bbfdd3564cd5251647e138f
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-02-12 15:51:39 +0100

    Clean up k8sdeployment

--- test/helpers/kubernetes/objects/k8sdeployment/get.go
+++ test/helpers/kubernetes/objects/k8sdeployment/get.go
@@ -33,24 +33,14 @@ func NewQuery(ctx context.Context, resource *resources.Resources, objectKey clie
 	}
 }
 
-func (query *Query) Get() (appsv1.Deployment, error) {
-	var deployment appsv1.Deployment
-	err := query.resource.Get(query.ctx, query.objectKey.Name, query.objectKey.Namespace, &deployment)
-
-	return deployment, err
-}
-
 func (query *Query) ForEachPod(consumer PodConsumer) error {
-	var pods corev1.PodList
-	deployment, err := query.Get()
-
-	if err != nil {
+	deployment := &appsv1.Deployment{}
+	if err := query.resource.Get(query.ctx, query.objectKey.Name, query.objectKey.Namespace, deployment); err != nil {
 		return err
 	}
 
-	err = query.resource.List(query.ctx, &pods, resources.WithLabelSelector(labels.FormatLabels(deployment.Spec.Selector.MatchLabels)))
-
-	if err != nil {
+	var pods corev1.PodList
+	if err := query.resource.List(query.ctx, &pods, resources.WithLabelSelector(labels.FormatLabels(deployment.Spec.Selector.MatchLabels))); err != nil {
 		return err
 	}
 
@@ -64,8 +54,8 @@ func (query *Query) ForEachPod(consumer PodConsumer) error {
 func IsReady(name, namespace string) features.Func {
 	return func(ctx context.Context, t *testing.T, envConfig *envconf.Config) context.Context {
 		resources := envConfig.Client().Resources()
-		deploy, err := NewQuery(ctx, resources, client.ObjectKey{Name: name, Namespace: namespace}).Get()
-		require.NoError(t, err)
+		deploy := &appsv1.Deployment{}
+		require.NoError(t, resources.Get(ctx, name, namespace, deploy))
 		assert.Equal(t, deploy.Status.Replicas, deploy.Status.ReadyReplicas)
 
 		return ctx
