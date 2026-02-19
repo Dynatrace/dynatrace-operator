0757b15f8a12430a9d00505cab7507a31f5fadf0
adam.orcholski <adam.orcholski@dynatrace.com>
2026-02-19 12:35:59 +0100

    rebase

--- test/e2e/helpers/kubernetes/objects/k8sdaemonset/daemonset.go
+++ test/e2e/helpers/kubernetes/objects/k8sdaemonset/daemonset.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Dynatrace/dynatrace-operator/test/helpers"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
