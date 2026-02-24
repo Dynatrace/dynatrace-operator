62816e3f9e02cf6ca60437a98cc5d5308a5168fc
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-02-24 13:36:49 +0100

    revert test: all image versions were updated"

--- pkg/controllers/dynakube/version/reconciler_test.go
+++ pkg/controllers/dynakube/version/reconciler_test.go
@@ -4,32 +4,40 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/activegate"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/oneagent"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/scheme/fake"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/status"
+	"github.com/Dynatrace/dynatrace-operator/pkg/controllers/dynakube/dtpullsecret"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8sconditions"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/timeprovider"
 	dtclientmock "github.com/Dynatrace/dynatrace-operator/test/mocks/pkg/clients/dynatrace"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
 	testNamespace      = "test-namespace"
 	testDockerRegistry = "ENVIRONMENTID.live.dynatrace.com"
 	testAPIURL         = "https://" + testDockerRegistry + "/api"
+
+	latestActiveGateVersion = "1.2.3.4-56"
+	latestOneAgentVersion = "1.2.3.4-5"
 )
 
 var anyCtx = mock.MatchedBy(func(context.Context) bool { return true })
 
 func TestReconcile(t *testing.T) {
 	ctx := t.Context()
+	latestAgentVersion := "1.2.3.4-5"
 	dynakubeTemplate := dynakube.DynaKube{
 		ObjectMeta: metav1.ObjectMeta{Namespace: testNamespace},
 		Spec: dynakube.DynaKubeSpec{
@@ -61,6 +69,53 @@ func TestReconcile(t *testing.T) {
 		condition := meta.FindStatusCondition(dk.Status.Conditions, activeGateVersionConditionType)
 		assert.Equal(t, metav1.ConditionFalse, condition.Status)
 		assert.Equal(t, k8sconditions.DynatraceAPIErrorReason, condition.Reason)
+	})
+
+	t.Run("all image versions were updated", func(t *testing.T) {
+		dk := dynakubeTemplate.DeepCopy()
+		fakeClient := fake.NewClient()
+		timeProvider := timeprovider.New().Freeze()
+
+		setupPullSecret(t, fakeClient, *dk)
+
+		dkStatus := &dk.Status
+		mockClient := dtclientmock.NewClient(t)
+		mockLatestAgentVersion(mockClient, latestAgentVersion, 3)
+		mockLatestActiveGateVersion(mockClient, latestActiveGateVersion)
+
+		versionReconciler := reconciler{
+			apiReader:    fakeClient,
+			timeProvider: timeProvider,
+			dtClient:     mockClient,
+		}
+		err := versionReconciler.ReconcileCodeModules(ctx, dk)
+		require.NoError(t, err)
+		err = versionReconciler.ReconcileActiveGate(ctx, dk)
+		require.NoError(t, err)
+		err = versionReconciler.ReconcileOneAgent(ctx, dk)
+		require.NoError(t, err)
+
+		condition := meta.FindStatusCondition(dk.Status.Conditions, activeGateVersionConditionType)
+		assert.Equal(t, metav1.ConditionTrue, condition.Status)
+		assert.Equal(t, verifiedReason, condition.Reason)
+		assert.Equal(t, "Version verified for component.", condition.Message)
+
+		assertStatusBasedOnTenantRegistry(t, dk.ActiveGate().GetDefaultImage(latestActiveGateVersion), latestActiveGateVersion, dkStatus.ActiveGate.VersionStatus)
+		assertStatusBasedOnTenantRegistry(t, dk.OneAgent().GetDefaultImage(latestOneAgentVersion), latestOneAgentVersion, dkStatus.OneAgent.VersionStatus)
+		assert.Equal(t, latestAgentVersion, dkStatus.CodeModules.Version)
+
+		// no change if probe not old enough
+		previousProbe := *dkStatus.CodeModules.LastProbeTimestamp
+		err = versionReconciler.ReconcileCodeModules(ctx, dk)
+		require.NoError(t, err)
+		assert.Equal(t, previousProbe, *dkStatus.CodeModules.LastProbeTimestamp)
+
+		// change if probe old enough
+		changeTime(timeProvider, 15*time.Minute+1*time.Second)
+
+		err = versionReconciler.ReconcileCodeModules(ctx, dk)
+		require.NoError(t, err)
+		assert.NotEqual(t, previousProbe, *dkStatus.CodeModules.LastProbeTimestamp)
 	})
 }
 
@@ -227,6 +282,29 @@ func TestHasCustomFieldChanged(t *testing.T) {
 	})
 }
 
+func setupPullSecret(t *testing.T, fakeClient client.Client, dk dynakube.DynaKube) {
+	t.Helper()
+	err := createTestPullSecret(t, fakeClient, dk)
+	require.NoError(t, err)
+}
+
+func changeTime(timeProvider *timeprovider.Provider, duration time.Duration) {
+	timeProvider.Set(timeProvider.Now().Add(duration))
+}
+
+func createTestPullSecret(t *testing.T, fakeClient client.Client, dk dynakube.DynaKube) error {
+	return fakeClient.Create(t.Context(), &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: dk.Namespace,
+			Name:      dk.Name + dtpullsecret.PullSecretSuffix,
+		},
+		Data: map[string][]byte{
+			".dockerconfigjson": []byte("{}"),
+		},
+	})
+}
+
+
 func setOneAgentCustomVersionStatus(dk *dynakube.DynaKube, version string) {
 	dk.Status.OneAgent.Source = status.CustomVersionVersionSource
 	dk.Status.OneAgent.Version = version
@@ -239,4 +317,8 @@ func setOneAgentCustomImageStatus(dk *dynakube.DynaKube, image string) {
 
 func mockLatestAgentVersion(mockClient *dtclientmock.Client, latestVersion string, expectedCalls int) {
 	mockClient.EXPECT().GetLatestAgentVersion(anyCtx, mock.Anything, mock.Anything).Return(latestVersion, nil).Times(expectedCalls)
+}
+
+func mockLatestActiveGateVersion(mockClient *dtclientmock.Client, latestVersion string) {
+	mockClient.EXPECT().GetLatestActiveGateVersion(anyCtx, mock.Anything).Return(latestVersion, nil).Once()
 }
