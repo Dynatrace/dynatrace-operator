0362ad352640dafe0d4cdd5a3b52c64a131db423
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-02-25 17:07:49 +0100

    Implement hostevent client

---
+++ pkg/clients/dynatrace/hostevent/client.go
@@ -0,0 +1,163 @@
+// Package hostevent implements a Dynatrace API client to fetch host information and sending events for nodes.
+package hostevent
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/clients/dynatrace/core"
+	"github.com/Dynatrace/dynatrace-operator/pkg/logd"
+)
+
+const (
+	MarkedForTerminationEvent = "MARKED_FOR_TERMINATION"
+
+	eventsPath = "/v1/events"
+	hostsPath  = "/v1/entity/infrastructure/hosts"
+)
+
+var log = logd.Get().WithName("dtclient-hostevent")
+
+type APIClient interface {
+	// GetEntityIDForIP returns the host entity ID for a given IP address.
+	GetEntityIDForIP(ctx context.Context, ip string) (string, error)
+	// SendEvent posts an event to the Dynatrace API.
+	SendEvent(ctx context.Context, event Event) error
+}
+
+type Client struct {
+	apiClient   core.APIClient
+	networkZone string
+}
+
+func NewClient(apiClient core.APIClient, networkZone string) *Client {
+	return &Client{
+		apiClient:   apiClient,
+		networkZone: networkZone,
+	}
+}
+
+type EntityNotFoundError struct {
+	IP string
+}
+
+func (e EntityNotFoundError) Error() string {
+	return "HOST entity not found for IP: " + e.IP
+}
+
+type hostsResponse struct {
+	EntityID      string   `json:"entityId"`
+	NetworkZoneID string   `json:"networkZoneId"`
+	IPAddresses   []string `json:"ipAddresses"`
+}
+
+// hostEntityMap maps IPs to their respective HOST entityID according to the Dynatrace API
+type hostEntityMap map[string]string
+
+// Update adds or overwrites the IP-to-Entity mapping if the IP already existed.
+// The reason we do this "overwrite check" is somewhat unknown, it used to be part of a "caching" logic, however that cache was actually never really used.
+// Kept it "as is" mainly to not introduce new behavior, it is unknown how the API we use handles repeated IP usage. But it can be just dead code.
+func (entityMap hostEntityMap) Update(info hostsResponse) {
+	for _, ip := range info.IPAddresses {
+		if oldEntityID, ok := entityMap[ip]; ok {
+			log.Info("hosts mapping: duplicate IP, replacing HOST entity to 'newer' one", "ip", ip, "new", info.EntityID, "old", oldEntityID)
+		}
+
+		entityMap[ip] = info.EntityID
+	}
+}
+
+// GetEntityIDForIP returns the host entity ID for a given IP address.
+func (c *Client) GetEntityIDForIP(ctx context.Context, ip string) (string, error) {
+	if ip == "" {
+		return "", errors.New("must provide IP")
+	}
+
+	var hosts []hostsResponse
+
+	err := c.apiClient.GET(ctx, hostsPath).
+		WithQueryParams(map[string]string{
+			"relativeTime":   "30mins",
+			"includeDetails": "false",
+		}).Execute(&hosts)
+	if err != nil {
+		return "", setEndpointNotAvailable(err, hostsPath)
+	}
+
+	entities := buildHostEntityMap(hosts, c.networkZone)
+
+	entityID := entities[ip]
+	if entityID == "" {
+		return "", EntityNotFoundError{IP: ip}
+	}
+
+	return entityID, nil
+}
+
+func buildHostEntityMap(hosts []hostsResponse, networkZone string) hostEntityMap {
+	entities := make(hostEntityMap)
+
+	for _, host := range hosts {
+		if (networkZone != "" && host.NetworkZoneID == networkZone) ||
+			(networkZone == "" && (host.NetworkZoneID == "default" || host.NetworkZoneID == "")) {
+			entities.Update(host)
+		}
+	}
+
+	return entities
+}
+
+// Event struct which defines what event payload should contain
+type Event struct {
+	EventType     string           `json:"eventType"`
+	Description   string           `json:"description"`
+	Source        string           `json:"source"`
+	AttachRules   EventAttachRules `json:"attachRules"`
+	StartInMillis uint64           `json:"start"`
+	EndInMillis   uint64           `json:"end"`
+}
+
+type EventAttachRules struct {
+	EntityIDs []string `json:"entityIds"`
+}
+
+func NewMarkForTerminationEvent(entityID, source, description string, timestamp time.Time) Event {
+	ts := uint64(timestamp.UnixNano() / int64(time.Millisecond))
+	return Event{
+		EventType:   MarkedForTerminationEvent,
+		Description: description,
+		Source:      source,
+		AttachRules: EventAttachRules{
+			EntityIDs: []string{entityID},
+		},
+		StartInMillis: ts,
+		EndInMillis:   ts,
+	}
+}
+
+// SendEvent posts an event to the Dynatrace API.
+func (c *Client) SendEvent(ctx context.Context, event Event) error {
+	if event.EventType == "" {
+		return errors.New("no key set for eventType in eventData payload")
+	}
+
+	err := c.apiClient.
+		POST(ctx, eventsPath).
+		WithJSONBody(event).
+		Execute(nil)
+
+	return setEndpointNotAvailable(err, eventsPath)
+}
+
+func setEndpointNotAvailable(err error, endpoint string) error {
+	if core.IsNotFound(err) {
+		return &core.HTTPError{
+			Message:    endpoint + " is not available on the tenant",
+			StatusCode: http.StatusNotFound,
+		}
+	}
+
+	return err
+}
