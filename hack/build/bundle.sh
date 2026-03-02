68d879d6298945266318f669420d56608175b276
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-03-02 15:03:43 +0100

    Improve bundle generation

--- hack/build/bundle.sh
+++ hack/build/bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 PLATFORM="${1:-openshift}"
 VERSION="${2:-0.0.1}"
@@ -7,80 +7,73 @@ BUNDLE_CHANNELS="${3:-}"
 BUNDLE_DEFAULT_CHANNEL="${4:-}"
 OCP_MIN_VERSION="v4.14"
 
-if [ -z "$OLM_IMAGE" ]; then
-  OLM_IMAGE="registry.connect.redhat.com/dynatrace/dynatrace-operator:v${VERSION}@${DIGEST}"
-  if [ "${PLATFORM}" == "kubernetes" ]; then
-    OLM_IMAGE="public.ecr.aws/dynatrace/dynatrace-operator:v${VERSION}@${DIGEST}"
-  fi
+if [[ -z ${OLM_IMAGE} ]]; then
+    OLM_IMAGE="registry.connect.redhat.com/dynatrace/dynatrace-operator:v${VERSION}@${DIGEST}"
+    if [[ ${PLATFORM} == "kubernetes" ]]; then
+        OLM_IMAGE="public.ecr.aws/dynatrace/dynatrace-operator:v${VERSION}@${DIGEST}"
+    fi
 fi
 echo "OLM image: ${OLM_IMAGE}"
 
 KUSTOMIZE="$(hack/build/command.sh kustomize 2>/dev/null)"
-if [ -z "${KUSTOMIZE}" ]; then
-  echo "'kustomize' command not found"
-  exit 2
+if [[ -z ${KUSTOMIZE} ]]; then
+    echo "'kustomize' command not found"
+    exit 2
 fi
 
 OPERATOR_SDK="$(hack/build/command.sh operator-sdk 2>/dev/null)"
-if [ -z "${OPERATOR_SDK}" ]; then
-  echo "'operator-sdk' command not found"
-  exit 2
+if [[ -z ${OPERATOR_SDK} ]]; then
+    echo "'operator-sdk' command not found"
+    exit 2
 fi
 
 SDK_PARAMS=(
---extra-service-accounts dynatrace-dynakube-oneagent
---extra-service-accounts dynatrace-activegate
---extra-service-accounts dynatrace-otel-collector
---extra-service-accounts dynatrace-edgeconnect
---extra-service-accounts dynatrace-extension-controller
---extra-service-accounts dynatrace-sql-ext-exec
---extra-service-accounts dynatrace-logmonitoring
---extra-service-accounts dynatrace-node-config-collector
+    --extra-service-accounts dynatrace-dynakube-oneagent
+    --extra-service-accounts dynatrace-activegate
+    --extra-service-accounts dynatrace-otel-collector
+    --extra-service-accounts dynatrace-edgeconnect
+    --extra-service-accounts dynatrace-extension-controller
+    --extra-service-accounts dynatrace-sql-ext-exec
+    --extra-service-accounts dynatrace-logmonitoring
+    --extra-service-accounts dynatrace-node-config-collector
 )
 
-if [ -n "${BUNDLE_CHANNELS}" ]; then
+if [[ -n ${BUNDLE_CHANNELS} ]]; then
     SDK_PARAMS+=("${BUNDLE_CHANNELS}")
 fi
 
-if [ -n "${BUNDLE_DEFAULT_CHANNEL}" ]; then
+if [[ -n ${BUNDLE_DEFAULT_CHANNEL} ]]; then
     SDK_PARAMS+=("${BUNDLE_DEFAULT_CHANNEL}")
 fi
 
-"${OPERATOR_SDK}" generate kustomize manifests -q --apis-dir ./pkg/api/
-(cd "config/deploy/${PLATFORM}" && ${KUSTOMIZE} edit set image quay.io/dynatrace/dynatrace-operator:snapshot="${OLM_IMAGE}")
-"${KUSTOMIZE}" build "config/olm/${PLATFORM}" | "${OPERATOR_SDK}" generate bundle --overwrite --version "${VERSION}" "${SDK_PARAMS[@]}"
+# Clear previous manifests to ensure local builds don't keep stale data
+rm -rf manifests
+
+"${OPERATOR_SDK}" generate kustomize manifests -q --apis-dir pkg/api/
+(cd "config/deploy/${PLATFORM}" && ${KUSTOMIZE} edit set image "${REGISTRY-ghcr.io}"/dynatrace/dynatrace-operator:snapshot="${OLM_IMAGE}")
+"${KUSTOMIZE}" build "config/olm/${PLATFORM}" | "${OPERATOR_SDK}" generate bundle --overwrite --output-dir . --version "${VERSION}" "${SDK_PARAMS[@]}"
 # Add missing aggregated ClusterRole and binding. This fixes the issue of missing cluster permissions in the CSV.
 # operator-sdk looks at the RBAC on disk, but aggregated roles are rendered in the cluster.
 # https://github.com/operator-framework/operator-lifecycle-manager/issues/2757
-yq 'select(.kind=="ClusterRole")|select(.metadata.name=="dynatrace-kubernetes-monitoring")|.' config/deploy/${PLATFORM}/${PLATFORM}.yaml > bundle/manifests/dynatrace-kubernetes-monitoring_rbac.authorization.k8s.io_v1_clusterrole.yaml
-yq 'select(.kind=="ClusterRoleBinding")|select(.metadata.name=="dynatrace-kubernetes-monitoring")|.' config/deploy/${PLATFORM}/${PLATFORM}.yaml > bundle/manifests/dynatrace-kubernetes-monitoring_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
-"${OPERATOR_SDK}" bundle validate ./bundle
+yq 'select(.kind=="ClusterRole")|select(.metadata.name=="dynatrace-kubernetes-monitoring")|.' "config/deploy/${PLATFORM}/${PLATFORM}.yaml" > manifests/dynatrace-kubernetes-monitoring_rbac.authorization.k8s.io_v1_clusterrole.yaml
+yq 'select(.kind=="ClusterRoleBinding")|select(.metadata.name=="dynatrace-kubernetes-monitoring")|.' "config/deploy/${PLATFORM}/${PLATFORM}.yaml" > manifests/dynatrace-kubernetes-monitoring_rbac.authorization.k8s.io_v1_clusterrolebinding.yaml
 
-rm -rf "./config/olm/${PLATFORM}/${VERSION}"
-mkdir -p "./config/olm/${PLATFORM}/${VERSION}"
-mv ./bundle/* "./config/olm/${PLATFORM}/${VERSION}"
-mv "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.clusterserviceversion.yaml" "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml"
-mv "./bundle.Dockerfile" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-grep -v 'scorecard' "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile" > "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output"
-mv "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-sed "s/bundle/${VERSION}/" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile" > "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output"
-mv "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-awk '/operators.operatorframework.io.metrics.project_layout/ { print; print "  operators.operatorframework.io.bundle.channel.default.v1: alpha"; next }1' "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml" >  "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml.output"
-mv "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml.output" "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml"
-awk "/operators.operatorframework.io.${VERSION}.mediatype.v1/ { print \"LABEL operators.operatorframework.io.bundle.channel.default.v1=alpha\"; print; next }1" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile" > "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output"
-mv "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-grep -v '# Labels for testing.' "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile" > "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output"
-mv "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile.output" "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-if [ "${PLATFORM}" = "openshift" ]; then
-  # shellcheck disable=SC2129
-	echo "LABEL com.redhat.openshift.versions=\"${OCP_MIN_VERSION}\"" >> "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-	echo 'LABEL com.redhat.delivery.operator.bundle=true' >> "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-	echo 'LABEL com.redhat.delivery.backport=true' >> "./config/olm/${PLATFORM}/bundle-${VERSION}.Dockerfile"
-	sed 's/\bkubectl\b/oc/g' "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml" > "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml.output"
-	mv "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml.output" "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml"
-	echo "  com.redhat.openshift.versions: ${OCP_MIN_VERSION}" >> "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml"
+# Set important metadata
+yq -i ".metadata.annotations[\"olm.skipRange\"] = \"<$VERSION\"" manifests/dynatrace-operator.clusterserviceversion.yaml
+yq -i ".metadata.annotations[\"createdAt\"] = now" manifests/dynatrace-operator.clusterserviceversion.yaml
+if [[ ${PLATFORM} = "openshift" ]]; then
+    {
+        echo "LABEL com.redhat.openshift.versions=\"${OCP_MIN_VERSION}\""
+        echo 'LABEL com.redhat.delivery.operator.bundle=true'
+        echo 'LABEL com.redhat.delivery.backport=true'
+    } >> bundle.Dockerfile
+
+    yq -i ".annotations[\"com.redhat.openshift.versions\"] = \"${OCP_MIN_VERSION}\"" metadata/annotations.yaml
 fi
-grep -v 'scorecard' "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml" > "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml.output"
-grep -v '  # Annotations for testing.' "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml.output" > "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml"
-rm "./config/olm/${PLATFORM}/${VERSION}/metadata/annotations.yaml.output"
-mv "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.v${VERSION}.clusterserviceversion.yaml" "./config/olm/${PLATFORM}/${VERSION}/manifests/dynatrace-operator.clusterserviceversion.yaml"
+
+BUNDLE_DIR=config/olm/${PLATFORM}/${VERSION}
+rm -rf "${BUNDLE_DIR}"
+mkdir -p "${BUNDLE_DIR}"
+mv bundle.Dockerfile metadata manifests "${BUNDLE_DIR}/"
+
+"${OPERATOR_SDK}" bundle validate "${BUNDLE_DIR}"
--- hack/make/bundle.mk
+++ hack/make/bundle.mk
@@ -5,9 +5,9 @@ PLATFORM ?= openshift
 # Needed variable for manifest generation
 OLM ?= false
 # Default bundle image with tag
-BUNDLE_IMG ?= controller-bundle:$(VERSION)
+BUNDLE_IMG ?= $(REGISTRY)/dynatrace/dynatrace-operator-bundle:$(VERSION)
 
-# Options for 'bundle-build'
+# Options for 'bundle'
 ifneq ($(origin CHANNELS), undefined)
 BUNDLE_CHANNELS := --channels=$(CHANNELS)
 endif
@@ -19,13 +19,29 @@ endif
 ## Generates bundle manifests and metadata, then validates generated files
 bundle: manifests/$(PLATFORM)
 	./hack/build/bundle.sh "$(PLATFORM)" "$(VERSION)" "$(BUNDLE_CHANNELS)" "$(BUNDLE_DEFAULT_CHANNEL)"
-
-.PHONY: bundle/minimal
-## Generates bundle manifests and metadata, validates generated files and removes everything but the CSV file
-bundle/minimal: bundle
-	find ./config/olm/${PLATFORM}/${VERSION}/manifests/ ! \( -name "dynatrace-operator.v${VERSION}.clusterserviceversion.yaml" -o -name "dynatrace.com_dynakubes.yaml" \) -type f -exec rm {} +
+	@git restore config/
 
 .PHONY: bundle/build
-## Builds the docker image used for OLM deployment
+## Build the bundle image
 bundle/build:
-	docker build -f ./config/olm/$(PLATFORM)/bundle-$(VERSION).Dockerfile -t $(BUNDLE_IMG) ./config/olm/$(PLATFORM)/
+	cd config/olm/$(PLATFORM)/$(VERSION) && docker build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
+
+.PHONY: bundle/push
+## Push the bundle image
+bundle/push:
+	docker push $(BUNDLE_IMG)
+
+.PHONY: bundle/install
+## Deploy the bundle
+bundle/install: bundle/build bundle/push
+	operator-sdk run bundle $(BUNDLE_IMG) --namespace dynatrace --timeout 5m
+
+.PHONY: bundle/upgrade
+## Upgrade previously installed bundle
+bundle/upgrade: bundle/build bundle/push
+	operator-sdk run bundle-upgrade $(BUNDLE_IMG) --namespace dynatrace --timeout 5m
+
+.PHONY: bundle/cleanup
+## Clean up bundle
+bundle/cleanup:
+	operator-sdk cleanup dynatrace-operator --delete-all --delete-crds --delete-operator-groups --namespace dynatrace --timeout 5m
