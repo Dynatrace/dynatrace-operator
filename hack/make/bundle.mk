751d7667e5c130a277d42b4dd155544fe6ed4f8f
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-03-02 15:03:43 +0100

    Improve bundle generation

--- hack/make/bundle.mk
+++ hack/make/bundle.mk
@@ -1,31 +1,62 @@
-# Current Operator version
+# VERSION defines the project version for the bundle.
+# Update this value when you upgrade the version of your project.
+# To re-generate a bundle for another specific version without changing the standard setup, you can:
+# - use the VERSION as arg of the bundle target (e.g make bundle VERSION=0.0.2)
+# - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
 VERSION ?= 0.0.1
+
+# CHANNELS define the bundle channels used in the bundle.
+# Add a new line here if you would like to change its default config. (E.g CHANNELS = "candidate,fast,stable")
+# To re-generate a bundle for other specific channels without changing the standard setup, you can:
+# - use the CHANNELS as arg of the bundle target (e.g make bundle CHANNELS=candidate,fast,stable)
+# - use environment variables to overwrite this value (e.g export CHANNELS="candidate,fast,stable")
+ifneq ($(origin CHANNELS), undefined)
+BUNDLE_CHANNELS := --channels=$(CHANNELS)
+endif
+
+# DEFAULT_CHANNEL defines the default channel used in the bundle.
+# Add a new line here if you would like to change its default config. (E.g DEFAULT_CHANNEL = "stable")
+# To re-generate a bundle for any other default channel without changing the default setup, you can:
+# - use the DEFAULT_CHANNEL as arg of the bundle target (e.g make bundle DEFAULT_CHANNEL=stable)
+# - use environment variables to overwrite this value (e.g export DEFAULT_CHANNEL="stable")
+ifneq ($(origin DEFAULT_CHANNEL), undefined)
+BUNDLE_DEFAULT_CHANNEL := --default-channel=$(DEFAULT_CHANNEL)
+endif
+
 # Default platform for bundles
 PLATFORM ?= openshift
 # Needed variable for manifest generation
 OLM ?= false
 # Default bundle image with tag
-BUNDLE_IMG ?= controller-bundle:$(VERSION)
-
-# Options for 'bundle-build'
-ifneq ($(origin CHANNELS), undefined)
-BUNDLE_CHANNELS := --channels=$(CHANNELS)
-endif
-ifneq ($(origin DEFAULT_CHANNEL), undefined)
-BUNDLE_DEFAULT_CHANNEL := --default-channel=$(DEFAULT_CHANNEL)
-endif
+BUNDLE_IMG ?= $(REGISTRY)/$(REPOSITORY)/dynatrace-operator-bundle:$(VERSION)
 
 .PHONY: bundle
 ## Generates bundle manifests and metadata, then validates generated files
 bundle: manifests/$(PLATFORM)
 	./hack/build/bundle.sh "$(PLATFORM)" "$(VERSION)" "$(BUNDLE_CHANNELS)" "$(BUNDLE_DEFAULT_CHANNEL)"
-
-.PHONY: bundle/minimal
-## Generates bundle manifests and metadata, validates generated files and removes everything but the CSV file
-bundle/minimal: bundle
-	find ./config/olm/${PLATFORM}/${VERSION}/manifests/ ! \( -name "dynatrace-operator.v${VERSION}.clusterserviceversion.yaml" -o -name "dynatrace.com_dynakubes.yaml" \) -type f -exec rm {} +
+	@git restore config/
 
 .PHONY: bundle/build
-## Builds the docker image used for OLM deployment
+## Build the bundle image
 bundle/build:
-	docker build -f ./config/olm/$(PLATFORM)/bundle-$(VERSION).Dockerfile -t $(BUNDLE_IMG) ./config/olm/$(PLATFORM)/
+	cd config/olm/$(PLATFORM)/$(VERSION) && docker build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
+
+.PHONY: bundle/push
+## Push the bundle image
+bundle/push:
+	docker push $(BUNDLE_IMG)
+
+.PHONY: bundle/install
+## Deploy the bundle
+bundle/install: bundle/build bundle/push
+	operator-sdk run bundle $(BUNDLE_IMG) --namespace dynatrace --timeout 5m
+
+.PHONY: bundle/upgrade
+## Upgrade previously installed bundle
+bundle/upgrade: bundle/build bundle/push
+	operator-sdk run bundle-upgrade $(BUNDLE_IMG) --namespace dynatrace --timeout 5m
+
+.PHONY: bundle/cleanup
+## Clean up bundle
+bundle/cleanup:
+	operator-sdk cleanup dynatrace-operator --delete-all --delete-crds --delete-operator-groups --namespace dynatrace --timeout 5m
