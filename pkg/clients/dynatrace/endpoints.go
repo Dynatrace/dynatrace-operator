ddfbd41d61f33ad62838883296d52b65a2252f5e
jokuniew <julia.okuniewska@dynatrace.com>
2026-03-19 13:57:24 +0100

    Remove unused methods from endpoints.go

--- pkg/clients/dynatrace/endpoints.go
+++ pkg/clients/dynatrace/endpoints.go
@@ -1,43 +1,9 @@
 package dynatrace
 
-import "fmt"
-
-func (dtc *dynatraceClient) getAgentURL(os, installerType, flavor, arch, version string, technologies []string, skipMetadata bool) string {
-	url := fmt.Sprintf("%s/v1/deployment/installer/agent/%s/%s/version/%s?flavor=%s&arch=%s&bitness=64&skipMetadata=%t",
-		dtc.url, os, installerType, version, flavor, arch, skipMetadata)
-
-	return appendTechnologies(url, technologies)
-}
-
-func (dtc *dynatraceClient) getLatestAgentURL(os, installerType, flavor, arch string, technologies []string, skipMetadata bool) string {
-	url := fmt.Sprintf("%s/v1/deployment/installer/agent/%s/%s/latest?bitness=64&flavor=%s&arch=%s&skipMetadata=%t",
-		dtc.url, os, installerType, flavor, arch, skipMetadata)
-
-	return appendTechnologies(url, technologies)
-}
-
-func (dtc *dynatraceClient) getAgentVersionsURL(os, installerType, flavor, arch string) string {
-	if arch != "" {
-		return fmt.Sprintf("%s/v1/deployment/installer/agent/versions/%s/%s?flavor=%s&arch=%s",
-			dtc.url, os, installerType, flavor, arch)
-	}
-
-	return fmt.Sprintf("%s/v1/deployment/installer/agent/versions/%s/%s?flavor=%s",
-		dtc.url, os, installerType, flavor)
-}
-
 func (dtc *dynatraceClient) getProcessModuleConfigURL() string {
 	return dtc.url + "/v1/deployment/installer/agent/processmoduleconfig?sections=general,agentType"
 }
 
 func (dtc *dynatraceClient) getTokensLookupURL() string {
 	return dtc.url + "/v2/apiTokens/lookup"
-}
-
-func appendTechnologies(url string, technologies []string) string {
-	for _, tech := range technologies {
-		url = fmt.Sprintf("%s&include=%s", url, tech)
-	}
-
-	return url
 }
