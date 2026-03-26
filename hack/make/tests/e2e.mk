9eb9602a66dd8b7cbf7ed13c130c4fc94cbba305
mihaitanasedt <mihai.tanase@dynatrace.com>
2026-03-26 10:59:50 +0100

    Introduce e2e tests for HPA support  (release-1.9) (#6385)

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -51,6 +51,10 @@ test/e2e/activegate:
 test/e2e/activegate/proxy:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/istio -run "activegate" $(SKIPCLEANUP)
 
+## Runs ActiveGate scaling e2e test only
+test/e2e/activegate/scaling:
+	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "activegate_scaling" $(SKIPCLEANUP)
+
 ## Runs ClassicFullStack e2e test only
 test/e2e/classic:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "classic"  $(SKIPCLEANUP)
@@ -123,6 +127,10 @@ test/e2e/extensions/upgrade:
 test/e2e/extensions/dbexecutor:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "extensions_db_executor" $(SKIPCLEANUP)
 
+## Runs DatabaseExecutor scaling e2e test only
+test/e2e/extensions/dbexecutor/scaling:
+	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "extensions_db_executor_scaling" $(SKIPCLEANUP)
+
 ## Runs Application Monitoring metadata-enrichment e2e test only
 test/e2e/applicationmonitoring/metadataenrichment:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "metadata_enrichment" $(SKIPCLEANUP)
@@ -171,6 +179,10 @@ test/e2e/edgeconnect/normal:
 test/e2e/edgeconnect/proxy:
 	$(GOTESTCMD) -timeout 20m  ./test/e2e/scenarios/nocsi -run "TestNoCSI_edgeconnect_install_proxy" $(SKIPCLEANUP)
 
+## Runs EdgeConnect scaling e2e test only
+test/e2e/edgeconnect/scaling:
+	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "TestNoCSI_edgeconnect_scaling" $(SKIPCLEANUP)
+
 ## Runs e2e tests on gke-autopilot
 test/e2e/gke-autopilot:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/standard -run "app_metadata_enrichment|app_read_only_csi_volume|app_read_only_csi_volume|app_without_csi|activegate" $(SKIPCLEANUP)
@@ -213,6 +225,10 @@ test/e2e/telemetryingest/otel-collector-endpoint-tls:
 
 test/e2e/telemetryingest/otel-collector-config-udpate:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "telemetryingest_configuration_update" $(SKIPCLEANUP)
+
+## Runs TelemetryIngest scaling e2e test only
+test/e2e/telemetryingest/scaling:
+	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "telemetryingest_scaling" $(SKIPCLEANUP)
 
 test/e2e/kspm:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "kspm" $(SKIPCLEANUP)
---
+++ test/e2e/features/telemetryingest/scaling.go
@@ -0,0 +1,92 @@
+//go:build e2e
+
+package telemetryingest
+
+import (
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers"
+	componentDynakube "github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/kubernetes/objects/k8shpa"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/kubernetes/objects/k8sstatefulset"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/tenant"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+var (
+	scaleReplicas = ptr.To(int32(3))
+	baseReplicas  = ptr.To(int32(2))
+)
+
+func WithHPA(t *testing.T) features.Feature {
+	builder := features.New("telemetryingest-with-hpa")
+
+	secretConfig := tenant.GetSingleTenantSecret(t)
+
+	options := []componentDynakube.Option{
+		componentDynakube.WithAPIURL(secretConfig.APIURL),
+		componentDynakube.WithTelemetryIngestEnabled(true),
+		componentDynakube.WithOTelCollectorImageRef(),
+	}
+
+	testDynakube := *componentDynakube.New(options...)
+
+	componentDynakube.Install(builder, helpers.LevelAssess, &secretConfig, testDynakube)
+
+	testHPA := &autoscalingv1.HorizontalPodAutoscaler{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-autoscaler",
+			Namespace: testDynakube.Namespace,
+		},
+		Spec: autoscalingv1.HorizontalPodAutoscalerSpec{
+			ScaleTargetRef: autoscalingv1.CrossVersionObjectReference{
+				Kind:       "StatefulSet",
+				Name:       testDynakube.OtelCollectorStatefulsetName(),
+				APIVersion: "apps/v1",
+			},
+			MinReplicas: scaleReplicas,
+			MaxReplicas: *scaleReplicas,
+		},
+	}
+
+	builder.Assess("create HPA with min replicas 3", k8shpa.Create(testHPA))
+	builder.Assess("check if the otelc statefulset has replicas set to 3", k8sstatefulset.WaitForReplicas(testDynakube.OtelCollectorStatefulsetName(), testDynakube.Namespace, *scaleReplicas))
+
+	componentDynakube.Delete(builder, helpers.LevelTeardown, testDynakube)
+	builder.Teardown(k8shpa.Delete(testHPA))
+	builder.WithTeardown("deleted tenant secret", tenant.DeleteTenantSecret(testDynakube.Name, testDynakube.Namespace))
+
+	return builder.Feature()
+}
+
+func EnforceReplicas(t *testing.T) features.Feature {
+	builder := features.New("telemetryingest-enforce-replicas")
+
+	secretConfig := tenant.GetSingleTenantSecret(t)
+
+	options := []componentDynakube.Option{
+		componentDynakube.WithAPIURL(secretConfig.APIURL),
+		componentDynakube.WithTelemetryIngestEnabled(true),
+		componentDynakube.WithOTelCollectorImageRef(),
+		componentDynakube.WithOTelCollectorReplicas(baseReplicas),
+	}
+
+	testDynakube := *componentDynakube.New(options...)
+
+	componentDynakube.Install(builder, helpers.LevelAssess, &secretConfig, testDynakube)
+
+	builder.Assess("scale otelc statefulset replicas to 3", k8sstatefulset.Update(testDynakube.OtelCollectorStatefulsetName(), testDynakube.Namespace, func(ss *appsv1.StatefulSet) {
+		ss.Spec.Replicas = scaleReplicas
+	}))
+
+	builder.Assess("check if otelc replicas were rolled back to 2", k8sstatefulset.WaitForReplicas(testDynakube.OtelCollectorStatefulsetName(), testDynakube.Namespace, *baseReplicas))
+
+	componentDynakube.Delete(builder, helpers.LevelTeardown, testDynakube)
+	builder.WithTeardown("deleted tenant secret", tenant.DeleteTenantSecret(testDynakube.Name, testDynakube.Namespace))
+
+	return builder.Feature()
+}
