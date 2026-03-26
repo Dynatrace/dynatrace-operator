911e27fe66f8bc6842ba636e50442d0c5dce01f2
Lukas Plietl <lukas.plietl@dynatrace.com>
2026-03-26 08:50:19 +0100

    ICP-1616: treat kubernetes version print as info and not as check

--- cmd/troubleshoot/kubernetes_version.go
+++ cmd/troubleshoot/kubernetes_version.go
@@ -8,23 +8,19 @@ import (
 )
 
 func checkKubernetesVersion(baseLog logd.Logger, kubeConfig *rest.Config) {
-	log := baseLog.WithName("k8s")
-
-	logNewCheckf(log, "checking Kubernetes version ...")
-
 	discoveryClient, err := discovery.NewDiscoveryClientForConfig(kubeConfig)
 	if err != nil {
-		logWarningf(log, "could not create discovery client: %v", err)
+		logWarningf(baseLog, "could not create Kubernetes discovery client: %v", err)
 
 		return
 	}
 
 	serverVersion, err := k8sversion.GetServerVersion(discoveryClient)
 	if err != nil {
-		logWarningf(log, "could not retrieve Kubernetes version: %v", err)
+		logWarningf(baseLog, "could not retrieve Kubernetes version: %v", err)
 
 		return
 	}
 
-	logOkf(log, "%s (%s, %s)", serverVersion.GitVersion, serverVersion.Platform, serverVersion.GoVersion)
+	logInfof(baseLog, "Kubernetes: %s (%s, %s)", serverVersion.GitVersion, serverVersion.Platform, serverVersion.GoVersion)
 }
