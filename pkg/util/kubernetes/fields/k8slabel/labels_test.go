57c2d186a4ff8a0ad9121ec19e3793a00bee727c
aorcholski <84514340+aorcholski@users.noreply.github.com>
2026-04-10 10:35:01 +0200

    Fixes the problem of too long labels based on branch name (#6434)

--- pkg/util/kubernetes/fields/k8slabel/labels_test.go
+++ pkg/util/kubernetes/fields/k8slabel/labels_test.go
@@ -3,6 +3,7 @@ package k8slabel
 import (
 	"testing"
 
+	"github.com/Dynatrace/dynatrace-operator/pkg/version"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +14,7 @@ const (
 	testComponent        = "test-component"
 	testComponentFeature = "test-component-feature"
 	testComponentVersion = "test-component-version"
+	testLongVersion      = "test-0000-test-1111-test-2222-test-3333-test-4444-test-long-5555-test-6666"
 )
 
 func TestConstructors(t *testing.T) {
@@ -56,4 +58,18 @@ func TestConstructors(t *testing.T) {
 	t.Run("verify labels for app", func(t *testing.T) {
 		assert.Equal(t, expectedAppLabels, appLabels.BuildLabels())
 	})
+}
+
+func TestLongVersion(t *testing.T) {
+	appLabels := NewAppLabels(testComponent, testName, testComponentFeature, testLongVersion)
+
+	oldVersion := version.Version
+	version.Version = testLongVersion
+
+	coreLabels := NewCoreLabels(testName, testComponent)
+
+	version.Version = oldVersion
+
+	assert.Len(t, appLabels.Version, 63)
+	assert.Len(t, coreLabels.Version, 63)
 }
