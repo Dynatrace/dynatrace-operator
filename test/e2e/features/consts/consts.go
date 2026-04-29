3f15401a1b8ea0571ecca06d856104c4523364ec
Simon Quatresols <simon.quatresols@dynatrace.com>
2026-04-29 09:47:03 +0200

    move more consts to where they are used.

--- test/e2e/features/consts/consts.go
+++ test/e2e/features/consts/consts.go
@@ -7,7 +7,4 @@ const (
 	AgSecretName                    = "ag-ca"
 	TelemetryIngestTLSSecretName    = "telemetry-ingest-tls"
 	DevRegistryPullSecretName       = "devregistry"
-
-	DefaultCodeModulesImage = "ghcr.io/dynatrace/dynatrace-bootstrapper:snapshot"
-	CodeModulesImageEnvVar  = "E2E_CODEMODULES_IMAGE"
 )
