3f4c1ed716868f38b84ac9b9279a8b5bab8b3621
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-04-30 09:06:44 +0200

    Remove dynatrace.com/updated-via-operator annotation (#6521)

--- pkg/injection/namespace/mapper/config.go
+++ pkg/injection/namespace/mapper/config.go
@@ -5,11 +5,8 @@ import (
 )
 
 const (
-	UpdatedViaDynakubeAnnotation = "dynatrace.com/updated-via-operator"
-	ErrorConflictingNamespace    = "namespace matches two or more DynaKubes which is unsupported. " +
+	ErrorConflictingNamespace = "namespace matches two or more DynaKubes which is unsupported. " +
 		"refine the labels on your namespace metadata or DynaKube/CodeModules specification"
 )
 
-var (
-	log = logd.Get().WithName("namespace-mapper")
-)
+var log = logd.Get().WithName("namespace-mapper")
