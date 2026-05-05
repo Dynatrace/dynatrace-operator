0960ae5c40e1b99dd3dfcf7724ceb06c9a3f1cd4
aorcholski <84514340+aorcholski@users.noreply.github.com>
2026-05-05 09:57:06 +0200

    Adds EdgeConnect oauth.endpoint validation (#6540)

---
+++ pkg/api/validation/edgeconnect/oauth.go
@@ -0,0 +1,71 @@
+package validation
+
+import (
+	"context"
+	"net/url"
+	"slices"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/v1alpha2/edgeconnect"
+)
+
+const (
+	errorInvalidOauthEndpoint = `The EdgeConnect's specification has an invalid oauth.endpoint value set.
+	Make sure you correctly specify the endpoint in your custom resource.
+	`
+
+	errorProtocolIsMissingOauthEndpoint = `The EdgeConnect's specification has an invalid oauth.endpoint value set.
+	Should include 'https' protocol.
+	Make sure you correctly specify the oauth.endpoint in your custom resource.
+	`
+
+	errorUnknownSSOServer = `The EdgeConnect's specification has an invalid oauth.endpoint value set.
+	Example valid values:
+	-  For prod: "sso.dynatrace.com"
+	-  For dev: "sso-dev.dynatracelabs.com",
+	-  For sprint: "sso.sprint.dynatracelabs.com"
+	Make sure you correctly specify the endpoint in your custom resource.
+	`
+)
+
+var (
+	allowedSSODomains = []string{
+		"sso-dev.dynatracelabs.com",
+		"sso.sprint.dynatracelabs.com",
+		"sso.dynatrace.com",
+	}
+)
+
+func isValidSSOServerURL(ctx context.Context, _ *Validator, ec *edgeconnect.EdgeConnect) string {
+	_, err := url.Parse(ec.Spec.OAuth.Endpoint)
+	if err != nil {
+		return errorInvalidOauthEndpoint
+	}
+
+	return ""
+}
+
+func checkSSOServerProtocol(ctx context.Context, _ *Validator, ec *edgeconnect.EdgeConnect) string {
+	url, err := url.Parse(ec.Spec.OAuth.Endpoint)
+	if err != nil {
+		return ""
+	}
+
+	if url.Scheme != "https" {
+		return errorProtocolIsMissingOauthEndpoint
+	}
+
+	return ""
+}
+
+func isAllowedSSOServer(ctx context.Context, _ *Validator, ec *edgeconnect.EdgeConnect) string {
+	url, err := url.Parse(ec.Spec.OAuth.Endpoint)
+	if err != nil {
+		return ""
+	}
+
+	if !slices.Contains(allowedSSODomains, url.Host) {
+		return errorUnknownSSOServer
+	}
+
+	return ""
+}
