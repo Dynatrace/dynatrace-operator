eadef801cdf87c0a75e957151e3f6c19d7340f47
jokuniew <julia.okuniewska@dynatrace.com>
2026-05-06 13:46:59 +0200

    add PublicRegistryOverride

--- pkg/api/latest/dynakube/dynakube_props.go
+++ pkg/api/latest/dynakube/dynakube_props.go
@@ -165,3 +165,8 @@ func (dk *DynaKube) IsCodeModulesStatusReady() bool {
 func (dk *DynaKube) GetResourceAttributes() map[string]string {
 	return dk.Spec.ResourceAttributes
 }
+
+// PublicRegistryOverride returns the public registry override for the DynaKube.
+func (dk *DynaKube) PublicRegistryOverride() string {
+	return dk.Spec.PublicRegistryOverride
+}
