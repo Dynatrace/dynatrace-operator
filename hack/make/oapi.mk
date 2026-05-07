d74d2a8eb51a54788dea27c2049c8aa87c73da9b
mihaitanasedt <mihai.tanase@dynatrace.com>
2026-05-07 14:48:08 +0200

    Remove old script, move new script to scripts folder

--- hack/make/oapi.mk
+++ hack/make/oapi.mk
@@ -1,3 +1,3 @@
 ## Generate Go SDKs from OpenAPI specs
 oapi/generate: prerequisites/openapi-generator-cli prerequisites/python
-	@OPENAPI_GENERATOR_CLI=$(OPENAPI_GENERATOR_CLI) $(PYTHON) hack/make/bin/oapi-generate.py
+	@OPENAPI_GENERATOR_CLI=$(OPENAPI_GENERATOR_CLI) $(PYTHON) hack/scripts/oapi-generate.py
