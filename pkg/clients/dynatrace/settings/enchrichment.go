b5d5823c0949860f60a698efd8e9e04dd8fec309
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-05-13 14:33:55 +0200

    Add support for new enrichment rule schema

--- pkg/clients/dynatrace/settings/enchrichment.go
+++ pkg/clients/dynatrace/settings/enchrichment.go
@@ -10,15 +10,15 @@ import (
 )
 
 const (
-	metadataEnrichmentSettingsSchemaID = "builtin:kubernetes.generic.metadata.enrichment"
-	scopeQueryParam                    = "scope"
-	globalScope                        = "environment"
-	effectiveValuesPath                = "/v2/settings/effectiveValues"
+	legacyMetadataEnrichmentSchemaID = "builtin:kubernetes.generic.metadata.enrichment"
+	metadataEnrichmentSchemaID       = "builtin:ingest.enrichment.config"
+	scopeQueryParam                  = "scope"
+	globalScope                      = "environment"
+	effectiveValuesPath              = "/v2/settings/effectiveValues"
 )
 
 type getRulesResponse struct {
-	Items      []ruleItem `json:"items"`
-	TotalCount int        `json:"totalCount"`
+	Items []ruleItem `json:"items"`
 }
 
 type ruleItem struct {
@@ -27,6 +27,20 @@ type ruleItem struct {
 
 type ruleItemValue struct {
 	Rules []metadataenrichment.Rule `json:"rules"`
+
+	// If true, use new schema
+	UseIngestEnrichmentConfigSchema bool `json:"useIngestEnrichmentConfigSchema"`
+
+	// These fields are embedded into the value instead of part of a rules list, when using the builtin:ingest.enrichment.config schema.
+	// Use a pointer to have a simple discriminator when looking at a response without the context of where it came from.
+	*IngestEnrichmentConfig `json:",inline"`
+}
+
+// We cannot unmarshal into unexported embedded fields. Do not use outside this package.
+type IngestEnrichmentConfig struct {
+	Type        string `json:"type"`
+	Target      string `json:"target"`
+	ValueSource string `json:"valueSource"`
 }
 
 // GetRules returns metadata enrichment rules with the number of settings objects and their values.
@@ -44,32 +58,91 @@ func (c *ClientImpl) GetRules(ctx context.Context, kubeSystemUUID, entityID stri
 		scope = globalScope
 	}
 
-	var resp getRulesResponse
+	params := map[string]string{
+		validateOnlyQueryParam: "true",
+		schemaIDsQueryParam:    legacyMetadataEnrichmentSchemaID,
+		scopeQueryParam:        scope,
+	}
 
-	err := c.apiClient.GET(ctx, effectiveValuesPath).
-		WithQueryParams(map[string]string{
-			validateOnlyQueryParam: "true",
-			schemaIDsQueryParam:    metadataEnrichmentSettingsSchemaID,
-			scopeQueryParam:        scope,
-		}).
-		Execute(&resp)
-	if err != nil {
-		if core.IsNotFound(err) {
-			log.Info("enrichment settings not available on cluster, skipping getting the enrichment rules", "schemaID", metadataEnrichmentSettingsSchemaID)
+	var (
+		resp         getRulesResponse
+		useNewSchema bool
+	)
+
+	if err := c.apiClient.GET(ctx, effectiveValuesPath).WithQueryParams(params).Execute(&resp); err != nil {
+		if !core.IsNotFound(err) {
+			return nil, err
+		}
+
+		// The schema is expected to be missing in two cases, the tenant is too new or too old.
+		log.Info("using fallback for unavailable schema", "schemaID", legacyMetadataEnrichmentSchemaID, "fallbackSchemaID", metadataEnrichmentSchemaID)
+	} else if useNewSchema = isNewSchemaRequested(resp); useNewSchema {
+		// Users can enable the use of the schema on their tenant before migrating to Latest Dynatrace environments.
+		// In this case both schemas coexist, but we should still use the new one.
+		log.Info("updating data with new schema enabled on tenant", "schemaID", metadataEnrichmentSchemaID)
+	} else {
+		// The legacy schema was found and the toggle is not enabled
+		return getRulesFromResponse(resp), nil
+	}
+
+	// Retry the request with the new schema. For managed this will always fail, but we have no practical way of knowing which environment we're running in.
+	params[schemaIDsQueryParam] = metadataEnrichmentSchemaID
+	// Clear the input so that we don't keep stale data
+	resp = getRulesResponse{}
+
+	if err := c.apiClient.GET(ctx, effectiveValuesPath).WithQueryParams(params).Execute(&resp); err != nil {
+		if !useNewSchema && core.IsNotFound(err) {
+			// Keep the established behavior of not failing when the legacy schema is not available
+			// This covers the managed use-case.
+			log.Info("enrichment settings not available on cluster, skipping getting the enrichment rules", "schemaID", legacyMetadataEnrichmentSchemaID)
 
 			return nil, nil
 		}
 
+		// The error is either not 404 or the user enabled the new schema explicitly. In this case a missing schema is an error.
 		log.Info("failed to retrieve enrichment rules")
 
 		return nil, fmt.Errorf("get rules settings: %w", err)
 	}
 
-	var rules []metadataenrichment.Rule
-
-	for _, item := range resp.Items {
-		rules = append(rules, item.Value.Rules...)
+	rules := getRulesFromResponse(resp)
+	if useNewSchema && len(rules) == 0 {
+		// Rules get auto-migrated when moving to Latest Dynatrace environments, but before then it's the users responsibility to migrate them.
+		// Since the user explicitly requested the new schema, missing rules at this point are not an error.
+		log.Info("requested enrichment rules, but got empty response. manual migration of rules is required", "schemaID", metadataEnrichmentSchemaID)
 	}
 
 	return rules, nil
+}
+
+func isNewSchemaRequested(resp getRulesResponse) bool {
+	for _, item := range resp.Items {
+		if item.Value.UseIngestEnrichmentConfigSchema {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getRulesFromResponse(resp getRulesResponse) []metadataenrichment.Rule {
+	var rules []metadataenrichment.Rule
+
+	// In practice, this loop is only actually required for the new schema where each rule is a separate items.
+	// The legacy schema put all rules into a single item's value.
+	for _, item := range resp.Items {
+		if cfg := item.Value.IngestEnrichmentConfig; cfg != nil {
+			rule := metadataenrichment.Rule{
+				Type:   metadataenrichment.RuleType(cfg.Type),
+				Target: cfg.Target,
+				Source: cfg.ValueSource,
+			}
+
+			rules = append(rules, rule)
+		} else {
+			rules = append(rules, item.Value.Rules...)
+		}
+	}
+
+	return rules
 }
