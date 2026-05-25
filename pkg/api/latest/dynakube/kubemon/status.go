8a9a5a5346975ba7d6d9f884dec6167633b57353
Jozef Daniecki <jozef.daniecki@dynatrace.com>
2026-05-25 11:43:40 +0200

    add kubemon .spec & .status

---
+++ pkg/api/latest/dynakube/kubemon/status.go
@@ -0,0 +1,15 @@
+package kubemon
+
+import (
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/shared/communication"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/status"
+)
+
+// +kubebuilder:object:generate=true
+
+type Status struct {
+	status.VersionStatus `json:",inline"`
+
+	// Information about KubernetesMonitoring's connections.
+	ConnectionInfo communication.ConnectionInfo `json:"connectionInfoStatus,omitempty"`
+}
