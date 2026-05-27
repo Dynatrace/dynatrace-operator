ea6db7ddd12f511eb821736f469fb190a32a2353
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-05-27 15:45:18 +0200

    e2e: fixup daily fips tests (#6760)

--- .github/scripts/create-cluster.sh
+++ .github/scripts/create-cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x
+set -eu -o pipefail
 
 kubectl version
 
--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -31,9 +31,7 @@ if [[ $FLC_ENVIRONMENT =~ "olm" ]]; then
   make test/e2e/no-csi/publish/olm
 elif [[ $FLC_ENVIRONMENT =~ "fips" ]]; then
   echo "run fips e2e test suites"
-  make test/e2e-publish/fips
-elif [[ -n "${TARGET_IMAGE}" ]]; then
-  make IMAGE_URI="$TARGET_IMAGE" test/e2e-publish
+  make BRANCH="$TARGET_BRANCH" FIPS=true test/e2e-publish
 else
   echo "fall back to default branch target"
   make BRANCH="$TARGET_BRANCH" test/e2e-publish
