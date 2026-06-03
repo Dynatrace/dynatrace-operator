10770bed7e2124dab92c3814b5f960b003346566
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2026-06-03 11:12:22 +0200

    Add validation for public registry feature not allowed for classicFullStack

--- pkg/api/validation/dynakube/public_registry.go
+++ pkg/api/validation/dynakube/public_registry.go
@@ -12,6 +12,7 @@ import (
 const (
 	errorPublicRegistryOverrideWithoutPublicRegistry = `The publicRegistryOverride field is set, but the feature flag "%s" is not enabled. Either enable the feature flag or remove the publicRegistryOverride field.`
 	warningPublicRegistryFlagIgnoredForPlatformToken = `The feature flag "%s" is set, but it is ignored because a platform token is in use. The public registry endpoint is used automatically with platform tokens.`
+	errorPublicRegistryFeatureNotAllowedForClassic   = `The public-registry feature (and relatedly the use of a platform token) is not allowed when using classicFullStack. Consider upgrading to cloudnativeFullStack if you would like to use the feature.`
 )
 
 func publicRegistryOverrideWithoutPublicRegistry(ctx context.Context, dv *Validator, dk *dynakube.DynaKube) string {
@@ -39,4 +40,22 @@ func publicRegistryFlagIgnoredForPlatformToken(ctx context.Context, dv *Validato
 	}
 
 	return fmt.Sprintf(warningPublicRegistryFlagIgnoredForPlatformToken, exp.UsePublicRegistryKey)
+}
+
+func publicRegistryNotAllowedForClassic(ctx context.Context, dv *Validator, dk *dynakube.DynaKube) string {
+	if !dk.OneAgent().IsClassicFullStackMode() {
+		return ""
+	}
+
+	if dk.PublicRegistryOverride() != "" || dk.FF().IsPublicRegistry() {
+		return errorPublicRegistryFeatureNotAllowedForClassic
+	}
+
+	// For new DynaKubes (status not yet set), check the token secret directly.
+	hasPlatformToken, err := token.NewReader(dv.apiReader, dk).HasPlatformToken(ctx)
+	if err == nil && hasPlatformToken {
+		return errorPublicRegistryFeatureNotAllowedForClassic
+	}
+
+	return ""
 }
