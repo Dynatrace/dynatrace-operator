ec799217c7cf7b8ac622c1ee3ac4c31ed4c56f4a
Nico Nö <nico.noessler@dynatrace.com>
2026-06-11 15:16:21 +0200

    Update pkg/injection/namespace/bootstrapperconfig/bootstrapperconfig.go
    Co-authored-by: StefanHauth <63204425+StefanHauth@users.noreply.github.com>

--- pkg/injection/namespace/bootstrapperconfig/bootstrapperconfig.go
+++ pkg/injection/namespace/bootstrapperconfig/bootstrapperconfig.go
@@ -245,7 +245,7 @@ func NeedsDownloadConfig(dk *dynakube.DynaKube) bool {
 }
 
 func NeedsPGC(dk *dynakube.DynaKube) bool {
-	return dk.OneAgent().IsAppInjectionNeeded() || dk.OneAgent().IsHostMonitoringMode()
+	return !dk.OneAgent().IsClassiFullStackMode()
 }
 
 // generateCerts gets the necessary info they create the init certs secret data
