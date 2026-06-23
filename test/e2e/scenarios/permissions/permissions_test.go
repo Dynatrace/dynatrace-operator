456f29e31b561d6c51e8dae0fc761405b8a52cf8
mihaitanasedt <mihai.tanase@dynatrace.com>
2026-06-23 17:20:20 +0200

    Add the deployer cluster roles and e2e tests

---
+++ test/e2e/scenarios/permissions/permissions_test.go
@@ -0,0 +1,50 @@
+//go:build e2e
+
+package permissions
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/features/deployersamples"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/events"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/kubernetes/environment"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+)
+
+var (
+	testEnv env.Environment
+	cfg     *envconf.Config
+)
+
+func TestMain(m *testing.M) {
+	cfg = environment.GetStandardKubeClusterEnvConfig()
+	testEnv = env.NewWithConfig(cfg)
+
+	testEnv.AfterEachTest(func(ctx context.Context, c *envconf.Config, t *testing.T) (context.Context, error) {
+		if t.Failed() {
+			events.LogEvents(ctx, c, t)
+		}
+
+		return ctx, nil
+	})
+
+	testEnv.Run(m)
+}
+
+func TestPermissions_deployer_escalate_no_csi(t *testing.T) {
+	testEnv.Test(t, deployersamples.EscalateNoCsi())
+}
+
+func TestPermissions_deployer_escalate_with_csi(t *testing.T) {
+	testEnv.Test(t, deployersamples.EscalateWithCsi())
+}
+
+func TestPermissions_deployer_no_escalate_no_csi(t *testing.T) {
+	testEnv.Test(t, deployersamples.NoEscalateNoCsi())
+}
+
+func TestPermissions_deployer_no_escalate_with_csi(t *testing.T) {
+	testEnv.Test(t, deployersamples.NoEscalateWithCsi())
+}
