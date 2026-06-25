720690cb83f4d6719e476b688bc21eb08f0abf74
Albian Krasniqi <62391739+waodim@users.noreply.github.com>
2026-06-25 12:46:52 +0200

    fix EdgeConnect imageRef.digest having no effect on image selected (#6884)

--- pkg/api/v1alpha2/edgeconnect/edgeconnect_types_test.go
+++ pkg/api/v1alpha2/edgeconnect/edgeconnect_types_test.go
@@ -3,9 +3,33 @@ package edgeconnect
 import (
 	"testing"
 
+	"github.com/Dynatrace/dynatrace-operator/pkg/api"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/shared/image"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func TestImage(t *testing.T) {
+	t.Run("defaults", func(t *testing.T) {
+		ec := EdgeConnect{}
+		require.Equal(t, defaultEdgeConnectRepository+":"+api.LatestTag, ec.Image())
+	})
+
+	t.Run("custom repository and tag", func(t *testing.T) {
+		ec := EdgeConnect{Spec: EdgeConnectSpec{ImageRef: image.Ref{Repository: "my.registry/edgeconnect", Tag: "1.2.3"}}}
+		require.Equal(t, "my.registry/edgeconnect:1.2.3", ec.Image())
+	})
+
+	t.Run("digest takes precedence over tag", func(t *testing.T) {
+		ec := EdgeConnect{Spec: EdgeConnectSpec{ImageRef: image.Ref{Repository: "my.registry/edgeconnect", Tag: "1.2.3", Digest: "sha256:abc123"}}}
+		require.Equal(t, "my.registry/edgeconnect@sha256:abc123", ec.Image())
+	})
+
+	t.Run("digest with default repository", func(t *testing.T) {
+		ec := EdgeConnect{Spec: EdgeConnectSpec{ImageRef: image.Ref{Digest: "sha256:abc123"}}}
+		require.Equal(t, defaultEdgeConnectRepository+"@sha256:abc123", ec.Image())
+	})
+}
 
 func TestHostMappings(t *testing.T) {
 	t.Run("Get HostMappings", func(t *testing.T) {
