d7547cc29df9f9270b7dd4cd914c54167cd54a2a
Marcell Sevcsik <31651557+0sewa0@users.noreply.github.com>
2026-07-13 15:59:05 +0200

    Add resources for OA DS init container (#6947)

--- pkg/controllers/dynakube/oneagent/daemonset/daemonset_test.go
+++ pkg/controllers/dynakube/oneagent/daemonset/daemonset_test.go
@@ -1,7 +1,6 @@
 package daemonset
 
 import (
-	"os"
 	"strings"
 	"testing"
 	"time"
@@ -11,7 +10,6 @@ import (
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/oneagent"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/status"
 	"github.com/Dynatrace/dynatrace-operator/pkg/controllers/dynakube/deploymentmetadata"
-	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8senv"
 	"github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/fields/k8slabel"
 	k8sversion "github.com/Dynatrace/dynatrace-operator/pkg/util/kubernetes/version"
 	"github.com/Dynatrace/dynatrace-operator/pkg/version"
@@ -29,17 +27,12 @@ import (
 const (
 	testImageTag  = "1.203.0.0-0"
 	testTokenHash = "test-token-hash"
-
-	testKubernetesClusterName = "cluster-name"
-	testKubernetesClusterUID  = "cluster-uid"
-	testKubernetesClusterMEID = "cluster-meid"
-	testOperatorImageName     = "operator-image-name"
 )
 
-func TestUseImmutableImage(t *testing.T) {
+func TestBuildDaemonSet(t *testing.T) {
 	t.Cleanup(k8sversion.DisableCacheForTest(123))
 
-	t.Run("use image from status", func(t *testing.T) {
+	t.Run("use image from status, serviceAccountName set", func(t *testing.T) {
 		imageID := "my.repo.com/image:my-tag"
 		dk := dynakube.DynaKube{
 			Spec: dynakube.DynaKubeSpec{
@@ -63,6 +56,7 @@ func TestUseImmutableImage(t *testing.T) {
 		podSpecs := ds.Spec.Template.Spec
 		assert.NotNil(t, podSpecs)
 		assert.Equal(t, imageID, podSpecs.Containers[0].Image)
+		assert.Equal(t, serviceAccountName, podSpecs.ServiceAccountName)
 	})
 }
 
@@ -546,46 +540,6 @@ func TestHostMonitoring_SecurityContext(t *testing.T) {
 	})
 }
 
-func TestPodSpecServiceAccountName(t *testing.T) {
-	t.Run("service account name is unprivileged + readonly by default", func(t *testing.T) {
-		builder := builder{
-			dk: &dynakube.DynaKube{},
-		}
-		podSpec, _ := builder.podSpec(t.Context())
-
-		assert.Equal(t, serviceAccountName, podSpec.ServiceAccountName)
-	})
-	t.Run("privileged and readonly is recognized", func(t *testing.T) {
-		builder := builder{
-			dk: &dynakube.DynaKube{
-				ObjectMeta: metav1.ObjectMeta{
-					Annotations: map[string]string{
-						exp.OAPrivilegedKey: "true",
-					},
-				},
-			},
-		}
-		podSpec, _ := builder.podSpec(t.Context())
-
-		assert.Equal(t, serviceAccountName, podSpec.ServiceAccountName)
-	})
-	t.Run("service account name is unprivileged if run as unprivileged", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			ObjectMeta: metav1.ObjectMeta{
-				Annotations: map[string]string{
-					exp.OAPrivilegedKey: "false",
-				},
-			},
-		}
-		builder := builder{
-			dk: dk,
-		}
-		podSpec, _ := builder.podSpec(t.Context())
-
-		assert.Equal(t, serviceAccountName, podSpec.ServiceAccountName)
-	})
-}
-
 func TestPodSpecProbes(t *testing.T) {
 	expectedHealthcheck := containerv1.HealthConfig{
 		Test:        []string{"echo", "super pod"},
@@ -604,6 +558,7 @@ func TestPodSpecProbes(t *testing.T) {
 					},
 				},
 			},
+			hostInjectSpec: &oneagent.HostInjectSpec{},
 		}
 		podSpec, _ := builder.podSpec(t.Context())
 
@@ -637,6 +592,7 @@ func TestPodSpecProbes(t *testing.T) {
 					},
 				},
 			},
+			hostInjectSpec: &oneagent.HostInjectSpec{},
 		}
 		podSpec, _ := builder.podSpec(t.Context())
 
@@ -650,7 +606,8 @@ func TestPodSpecProbes(t *testing.T) {
 	})
 	t.Run("nil probes when dynakube oneagent status has no healthcheck", func(t *testing.T) {
 		builder := builder{
-			dk: &dynakube.DynaKube{},
+			dk:             &dynakube.DynaKube{},
+			hostInjectSpec: &oneagent.HostInjectSpec{},
 		}
 		podSpec, _ := builder.podSpec(t.Context())
 
@@ -671,6 +628,7 @@ func TestPodSpecProbes(t *testing.T) {
 					},
 				},
 			},
+			hostInjectSpec: &oneagent.HostInjectSpec{},
 		}
 		podSpec, _ := builder.podSpec(t.Context())
 
@@ -1120,192 +1078,4 @@ func TestDefaultArguments(t *testing.T) {
 		}
 		assert.Equal(t, expectedDefaultArguments, ds.Spec.Template.Spec.Containers[0].Args)
 	})
-}
-
-func TestInitContainerSpec(t *testing.T) {
-	dk := &dynakube.DynaKube{
-		Status: dynakube.DynaKubeStatus{},
-	}
-
-	dsBuilder := builder{
-		dk: dk,
-	}
-
-	require.NoError(t, os.Setenv(k8senv.DTOperatorImageEnvName, testOperatorImageName))
-	spec := dsBuilder.initContainerSpec()
-	require.NoError(t, os.Unsetenv(k8senv.DTOperatorImageEnvName))
-
-	assert.Equal(t, testOperatorImageName, spec.Image)
-	assert.Equal(t, initContainerName, spec.Name)
-	assert.Empty(t, spec.ImagePullPolicy)
-	assert.NotEmpty(t, spec.Env)
-	assert.NotEmpty(t, spec.Args)
-	assert.NotEmpty(t, spec.VolumeMounts)
-	assert.NotNil(t, spec.SecurityContext)
-}
-
-func TestInitContainerEnvVars(t *testing.T) {
-	dsBuilder := builder{}
-
-	envVars := dsBuilder.initContainerEnvVars()
-
-	assert.Len(t, envVars, 1)
-	assert.Equal(t, dtNodeName, envVars[0].Name)
-	assert.Equal(t, &corev1.EnvVarSource{
-		FieldRef: &corev1.ObjectFieldSelector{
-			FieldPath: "spec.nodeName",
-		},
-	}, envVars[0].ValueFrom)
-}
-
-func TestInitContainerArguments(t *testing.T) {
-	dk := &dynakube.DynaKube{
-		Status: dynakube.DynaKubeStatus{
-			KubeSystemUUID:        testKubernetesClusterUID,
-			KubernetesClusterMEID: testKubernetesClusterMEID,
-			KubernetesClusterName: testKubernetesClusterName,
-		},
-	}
-
-	dsBuilder := builder{
-		dk: dk,
-	}
-
-	arguments := dsBuilder.initContainerArguments()
-	assert.Equal(t, "generate-metadata", arguments[0])
-	assert.Equal(t, "--file", arguments[1])
-	assert.Equal(t, nodeMetadataFilePath, arguments[2])
-	assert.Equal(t, "--attributes", arguments[3])
-
-	attributes := strings.Split(arguments[4], ",")
-
-	assert.Equal(t, "k8s.cluster.name="+testKubernetesClusterName, attributes[0])
-	assert.Equal(t, "k8s.cluster.uid="+testKubernetesClusterUID, attributes[1])
-	assert.Equal(t, "k8s.node.name=$(DT_K8S_NODE_NAME)", attributes[2])
-	assert.Equal(t, "dt.entity.kubernetes_cluster="+testKubernetesClusterMEID, attributes[3])
-}
-
-func TestInitContainerArguments_ResourceAttributes(t *testing.T) {
-	baseStatus := dynakube.DynaKubeStatus{
-		KubeSystemUUID:        testKubernetesClusterUID,
-		KubernetesClusterMEID: testKubernetesClusterMEID,
-		KubernetesClusterName: testKubernetesClusterName,
-	}
-
-	t.Run("global resource attributes are sorted and appended", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			Spec: dynakube.DynaKubeSpec{
-				ResourceAttributes: map[string]string{"team": "platform", "env": "prod"},
-				OneAgent:           oneagent.Spec{HostMonitoring: &oneagent.HostInjectSpec{}},
-			},
-			Status: baseStatus,
-		}
-
-		dsBuilder := builder{dk: dk}
-		attributes := strings.Split(dsBuilder.initContainerArguments()[4], ",")
-
-		assert.Equal(t, "k8s.cluster.name="+testKubernetesClusterName, attributes[0])
-		assert.Equal(t, "k8s.cluster.uid="+testKubernetesClusterUID, attributes[1])
-		assert.Equal(t, "k8s.node.name=$(DT_K8S_NODE_NAME)", attributes[2])
-		assert.Equal(t, "dt.entity.kubernetes_cluster="+testKubernetesClusterMEID, attributes[3])
-		assert.Equal(t, "env=prod", attributes[4])
-		assert.Equal(t, "team=platform", attributes[5])
-	})
-
-	t.Run("hostMonitoring additionalResourceAttributes overrides global", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			Spec: dynakube.DynaKubeSpec{
-				ResourceAttributes: map[string]string{"shared": "global", "only-global": "g"},
-				OneAgent: oneagent.Spec{HostMonitoring: &oneagent.HostInjectSpec{
-					AdditionalResourceAttributes: map[string]string{"shared": "host", "only-host": "h"},
-				}},
-			},
-			Status: baseStatus,
-		}
-
-		dsBuilder := builder{dk: dk}
-		attributes := strings.Split(dsBuilder.initContainerArguments()[4], ",")
-		assert.Contains(t, attributes, "shared=host")
-		assert.Contains(t, attributes, "only-global=g")
-		assert.Contains(t, attributes, "only-host=h")
-		assert.NotContains(t, attributes, "shared=global")
-	})
-
-	t.Run("classicFullStack additionalResourceAttributes overrides global", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			Spec: dynakube.DynaKubeSpec{
-				ResourceAttributes: map[string]string{"env": "global"},
-				OneAgent: oneagent.Spec{ClassicFullStack: &oneagent.HostInjectSpec{
-					AdditionalResourceAttributes: map[string]string{"env": "classic"},
-				}},
-			},
-			Status: baseStatus,
-		}
-
-		dsBuilder := builder{dk: dk}
-		attributes := strings.Split(dsBuilder.initContainerArguments()[4], ",")
-		assert.Contains(t, attributes, "env=classic")
-		assert.NotContains(t, attributes, "env=global")
-	})
-
-	t.Run("cloudNativeFullStack additionalResourceAttributes overrides global", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			Spec: dynakube.DynaKubeSpec{
-				ResourceAttributes: map[string]string{"shared": "global"},
-				OneAgent: oneagent.Spec{CloudNativeFullStack: &oneagent.CloudNativeFullStackSpec{
-					HostInjectSpec: oneagent.HostInjectSpec{
-						AdditionalResourceAttributes: map[string]string{"shared": "cnf", "extra": "val"},
-					},
-				}},
-			},
-			Status: baseStatus,
-		}
-
-		dsBuilder := builder{dk: dk}
-		attributes := strings.Split(dsBuilder.initContainerArguments()[4], ",")
-		assert.Contains(t, attributes, "shared=cnf")
-		assert.Contains(t, attributes, "extra=val")
-		assert.NotContains(t, attributes, "shared=global")
-	})
-
-	t.Run("no resource attributes leaves existing cluster metadata unchanged", func(t *testing.T) {
-		dk := &dynakube.DynaKube{
-			Spec:   dynakube.DynaKubeSpec{OneAgent: oneagent.Spec{HostMonitoring: &oneagent.HostInjectSpec{}}},
-			Status: baseStatus,
-		}
-
-		dsBuilder := builder{dk: dk}
-		attributes := strings.Split(dsBuilder.initContainerArguments()[4], ",")
-		assert.Len(t, attributes, 4)
-	})
-}
-
-func TestInitContainerVolumeMounts(t *testing.T) {
-	dsBuilder := builder{}
-
-	volumeMounts := dsBuilder.initContainerVolumeMounts()
-
-	assert.Len(t, volumeMounts, 1)
-	assert.Contains(t, volumeMounts, corev1.VolumeMount{
-		Name:      nodeMetadataVolumeName,
-		MountPath: nodeMetadataFolderPath,
-		ReadOnly:  false,
-	})
-}
-
-func TestInitContainerSecurityContext(t *testing.T) {
-	dsBuilder := builder{}
-
-	securityContext := dsBuilder.initContainerSecurityContext()
-
-	assert.False(t, *securityContext.Privileged)
-	assert.False(t, *securityContext.AllowPrivilegeEscalation)
-	assert.True(t, *securityContext.RunAsNonRoot)
-	assert.Equal(t, userGroupID, *securityContext.RunAsUser)
-	assert.Equal(t, userGroupID, *securityContext.RunAsGroup)
-	assert.Empty(t, securityContext.Capabilities.Add)
-	assert.Len(t, securityContext.Capabilities.Drop, 1)
-	assert.Contains(t, securityContext.Capabilities.Drop, corev1.Capability("ALL"))
-	assert.Equal(t, corev1.SeccompProfileTypeRuntimeDefault, securityContext.SeccompProfile.Type)
-	assert.True(t, *securityContext.ReadOnlyRootFilesystem)
 }
