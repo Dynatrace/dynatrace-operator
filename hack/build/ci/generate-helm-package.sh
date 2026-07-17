8b7232a272ceb397228a9a83b957836b1787f3a3
Nico Nößler <nico.noessler@dynatrace.com>
2026-07-17 12:23:19 +0200

    generate-helm-package name and image overrides

--- hack/build/ci/generate-helm-package.sh
+++ hack/build/ci/generate-helm-package.sh
@@ -7,9 +7,19 @@ echo "${PASSPHRASE}" >~/.gnupg/passphrase
 cleanup() {
     rm -f ~/.gnupg/secring.gpg
     rm -f ~/.gnupg/passphrase
+    [[ -n "${CHART_NAME_OVERRIDE}" ]] && git checkout -- config/helm/chart/default/Chart.yaml
+    [[ -n "${IMAGE_TAG_OVERRIDE}" ]] && git checkout -- config/helm/chart/default/values.yaml
 }
 
 trap 'cleanup' ERR
+
+if [[ -n "${CHART_NAME_OVERRIDE}" ]]; then
+    yq -i ".name = \"${CHART_NAME_OVERRIDE}\"" config/helm/chart/default/Chart.yaml
+fi
+
+if [[ -n "${IMAGE_TAG_OVERRIDE}" ]]; then
+    yq -i ".imageRef.tag = \"${IMAGE_TAG_OVERRIDE}\"" config/helm/chart/default/values.yaml
+fi
 
 helm package \
     "./config/helm/chart/default/" \
@@ -20,3 +30,5 @@ helm package \
     --key "Dynatrace LLC" \
     --keyring ~/.gnupg/secring.gpg \
     --passphrase-file ~/.gnupg/passphrase
+
+cleanup
