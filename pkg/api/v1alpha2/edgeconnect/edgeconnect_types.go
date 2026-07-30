0cb7b76c517c7e065e1aab9e1e168698708611c4
Nico Nößler <nico.noessler@dynatrace.com>
2026-07-30 14:28:08 +0200

    moved Hub() to the top in edgeconnect_types.go

--- pkg/api/v1alpha2/edgeconnect/edgeconnect_types.go
+++ pkg/api/v1alpha2/edgeconnect/edgeconnect_types.go
@@ -15,6 +15,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// Hub tags this version as the 'source' of the conversion for controller runtime.
+func (*EdgeConnect) Hub() {}
+
 // EdgeConnectSpec defines the desired state of EdgeConnect.
 type EdgeConnectSpec struct { //nolint:revive
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
@@ -137,6 +140,3 @@ const (
 	KubernetesDefaultDNS     = "kubernetes.default.svc.cluster.local"
 	kubernetesHostnameSuffix = "kubernetes-automation"
 )
-
-// Hub tags this version as the 'source' of the conversion for controller runtime.
-func (*EdgeConnect) Hub() {}
