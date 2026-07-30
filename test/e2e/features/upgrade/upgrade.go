e53953f275b29b7b3519d148281f62f8ac0867fa
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-07-30 15:49:45 +0200

    Add EC CR to upgrade operator tests

--- test/e2e/features/upgrade/upgrade.go
+++ test/e2e/features/upgrade/upgrade.go
@@ -6,16 +6,19 @@
 package upgrade
 
 import (
+	"fmt"
 	"testing"
 
 	dynakubev1beta5 "github.com/Dynatrace/dynatrace-operator/pkg/api/v1beta5/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/features/cloudnative"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/dynakube"
+	edgeconnectComponents "github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/edgeconnect"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/operator"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/kubernetes/objects/k8snamespace"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/sample"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/tenant"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -23,7 +26,7 @@ import (
 const withCSI = true
 
 func Feature(t *testing.T, releaseTag string) features.Feature {
-	builder := features.New("cloudnative-upgrade")
+	builder := features.New("dk-cloudnative-and-ec-upgrade-operator")
 
 	builder.Assess("install operator "+releaseTag, helpers.ToFeatureFunc(operator.Install(releaseTag, withCSI), true))
 	secretConfig := tenant.GetSingleTenantSecret(t)
@@ -31,6 +34,26 @@ func Feature(t *testing.T, releaseTag string) features.Feature {
 		dynakube.WithAPIURL(secretConfig.APIURL),
 		dynakube.WithCloudNativeSpec(cloudnative.DefaultCloudNativeSpec()),
 	)
+	testECname := uuid.NewString()
+	testHostPattern := fmt.Sprintf("%s.e2eTestHostPattern.internal.org", testECname)
+	edgeConnectTenantConfig := &edgeconnectComponents.TenantConfig{}
+	edgeconnectSecretConfig := tenant.GetEdgeConnectTenantSecret(t)
+	builder.Assess("create EC configuration on the tenant", edgeconnectComponents.CreateTenantConfig(testECname, edgeconnectSecretConfig, edgeConnectTenantConfig, testHostPattern))
+
+	testEdgeConnect := *edgeconnectComponents.New(
+		edgeconnectComponents.WithName(testECname),
+		edgeconnectComponents.WithAPIServer(edgeconnectSecretConfig.APIServer),
+		edgeconnectComponents.WithOAuthClientSecret(edgeconnectComponents.BuildOAuthClientSecretName(testECname)),
+		edgeconnectComponents.WithOAuthEndpoint("https://sso-dev.dynatracelabs.com/sso/oauth2/token"),
+		edgeconnectComponents.WithOAuthResource(fmt.Sprintf("urn:dtenvironment:%s", edgeconnectSecretConfig.TenantUID)),
+	)
+
+	// create OAuth client secret related to the specific EdgeConnect configuration on the tenant
+	builder.Assess("create client secret", tenant.CreateClientSecret(&edgeConnectTenantConfig.Secret, edgeconnectComponents.BuildOAuthClientSecretName(testEdgeConnect.Name), testEdgeConnect.Namespace))
+
+	// install EC
+	edgeconnectComponents.Install(builder, nil, testEdgeConnect)
+	builder.Assess("check EC configuration on the tenant", edgeconnectComponents.CheckECExistsOnTheTenant(edgeconnectSecretConfig, edgeConnectTenantConfig))
 
 	sampleNamespace := *k8snamespace.New("upgrade-sample")
 	sampleApp := sample.NewApp(t, &testDynakube,
@@ -56,6 +79,9 @@ func Feature(t *testing.T, releaseTag string) features.Feature {
 	cloudnative.AssessSampleInitContainers(builder, sampleApp)
 
 	builder.Teardown(sampleApp.Uninstall())
+
+	builder.Teardown(tenant.DeleteTenantSecret(edgeconnectComponents.BuildOAuthClientSecretName(testEdgeConnect.Name), testEdgeConnect.Namespace))
+	builder.Teardown(edgeconnectComponents.DeleteTenantConfig(edgeconnectSecretConfig, edgeConnectTenantConfig))
 
 	return builder.Feature()
 }
