37679fc0b58d726ed09d179cd3e1a54585b75b61
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-08-04 13:26:41 +0200

    Validate namespace selectors

--- pkg/api/validation/dynakube/namespace_selector.go
+++ pkg/api/validation/dynakube/namespace_selector.go
@@ -5,11 +5,11 @@ package validation
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
 	"github.com/Dynatrace/dynatrace-operator/pkg/injection/namespace/mapper"
 	"github.com/Dynatrace/dynatrace-operator/pkg/logd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
@@ -18,7 +18,9 @@ const (
 	errorConflictingNamespaceSelector = `The DynaKube's specification tries to inject into namespaces where another Dynakube already injects into, which is not supported.
 Make sure the namespaceSelector doesn't conflict with other Dynakubes namespaceSelector`
 
-	errorNamespaceSelectorMatchLabelsViolateLabelSpec = "The DynaKube's namespaceSelector contains matchLabels that are not conform to spec."
+	errorInvalidOneAgentNamespaceSelector           = "OneAgent namespaceSelector contains invalid matchLabels or matchExpressions."
+	errorInvalidMetadataEnrichmentNamespaceSelector = "Metadata enrichment namespaceSelector contains invalid matchLabels or matchExpressions."
+	errorInvalidOTLPExporterNamespaceSelector       = "OTLP exporter configuration namespaceSelector contains invalid matchLabels or matchExpressions."
 )
 
 func conflictingNamespaceSelector(ctx context.Context, dv *Validator, dk *dynakube.DynaKube) string {
@@ -42,11 +44,65 @@ func conflictingNamespaceSelector(ctx context.Context, dv *Validator, dk *dynaku
 	return ""
 }
 
-func namespaceSelectorViolateLabelSpec(_ context.Context, _ *Validator, dk *dynakube.DynaKube) string {
-	errs := validation.ValidateLabelSelector(dk.OneAgent().GetNamespaceSelector(), validation.LabelSelectorValidationOptions{AllowInvalidLabelValueInSelector: false}, field.NewPath("spec", "namespaceSelector"))
+func invalidOneAgentNamespaceSelector(ctx context.Context, _ *Validator, dk *dynakube.DynaKube) string {
+	var (
+		oneAgentSelector *metav1.LabelSelector
+		oneAgentMode     string
+	)
+
+	switch oa := dk.OneAgent(); {
+	case oa.IsCloudNativeFullstackMode():
+		oneAgentSelector = &oa.CloudNativeFullStack.NamespaceSelector
+		oneAgentMode = "cloudNativeFullStack"
+	case oa.IsApplicationMonitoringMode():
+		oneAgentSelector = &oa.ApplicationMonitoring.NamespaceSelector
+		oneAgentMode = "applicationMonitoring"
+	}
+
+	errs := validation.ValidateLabelSelector(oneAgentSelector, validation.LabelSelectorValidationOptions{}, field.NewPath("spec", "oneAgent", oneAgentMode, "namespaceSelector"))
 	if len(errs) == 0 {
 		return ""
 	}
 
-	return fmt.Sprintf("%s (%s)", errorNamespaceSelectorMatchLabelsViolateLabelSpec, errs)
+	logErrorList(ctx, errorInvalidOneAgentNamespaceSelector, errs)
+
+	return errorInvalidOneAgentNamespaceSelector
+}
+
+func invalidMetadataNamespaceSelectors(ctx context.Context, _ *Validator, dk *dynakube.DynaKube) string {
+	errs := validation.ValidateLabelSelector(
+		dk.MetadataEnrichment().GetNamespaceSelector(), validation.LabelSelectorValidationOptions{},
+		field.NewPath("spec", "metadataEnrichment", "namespaceSelector"),
+	)
+
+	if len(errs) == 0 {
+		return ""
+	}
+
+	logErrorList(ctx, errorInvalidMetadataEnrichmentNamespaceSelector, errs)
+
+	return errorInvalidMetadataEnrichmentNamespaceSelector
+}
+
+func invalidOTLPExporterNamespaceSelector(ctx context.Context, _ *Validator, dk *dynakube.DynaKube) string {
+	errs := validation.ValidateLabelSelector(
+		dk.OTLPExporterConfiguration().GetNamespaceSelector(), validation.LabelSelectorValidationOptions{},
+		field.NewPath("spec", "metadataEnrichment", "namespaceSelector"),
+	)
+
+	if len(errs) == 0 {
+		return ""
+	}
+
+	logErrorList(ctx, errorInvalidOTLPExporterNamespaceSelector, errs)
+
+	return errorInvalidOTLPExporterNamespaceSelector
+}
+
+func logErrorList(ctx context.Context, msg string, errs field.ErrorList) {
+	logd.FromContext(ctx).Info(
+		msg,
+		// circumvent zap error "details" formatting, we just want the string
+		"details", errs.ToAggregate().Error(),
+	)
 }
