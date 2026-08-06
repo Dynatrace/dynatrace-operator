79d6ca701296f68c35b5b63e1517ef163bc178cf
Nico Nößler <nico.noessler@dynatrace.com>
2026-08-06 14:41:52 +0200

    added small comment

--- cmd/supportarchive/node_taint_analysis.go
+++ cmd/supportarchive/node_taint_analysis.go
@@ -212,7 +212,7 @@ func findNodesWithUntoleratedTaints(nodes *corev1.NodeList, tolerations []corev1
 
 func isTaintTolerated(taint corev1.Taint, tolerations []corev1.Toleration) bool {
 	for i := range tolerations {
-		if tolerations[i].ToleratesTaint(klog.Background(), &taint, true) {
+		if tolerations[i].ToleratesTaint(klog.Background(), &taint, true /*enableComparisonOperators*/) {
 			return true
 		}
 	}
