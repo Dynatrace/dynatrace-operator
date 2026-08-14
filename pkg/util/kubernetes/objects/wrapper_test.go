4ecf9d9f92a2c0f536cad4df830fdcc7669fcd7e
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-08-14 10:59:23 +0200

    Add unit test for ApplyStatus

--- pkg/util/kubernetes/objects/wrapper_test.go
+++ pkg/util/kubernetes/objects/wrapper_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/scheme/fake"
 	"github.com/stretchr/testify/require"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -70,4 +71,16 @@ func TestRetryCreateOrUpate(t *testing.T) {
 		require.ErrorIs(t, err, expectErr)
 		require.Equal(t, 1, calls)
 	})
+}
+
+func TestApplyStatus(t *testing.T) {
+	deploy := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "bar", Namespace: "foo", Generation: 123}}
+	c := fake.NewClientWithManagedFields(deploy.DeepCopy())
+	deploy.Status.ObservedGeneration = 123
+	require.NoError(t, ApplyStatus(t.Context(), c, deploy))
+	got := &appsv1.Deployment{}
+	require.NoError(t, c.Get(t.Context(), client.ObjectKeyFromObject(deploy), got))
+	require.EqualValues(t, 123, got.Status.ObservedGeneration)
+	require.Len(t, got.ManagedFields, 1)
+	require.Equal(t, "dynatrace-operator", got.ManagedFields[0].Manager)
 }
