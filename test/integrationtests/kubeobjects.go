2abf6a83ea15fca73cf62b0c40a10a860dd217a0
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-08-20 14:07:14 +0200

    Remove context param from kubeobjects helpers

--- test/integrationtests/kubeobjects.go
+++ test/integrationtests/kubeobjects.go
@@ -4,7 +4,6 @@
 package integrationtests
 
 import (
-	"context"
 	"testing"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube"
@@ -14,29 +13,29 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func CreateNamespace(t *testing.T, ctx context.Context, clt client.Client, namespace string) {
+func CreateNamespace(t *testing.T, clt client.Client, namespace string) {
 	ns := corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: namespace,
 		},
 	}
-	err := clt.Create(ctx, &ns)
+	err := clt.Create(t.Context(), &ns)
 	require.NoError(t, err)
 }
 
-func CreateKubernetesObject(t *testing.T, ctx context.Context, clt client.Client, object client.Object) {
-	err := clt.Create(ctx, object)
+func CreateKubernetesObject(t *testing.T, clt client.Client, object client.Object) {
+	err := clt.Create(t.Context(), object)
 	require.NoError(t, err)
 }
 
-func CreateDynakube(t *testing.T, ctx context.Context, clt client.Client, dk *dynakube.DynaKube) {
+func CreateDynakube(t *testing.T, clt client.Client, dk *dynakube.DynaKube) {
 	dkStatus := dk.Status
 
-	err := clt.Create(ctx, dk)
+	err := clt.Create(t.Context(), dk)
 	require.NoError(t, err)
 
 	dk.Status = dkStatus
 
-	err = dk.UpdateStatus(ctx, clt)
+	err = dk.UpdateStatus(t.Context(), clt)
 	require.NoError(t, err)
 }
