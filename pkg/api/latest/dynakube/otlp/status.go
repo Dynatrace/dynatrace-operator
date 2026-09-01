33631ce4f13f39b8ebc98b8c288f03a86ce908d8
jokuniew <julia.okuniewska@dynatrace.com>
2026-09-01 12:37:39 +0200

    copy-paste typo (Kubemon -> OtelCol)

--- pkg/api/latest/dynakube/otlp/status.go
+++ pkg/api/latest/dynakube/otlp/status.go
@@ -6,7 +6,7 @@ package otlp
 // +kubebuilder:object:generate=true
 
 type Status struct {
-	// The resolved KubernetesMonitoring image that is currently deployed.
+	// The resolved OTelCollector image that is currently deployed.
 	// The JSON tag uses "image" (matching the Spec field name) while the Go field name is ResolvedImage to distinguish it from Spec.Image.
 	ResolvedImage string `json:"image,omitempty"`
 }
