90d7e29510e7b5b7e0025b184a76e6acb40135dc
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-09-08 14:46:17 +0200

    init

---
+++ test/e2e/features/applicationmonitoring/image_volumes.go
@@ -0,0 +1,78 @@
+// Copyright Dynatrace LLC
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build e2e
+
+package applicationmonitoring
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/exp"
+	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/oneagent"
+	webhook "github.com/Dynatrace/dynatrace-operator/pkg/webhook/mutation/pod/mutator"
+	dynakubeComponents "github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/sample"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/tenant"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+// ImageVolumeNoCSI feature using image volumes deployment without CSI driver
+func ImageVolumeNoCSI(t *testing.T) features.Feature {
+	builder := features.New("app-monitoring-with-image-volumes-without-csi")
+	secretConfig := tenant.GetSingleTenantSecret(t)
+	appOnlyDynakube := *dynakubeComponents.New(
+		dynakubeComponents.WithAnnotations(map[string]string{exp.OAImageVolumeKey: "true"}),
+		dynakubeComponents.WithAPIURL(secretConfig.APIURL),
+		dynakubeComponents.WithApplicationMonitoringSpec(&oneagent.ApplicationMonitoringSpec{}),
+	)
+
+	dynakubeComponents.Install(builder, &secretConfig, appOnlyDynakube)
+
+	sampleApp := sample.NewApp(t, &appOnlyDynakube, sample.AsDeployment())
+	builder.Assess("install sample app", sampleApp.Install())
+	builder.Assess("check injection of additional pod", checkInjection(sampleApp))
+
+	podSample := sample.NewApp(t, &appOnlyDynakube,
+		sample.WithName("only-pod-sample"),
+	)
+	builder.Assess("install additional pod", podSample.Install())
+	builder.Assess("check injection of additional pod", checkInjection(podSample))
+
+	randomUserSample := sample.NewApp(t, &appOnlyDynakube,
+		sample.WithName("random-user"),
+		sample.AsDeployment(),
+		sample.WithPodSecurityContext(corev1.PodSecurityContext{
+			RunAsUser:  new(int64(1234)),
+			RunAsGroup: new(int64(1234)),
+		}),
+	)
+	builder.Assess("install sample app with random users set", randomUserSample.Install())
+	builder.Assess("check injection of pods with random user", checkImageVolumeInjection(randomUserSample))
+
+	builder.Teardown(sampleApp.Uninstall())
+	builder.Teardown(podSample.Uninstall())
+	builder.Teardown(randomUserSample.Uninstall())
+
+	return builder.Feature()
+}
+
+func checkImageVolumeInjection(deployment *sample.App) features.Func {
+	return func(ctx context.Context, t *testing.T, envConfig *envconf.Config) context.Context {
+		resource := envConfig.Client().Resources()
+		samplePods := deployment.ListPods(ctx, t, resource)
+
+		require.NotNil(t, samplePods)
+
+		for _, item := range samplePods.Items {
+			require.NotNil(t, item.Spec.InitContainers)
+			require.Equal(t, webhook.InstallContainerName, item.Spec.InitContainers[0].Name)
+		}
+
+		return ctx
+	}
+}
