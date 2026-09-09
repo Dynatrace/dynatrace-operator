8af8d575eca5cf65da50c79b312bd75b82c83a5d
aorcholski <84514340+aorcholski@users.noreply.github.com>
2026-09-09 13:11:54 +0200

    Apply suggestion from @aorcholski

--- pkg/controllers/dynakube/kubemon/deploymentproperties/reconciler.go
+++ pkg/controllers/dynakube/kubemon/deploymentproperties/reconciler.go
@@ -64,7 +64,7 @@ func secretData(dk *dynakube.DynaKube) map[string][]byte {
 	if len(dk.GetResourceAttributes()) > 0 {
 		data += deploymentproperties.BuildContent(dk.GetResourceAttributes())
 	}
-
+data += "\n"
 	if dk.NeedsCustomNoProxy() {
 		noProxyValue := strings.ReplaceAll(dk.FF().GetNoProxy(), ",", "|")
 		data += fmt.Sprintf("%s\n%s=%s", agconsts.PropertiesClientInternalSection, agconsts.PropertiesNoProxyFieldName, noProxyValue)
