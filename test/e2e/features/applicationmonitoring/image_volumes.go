8696c7df5d50b44ea5a2e07b0a86430dfa1f0693
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2026-09-09 13:52:17 +0200

    add e2e tests for app mon + cnfs image volumes inj + fix error message

--- test/e2e/features/applicationmonitoring/image_volumes.go
+++ test/e2e/features/applicationmonitoring/image_volumes.go
@@ -6,18 +6,16 @@
 package applicationmonitoring
 
 import (
-	"context"
 	"testing"
 
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/exp"
 	"github.com/Dynatrace/dynatrace-operator/pkg/api/latest/dynakube/oneagent"
-	webhook "github.com/Dynatrace/dynatrace-operator/pkg/webhook/mutation/pod/mutator"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/codemodules"
 	dynakubeComponents "github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/components/dynakube"
+	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/registry"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/sample"
 	"github.com/Dynatrace/dynatrace-operator/test/e2e/helpers/tenant"
-	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
 
@@ -25,23 +23,25 @@ import (
 func ImageVolumeNoCSI(t *testing.T) features.Feature {
 	builder := features.New("app-monitoring-with-image-volumes-without-csi")
 	secretConfig := tenant.GetSingleTenantSecret(t)
+	codeModuleImage := registry.GetLatestOneAgentImageTagURI(t)
 	appOnlyDynakube := *dynakubeComponents.New(
 		dynakubeComponents.WithAnnotations(map[string]string{exp.OAImageVolumeKey: "true"}),
 		dynakubeComponents.WithAPIURL(secretConfig.APIURL),
 		dynakubeComponents.WithApplicationMonitoringSpec(&oneagent.ApplicationMonitoringSpec{}),
+		dynakubeComponents.WithCodeModulesImage(codeModuleImage),
 	)
 
 	dynakubeComponents.Install(builder, &secretConfig, appOnlyDynakube)
 
 	sampleApp := sample.NewApp(t, &appOnlyDynakube, sample.AsDeployment())
 	builder.Assess("install sample app", sampleApp.Install())
-	builder.Assess("check injection of additional pod", checkInjection(sampleApp))
+	builder.Assess("check injection with image volume of additional pod", codemodules.CheckImageVolumeInjection(sampleApp, codeModuleImage))
 
 	podSample := sample.NewApp(t, &appOnlyDynakube,
 		sample.WithName("only-pod-sample"),
 	)
 	builder.Assess("install additional pod", podSample.Install())
-	builder.Assess("check injection of additional pod", checkInjection(podSample))
+	builder.Assess("check injection with image volume of additional pod", codemodules.CheckImageVolumeInjection(podSample, codeModuleImage))
 
 	randomUserSample := sample.NewApp(t, &appOnlyDynakube,
 		sample.WithName("random-user"),
@@ -52,27 +52,11 @@ func ImageVolumeNoCSI(t *testing.T) features.Feature {
 		}),
 	)
 	builder.Assess("install sample app with random users set", randomUserSample.Install())
-	builder.Assess("check injection of pods with random user", checkImageVolumeInjection(randomUserSample))
+	builder.Assess("check injection with image volume of pods with random user", codemodules.CheckImageVolumeInjection(randomUserSample, codeModuleImage))
 
 	builder.Teardown(sampleApp.Uninstall())
 	builder.Teardown(podSample.Uninstall())
 	builder.Teardown(randomUserSample.Uninstall())
 
 	return builder.Feature()
-}
-
-func checkImageVolumeInjection(deployment *sample.App) features.Func {
-	return func(ctx context.Context, t *testing.T, envConfig *envconf.Config) context.Context {
-		resource := envConfig.Client().Resources()
-		samplePods := deployment.ListPods(ctx, t, resource)
-
-		require.NotNil(t, samplePods)
-
-		for _, item := range samplePods.Items {
-			require.NotNil(t, item.Spec.InitContainers)
-			require.Equal(t, webhook.InstallContainerName, item.Spec.InitContainers[0].Name)
-		}
-
-		return ctx
-	}
 }
