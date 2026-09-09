e2309c7cb6cd53b1529d91afc60aa5956180a254
jokuniew <julia.okuniewska@dynatrace.com>
2026-09-09 11:37:06 +0200

    snake_case for kubemon tests

--- hack/make/tests/e2e.mk
+++ hack/make/tests/e2e.mk
@@ -418,11 +418,11 @@ test/e2e/kubemon:
 
 ## Runs Kubernetes Monitoring split-AG provisioning, RBAC and cleanup e2e test
 test/e2e/kubemon/split-ag:
-	$(GOTESTCMD) -timeout 30m ./test/e2e/scenarios/nocsi -run "kubemon_SplitAG" $(SKIPCLEANUP)
+	$(GOTESTCMD) -timeout 30m ./test/e2e/scenarios/nocsi -run "kubemon_split_ag" $(SKIPCLEANUP)
 
 ## Runs Kubernetes Monitoring restart-triggers e2e test
 test/e2e/kubemon/restart-triggers:
-	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "kubemon_RestartTriggers" $(SKIPCLEANUP)
+	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "kubemon_kubemon_restart_triggers" $(SKIPCLEANUP)
 
 test/e2e/token/migration:
 	$(GOTESTCMD) -timeout 20m ./test/e2e/scenarios/nocsi -run "token_migration" $(SKIPCLEANUP)
--- test/e2e/scenarios/nocsi/no_csi_test.go
+++ test/e2e/scenarios/nocsi/no_csi_test.go
@@ -255,11 +255,11 @@ func TestNoCSI_kspm_with_kubemon(t *testing.T) {
 	testEnv.Test(t, kspm.FeatureWithKubemon(t))
 }
 
-func TestNoCSI_kubemon_SplitAG(t *testing.T) {
+func TestNoCSI_kubemon_split_ag(t *testing.T) {
 	testEnv.Test(t, kubemon.FeatureSplitAG(t))
 }
 
-func TestNoCSI_kubemon_RestartTriggers(t *testing.T) {
+func TestNoCSI_kubemon_restart_triggers(t *testing.T) {
 	testEnv.Test(t, kubemon.FeatureRestartTriggers(t))
 }
 
