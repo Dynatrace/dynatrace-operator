1e42fc382761c390bc06e6d316f72548bb20f317
Gabriel Krenn <gabriel.krenn@gmail.com>
2026-09-17 16:42:17 +0200

    Add pull secret to prometheus components (#7435)

--- pkg/controllers/prometheusmonitoring/scraper/reconciler.go
+++ pkg/controllers/prometheusmonitoring/scraper/reconciler.go
@@ -62,6 +62,7 @@ type Reconciler struct {
 type reconcileScope struct {
 	// Required for reconcile
 	Owner       *prometheusmonitoring.PrometheusMonitoring
+	DynaKube    *dynakube.DynaKube
 	Spec        *prometheusmonitoring.Scraper
 	AppLabels   *k8slabel.Labels
 	ImageClient image.Client
@@ -80,6 +81,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, pm *prometheusmonitoring.Pro
 
 	scope := &reconcileScope{
 		Owner:       pm,
+		DynaKube:    dk,
 		Spec:        pm.Scraper(),
 		AppLabels:   k8slabel.OTelScraper(),
 		ImageClient: imageClient,
@@ -235,6 +237,7 @@ func mutateDeployment(deploy *appsv1.Deployment, s *reconcileScope) {
 	deploy.Spec.Template.Spec.PriorityClassName = s.Spec.PriorityClassName
 	deploy.Spec.Template.Spec.Tolerations = s.Spec.Tolerations
 	deploy.Spec.Template.Spec.TopologySpreadConstraints = s.Spec.TopologySpreadConstraints
+	deploy.Spec.Template.Spec.ImagePullSecrets = s.DynaKube.CustomPullSecretReferences()
 	deploy.Spec.Template.Spec.Volumes = buildVolumes(s)
 	// The stored container is passed in so buildContainer can preserve apiserver-defaulted
 	// fields (e.g. ImagePullPolicy, probe timeouts) and avoid spurious diffs.
