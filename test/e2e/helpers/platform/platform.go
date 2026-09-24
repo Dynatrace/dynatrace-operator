21856de2c3f5803f449a90941ec7cfc09c64001f
Mario Valderrama <mario.valderrama@dynatrace.com>
2026-09-24 11:40:58 +0000

    Use FIPS images in e2e tests (#7475)
    Co-authored-by: Andrii Soldatenko <andrii.soldatenko@dynatrace.com>

--- test/e2e/helpers/platform/platform.go
+++ test/e2e/helpers/platform/platform.go
@@ -6,6 +6,10 @@
 package platform
 
 import (
+	"os"
+	"strconv"
+	"sync"
+
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/discovery"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -71,4 +75,12 @@ func getDiscoveryClient() (discovery.DiscoveryInterface, error) {
 	}
 
 	return client, nil
+}
+
+func IsFIPS() bool {
+	return sync.OnceValue(func() bool {
+		v, _ := strconv.ParseBool(os.Getenv("FIPS"))
+
+		return v
+	})()
 }
