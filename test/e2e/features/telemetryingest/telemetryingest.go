19cb4f180568e88bb75834705e3dea314d86b2c0
jokuniew <julia.okuniewska@dynatrace.com>
2026-09-24 07:24:13 +0200

    return nil if createAgTLSSecret err!=nil

--- test/e2e/features/telemetryingest/telemetryingest.go
+++ test/e2e/features/telemetryingest/telemetryingest.go
@@ -398,12 +398,12 @@ func waitForShutdown(name string, namespace string) features.Func {
 func createAgTLSSecret(namespace string) (*corev1.Secret, error) {
 	agCrt, err := os.ReadFile(filepath.Join(project.TestDataDir(), consts.AgCertificate))
 	if err != nil {
-		return &corev1.Secret{}, err
+		return nil, err
 	}
 
 	agP12, err := os.ReadFile(filepath.Join(project.TestDataDir(), consts.AgCertificateAndPrivateKey))
 	if err != nil {
-		return &corev1.Secret{}, err
+		return nil, err
 	}
 
 	return k8ssecret.New(consts.AgSecretName, namespace,
